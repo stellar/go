dfe1ec45062534f2513842009af1f999a21f5849
Bartek Nowotarski <bartek@nowotarski.info>
2017-03-07 19:28:41 +0100

    Support integer memo

--- protocols/federation/main.go
+++ protocols/federation/main.go
@@ -1,11 +1,18 @@
 package federation
 
+import (
+	"encoding/json"
+)
+
 // NameResponse represents the result of a federation request
 // for `name` and `forward` requests.
 type NameResponse struct {
 	AccountID string `json:"account_id"`
 	MemoType  string `json:"memo_type,omitempty"`
-	Memo      string `json:"memo,omitempty"`
+	// We are using json.Number to support unmarshalling integer
+	// values into `memo` field. This will contain string value of
+	// numeric `memo` (123/"123") but also normal string values.
+	Memo json.Number `json:"memo,omitempty"`
 }
 
 // IDResponse represents the result of a federation request
