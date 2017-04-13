ee7f646bad86b9f8e6728e2e2cfaf7adb1be6d7a
Bartek Nowotarski <bartek@nowotarski.info>
2017-04-13 14:12:14 +0200

    Price.Invert

--- xdr/price.go
+++ xdr/price.go
@@ -8,3 +8,8 @@ import (
 func (p *Price) String() string {
 	return big.NewRat(int64(p.N), int64(p.D)).FloatString(7)
 }
+
+// Invert inverts Price.
+func (p *Price) Invert() {
+	p.N, p.D = p.D, p.N
+}
