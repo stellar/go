0c253bea71004d9fbb5d8f9185462d17a1cde074
Nikhil Saraf <1028334+nikhilsaraf@users.noreply.github.com>
2017-11-03 16:14:52 -0700

    Fix incorrect detail message in /order_book
    *_issuer -> *_asset_issuer

--- services/horizon/internal/actions_order_book.go
+++ services/horizon/internal/actions_order_book.go
@@ -35,8 +35,8 @@ func (action *OrderBookShowAction) LoadQuery() {
 			Detail: "The parameters that specify what order book to view are invalid in some way. " +
 				"Please ensure that your type parameters (selling_asset_type and buying_asset_type) are one the " +
 				"following valid values: native, credit_alphanum4, credit_alphanum12.  Also ensure that you " +
-				"have specified selling_asset_code and selling_issuer if selling_asset_type is not 'native', as well " +
-				"as buying_asset_code and buying_issuer if buying_asset_type is not 'native'",
+				"have specified selling_asset_code and selling_asset_issuer if selling_asset_type is not 'native', as well " +
+				"as buying_asset_code and buying_asset_issuer if buying_asset_type is not 'native'",
 		}
 	}
 }
