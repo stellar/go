a3c50f11e45ca35d43f0e1bc7a7f35e6839cb9c8
Bartek Nowotarski <bartek@nowotarski.info>
2017-11-10 13:15:40 +0100

    Use 256 bits of entropy

--- tools/stellar-hd-wallet/commands/new.go
+++ tools/stellar-hd-wallet/commands/new.go
@@ -9,30 +9,12 @@ import (
 	"github.com/tyler-smith/go-bip39"
 )
 
-var wordsCount uint32
-
 var NewCmd = &cobra.Command{
 	Use:   "new",
 	Short: "Generates a new mnemonic code",
 	Long:  "",
 	Run: func(cmd *cobra.Command, args []string) {
-		var entSize int
-		switch wordsCount {
-		case 12:
-			entSize = 128
-		case 15:
-			entSize = 160
-		case 18:
-			entSize = 192
-		case 21:
-			entSize = 224
-		case 24:
-			entSize = 256
-		default:
-			log.Fatal("`words` param invalid")
-		}
-
-		entropy, err := bip39.NewEntropy(entSize)
+		entropy, err := bip39.NewEntropy(256)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -43,7 +25,7 @@ var NewCmd = &cobra.Command{
 		}
 
 		words := strings.Split(mnemonic, " ")
-		for i := uint32(0); i < wordsCount; i++ {
+		for i := 0; i < len(words); i++ {
 			fmt.Printf("#%-5d %10s", i+1, words[i])
 			readString()
 		}
@@ -51,9 +33,7 @@ var NewCmd = &cobra.Command{
 		fmt.Println("WARNING! Store the words above in a save place!")
 		fmt.Println("WARNING! If you lose your words, you will lose access to funds in all derived accounts!")
 		fmt.Println("WARNING! Anyone who has access to these words can spend your funds!")
+		fmt.Println("")
+		fmt.Println("Use: `stellar-hd-wallet accounts` command to see generated accounts.")
 	},
-}
-
-func init() {
-	NewCmd.Flags().Uint32VarP(&wordsCount, "words", "w", 24, "number of words to generate")
 }
