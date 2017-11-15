be46378a897f0209f43f2774b9652571e5eb9434
Tomer Weller <tomer.weller@gmail.com>
2017-11-15 13:26:26 -0800

    naming fixups

--- support/time/main_test.go
+++ support/time/main_test.go
@@ -5,17 +5,17 @@ import (
 	"testing"
 )
 
-func TestRoundUp(t *testing.T) {
+func TestMillisRoundUp(t *testing.T) {
 	assert.Equal(t, MillisFromInt64(20), MillisFromInt64(13).RoundUp(10))
 	assert.Equal(t, MillisFromInt64(10), MillisFromInt64(10).RoundUp(10))
 }
 
-func TestRoundDown(t *testing.T) {
+func TestMillisRoundDown(t *testing.T) {
 	assert.Equal(t, MillisFromInt64(10), MillisFromInt64(13).RoundDown(10))
 	assert.Equal(t, MillisFromInt64(10), MillisFromInt64(10).RoundDown(10))
 }
 
-func TestParsing(t *testing.T) {
+func TestMillisParsing(t *testing.T) {
 	expected := Now()
 	actual, err := MillisFromString(expected.String())
 	assert.NoError(t, err)
