68caafd599c645c7b849af5b2e012e64fb3d6efc
Scott Fleckenstein <nullstyle@gmail.com>
2017-12-15 16:12:39 -0600

    horizon: ingest header xdr

--- services/horizon/internal/db2/core/ledger_header.go
+++ services/horizon/internal/db2/core/ledger_header.go
@@ -2,7 +2,17 @@ package core
 
 import (
 	sq "github.com/Masterminds/squirrel"
+	"github.com/stellar/go/xdr"
 )
+
+// DataXDR returns the base64 encoded ledger header
+func (lh *LedgerHeader) DataXDR() string {
+	out, err := xdr.MarshalBase64(lh.Data)
+	if err != nil {
+		panic(err)
+	}
+	return out
+}
 
 // LedgerHeaderBySequence is a query that loads a single row from the
 // `ledgerheaders` table.
