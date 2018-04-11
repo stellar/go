77e46e4b5f83333c76a215c3034b8e8766713d9b
Bartek Nowotarski <bartek@nowotarski.info>
2018-04-11 16:25:50 +0200

    Remove Authorize errors

--- services/internal/bridge-compliance-shared/protocols/bridge/authorize.go
+++ services/internal/bridge-compliance-shared/protocols/bridge/authorize.go
@@ -1,22 +1,5 @@
 package bridge
 
-import (
-	"net/http"
-
-	"github.com/stellar/go/services/internal/bridge-compliance-shared/http/helpers"
-)
-
-var (
-	// AllowTrustMalformed is an error response
-	AllowTrustMalformed = &helpers.ErrorResponse{Code: "allow_trust_malformed", Message: "Asset name is malformed.", Status: http.StatusBadRequest}
-	// AllowTrustNoTrustline is an error response
-	AllowTrustNoTrustline = &helpers.ErrorResponse{Code: "allow_trust_no_trustline", Message: "Trustor does not have a trustline yet.", Status: http.StatusBadRequest}
-	// AllowTrustTrustNotRequired is an error response
-	AllowTrustTrustNotRequired = &helpers.ErrorResponse{Code: "allow_trust_trust_not_required", Message: "Authorizing account does not require allowing trust. Set AUTH_REQUIRED_FLAG on your account to use this feature.", Status: http.StatusBadRequest}
-	// AllowTrustCantRevoke is an error response
-	AllowTrustCantRevoke = &helpers.ErrorResponse{Code: "allow_trust_cant_revoke", Message: "Authorizing account has AUTH_REVOCABLE_FLAG set. Can't revoke the trustline.", Status: http.StatusBadRequest}
-)
-
 // AuthorizeRequest represents request made to /authorize endpoint of bridge server
 type AuthorizeRequest struct {
 	AccountID string `name:"account_id" required:""`
