8e902660466e35ff9f2588408d24dbf839901c00
tomerweller <tomer.weller@gmail.com>
2018-06-05 09:57:02 -0700

    fix offers for account timestamps. closes #478 (#480)

--- services/horizon/internal/actions_offer_test.go
+++ services/horizon/internal/actions_offer_test.go
@@ -18,6 +18,7 @@ func TestOfferActions_Index(t *testing.T) {
 		//test last modified timestamp
 		var records []map[string]interface{}
 		ht.UnmarshalPage(w.Body, &records)
-		ht.Assert.Equal("1970-01-01T00:00:05Z", records[2]["last_modified"])
+		ht.Assert.Equal("2018-06-01T17:10:08Z", records[2]["last_modified_time"])
+		ht.Assert.EqualValues(5, records[2]["last_modified_ledger"])
 	}
 }
