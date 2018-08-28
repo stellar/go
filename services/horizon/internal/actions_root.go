b306c23f3be460142e5159f542ce79ebc293e21a
Sean Sorrell <sean@chain.com>
2018-08-28 15:36:55 -0700

    gofmt

--- services/horizon/internal/actions_root.go
+++ services/horizon/internal/actions_root.go
@@ -1,9 +1,9 @@
 package horizon
 
 import (
+	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/services/horizon/internal/ledger"
 	"github.com/stellar/go/services/horizon/internal/resourceadapter"
-	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/support/render/hal"
 )
 
