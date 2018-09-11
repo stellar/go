ad1246b8efaa89032e4bf59f3062b38a1ce5fb21
Tomer Weller <tomer.weller@gmail.com>
2018-09-11 15:54:39 -0700

    sql: add base_offer_id, counter_offer id to trades table

---
+++ services/horizon/internal/db2/schema/migrations/13_trade_offer_ids.sql
@@ -0,0 +1,15 @@
+-- +migrate Up
+
+ALTER TABLE history_trades ADD base_offer_id VARCHAR[21];
+ALTER TABLE history_trades ADD counter_offer_id VARCHAR[21];
+
+CREATE INDEX htrd_by_base_offer ON history_trades USING btree(base_offer_id);
+CREATE INDEX htrd_by_counter_offer ON history_trades USING btree(base_offer_id);
+
+-- +migrate Down
+
+DROP INDEX htrd_by_base_offer
+DROP INDEX htrd_by_counter_offer
+
+ALTER TABLE history_trades DROP COLUMN base_offer_id;
+ALTER TABLE history_trades DROP COLUMN counter_offer_id;
