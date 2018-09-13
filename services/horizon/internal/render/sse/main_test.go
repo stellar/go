cfb869fac9cd18400da7777bf45a8363dbbd908f
Le Cheng Fan <tommy@chain.com>
2018-09-13 11:32:31 -0700

    Partial conversion

--- services/horizon/internal/render/sse/main_test.go
+++ services/horizon/internal/render/sse/main_test.go
@@ -1,40 +1,53 @@
 package sse
 
 import (
+	"bytes"
+	"context"
 	"errors"
+	"github.com/stellar/go/support/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"net/http/httptest"
 	"testing"
-
-	. "github.com/smartystreets/goconvey/convey"
-	"github.com/stellar/go/support/test"
 )
 
-func TestSsePackage(t *testing.T) {
-	ctx, log := test.ContextWithLogBuffer()
+type SsePackageTestSuite struct {
+	suite.Suite
+	ctx context.Context
+	log *bytes.Buffer
+}
 
-	Convey("sse.WriteEvent outputs data properly", t, func() {
-		expectations := []struct {
-			Event     Event
-			Substring string
-		}{
-			{Event{Data: "test"}, "data: \"test\"\n\n"},
-			{Event{ID: "1", Data: "test"}, "id: 1\n"},
-			{Event{Retry: 1000, Data: "test"}, "retry: 1000\n"},
-			{Event{Error: errors.New("busted")}, "event: err\ndata: busted\n\n"},
-			{Event{Event: "test", Data: "test"}, "event: test\ndata: \"test\"\n\n"},
-		}
+func (suite *SsePackageTestSuite) SetupTest() {
+	suite.ctx, suite.log = test.ContextWithLogBuffer()
+}
 
-		for _, e := range expectations {
-			w := httptest.NewRecorder()
-			WriteEvent(ctx, w, e.Event)
-			So(w.Body.String(), ShouldContainSubstring, e.Substring)
-		}
-	})
+func (suite *SsePackageTestSuite) TestWriteEventOutput() {
+	expectations := []struct {
+		Event     Event
+		Substring string
+	}{
+		{Event{Data: "test"}, "data: \"test\"\n\n"},
+		{Event{ID: "1", Data: "test"}, "id: 1\n"},
+		{Event{Retry: 1000, Data: "test"}, "retry: 1000\n"},
+		{Event{Error: errors.New("busted")}, "event: err\ndata: busted\n\n"},
+		{Event{Event: "test", Data: "test"}, "event: test\ndata: \"test\"\n\n"},
+	}
 
-	Convey("sse.WriteEvent logs errors", t, func() {
+	for _, e := range expectations {
 		w := httptest.NewRecorder()
-		WriteEvent(ctx, w, Event{Error: errors.New("busted")})
-		So(log.String(), ShouldContainSubstring, "level=error")
-		So(log.String(), ShouldContainSubstring, "busted")
-	})
+		WriteEvent(suite.ctx, w, e.Event)
+		bodyString := w.Body.String()
+		assert.Equal(suite.T(), e.Substring, bodyString)
+	}
+}
+
+func (suite *SsePackageTestSuite) TestWriteEventLogs() {
+	w := httptest.NewRecorder()
+	WriteEvent(suite.ctx, w, Event{Error: errors.New("busted")})
+	assert.Contains(suite.T(), suite.log.String(), "level=error")
+	assert.Contains(suite.T(), suite.log.String(), "busted")
+}
+
+func TestSsePackageTestSuite(t *testing.T) {
+	suite.Run(t, new(SsePackageTestSuite))
 }
