b2de6a39c3e73e95800365158fae145e80b2ce1a
Le Cheng Fan <tommy@chain.com>
2018-09-13 14:57:25 -0700

    Remove GoConvey dependency in internal/log

--- services/horizon/internal/log/main_test.go
+++ services/horizon/internal/log/main_test.go
@@ -6,178 +6,187 @@ import (
 	"errors"
 	"testing"
 
-	ge "github.com/go-errors/errors"
 	"github.com/sirupsen/logrus"
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+
+	ge "github.com/go-errors/errors"
 )
 
-func TestLogPackage(t *testing.T) {
+func TestSet(t *testing.T) {
+	assert.Nil(t, context.Background().Value(&contextKey))
+	l, _ := New()
+	ctx := Set(context.Background(), l)
+	assert.Equal(t, l, ctx.Value(&contextKey))
+}
 
-	Convey("Set", t, func() {
-		So(context.Background().Value(&contextKey), ShouldBeNil)
-		l, _ := New()
-		ctx := Set(context.Background(), l)
-		So(ctx.Value(&contextKey), ShouldEqual, l)
+func TestCtx(t *testing.T) {
+	// defaults to the default logger
+	assert.Equal(t, DefaultLogger, Ctx(context.Background()))
+
+	// a set value overrides the default
+	l, _ := New()
+
+	ctx := Set(context.Background(), l)
+	assert.Equal(t, l, Ctx(ctx))
+
+	// the deepest set value is returns
+	nested, _ := New()
+	nctx := Set(ctx, nested)
+	assert.Equal(t, nested, Ctx(nctx))
+}
+
+func TestPushContext(t *testing.T) {
+	output := new(bytes.Buffer)
+	l, _ := New()
+	l.Logger.Formatter.(*logrus.TextFormatter).DisableColors = true
+	l.Logger.Out = output
+	ctx := Set(context.Background(), l.WithField("foo", "bar"))
+
+	Ctx(ctx).Warn("hello")
+	assert.Contains(t, output.String(), "foo=bar")
+	assert.NotContains(t, output.String(), "foo=baz")
+
+	ctx = PushContext(ctx, func(logger *Entry) *Entry {
+		return logger.WithField("foo", "baz")
 	})
 
-	Convey("Ctx", t, func() {
-		// defaults to the default logger
-		So(Ctx(context.Background()), ShouldEqual, DefaultLogger)
+	Ctx(ctx).Warn("hello")
+	assert.Contains(t, output.String(), "foo=baz")
+}
 
-		// a set value overrides the default
-		l, _ := New()
+type LoggingStatementsTestSuite struct {
+	suite.Suite
+	output *bytes.Buffer
+	l      *Entry
+	m      *Metrics
+}
 
-		ctx := Set(context.Background(), l)
-		So(Ctx(ctx), ShouldEqual, l)
+func (suite *LoggingStatementsTestSuite) SetupTest() {
+	suite.output = new(bytes.Buffer)
+	suite.l, suite.m = New()
+	suite.l.Logger.Formatter.(*logrus.TextFormatter).DisableColors = true
+	suite.l.Logger.Out = suite.output
+}
 
-		// the deepest set value is returns
-		nested, _ := New()
-		nctx := Set(ctx, nested)
-		So(Ctx(nctx), ShouldEqual, nested)
+// Tests that the default logging severity level is Warn
+func (suite *LoggingStatementsTestSuite) TestDefaultSeverityLevel() {
+	suite.l.Debug("debug")
+	suite.l.Info("info")
+	suite.l.Warn("warn")
+
+	assert.NotContains(suite.T(), suite.output.String(), "level=info")
+	assert.NotContains(suite.T(), suite.output.String(), "level=debug")
+	assert.Contains(suite.T(), suite.output.String(), "level=warn")
+
+}
+
+// Tests logging statements that fall under the Debug level
+func (suite *LoggingStatementsTestSuite) TestSeverityLevelDebug() {
+	suite.l.Logger.Level = logrus.InfoLevel
+	suite.l.Debug("Debug")
+	assert.Equal(suite.T(), "", suite.output.String())
+
+	suite.l.Logger.Level = logrus.DebugLevel
+	suite.l.Debug("Debug")
+	assert.Contains(suite.T(), suite.output.String(), "level=debug")
+	assert.Contains(suite.T(), suite.output.String(), "msg=Debug")
+}
+
+// Tests logging statements that fall under the Info level
+func (suite *LoggingStatementsTestSuite) TestSeverityLevelInfo() {
+	suite.l.Logger.Level = logrus.WarnLevel
+	suite.l.Debug("foo")
+	suite.l.Info("foo")
+	assert.Equal(suite.T(), "", suite.output.String())
+
+	suite.l.Logger.Level = logrus.InfoLevel
+	suite.l.Info("foo")
+	assert.Contains(suite.T(), suite.output.String(), "level=info")
+	assert.Contains(suite.T(), suite.output.String(), "msg=foo")
+}
+
+// Tests logging statements that fall under the Warn level
+func (suite *LoggingStatementsTestSuite) TestSeverityLevelWarn() {
+	suite.l.Logger.Level = logrus.ErrorLevel
+	suite.l.Debug("foo")
+	suite.l.Info("foo")
+	suite.l.Warn("foo")
+	assert.Equal(suite.T(), "", suite.output.String())
+
+	suite.l.Logger.Level = logrus.WarnLevel
+	suite.l.Warn("foo")
+	assert.Contains(suite.T(), suite.output.String(), "level=warn")
+	assert.Contains(suite.T(), suite.output.String(), "msg=foo")
+}
+
+// Tests logging statements that fall under the Error level
+func (suite *LoggingStatementsTestSuite) TestSeverityLevelError() {
+	suite.l.Logger.Level = logrus.FatalLevel
+	suite.l.Debug("foo")
+	suite.l.Info("foo")
+	suite.l.Warn("foo")
+	suite.l.Error("foo")
+	assert.Equal(suite.T(), "", suite.output.String())
+
+	suite.l.Logger.Level = logrus.ErrorLevel
+	suite.l.Error("foo")
+	assert.Contains(suite.T(), suite.output.String(), "level=error")
+	assert.Contains(suite.T(), suite.output.String(), "msg=foo")
+}
+
+// Tests logging statements that fall under the Panic level
+func (suite *LoggingStatementsTestSuite) TestSeverityLevelPanic() {
+	suite.l.Logger.Level = logrus.PanicLevel
+	suite.l.Debug("foo")
+	suite.l.Info("foo")
+	suite.l.Warn("foo")
+	suite.l.Error("foo")
+	assert.Equal(suite.T(), "", suite.output.String())
+
+	assert.Panics(suite.T(), func() {
+		suite.l.Panic("foo")
 	})
 
-	Convey("PushContext", t, func() {
-		output := new(bytes.Buffer)
-		l, _ := New()
-		l.Logger.Formatter.(*logrus.TextFormatter).DisableColors = true
-		l.Logger.Out = output
-		ctx := Set(context.Background(), l.WithField("foo", "bar"))
+	assert.Contains(suite.T(), suite.output.String(), "level=panic")
+	assert.Contains(suite.T(), suite.output.String(), "msg=foo")
+}
 
-		Ctx(ctx).Warn("hello")
-		So(output.String(), ShouldContainSubstring, "foo=bar")
-		So(output.String(), ShouldNotContainSubstring, "foo=baz")
+// Adds the stack properly if a go-errors.Error is provided
+func (suite *LoggingStatementsTestSuite) TestWithStack() {
+	err := ge.New("broken")
+	suite.l.WithStack(err).Error("test")
+	// simply ensure that the line creating the above error is in the log
+	assert.Contains(suite.T(), suite.output.String(), "main_test.go:")
+}
 
-		ctx = PushContext(ctx, func(logger *Entry) *Entry {
-			return logger.WithField("foo", "baz")
-		})
+//Adds stack=unknown when the provided err has not stack info
+func (suite *LoggingStatementsTestSuite) TestWithUnknownStack() {
+	suite.l.WithStack(errors.New("broken")).Error("test")
+	assert.Contains(suite.T(), suite.output.String(), "stack=unknown")
+}
 
-		Ctx(ctx).Warn("hello")
-		So(output.String(), ShouldContainSubstring, "foo=baz")
+// Tests that metric counts are correct
+func (suite *LoggingStatementsTestSuite) TestMetrics() {
+	suite.l.Logger.Level = logrus.DebugLevel
+
+	for _, meter := range *suite.m {
+		assert.Equal(suite.T(), int64(0), meter.Count())
+	}
+
+	suite.l.Debug("foo")
+	suite.l.Info("foo")
+	suite.l.Warn("foo")
+	suite.l.Error("foo")
+	assert.Panics(suite.T(), func() {
+		suite.l.Panic("foo")
 	})
 
-	Convey("Logging Statements", t, func() {
-		output := new(bytes.Buffer)
-		l, _ := New()
-		l.Logger.Formatter.(*logrus.TextFormatter).DisableColors = true
-		l.Logger.Out = output
+	for _, meter := range *suite.m {
+		assert.Equal(suite.T(), int64(1), meter.Count())
+	}
+}
 
-		Convey("defaults to warn", func() {
-
-			l.Debug("debug")
-			l.Info("info")
-			l.Warn("warn")
-
-			So(output.String(), ShouldNotContainSubstring, "level=info")
-			So(output.String(), ShouldNotContainSubstring, "level=debug")
-			So(output.String(), ShouldContainSubstring, "level=warn")
-		})
-
-		Convey("Debug severity", func() {
-			l.Logger.Level = logrus.InfoLevel
-			l.Debug("Debug")
-			So(output.String(), ShouldEqual, "")
-
-			l.Logger.Level = logrus.DebugLevel
-			l.Debug("Debug")
-			So(output.String(), ShouldContainSubstring, "level=debug")
-			So(output.String(), ShouldContainSubstring, "msg=Debug")
-		})
-
-		Convey("Info severity", func() {
-			l.Logger.Level = logrus.WarnLevel
-			l.Debug("foo")
-			l.Info("foo")
-			So(output.String(), ShouldEqual, "")
-
-			l.Logger.Level = logrus.InfoLevel
-			l.Info("foo")
-			So(output.String(), ShouldContainSubstring, "level=info")
-			So(output.String(), ShouldContainSubstring, "msg=foo")
-		})
-
-		Convey("Warn severity", func() {
-			l.Logger.Level = logrus.ErrorLevel
-			l.Debug("foo")
-			l.Info("foo")
-			l.Warn("foo")
-			So(output.String(), ShouldEqual, "")
-
-			l.Logger.Level = logrus.WarnLevel
-			l.Warn("foo")
-			So(output.String(), ShouldContainSubstring, "level=warn")
-			So(output.String(), ShouldContainSubstring, "msg=foo")
-		})
-
-		Convey("Error severity", func() {
-			l.Logger.Level = logrus.FatalLevel
-			l.Debug("foo")
-			l.Info("foo")
-			l.Warn("foo")
-			l.Error("foo")
-			So(output.String(), ShouldEqual, "")
-
-			l.Logger.Level = logrus.ErrorLevel
-			l.Error("foo")
-			So(output.String(), ShouldContainSubstring, "level=error")
-			So(output.String(), ShouldContainSubstring, "msg=foo")
-		})
-
-		Convey("Panic severity", func() {
-			l.Logger.Level = logrus.PanicLevel
-			l.Debug("foo")
-			l.Info("foo")
-			l.Warn("foo")
-			l.Error("foo")
-			So(output.String(), ShouldEqual, "")
-
-			So(func() {
-				l.Panic("foo")
-			}, ShouldPanic)
-
-			So(output.String(), ShouldContainSubstring, "level=panic")
-			So(output.String(), ShouldContainSubstring, "msg=foo")
-		})
-	})
-
-	Convey("WithStack", t, func() {
-		output := new(bytes.Buffer)
-		l, _ := New()
-		l.Logger.Formatter.(*logrus.TextFormatter).DisableColors = true
-		l.Logger.Out = output
-
-		Convey("Adds stack=unknown when the provided err has not stack info", func() {
-			l.WithStack(errors.New("broken")).Error("test")
-			So(output.String(), ShouldContainSubstring, "stack=unknown")
-		})
-		Convey("Adds the stack properly if a go-errors.Error is provided", func() {
-			err := ge.New("broken")
-			l.WithStack(err).Error("test")
-			// simply ensure that the line creating the above error is in the log
-			So(output.String(), ShouldContainSubstring, "main_test.go:")
-		})
-	})
-
-	Convey("Metrics", t, func() {
-		output := new(bytes.Buffer)
-		l, m := New()
-		l.Logger.Formatter.(*logrus.TextFormatter).DisableColors = true
-		l.Logger.Level = logrus.DebugLevel
-		l.Logger.Out = output
-
-		for _, meter := range *m {
-			So(meter.Count(), ShouldEqual, 0)
-		}
-
-		l.Debug("foo")
-		l.Info("foo")
-		l.Warn("foo")
-		l.Error("foo")
-		So(func() {
-			l.Panic("foo")
-		}, ShouldPanic)
-
-		for _, meter := range *m {
-			So(meter.Count(), ShouldEqual, 1)
-		}
-	})
+func TestLoggingStatementsTestSuite(t *testing.T) {
+	suite.Run(t, new(LoggingStatementsTestSuite))
 }
