9251ee3ec08049fefedcce52e83d81f99d2df34e
Le Cheng Fan <tommy@interstellar.com>
2018-09-17 11:04:12 -0700

    Remove goconvey from hal/link_builder_test.go (#661)
    Remove goconvey from hal/link_builder_test.go

--- support/render/hal/link_builder_test.go
+++ support/render/hal/link_builder_test.go
@@ -4,33 +4,29 @@ import (
 	"net/url"
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLinkBuilder(t *testing.T) {
+	// Link expansion test
+	check := func(href string, base string, expectedResult string) {
+		lb := LinkBuilder{mustParseURL(base)}
+		result := lb.expandLink(href)
+		assert.Equal(t, expectedResult, result)
+	}
 
-	Convey("Link Expansion", t, func() {
+	check("/root", "", "/root")
+	check("/root", "//stellar.org", "//stellar.org/root")
+	check("/root", "https://stellar.org", "https://stellar.org/root")
+	check("//else.org/root", "", "//else.org/root")
+	check("//else.org/root", "//stellar.org", "//else.org/root")
+	check("//else.org/root", "https://stellar.org", "//else.org/root")
+	check("https://else.org/root", "", "https://else.org/root")
+	check("https://else.org/root", "//stellar.org", "https://else.org/root")
+	check("https://else.org/root", "https://stellar.org", "https://else.org/root")
 
-		check := func(href string, base string, expectedResult string) {
-			lb := LinkBuilder{mustParseURL(base)}
-			result := lb.expandLink(href)
-			So(result, ShouldEqual, expectedResult)
-		}
-
-		check("/root", "", "/root")
-		check("/root", "//stellar.org", "//stellar.org/root")
-		check("/root", "https://stellar.org", "https://stellar.org/root")
-		check("//else.org/root", "", "//else.org/root")
-		check("//else.org/root", "//stellar.org", "//else.org/root")
-		check("//else.org/root", "https://stellar.org", "//else.org/root")
-		check("https://else.org/root", "", "https://else.org/root")
-		check("https://else.org/root", "//stellar.org", "https://else.org/root")
-		check("https://else.org/root", "https://stellar.org", "https://else.org/root")
-
-		// Regression: ensure that parameters are not escaped
-		check("/accounts/{id}", "https://stellar.org", "https://stellar.org/accounts/{id}")
-	})
-
+	// Regression: ensure that parameters are not escaped
+	check("/accounts/{id}", "https://stellar.org", "https://stellar.org/accounts/{id}")
 }
 
 func mustParseURL(base string) *url.URL {
