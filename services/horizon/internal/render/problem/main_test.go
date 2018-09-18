c0de301b8d38225b4ddfe3def455dd23b1fb7cf5
Le Cheng Fan <tommy@interstellar.com>
2018-09-18 03:14:47 -0700

    Refactor to remove goconvey from internal/render/problem (#666)

--- services/horizon/internal/render/problem/main_test.go
+++ services/horizon/internal/render/problem/main_test.go
@@ -5,45 +5,45 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stellar/go/support/context/requestid"
 	"github.com/stellar/go/support/render/problem"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestProblemPackage(t *testing.T) {
-	ctx := context.Background()
+var ctx = context.Background()
+var testRender = func(ctx context.Context, p interface{}) *httptest.ResponseRecorder {
+	w := httptest.NewRecorder()
+	problem.Render(ctx, w, p)
+	return w
+}
 
-	testRender := func(ctx context.Context, p interface{}) *httptest.ResponseRecorder {
-		w := httptest.NewRecorder()
-		problem.Render(ctx, w, p)
-		return w
+func TestCommonProblems(t *testing.T) {
+	testCases := []struct {
+		testName     string
+		p            problem.P
+		expectedCode int
+	}{
+		{"NotFound", problem.NotFound, 404},
+		{"RateLimitExceeded", RateLimitExceeded, 429},
 	}
 
-	Convey("Common Problems", t, func() {
-		Convey("NotFound", func() {
-			w := testRender(ctx, problem.NotFound)
-			So(w.Code, ShouldEqual, 404)
-			t.Log(w.Body.String())
+	for _, tc := range testCases {
+		t.Run(tc.testName, func(t *testing.T) {
+			w := testRender(ctx, tc.p)
+			assert.Equal(t, tc.expectedCode, w.Code)
 		})
+	}
+}
 
-		Convey("RateLimitExceeded", func() {
-			w := testRender(ctx, RateLimitExceeded)
-			So(w.Code, ShouldEqual, 429)
-			t.Log(w.Body.String())
-		})
-	})
+func TestInflate(t *testing.T) {
+	// Sets Instance to the request id based on the context
+	ctx2 := requestid.Context(ctx, "2")
+	p := problem.P{}
 
-	Convey("problem.Inflate", t, func() {
-		Convey("sets Instance to the request id based upon the context", func() {
-			ctx2 := requestid.Context(ctx, "2")
-			p := problem.P{}
-			Inflate(ctx2, &p)
+	Inflate(ctx2, &p)
+	assert.Equal(t, "2", p.Instance)
 
-			So(p.Instance, ShouldEqual, "2")
-
-			// when no request id is set, instance should be ""
-			Inflate(ctx, &p)
-			So(p.Instance, ShouldEqual, "")
-		})
-	})
+	// when no request id is set, instance should be ""
+	Inflate(ctx, &p)
+	assert.Equal(t, "", p.Instance)
 }
