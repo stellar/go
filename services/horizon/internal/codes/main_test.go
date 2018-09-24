924decbfd3d50e3f6f1e6effac26686569d8a668
Le Cheng Fan <tommy@interstellar.com>
2018-09-24 05:18:45 -0700

    Remove goconvey from internal/codes/main_test (#681)

--- services/horizon/internal/codes/main_test.go
+++ services/horizon/internal/codes/main_test.go
@@ -3,40 +3,36 @@ package codes
 import (
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stellar/go/xdr"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCodes(t *testing.T) {
-	Convey("codes.String", t, func() {
-		tests := []struct {
-			Input    interface{}
-			Expected string
-			Err      error
-		}{
-			{xdr.TransactionResultCodeTxSuccess, "tx_success", nil},
-			{xdr.OperationResultCodeOpBadAuth, "op_bad_auth", nil},
-			{xdr.CreateAccountResultCodeCreateAccountLowReserve, "op_low_reserve", nil},
-			{xdr.PaymentResultCodePaymentSrcNoTrust, "op_src_no_trust", nil},
-			{0, "", ErrUnknownCode},
+func TestString(t *testing.T) {
+	tests := []struct {
+		Input    interface{}
+		Expected string
+		Err      error
+	}{
+		{xdr.TransactionResultCodeTxSuccess, "tx_success", nil},
+		{xdr.OperationResultCodeOpBadAuth, "op_bad_auth", nil},
+		{xdr.CreateAccountResultCodeCreateAccountLowReserve, "op_low_reserve", nil},
+		{xdr.PaymentResultCodePaymentSrcNoTrust, "op_src_no_trust", nil},
+		{0, "", ErrUnknownCode},
+	}
+
+	for _, test := range tests {
+		actual, err := String(test.Input)
+
+		if test.Err != nil {
+			assert.NotNil(t, err)
+			assert.Equal(t, test.Err.Error(), err.Error())
+		} else {
+			assert.Nil(t, err)
+			assert.Equal(t, test.Expected, actual)
 		}
-
-		for _, test := range tests {
-			actual, err := String(test.Input)
-
-			if test.Err != nil {
-				So(err, ShouldNotBeNil)
-				So(err.Error(), ShouldEqual, test.Err.Error())
-			} else {
-				So(err, ShouldBeNil)
-				So(actual, ShouldEqual, test.Expected)
-			}
-		}
-	})
-
-	Convey("codes.ForOperationResult", t, func() {
-		//TODO: op_inner refers to inner result code
-		//TODO: non op_inner uses the outer result code
-		//TODO: one test for each operation type
-	})
+	}
 }
+
+//TODO: op_inner refers to inner result code
+//TODO: non op_inner uses the outer result code
+//TODO: one test for each operation type
