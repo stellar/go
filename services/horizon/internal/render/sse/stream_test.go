d256bdcc0eae79a073c5c1648b4cd5946af08062
Le Cheng Fan <tommy@interstellar.com>
2018-09-26 13:38:21 -0700

    Add more tests

--- services/horizon/internal/render/sse/stream_test.go
+++ services/horizon/internal/render/sse/stream_test.go
@@ -2,8 +2,10 @@ package sse
 
 import (
 	"context"
+	"errors"
 	"github.com/stretchr/testify/suite"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"time"
 
@@ -18,15 +20,31 @@ type StreamTestSuite struct {
 	stream Stream
 }
 
+func (suite *StreamTestSuite) checkHeadersAndPreamble() {
+	assert.Equal(suite.T(), "text/event-stream; charset=utf-8", suite.w.Header().Get("Content-Type"))
+	assert.Equal(suite.T(), "no-cache", suite.w.Header().Get("Cache-Control"))
+	assert.Equal(suite.T(), "keep-alive", suite.w.Header().Get("Connection"))
+	assert.Equal(suite.T(), "*", suite.w.Header().Get("Access-Control-Allow-Origin"))
+	assert.Equal(suite.T(), 200, suite.w.Code)
+	assert.Contains(suite.T(), suite.w.Body.String(), "retry: 1000\nevent: open\ndata: \"hello\"\n\n")
+}
+
 func (suite *StreamTestSuite) SetupTest() {
 	suite.ctx, _ = test.ContextWithLogBuffer()
 	suite.w = httptest.NewRecorder()
 	suite.stream = NewStream(suite.ctx, suite.w, nil)
 }
 
-func TestStream_Send(t *testing.T) {
-	// Before sending,
-	suite.S
+// Tests that the stream sends the preamble before any events and that events are correctly sent.
+func (suite *StreamTestSuite) TestStream_Send() {
+	e := Event{Data:"test message"}
+	suite.stream.Send(e)
+	suite.stream.Done()
+	// Before sending, it should have sent the preamble first and set the headers.
+	suite.checkHeadersAndPreamble()
+	// Now check that the data got written
+	assert.Contains(suite.T(), suite.w.Body.String(), "test message")
+	assert.Equal(suite.T(), 1, suite.stream.SentCount())
 }
 
 // Tests that heartbeat events are sent by Stream.
@@ -40,6 +58,27 @@ func (suite *StreamTestSuite) TestStream_SendHeartbeats() {
 	assert.Contains(suite.T(), suite.w.Body.String(), ":heartbeat\n")
 }
 
+// Tests that Stream can send error events.
+func (suite *StreamTestSuite) TestStream_Err() {
+	err := errors.New("example error")
+	suite.stream.Err(err)
+	// Even if no events have been sent, Err should still send the preamble before the error event.
+	suite.checkHeadersAndPreamble()
+	assert.Contains(suite.T(), suite.w.Body.String(), "event: err\ndata: example error\n\n")
+	assert.True(suite.T(), suite.stream.IsDone())
+}
+
+// Tests that SetLimit stops the stream after the limit has been reached.
+func (suite *StreamTestSuite) TestStream_SetLimit() {
+	suite.stream.SetLimit(3)
+	// Send more than the limit
+	for i := 0; i < 5; i++ {
+		message := "test message " + strconv.Itoa(i)
+		suite.stream.Send(Event{Data:message})
+	}
+	assert.True(suite.T(), suite.stream.IsDone())
+	println(suite.w.Body.String())
+}
 // Runs the test suite.
 func TestStreamTestSuite(t *testing.T) {
 	suite.Run(t, new(StreamTestSuite))
