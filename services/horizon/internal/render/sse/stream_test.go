e16a00785bb8c81d2befd5bd2b36a6af0e6f14d8
Le Cheng Fan <tommy@interstellar.com>
2018-09-26 11:40:55 -0700

    formatting

--- services/horizon/internal/render/sse/stream_test.go
+++ services/horizon/internal/render/sse/stream_test.go
@@ -1,14 +1,13 @@
 package sse
 
 import (
-	"github.com/stretchr/testify/assert"
 	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/stellar/go/support/test"
+	"github.com/stretchr/testify/assert"
 )
-
 
 // Tests that heartbeat events are sent by Stream.
 func TestStream_SendHeartbeats(t *testing.T) {
@@ -16,7 +15,7 @@ func TestStream_SendHeartbeats(t *testing.T) {
 	w := httptest.NewRecorder()
 	stream := NewStream(ctx, w, nil)
 	// Set heartbeat interval to a low value for testing.
-	stream.SetHeartbeatInterval(500*time.Millisecond)
+	stream.SetHeartbeatInterval(500 * time.Millisecond)
 	stream.Init()
 	// Wait long enough for heartbeat to send
 	time.Sleep(time.Second)
