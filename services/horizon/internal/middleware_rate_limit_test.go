5dadad27ddd96115f25331046a77cbf0aa1c5010
Le Cheng Fan <tommy@interstellar.com>
2018-10-02 13:28:34 -0700

    Remove goconvey from middleware rate limit (#693)

--- services/horizon/internal/middleware_rate_limit_test.go
+++ services/horizon/internal/middleware_rate_limit_test.go
@@ -5,108 +5,134 @@ import (
 	"testing"
 
 	"github.com/PuerkitoBio/throttled"
-	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stellar/go/services/horizon/internal/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestRateLimitMiddleware(t *testing.T) {
+type RateLimitMiddlewareTestSuite struct {
+	suite.Suite
+	ht  *HTTPT
+	c   Config
+	app *App
+	rh  test.RequestHelper
+}
+
+func (suite *RateLimitMiddlewareTestSuite) SetupSuite() {
+	suite.ht = StartHTTPTest(suite.T(), "base")
+}
+
+func (suite *RateLimitMiddlewareTestSuite) SetupTest() {
+	suite.c = NewTestConfig()
+	suite.c.RateLimit = throttled.PerHour(10)
+	suite.app, _ = NewApp(suite.c)
+	suite.rh = NewRequestHelper(suite.app)
+}
+
+func (suite *RateLimitMiddlewareTestSuite) TearDownSuite() {
+	suite.ht.Finish()
+}
+
+func (suite *RateLimitMiddlewareTestSuite) TearDownTest() {
+	suite.app.Close()
+}
+
+// Sets X-RateLimit-Limit headers correctly.
+func (suite *RateLimitMiddlewareTestSuite) TestRateLimit_LimitHeaders() {
+	w := suite.rh.Get("/")
+	assert.Equal(suite.T(), 200, w.Code)
+	assert.Equal(suite.T(), "10", w.Header().Get("X-RateLimit-Limit"))
+}
+
+// Sets X-RateLimit-Remaining headers correctly.
+func (suite *RateLimitMiddlewareTestSuite) TestRateLimit_RemainingHeaders() {
+	for i := 0; i < 10; i++ {
+		w := suite.rh.Get("/")
+		expected := 10 - (i + 1)
+		assert.Equal(suite.T(), strconv.Itoa(expected), w.Header().Get("X-RateLimit-Remaining"))
+	}
+
+	// confirm remaining stays at 0
+	for i := 0; i < 10; i++ {
+		w := suite.rh.Get("/")
+		assert.Equal(suite.T(), "0", w.Header().Get("X-RateLimit-Remaining"))
+	}
+}
+
+// Sets X-RateLimit-Reset header correctly.
+func (suite *RateLimitMiddlewareTestSuite) TestRateLimit_ResetHeaders() {
+	w := suite.rh.Get("/")
+	assert.Equal(suite.T(), "3599", w.Header().Get("X-RateLimit-Reset"))
+}
+
+// Restricts based on RemoteAddr IP after too many requests.
+func (suite *RateLimitMiddlewareTestSuite) TestRateLimit_RemoteAddr() {
+	for i := 0; i < 10; i++ {
+		w := suite.rh.Get("/")
+		assert.Equal(suite.T(), 200, w.Code)
+	}
+
+	w := suite.rh.Get("/")
+	assert.Equal(suite.T(), 429, w.Code)
+
+	w = suite.rh.Get("/", test.RequestHelperRemoteAddr("127.0.0.2"))
+	assert.Equal(suite.T(), 200, w.Code)
+
+	// Ignores ports
+	w = suite.rh.Get("/", test.RequestHelperRemoteAddr("127.0.0.1:4312"))
+	assert.Equal(suite.T(), 429, w.Code)
+}
+
+// Restrict based upon X-Forwarded-For correctly.
+func (suite *RateLimitMiddlewareTestSuite) TestRateLimit_XForwardedFor() {
+	for i := 0; i < 10; i++ {
+		w := suite.rh.Get("/", test.RequestHelperXFF("4.4.4.4"))
+		assert.Equal(suite.T(), 200, w.Code)
+	}
+
+	w := suite.rh.Get("/", test.RequestHelperXFF("4.4.4.4"))
+	assert.Equal(suite.T(), 429, w.Code)
+
+	// allow other ips
+	w = suite.rh.Get("/", test.RequestHelperRemoteAddr("4.4.4.3"))
+	assert.Equal(suite.T(), 200, w.Code)
+
+	// Ignores leading private ips
+	w = suite.rh.Get("/", test.RequestHelperXFF("10.0.0.1, 4.4.4.4"))
+	assert.Equal(suite.T(), 429, w.Code)
+
+	// Ignores trailing ips
+	w = suite.rh.Get("/", test.RequestHelperXFF("4.4.4.4, 4.4.4.5, 127.0.0.1"))
+	assert.Equal(suite.T(), 429, w.Code)
+}
+
+func TestRateLimitMiddlewareTestSuite(t *testing.T) {
+	suite.Run(t, new(RateLimitMiddlewareTestSuite))
+}
+
+// Rate Limiting works with redis
+func TestRateLimit_Redis(t *testing.T) {
 	ht := StartHTTPTest(t, "base")
 	defer ht.Finish()
+	c := NewTestConfig()
+	c.RateLimit = throttled.PerHour(10)
+	c.RedisURL = "redis://127.0.0.1:6379/"
+	app, _ := NewApp(c)
+	defer app.Close()
+	rh := NewRequestHelper(app)
 
-	Convey("Rate Limiting", t, func() {
-		c := NewTestConfig()
-		c.RateLimit = throttled.PerHour(10)
-		app, _ := NewApp(c)
-		defer app.Close()
-		rh := NewRequestHelper(app)
+	redis := app.redis.Get()
+	_, err := redis.Do("FLUSHDB")
+	assert.Nil(t, err)
 
-		Convey("sets X-RateLimit-Limit headers correctly", func() {
-			w := rh.Get("/")
-			So(w.Code, ShouldEqual, 200)
-			So(w.Header().Get("X-RateLimit-Limit"), ShouldEqual, "10")
-		})
-
-		Convey("sets X-RateLimit-Remaining headers correctly", func() {
-			for i := 0; i < 10; i++ {
-				w := rh.Get("/")
-				expected := 10 - (i + 1)
-				So(w.Header().Get("X-RateLimit-Remaining"), ShouldEqual, strconv.Itoa(expected))
-			}
-
-			// confirm remaining stays at 0
-			for i := 0; i < 10; i++ {
-				w := rh.Get("/")
-				So(w.Header().Get("X-RateLimit-Remaining"), ShouldEqual, "0")
-			}
-		})
-
-		Convey("sets X-RateLimit-Reset header correctly", func() {
-			w := rh.Get("/")
-			So(w.Header().Get("X-RateLimit-Reset"), ShouldEqual, "3599")
-		})
-
-		Convey("Restricts based on RemoteAddr IP after too many requests", func() {
-			for i := 0; i < 10; i++ {
-				w := rh.Get("/")
-				So(w.Code, ShouldEqual, 200)
-			}
-
-			w := rh.Get("/")
-			So(w.Code, ShouldEqual, 429)
-
-			w = rh.Get("/", test.RequestHelperRemoteAddr("127.0.0.2"))
-			So(w.Code, ShouldEqual, 200)
-
-			// Ignores ports
-			w = rh.Get("/", test.RequestHelperRemoteAddr("127.0.0.1:4312"))
-			So(w.Code, ShouldEqual, 429)
-		})
-
-		Convey("Restrict based upon X-Forwarded-For correctly", func() {
-			for i := 0; i < 10; i++ {
-				w := rh.Get("/", test.RequestHelperXFF("4.4.4.4"))
-				So(w.Code, ShouldEqual, 200)
-			}
-
-			w := rh.Get("/", test.RequestHelperXFF("4.4.4.4"))
-			So(w.Code, ShouldEqual, 429)
-
-			// allow other ips
-			w = rh.Get("/", test.RequestHelperRemoteAddr("4.4.4.3"))
-			So(w.Code, ShouldEqual, 200)
-
-			// Ignores leading private ips
-			w = rh.Get("/", test.RequestHelperXFF("10.0.0.1, 4.4.4.4"))
-			So(w.Code, ShouldEqual, 429)
-
-			// Ignores trailing ips
-			w = rh.Get("/", test.RequestHelperXFF("4.4.4.4, 4.4.4.5, 127.0.0.1"))
-			So(w.Code, ShouldEqual, 429)
-
-		})
-	})
-
-	Convey("Rate Limiting works with redis", t, func() {
-		c := NewTestConfig()
-		c.RateLimit = throttled.PerHour(10)
-		c.RedisURL = "redis://127.0.0.1:6379/"
-		app, _ := NewApp(c)
-		defer app.Close()
-		rh := NewRequestHelper(app)
-
-		redis := app.redis.Get()
-		_, err := redis.Do("FLUSHDB")
-		So(err, ShouldBeNil)
-
-		for i := 0; i < 10; i++ {
-			w := rh.Get("/")
-			So(w.Code, ShouldEqual, 200)
-		}
-
+	for i := 0; i < 10; i++ {
 		w := rh.Get("/")
-		So(w.Code, ShouldEqual, 429)
+		assert.Equal(t, 200, w.Code)
+	}
 
-		w = rh.Get("/", test.RequestHelperRemoteAddr("127.0.0.2"))
-		So(w.Code, ShouldEqual, 200)
-	})
+	w := rh.Get("/")
+	assert.Equal(t, 429, w.Code)
+
+	w = rh.Get("/", test.RequestHelperRemoteAddr("127.0.0.2"))
+	assert.Equal(t, 200, w.Code)
 }
