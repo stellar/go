ceb378a6ee6ac84bfee3f8d8dc119822deae5458
Bartek Nowotarski <bartek@nowotarski.info>
2018-10-17 23:41:13 +0200

    Add trade order test

--- services/horizon/internal/simplepath/finder_test.go
+++ services/horizon/internal/simplepath/finder_test.go
@@ -26,23 +26,78 @@ func TestFinder(t *testing.T) {
 		xdr.AssetTypeAssetTypeCreditAlphanum4,
 		"EUR",
 		"GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN")
+	inter1 := makeAsset(
+		xdr.AssetTypeAssetTypeCreditAlphanum4,
+		"1",
+		"GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN")
+	inter21 := makeAsset(
+		xdr.AssetTypeAssetTypeCreditAlphanum4,
+		"21",
+		"GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN")
+	inter22 := makeAsset(
+		xdr.AssetTypeAssetTypeCreditAlphanum4,
+		"22",
+		"GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN")
 
 	query := paths.Query{
 		DestinationAddress: "GAEDTJ4PPEFVW5XV2S7LUXBEHNQMX5Q2GM562RJGOQG7GVCE5H3HIB4V",
 		DestinationAsset:   eur,
-		DestinationAmount:  xdr.Int64(200000000),
+		DestinationAmount:  xdr.Int64(200000000), // 20.0000000
 		SourceAssets:       []xdr.Asset{usd},
 	}
 
 	p, err := finder.Find(query, MaxPathLength)
 	if tt.Assert.NoError(err) {
 		tt.Assert.Len(p, 3)
+
+		// Consuming offers:
+		// - selling 10 USD for EUR, price = 0.5
+		// - selling 10 USD for EUR, price = 0.5
+		tt.Assert.Equal(p[0].Source.String(), usd.String())
+		tt.Assert.Equal(p[0].Destination.String(), eur.String())
+		tt.Assert.Equal(p[0].Cost, xdr.Int64(100000000)) // 10.0000000
+		tt.Assert.Len(p[0].Path, 0)
+
+		// Consuming offers:
+		// - selling 20 USD for `1`, price = 1
+		// - selling 20 `1` for EUR, price = 1
+		tt.Assert.Equal(p[1].Source.String(), usd.String())
+		tt.Assert.Equal(p[1].Destination.String(), eur.String())
+		tt.Assert.Equal(p[1].Cost, xdr.Int64(200000000))
+		if tt.Assert.Len(p[1].Path, 1) {
+			tt.Assert.Equal(p[1].Path[0].String(), inter1.String())
+		}
+
+		// Consuming offers:
+		// - selling 20 USD for `21`, price = 1
+		// - selling 20 `21` for `22`, price = 1
+		// - selling 20 `22` for EUR, price = 1
+		tt.Assert.Equal(p[2].Source.String(), usd.String())
+		tt.Assert.Equal(p[2].Destination.String(), eur.String())
+		tt.Assert.Equal(p[2].Cost, xdr.Int64(200000000))
+		if tt.Assert.Len(p[2].Path, 2) {
+			tt.Assert.Equal(p[2].Path[0].String(), inter21.String())
+			tt.Assert.Equal(p[2].Path[1].String(), inter22.String())
+		}
 	}
 
 	query.DestinationAmount = xdr.Int64(200000001)
 	p, err = finder.Find(query, MaxPathLength)
 	if tt.Assert.NoError(err) {
 		tt.Assert.Len(p, 2)
+
+		tt.Assert.Equal(p[0].Source.String(), usd.String())
+		tt.Assert.Equal(p[0].Destination.String(), eur.String())
+		tt.Assert.Equal(p[0].Cost, xdr.Int64(100000001))
+		tt.Assert.Len(p[0].Path, 0)
+
+		tt.Assert.Equal(p[1].Source.String(), usd.String())
+		tt.Assert.Equal(p[1].Destination.String(), eur.String())
+		tt.Assert.Equal(p[1].Cost, xdr.Int64(200000001))
+		if tt.Assert.Len(p[1].Path, 2) {
+			tt.Assert.Equal(p[1].Path[0].String(), inter21.String())
+			tt.Assert.Equal(p[1].Path[1].String(), inter22.String())
+		}
 	}
 
 	query.DestinationAmount = xdr.Int64(500000001)
@@ -62,5 +117,41 @@ func TestFinder(t *testing.T) {
 	p, err = finder.Find(query, MaxPathLength)
 	if tt.Assert.NoError(err) {
 		tt.Assert.Len(p, 2)
+	}
+
+	// Extreme example to test the order of trades.
+	// In the past the order of trades was reversed. While it worked for some
+	// paths, for others it introduced rounding errors that accumulated with each
+	// trade.
+	// For example, the previous code would calculate the cost = 23 instead of 1.
+	aaa := makeAsset(
+		xdr.AssetTypeAssetTypeCreditAlphanum4,
+		"AAA",
+		"GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN")
+	bbb := makeAsset(
+		xdr.AssetTypeAssetTypeCreditAlphanum4,
+		"BBB",
+		"GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN")
+	ccc := makeAsset(
+		xdr.AssetTypeAssetTypeCreditAlphanum4,
+		"CCC",
+		"GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN")
+
+	query = paths.Query{
+		DestinationAddress: "GDSBCQO34HWPGUGQSP3QBFEXVTSR2PW46UIGTHVWGWJGQKH3AFNHXHXN",
+		DestinationAsset:   ccc,
+		DestinationAmount:  xdr.Int64(10000000), // 1.0
+		SourceAssets:       []xdr.Asset{aaa},
+	}
+	p, err = finder.Find(query, MaxPathLength)
+	if tt.Assert.NoError(err) {
+		tt.Assert.Len(p, 0)
+
+		tt.Assert.Equal(p[0].Source.String(), aaa.String())
+		tt.Assert.Equal(p[0].Destination.String(), ccc.String())
+		tt.Assert.Equal(p[0].Cost, xdr.Int64(1))
+		if tt.Assert.Len(p[0].Path, 1) {
+			tt.Assert.Equal(p[0].Path[0].String(), bbb.String())
+		}
 	}
 }
