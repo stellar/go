c96e57974bf510f222db20bba3bf6ff48f1b2ef0
Le Cheng Fan <tommy@interstellar.com>
2018-10-25 11:03:35 -0700

    Add commenting

--- services/horizon/internal/actions/rate_limiter_provider.go
+++ services/horizon/internal/actions/rate_limiter_provider.go
@@ -2,6 +2,7 @@ package actions
 
 import "github.com/throttled/throttled"
 
+// RateLimiterProvider is an interface that provides access to the type's HTTPRateLimiter.
 type RateLimiterProvider interface {
 	GetRateLimiter() *throttled.HTTPRateLimiter
 }
