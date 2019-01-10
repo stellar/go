207703ec9f199d06b9f55bdea70fcd8c4f8f55d9
Bartek Nowotarski <bartek@nowotarski.info>
2019-01-10 14:53:00 +0100

    Fix actions_offer.go

--- services/horizon/internal/actions_offer.go
+++ services/horizon/internal/actions_offer.go
@@ -1,9 +1,6 @@
 package horizon
 
 import (
-	"errors"
-	"fmt"
-
 	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/services/horizon/internal/db2"
 	"github.com/stellar/go/services/horizon/internal/db2/core"
