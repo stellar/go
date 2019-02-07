987169263ad415e9030e2031c9750e10736bb12d
Bartek Nowotarski <bartek@nowotarski.info>
2019-02-07 15:04:07 +0100

    Run gofmt in Travis

--- gofmt.sh
+++ gofmt.sh
@@ -3,10 +3,10 @@ set -e
 
 OUTPUT=$(find . -maxdepth 1 -mindepth 1 -type d \
   | egrep -v '^\.\/vendor|^.\/docs|^\.\/\..*' \
-  | xargs -I {} -P 4 go tool vet -all -composites=false -unreachable=false -tests=false -shadow {})
+  | xargs -I {} -P 4 gofmt -l {})
 
 if [[ $OUTPUT ]]; then
-  printf "govet found some issues:\n\n"
+  printf "gofmt found unformatted files:\n\n"
   echo "$OUTPUT"
   exit 1
 fi
