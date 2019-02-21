4cffb103db98104c8ae11685c6ef5e7b0a934d20
Bartek Nowotarski <bartek@nowotarski.info>
2019-02-21 21:44:09 +0100

    Round() operation fee stats

--- services/horizon/internal/db2/history/transaction.go
+++ services/horizon/internal/db2/history/transaction.go
@@ -22,19 +22,19 @@ func (q *Q) TransactionByHash(dest interface{}, hash string) error {
 func (q *Q) TransactionsForLastXLedgers(currentSeq int32, dest interface{}) error {
 	return q.GetRaw(dest, `
 		SELECT
-			min(fee_paid/operation_count),
-			mode() within group (order by fee_paid/operation_count),
-			percentile_cont(0.10) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p10",
-			percentile_cont(0.20) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p20",
-			percentile_cont(0.30) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p30",
-			percentile_cont(0.40) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p40",
-			percentile_cont(0.50) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p50",
-			percentile_cont(0.60) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p60",
-			percentile_cont(0.70) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p70",
-			percentile_cont(0.80) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p80",
-			percentile_cont(0.90) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p90",
-			percentile_cont(0.95) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p95",
-			percentile_cont(0.99) WITHIN GROUP (ORDER BY fee_paid/operation_count) AS "p99"
+			round(min(fee_paid/operation_count)) as "min",
+			round(mode() within group (order by fee_paid/operation_count)) as "mode",
+			round(percentile_cont(0.10) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p10",
+			round(percentile_cont(0.20) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p20",
+			round(percentile_cont(0.30) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p30",
+			round(percentile_cont(0.40) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p40",
+			round(percentile_cont(0.50) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p50",
+			round(percentile_cont(0.60) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p60",
+			round(percentile_cont(0.70) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p70",
+			round(percentile_cont(0.80) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p80",
+			round(percentile_cont(0.90) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p90",
+			round(percentile_cont(0.95) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p95",
+			round(percentile_cont(0.99) WITHIN GROUP (ORDER BY fee_paid/operation_count)) AS "p99"
 		FROM history_transactions
 		WHERE ledger_sequence > $1 AND ledger_sequence <= $2
 	`, currentSeq-5, currentSeq)
