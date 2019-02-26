8f6127141e6562e3504531a73a354eca38a113dc
Bartek Nowotarski <bartek@nowotarski.info>
2019-02-26 14:47:28 +0100

    Remove failed operations in migrate down script

--- services/horizon/internal/db2/schema/migrations/16_ingest_failed_transactions.sql
+++ services/horizon/internal/db2/schema/migrations/16_ingest_failed_transactions.sql
@@ -5,6 +5,8 @@ ALTER TABLE history_transactions ADD successful boolean;
 
 -- +migrate Down
 
--- Remove failed transactions!
+-- Remove failed transactions and operations from failed transactions!
+DELETE FROM history_operations USING history_transactions
+  WHERE history_transactions.id = history_operations.transaction_id AND successful = false;
 DELETE FROM history_transactions WHERE successful = false;
 ALTER TABLE history_transactions DROP COLUMN successful;
