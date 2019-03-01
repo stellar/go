aa613df9517896cc4f6370cb06b5a7ee616c0b83
ire-and-curses <eric.saunders@gmail.com>
2019-02-28 21:15:45 -0800

    factored tests

--- exp/txnbuild/transaction_test.go
+++ exp/txnbuild/transaction_test.go
@@ -7,19 +7,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestInflation(t *testing.T) {
-	var err error
+func newKeypair0() *keypair.Full {
+	return newKeypair("SBPQUZ6G4FZNWFHKUWC5BEYWF6R52E3SEP7R3GWYSM2XTKGF5LNTWW4R")
+}
 
-	secretSeed := "SBPQUZ6G4FZNWFHKUWC5BEYWF6R52E3SEP7R3GWYSM2XTKGF5LNTWW4R"
-	sourceAddress := "GDQNY3PBOJOKYZSRMK2S7LHHGWZIUISD4QORETLMXEWXBI7KFZZMKTL3"
+func newKeypair1() *keypair.Full {
+	return newKeypair("SBMSVD4KKELKGZXHBUQTIROWUAPQASDX7KEJITARP4VMZ6KLUHOGPTYW")
+}
+
+func newKeypair(seed string) *keypair.Full {
+	myKeypair, _ := keypair.Parse(seed)
+	return myKeypair.(*keypair.Full)
+}
+
+func buildSignEncode(tx Transaction, kp *keypair.Full, t *testing.T) (txeBase64 string) {
+	var err error
+	err = tx.Build()
+	assert.Nil(t, err)
+
+	err = tx.Sign(kp)
+	assert.Nil(t, err)
+
+	txeBase64, err = tx.Base64()
+	assert.Nil(t, err)
+
+	return
+}
+
+func TestInflation(t *testing.T) {
+	kp0 := newKeypair0()
 	sourceAccount := Account{
-		ID:             sourceAddress,
+		ID:             kp0.Address(),
 		SequenceNumber: 9605939170639897,
 	}
-
-	sourceKeypair, err := keypair.Parse(secretSeed)
-	assert.Nil(t, err)
-	signer := sourceKeypair.(*keypair.Full)
 
 	inflation := Inflation{}
 
@@ -29,32 +49,18 @@ func TestInflation(t *testing.T) {
 		Network:       UseTestNetwork(),
 	}
 
-	err = tx.Build()
-	assert.Nil(t, err)
-
-	err = tx.Sign(signer)
-	assert.Nil(t, err)
-
-	txeBase64, err := tx.Base64()
-	assert.Nil(t, err)
-
+	received := buildSignEncode(tx, kp0, t)
 	expected := "AAAAAODcbeFyXKxmUWK1L6znNbKKIkPkHRJNbLktcKPqLnLFAAAAZAAiII0AAAAaAAAAAAAAAAAAAAABAAAAAAAAAAkAAAAAAAAAAeoucsUAAABAWqznvTxLfn6Q+zIloGmLDXCJQWsFPlfIf/EVFF+FfpL/gNbsvTC/U2G/ZtxMTgvqTLsBJfZAailGvPS04rfYCw=="
-
-	assert.Equal(t, expected, txeBase64, "Base 64 XDR should match")
+	assert.Equal(t, expected, received, "Base 64 XDR should match")
 }
 
 func TestCreateAccount(t *testing.T) {
-	var err error
+	kp0 := newKeypair0()
 
-	secretSeed := "SBPQUZ6G4FZNWFHKUWC5BEYWF6R52E3SEP7R3GWYSM2XTKGF5LNTWW4R"
-	sourceAddress := "GDQNY3PBOJOKYZSRMK2S7LHHGWZIUISD4QORETLMXEWXBI7KFZZMKTL3"
 	sourceAccount := Account{
-		ID:             sourceAddress,
+		ID:             kp0.Address(),
 		SequenceNumber: 9605939170639897,
 	}
-	sourceKeypair, err := keypair.Parse(secretSeed)
-	assert.Nil(t, err)
-	signer := sourceKeypair.(*keypair.Full)
 
 	createAccount := CreateAccount{
 		Destination: "GCCOBXW2XQNUSL467IEILE6MMCNRR66SSVL4YQADUNYYNUVREF3FIV2Z",
@@ -68,32 +74,17 @@ func TestCreateAccount(t *testing.T) {
 		Network:       UseTestNetwork(),
 	}
 
-	err = tx.Build()
-	assert.Nil(t, err)
-
-	err = tx.Sign(signer)
-	assert.Nil(t, err)
-
-	txeBase64, err := tx.Base64()
-	assert.Nil(t, err)
-
+	received := buildSignEncode(tx, kp0, t)
 	expected := "AAAAAODcbeFyXKxmUWK1L6znNbKKIkPkHRJNbLktcKPqLnLFAAAAZAAiII0AAAAaAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAhODe2rwbSS+e+giFk8xgmxj70pVXzEADo3GG0rEhdlQAAAAABfXhAAAAAAAAAAAB6i5yxQAAAEBa4swhXSxQ2SYXoT0FcwIrrslFrv/Q/pnXK2+f6XigqjxW0yjNQwIrpVZuNz4zNGXB3DULxyYkUi8wDwwbiKIB"
-
-	assert.Equal(t, expected, txeBase64, "Base 64 XDR should match")
+	assert.Equal(t, expected, received, "Base 64 XDR should match")
 }
 
 func TestPayment(t *testing.T) {
-	var err error
-
-	secretSeed := "SBPQUZ6G4FZNWFHKUWC5BEYWF6R52E3SEP7R3GWYSM2XTKGF5LNTWW4R"
-	sourceAddress := "GDQNY3PBOJOKYZSRMK2S7LHHGWZIUISD4QORETLMXEWXBI7KFZZMKTL3"
+	kp0 := newKeypair0()
 	sourceAccount := Account{
-		ID:             sourceAddress,
+		ID:             kp0.Address(),
 		SequenceNumber: 9605939170639898,
 	}
-	sourceKeypair, err := keypair.Parse(secretSeed)
-	assert.Nil(t, err)
-	signer := sourceKeypair.(*keypair.Full)
 
 	payment := Payment{
 		Destination: "GB7BDSZU2Y27LYNLALKKALB52WS2IZWYBDGY6EQBLEED3TJOCVMZRH7H",
@@ -106,32 +97,17 @@ func TestPayment(t *testing.T) {
 		Network:       UseTestNetwork(),
 	}
 
-	err = tx.Build()
-	assert.Nil(t, err)
-
-	err = tx.Sign(signer)
-	assert.Nil(t, err)
-
-	txeBase64, err := tx.Base64()
-	assert.Nil(t, err)
-
+	received := buildSignEncode(tx, kp0, t)
 	expected := "AAAAAODcbeFyXKxmUWK1L6znNbKKIkPkHRJNbLktcKPqLnLFAAAAZAAiII0AAAAbAAAAAAAAAAAAAAABAAAAAAAAAAEAAAAAfhHLNNY19eGrAtSgLD3VpaRm2AjNjxIBWQg9zS4VWZgAAAAAAAAAAAX14QAAAAAAAAAAAeoucsUAAABA5rSL7gy8OGiMq2Rocvv6l6HwOdePwhIMw2aJ2j5mVumAmeADjMeeCcGQIj3A7bISo6eWoF49w3qcd7uBS4j6AQ=="
-
-	assert.Equal(t, expected, txeBase64, "Base 64 XDR should match")
+	assert.Equal(t, expected, received, "Base 64 XDR should match")
 }
 
 func TestBumpSequence(t *testing.T) {
-	var err error
-
-	secretSeed := "SBMSVD4KKELKGZXHBUQTIROWUAPQASDX7KEJITARP4VMZ6KLUHOGPTYW"
-	sourceAddress := "GAS4V4O2B7DW5T7IQRPEEVCRXMDZESKISR7DVIGKZQYYV3OSQ5SH5LVP"
+	kp1 := newKeypair1()
 	sourceAccount := Account{
-		ID:             sourceAddress,
+		ID:             kp1.Address(),
 		SequenceNumber: 9606132444168199,
 	}
-	sourceKeypair, err := keypair.Parse(secretSeed)
-	assert.Nil(t, err)
-	signer := sourceKeypair.(*keypair.Full)
 
 	bumpSequence := BumpSequence{
 		BumpTo: 9606132444168300,
@@ -143,16 +119,7 @@ func TestBumpSequence(t *testing.T) {
 		Network:       UseTestNetwork(),
 	}
 
-	err = tx.Build()
-	assert.Nil(t, err)
-
-	err = tx.Sign(signer)
-	assert.Nil(t, err)
-
-	txeBase64, err := tx.Base64()
-	assert.Nil(t, err)
-
+	received := buildSignEncode(tx, kp1, t)
 	expected := "AAAAACXK8doPx27P6IReQlRRuweSSUiUfjqgyswxiu3Sh2R+AAAAZAAiILoAAAAIAAAAAAAAAAAAAAABAAAAAAAAAAsAIiC6AAAAbAAAAAAAAAAB0odkfgAAAEDLsgDc3tPETqlKxVMF16UePDbSXQ1X0i5b3U3DRHDEchU91YwsDb4oMZrCj0mwKhkiXzCUyg9pPmUG/vKtQVQD"
-
-	assert.Equal(t, expected, txeBase64, "Base 64 XDR should match")
+	assert.Equal(t, expected, received, "Base 64 XDR should match")
 }
