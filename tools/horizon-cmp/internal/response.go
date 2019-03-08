986bbfd55979d11c172a167fce39baaca135ef46
Bartek Nowotarski <bartek@nowotarski.info>
2019-03-08 20:13:59 +0100

    Improvements

---
+++ tools/horizon-cmp/internal/response.go
@@ -0,0 +1,117 @@
+package cmp
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os/exec"
+	"regexp"
+	"strings"
+)
+
+var findResultMetaXDR = regexp.MustCompile(`"result_meta_xdr": "(.*)",`)
+
+type Response struct {
+	Domain string
+	Path   string
+
+	Body string
+	// NormalizedBody is body without parts that identify a single
+	// server (ex. domain) and fields known to be different between
+	// instances (ex. `result_meta_xdr`).
+	NormalizedBody string
+}
+
+func NewResponse(domain, path string) *Response {
+	response := &Response{
+		Domain: domain,
+		Path:   path,
+	}
+
+	resp, err := http.Get(domain + path)
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		panic(resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	response.Body = string(body)
+
+	normalizedBody := response.Body
+	// `result_meta_xdr` can differ between core instances (confirmed this with core team)
+	normalizedBody = findResultMetaXDR.ReplaceAllString(normalizedBody, "")
+	// Remove Horizon URL from the _links
+	normalizedBody = strings.Replace(normalizedBody, domain, "", -1)
+
+	response.NormalizedBody = normalizedBody
+	return response
+}
+
+func (r *Response) Equal(other *Response) bool {
+	return r.NormalizedBody == other.NormalizedBody
+}
+
+func (r *Response) SaveDiff(outputDir string, other *Response) {
+	if r.Path != other.Path {
+		panic("Paths are different")
+	}
+
+	fileName := pathToFileName(r.Path)
+
+	fileA := fmt.Sprintf("%s/%s.old", outputDir, fileName)
+	fileB := fmt.Sprintf("%s/%s.new", outputDir, fileName)
+	fileDiff := fmt.Sprintf("%s/%s.diff", outputDir, fileName)
+
+	err := ioutil.WriteFile(fileA, []byte(r.Body), 0744)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(fileB, []byte(other.Body), 0744)
+	if err != nil {
+		panic(err)
+	}
+
+	out, err := exec.Command("diff", fileA, fileB).Output()
+	if err != nil {
+		// Ignore, user will generate diff manually
+	}
+
+	if len(out) != 0 {
+		err = ioutil.WriteFile(fileDiff, out, 0744)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+// GetPaths finds all URLs in the response body and returns paths
+// (without domain).
+func (r *Response) GetPaths() []string {
+	// escapedDomain := strings.Replace(r.Domain, `\`, `\\`, -1)
+	var linksRegexp = regexp.MustCompile(`"` + r.Domain + `(.*?)["{]`)
+	found := linksRegexp.FindAllSubmatch([]byte(r.Body), -1)
+	links := make([]string, 0, len(found))
+
+	for _, link := range found {
+		l := strings.Replace(string(link[1]), "\\u0026", "&", -1)
+		links = append(links, l)
+	}
+
+	return links
+}
+
+func pathToFileName(path string) string {
+	path = strings.Replace(path, "/", "_", -1)
+	path = strings.Replace(path, "?", "_", -1)
+	path = strings.Replace(path, "&", "_", -1)
+	path = strings.Replace(path, "=", "_", -1)
+	return path
+}
--- tools/horizon-cmp/main.go
+++ tools/horizon-cmp/main.go
@@ -1,109 +1,136 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"regexp"
+	"net/url"
+	"os"
 	"strings"
+	"time"
+
+	protocol "github.com/stellar/go/protocols/horizon"
+	cmp "github.com/stellar/go/tools/horizon-cmp/internal"
 )
 
-var paths []string = []string{
-	"/payments?order=desc&limit=200&include_failed=true&cursor=97761976872132608",
-	"/payments?order=desc&limit=200&include_failed=false&cursor=97761976872132608",
-	"/payments?order=desc&limit=200&cursor=97761976872132608",
+const HorizonOld = "https://horizon.stellar.org"
+const HorizonNew = "https://horizon-dev-pubnet.stellar.org"
 
-	"/operations?order=desc&limit=200&include_failed=true&cursor=97761976872132608",
-	"/operations?order=desc&limit=200&include_failed=false&cursor=97761976872132608",
-	"/operations?order=desc&limit=200&cursor=97761976872132608",
+const MaxLevels = 3
 
-	"/accounts/GBXY56UJJH4MSEHTP35VX7B5JSLKD72XWLXK4JROQBYRASOXNDIMIL2D/payments?order=desc&limit=200&include_failed=true&cursor=97761976872132608",
-	"/accounts/GBXY56UJJH4MSEHTP35VX7B5JSLKD72XWLXK4JROQBYRASOXNDIMIL2D/operations?order=desc&limit=200&include_failed=true&cursor=97761976872132608",
-	"/accounts/GBXY56UJJH4MSEHTP35VX7B5JSLKD72XWLXK4JROQBYRASOXNDIMIL2D/transactions?order=desc&limit=200&include_failed=true&cursor=97761976872132608",
-
-	"/accounts/GBXY56UJJH4MSEHTP35VX7B5JSLKD72XWLXK4JROQBYRASOXNDIMIL2D/payments?order=desc&limit=2&cursor=97761976872132608",
-	"/accounts/GBXY56UJJH4MSEHTP35VX7B5JSLKD72XWLXK4JROQBYRASOXNDIMIL2D/operations?order=desc&limit=2&cursor=97761976872132608",
-	"/accounts/GBXY56UJJH4MSEHTP35VX7B5JSLKD72XWLXK4JROQBYRASOXNDIMIL2D/transactions?order=desc&limit=2&cursor=97761976872132608",
-
-	"/accounts/GB3BVRX2D2WBTUYPXOB6S25VTFK36LDCII2AILMMLIJT3SWG66CCS5AK/transactions?order=desc&cursor=97762191620468736",
-	"/accounts/GB3BVRX2D2WBTUYPXOB6S25VTFK36LDCII2AILMMLIJT3SWG66CCS5AK/transactions?order=desc&include_failed=true&cursor=97762191620468736",
-	"/accounts/GB3BVRX2D2WBTUYPXOB6S25VTFK36LDCII2AILMMLIJT3SWG66CCS5AK/transactions?order=desc&include_failed=false&cursor=97762191620468736",
-
-	"/accounts/GB3BVRX2D2WBTUYPXOB6S25VTFK36LDCII2AILMMLIJT3SWG66CCS5AK/transactions?order=desc&include_failed=true&limit=50&cursor=97696238102654976",
-	"/accounts/GB3BVRX2D2WBTUYPXOB6S25VTFK36LDCII2AILMMLIJT3SWG66CCS5AK/transactions?order=desc&include_failed=false&limit=50&cursor=97696238102654976",
-
-	"/ledgers/22764327/operations?limit=200",
-	"/ledgers/22764327/operations?limit=200&include_failed=true",
-	"/ledgers/22764327/payments?limit=200",
-	"/ledgers/22764327/payments?limit=200&include_failed=true",
-	"/ledgers/22764327/transactions?limit=200",
-	"/ledgers/22764327/transactions?limit=200&include_failed=true",
-	"/ledgers/22764327/effects?limit=200",
-	"/ledgers/22764327/effects?limit=200&include_failed=true",
-
-	"/ledgers/22762425/operations?limit=200",
-	"/ledgers/22762425/operations?limit=200&include_failed=true",
-	"/ledgers/22762425/payments?limit=200",
-	"/ledgers/22762425/payments?limit=200&include_failed=true",
-	"/ledgers/22762425/transactions?limit=200",
-	"/ledgers/22762425/transactions?limit=200&include_failed=true",
-	"/ledgers/22762425/effects?limit=200",
-	"/ledgers/22762425/effects?limit=200&include_failed=true",
-
-	"/payments?limit=200&cursor=97774668500484098",
-	"/payments?limit=200&cursor=97774668500484098&include_failed=false",
-	"/payments?limit=200&cursor=97774668500484098&include_failed=true",
-
-	"/operations/97774668500484098",
-	"/operations/97774689975341057",
-
-	"/operations?limit=200&cursor=97774668500484098",
-	"/operations?limit=200&cursor=97774668500484098&include_failed=false",
-	"/operations?limit=200&cursor=97774668500484098&include_failed=true",
-
-	"/transactions?limit=200&cursor=97774668500484098",
-	"/transactions?limit=200&cursor=97774668500484098&include_failed=false",
-	"/transactions?limit=200&cursor=97774668500484098&include_failed=true",
-
-	// failed
-	"/transactions/96583f831cefcc5dbab65f918e1818468ed7102f9da1a35159b40c37253ef4a3",
-	"/transactions/96583f831cefcc5dbab65f918e1818468ed7102f9da1a35159b40c37253ef4a3/operations",
-	"/transactions/96583f831cefcc5dbab65f918e1818468ed7102f9da1a35159b40c37253ef4a3/payments",
-	"/transactions/96583f831cefcc5dbab65f918e1818468ed7102f9da1a35159b40c37253ef4a3/effects",
-
-	// succ
-	"/transactions/702cbb5fead49b16d64b55a3bfcbabc7d4674a6d5a9e4012caf422ab87db3010",
-	"/transactions/702cbb5fead49b16d64b55a3bfcbabc7d4674a6d5a9e4012caf422ab87db3010/operations",
-	"/transactions/702cbb5fead49b16d64b55a3bfcbabc7d4674a6d5a9e4012caf422ab87db3010/payments",
-	"/transactions/702cbb5fead49b16d64b55a3bfcbabc7d4674a6d5a9e4012caf422ab87db3010/effects",
+type PathWithLevel struct {
+	Path  string
+	Level int
 }
 
-var removeMeta = regexp.MustCompile(`"result_meta_xdr": "(.*)"`)
+// Starting corpus of paths to test
+var paths []PathWithLevel = []PathWithLevel{
+	PathWithLevel{"/transactions?order=desc", 0},
+	PathWithLevel{"/transactions?order=desc&include_failed=true", 0},
+
+	PathWithLevel{"/operations?order=desc", 0},
+	PathWithLevel{"/operations?order=desc&include_failed=true", 0},
+
+	PathWithLevel{"/payments?order=desc", 0},
+	PathWithLevel{"/payments?order=desc&include_failed=true", 0},
+
+	PathWithLevel{"/ledgers?order=desc", 0},
+	PathWithLevel{"/effects?order=desc", 0},
+	PathWithLevel{"/trades?order=desc", 0},
+	PathWithLevel{"/assets", 0},
+	PathWithLevel{"/fee_stats", 0},
+}
+
+var visitedPaths map[string]bool
+
+func init() {
+	visitedPaths = make(map[string]bool)
+}
 
 func main() {
-	for _, p := range paths {
-		a := getResponse("https://horizon.stellar.org", p)
+	// Get latest ledger and operate on it's cursor to get responses at a given ledger.
+	ledger := getLatestLedger()
+	cursor := ledger.PagingToken()
+
+	// Sleep for a few sec to make sure second Horizon is up to speed
+	time.Sleep(2 * time.Second)
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	outputDir := fmt.Sprintf("%s/horizon-cmp-diff/%d", pwd, time.Now().Unix())
+
+	fmt.Println("Comparing:")
+	fmt.Printf("%s vs %s\n", HorizonOld, HorizonNew)
+	fmt.Printf("[ledger=%d cursor=%s outputDir=%s]\n\n", ledger.Sequence, cursor, outputDir)
+
+	err = os.MkdirAll(outputDir, 0744)
+	if err != nil {
+		panic(err)
+	}
+
+	for {
+		if len(paths) == 0 {
+			return
+		}
+
+		var pl PathWithLevel
+		pl, paths = paths[0], paths[1:]
+
+		p := pl.Path
+		level := pl.Level
+
+		if level > MaxLevels {
+			continue
+		}
+
+		if visitedPaths[p] {
+			continue
+		}
+
+		visitedPaths[p] = true
+
+		p = getPathWithCursor(p, cursor)
+
+		a := cmp.NewResponse(HorizonOld, p)
 		fmt.Print(".")
-		b := getResponse("https://horizon-dev-pubnet.stellar.org", p)
+		b := cmp.NewResponse(HorizonNew, p)
 		fmt.Print(".")
 
 		status := ""
-		if a == b {
+		if a.Equal(b) {
 			status = "ok"
 		} else {
-			status = "fail"
+			status = "diff"
 		}
 		fmt.Println(status, p)
-		if status == "fail" {
-			fmt.Println(a)
-			fmt.Println(b)
-			return
+		if status == "diff" {
+			a.SaveDiff(outputDir, b)
+		}
+
+		newPaths := a.GetPaths()
+		for _, newPath := range newPaths {
+			// Such links can get recent ledgers data that may be different
+			// if Horizon nodes are not at the same ledger.
+			if strings.Contains(newPath, "desc=asc") {
+				continue
+			}
+
+			paths = append(paths, PathWithLevel{newPath, level + 1})
 		}
 	}
 }
 
-func getResponse(domain, url string) string {
-	resp, err := http.Get(domain + url)
+func getLatestLedger() protocol.Ledger {
+	ledgersResponse := struct {
+		Embedded struct {
+			Records []protocol.Ledger `json:"records"`
+		} `json:"_embedded"`
+	}{}
+
+	resp, err := http.Get(HorizonOld + "/ledgers?order=desc&limit=1")
 	if err != nil {
 		panic(err)
 	}
@@ -117,11 +144,26 @@ func getResponse(domain, url string) string {
 		panic(err)
 	}
 
-	bodyString := string(body)
-	// `result_meta_xdr` can differ between core instances (confirmed this with core team)
-	bodyString = removeMeta.ReplaceAllString(bodyString, "")
-	// Remove Horizon URL from the _links
-	bodyString = strings.Replace(bodyString, domain, "", -1)
+	err = json.Unmarshal(body, &ledgersResponse)
+	if err != nil {
+		panic(err)
+	}
 
-	return bodyString
+	return ledgersResponse.Embedded.Records[0]
+}
+
+func getPathWithCursor(path, cursor string) string {
+	urlObj, err := url.Parse(path)
+	if err != nil {
+		panic(err)
+	}
+
+	// Add cursor if not present
+	q := urlObj.Query()
+	if q.Get("cursor") == "" {
+		q.Set("cursor", cursor)
+	}
+
+	urlObj.RawQuery = q.Encode()
+	return urlObj.String()
 }
