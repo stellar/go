d042c0b84dd22602a924beff24187a0c832cb41e
Howard Chen <howard@chain.com>
2019-03-08 13:58:10 -0500

    add comments

--- support/render/hal/handler.go
+++ support/render/hal/handler.go
@@ -9,6 +9,7 @@ import (
 
 // Handler returns an HTTP Handler for function fn.
 // If fn returns a non-nil error, the handler will use problem.Render.
+// TODO: Use reflection to make the hal handler more generic.
 func Handler(fn func(context.Context, string) (interface{}, error), id string) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
