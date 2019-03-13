db0c34c5fbd716d4d04f614aa0a587dd746a3df9
Bartek Nowotarski <bartek@nowotarski.info>
2019-03-13 13:50:11 +0100

    Fix

--- services/horizon/internal/helpers.go
+++ services/horizon/internal/helpers.go
@@ -12,7 +12,11 @@ func countNonEmpty(params ...interface{}) (int, error) {
 		default:
 			return 0, errors.Errorf("unexpected type %T", param)
 		case int32:
-			if param != 0 {
+			if param != int32(0) {
+				count++
+			}
+		case int64:
+			if param != int64(0) {
 				count++
 			}
 		case string:
