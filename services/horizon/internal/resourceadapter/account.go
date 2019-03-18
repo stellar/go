8a330cb9e8ea4791a76e199dd998126dd2a6c3bb
Howard Chen <howard7708@gmail.com>
2019-03-18 12:28:29 -0400

    horizon: display trustline authorization state in the balances list (#1011)
    This PR adds a new field `IsAuthorized` in the `Balance` struct

--- services/horizon/internal/resourceadapter/account.go
+++ services/horizon/internal/resourceadapter/account.go
@@ -35,16 +35,16 @@ func PopulateAccount(
 	// populate balances
 	dest.Balances = make([]Balance, len(ct)+1)
 	for i, tl := range ct {
-		err = PopulateBalance(ctx, &dest.Balances[i], tl)
+		err = PopulateBalance(&dest.Balances[i], tl)
 		if err != nil {
-			return
+			return err
 		}
 	}
 
 	// add native balance
 	err = PopulateNativeBalance(&dest.Balances[len(dest.Balances)-1], ca.Balance, ca.BuyingLiabilities, ca.SellingLiabilities)
 	if err != nil {
-		return
+		return err
 	}
 
 	// populate data
@@ -72,5 +72,5 @@ func PopulateAccount(
 	dest.Links.Trades = lb.PagedLink(self, "trades")
 	dest.Links.Data = lb.Link(self, "data/{key}")
 	dest.Links.Data.PopulateTemplated()
-	return
+	return nil
 }
--- services/horizon/internal/resourceadapter/balance.go
+++ services/horizon/internal/resourceadapter/balance.go
@@ -1,8 +1,6 @@
 package resourceadapter
 
 import (
-	"context"
-
 	"github.com/stellar/go/amount"
 	. "github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/services/horizon/internal/assets"
@@ -10,7 +8,7 @@ import (
 	"github.com/stellar/go/xdr"
 )
 
-func PopulateBalance(ctx context.Context, dest *Balance, row core.Trustline) (err error) {
+func PopulateBalance(dest *Balance, row core.Trustline) (err error) {
 	dest.Type, err = assets.String(row.Assettype)
 	if err != nil {
 		return
@@ -23,6 +21,9 @@ func PopulateBalance(ctx context.Context, dest *Balance, row core.Trustline) (er
 	dest.Issuer = row.Issuer
 	dest.Code = row.Assetcode
 	dest.LastModifiedLedger = row.LastModified
+	if row.IsAuthorized() {
+		dest.IsAuthorized = true
+	}
 	return
 }
 
@@ -39,5 +40,6 @@ func PopulateNativeBalance(dest *Balance, stroops, buyingLiabilities, sellingLia
 	dest.Limit = ""
 	dest.Issuer = ""
 	dest.Code = ""
+	dest.IsAuthorized = true
 	return
 }
