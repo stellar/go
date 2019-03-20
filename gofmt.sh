ad41bb5cb1ae0260482947b7f1b8cec983feee1e
ire-and-curses <eric.saunders@gmail.com>
2019-03-20 11:31:26 -0700

    only run go fmt on one Go version

--- gofmt.sh
+++ gofmt.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 set -e
 
+# Only run format checks on the recommended developer version of Go
+if [ $TRAVIS_GO_VERSION != '1.11' ]; then
+    exit 0
+fi
+
 OUTPUT=$(ls -d */ \
   | egrep -v '^vendor|^docs' \
   | xargs -I {} -P 4 gofmt -l {})
