36fb92bc6148943ef5a92ce75c6cdd18a66d955d
ire-and-curses <eric.saunders@gmail.com>
2019-03-20 13:21:26 -0700

    add DEVEL_GO_VERSION

--- gofmt.sh
+++ gofmt.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 set -e
 
+DEVEL_GO_VERSION='1.11'
+
 # Only run format checks on the recommended developer version of Go
-if [ "$TRAVIS_GO_VERSION" != '1.11' ]; then
+if [ "$TRAVIS_GO_VERSION" != "$DEVEL_GO_VERSION" ]; then
     printf "Skipping gofmt checks for this version of Go...\n"
     exit 0
 fi
