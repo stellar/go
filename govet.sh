4f6af552989a8d3a3b318221c5a765d8d9e21b4b
ire-and-curses <eric.saunders@gmail.com>
2019-03-21 13:46:06 -0700

    remove go vet check for Go 1.12+

--- govet.sh
+++ govet.sh
@@ -1,17 +1,21 @@
 #! /bin/bash
 set -e
 
-# TODO: Build and install shadow analyzer for go vet in 1.12+
+# TODO: 1) Build and install shadow analyzer for go vet in 1.12+
 # See https://github.com/golang/go/issues/29260
+# https://golang.org/doc/go1.12#vet
 # For now we just skip the shadow checking for Go 1.12+
+# TODO: 2) Not triggering on shadowed vars (output not stdout?)
+# TODO: 3) Fix syntax for Go 1.12+ (go tool vet -> go vet, but fails to find packages?)
 if [ "$TRAVIS_GO_VERSION" = '1.9' ] || [ "$TRAVIS_GO_VERSION" = '1.10' ] || [ "$TRAVIS_GO_VERSION" = '1.11' ]; then
-GOVET_SHADOW='-shadow'
-GOVET_TOOL='tool'
-fi
-
+    echo "Running go vet checks..."
 OUTPUT=$(ls -d */ \
   | egrep -v '^vendor|^docs' \
-  | xargs -I {} -P 4 go $GOVET_TOOL vet -all -composites=false -unreachable=false -tests=false $GOVET_SHADOW {})
+  | xargs -I {} -P 4 go tool vet -all -composites=false -unreachable=false -tests=false -shadow {})
+else
+    echo "Skipping go vet checks for this version of Go..."
+fi
+
 
 if [[ $OUTPUT ]]; then
   printf "govet found some issues:\n\n"
