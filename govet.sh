16d3dd9b8a7971e71ade13aa188eb6d35c1ae7d4
ire-and-curses <eric.saunders@gmail.com>
2019-03-21 11:54:48 -0700

    dont check var shadowing in Go >= 1.12

--- govet.sh
+++ govet.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 set -e
 
+# TODO: Build and install shadow analyzer for go vet in 1.12+
+# See https://github.com/golang/go/issues/29260
+# For now we just skip the shadow checking for Go 1.12+
+if [ "$TRAVIS_GO_VERSION" = '1.9' ] || [ "$TRAVIS_GO_VERSION" = '1.10' ] || [ "$TRAVIS_GO_VERSION" = '1.11' ]; then
+GOVET_SHADOW='-shadow'
+fi
+
 OUTPUT=$(ls -d */ \
   | egrep -v '^vendor|^docs' \
-  | xargs -I {} -P 4 go tool vet -all -composites=false -unreachable=false -tests=false -shadow {})
+  | xargs -I {} -P 4 go tool vet -all -composites=false -unreachable=false -tests=false $GOVET_SHADOW {})
 
 if [[ $OUTPUT ]]; then
   printf "govet found some issues:\n\n"
