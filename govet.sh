6e086161ef15175dcba74b8a68c659478fe14224
ire-and-curses <eric.saunders@gmail.com>
2019-03-21 11:58:51 -0700

    go >=1.12 drops tool command

--- govet.sh
+++ govet.sh
@@ -6,11 +6,12 @@ set -e
 # For now we just skip the shadow checking for Go 1.12+
 if [ "$TRAVIS_GO_VERSION" = '1.9' ] || [ "$TRAVIS_GO_VERSION" = '1.10' ] || [ "$TRAVIS_GO_VERSION" = '1.11' ]; then
 GOVET_SHADOW='-shadow'
+GOVET_TOOL='tool'
 fi
 
 OUTPUT=$(ls -d */ \
   | egrep -v '^vendor|^docs' \
-  | xargs -I {} -P 4 go tool vet -all -composites=false -unreachable=false -tests=false $GOVET_SHADOW {})
+  | xargs -I {} -P 4 go $GOVET_TOOL vet -all -composites=false -unreachable=false -tests=false $GOVET_SHADOW {})
 
 if [[ $OUTPUT ]]; then
   printf "govet found some issues:\n\n"
