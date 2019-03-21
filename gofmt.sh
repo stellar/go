a520740b7b52af8c7d503eefb5e9dc85f9688470
ire-and-curses <eric.saunders@gmail.com>
2019-03-21 11:09:12 -0700

    pull up version check for visiblity

--- gofmt.sh
+++ gofmt.sh
@@ -1,14 +1,6 @@
 #! /bin/bash
 set -e
 
-DEVEL_GO_VERSION='1.11'
-
-# Only run format checks on the recommended developer version of Go
-if [ "$TRAVIS_GO_VERSION" != "$DEVEL_GO_VERSION" ]; then
-    printf "Skipping gofmt checks for this version of Go...\n"
-    exit 0
-fi
-
 printf "Running gofmt checks...\n"
 OUTPUT=$(ls -d */ \
   | egrep -v '^vendor|^docs' \
