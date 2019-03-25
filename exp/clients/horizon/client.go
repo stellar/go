3bdc9e63c6f188fc3beeb93ac99e64806b36943b
poliha <poliha2002@gmail.com>
2019-03-25 10:03:45 +0100

    exp/client/horizon: timeout (#1042)
    * add timeouts
    
    * fix slashes on HorizonURRL
    
    * fix naming issues

--- exp/clients/horizon/client.go
+++ exp/clients/horizon/client.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"net/http"
+	"strings"
+	"time"
 
 	hProtocol "github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/protocols/horizon/operations"
@@ -17,8 +19,8 @@ func (c *Client) sendRequest(hr HorizonRequest, a interface{}) (err error) {
 		return
 	}
 
+	c.HorizonURL = c.getHorizonURL()
 	var req *http.Request
-
 	// check if it is a submitRequest
 	_, ok := hr.(submitRequest)
 	if ok {
@@ -34,13 +36,36 @@ func (c *Client) sendRequest(hr HorizonRequest, a interface{}) (err error) {
 	req.Header.Set("X-Client-Name", "go-stellar-sdk")
 	req.Header.Set("X-Client-Version", app.Version())
 
-	resp, err := c.HTTP.Do(req)
+	if c.horizonTimeOut == 0 {
+		c.horizonTimeOut = HorizonTimeOut
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*c.horizonTimeOut)
+
+	resp, err := c.HTTP.Do(req.WithContext(ctx))
 	if err != nil {
+		cancel()
 		return
 	}
 
 	err = decodeResponse(resp, &a)
+	cancel()
 	return
+}
+
+// getHorizonUrl strips all slashes(/) at the end of HorizonURL if any, then adds a single slash
+func (c *Client) getHorizonURL() string {
+	return strings.TrimRight(c.HorizonURL, "/") + "/"
+}
+
+// SetHorizonTimeOut allows users to set the number of seconds before a horizon request is cancelled.
+func (c *Client) SetHorizonTimeOut(t uint) *Client {
+	c.horizonTimeOut = time.Duration(t)
+	return c
+}
+
+// GetHorizonTimeOut returns the current timeout for an horizon client
+func (c *Client) GetHorizonTimeOut() time.Duration {
+	return c.horizonTimeOut
 }
 
 // AccountDetail returns information for a single account.
@@ -90,7 +115,7 @@ func (c *Client) Assets(request AssetRequest) (assets hProtocol.AssetsPage, err 
 // Stream is for endpoints that support streaming
 func (c *Client) Stream(ctx context.Context, request StreamRequest, handler func(interface{})) (err error) {
 
-	err = request.Stream(ctx, c.HorizonURL, handler)
+	err = request.Stream(ctx, c.getHorizonURL(), handler)
 	return
 }
 
@@ -101,7 +126,7 @@ func (c *Client) Ledgers(request LedgerRequest) (ledgers hProtocol.LedgersPage, 
 	return
 }
 
-// LedgerDetails returns information about a particular ledger for a given sequence number
+// LedgerDetail returns information about a particular ledger for a given sequence number
 // See https://www.stellar.org/developers/horizon/reference/endpoints/ledgers-single.html
 func (c *Client) LedgerDetail(sequence uint32) (ledger hProtocol.Ledger, err error) {
 	if sequence <= 0 {
