cf09fcc022feb1949ff0cec95ae2f4596322bd12
ire-and-curses <eric.saunders@gmail.com>
2019-04-02 10:41:56 -0700

    bump_sequence: move xdrOp

--- exp/txnbuild/bump_sequence.go
+++ exp/txnbuild/bump_sequence.go
@@ -9,15 +9,16 @@ import (
 // https://www.stellar.org/developers/guides/concepts/list-of-operations.html
 type BumpSequence struct {
 	BumpTo int64
-	xdrOp  xdr.BumpSequenceOp
 }
 
 // BuildXDR for BumpSequence returns a fully configured XDR Operation.
 func (bs *BumpSequence) BuildXDR() (xdr.Operation, error) {
-	bs.xdrOp.BumpTo = xdr.SequenceNumber(bs.BumpTo)
+	var xdrOp xdr.BumpSequenceOp
+
+	xdrOp.BumpTo = xdr.SequenceNumber(bs.BumpTo)
 
 	opType := xdr.OperationTypeBumpSequence
-	body, err := xdr.NewOperationBody(opType, bs.xdrOp)
+	body, err := xdr.NewOperationBody(opType, xdrOp)
 
 	if err != nil {
 		return xdr.Operation{}, errors.Wrap(err, "Failed to build XDR OperationBody")
