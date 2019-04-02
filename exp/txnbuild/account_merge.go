212ca104acb8bd60de47919452a90eb18887dc9e
ire-and-curses <eric.saunders@gmail.com>
2019-04-02 10:35:05 -0700

    account_merge: move xdrOp

--- exp/txnbuild/account_merge.go
+++ exp/txnbuild/account_merge.go
@@ -8,21 +8,21 @@ import (
 // AccountMerge represents the Stellar merge account operation. See
 // https://www.stellar.org/developers/guides/concepts/list-of-operations.html
 type AccountMerge struct {
-	destAccountID xdr.AccountId
-	Destination   string
-	xdrOp         xdr.AccountId
+	Destination string
 }
 
 // BuildXDR for AccountMerge returns a fully configured XDR Operation.
 func (am *AccountMerge) BuildXDR() (xdr.Operation, error) {
-	err := am.destAccountID.SetAddress(am.Destination)
+	var err error
+	var xdrOp xdr.AccountId
+
+	err = xdrOp.SetAddress(am.Destination)
 	if err != nil {
 		return xdr.Operation{}, errors.Wrap(err, "Failed to set destination address")
 	}
-	am.xdrOp = am.destAccountID
 
 	opType := xdr.OperationTypeAccountMerge
-	body, err := xdr.NewOperationBody(opType, am.xdrOp)
+	body, err := xdr.NewOperationBody(opType, xdrOp)
 	if err != nil {
 		return xdr.Operation{}, errors.Wrap(err, "Failed to build XDR OperationBody")
 	}
