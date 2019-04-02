b5e2f4336b19bc9c377587338fbeba3c1c6ea6b4
Alexandre Cordeiro <accordeiro@gmail.com>
2019-04-02 18:03:44 -0300

    services/ticker: filter non-significant/non-compliant assets retrieved from Horizon

--- services/ticker/main.go
+++ services/ticker/main.go
@@ -6,7 +6,8 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/stellar/go/services/ticker/internal/assetscraper"
+	horizonclient "github.com/stellar/go/exp/clients/horizon"
+	"github.com/stellar/go/services/ticker/internal/scraper"
 )
 
 func check(e error) {
@@ -15,21 +16,29 @@ func check(e error) {
 	}
 }
 
-func main() {
-	assetStatList, err := assetscraper.FetchAllAssets()
-	check(err)
-
-	marshAssets, err := json.MarshalIndent(assetStatList, "", "\t")
-	check(err)
-
+func writeResultsToFile(jsonBytes []byte, filename string) (numBytes int, err error) {
 	path := filepath.Join(".", "tmp")
 	_ = os.Mkdir(path, os.ModePerm) // ignore if dir already exists
 
-	f, err := os.Create(filepath.Join(".", "tmp", "assets.json"))
+	f, err := os.Create(filepath.Join(".", "tmp", filename))
 	check(err)
 	defer f.Close()
 
-	n, err := f.Write(marshAssets)
-	fmt.Printf("wrote %d bytes\n", n)
+	numBytes, err = f.Write(jsonBytes)
+	fmt.Printf("Wrote %d bytes to %s\n", numBytes, filename)
 	f.Sync()
+
+	return
+}
+
+func main() {
+	// Temporary main function to run / test packages
+	c := horizonclient.DefaultPublicNetClient
+	assetStatList, err := scraper.FetchAllAssets(c)
+	check(err)
+
+	jsonAssets, err := json.MarshalIndent(assetStatList, "", "\t")
+	check(err)
+
+	writeResultsToFile(jsonAssets, "assets.json")
 }
