dbcec091db383e356119233e24c7494ca614dbfd
poliha <poliha2002@gmail.com>
2019-04-09 09:38:43 +0100

    exp/clients/horizon: StreamLedgers (#1113)
    * add StreamLedgers
    
    * add more infor to error messages
    
    * fix typo

--- exp/clients/horizon/client.go
+++ exp/clients/horizon/client.go
@@ -433,5 +433,12 @@ func (c *Client) StreamEffects(ctx context.Context, request EffectRequest, handl
 	return request.StreamEffects(ctx, c, handler)
 }
 
+// StreamLedgers streams stellar ledgers. It can be used to stream all ledgers. Use context.WithCancel
+// to stop streaming or context.Background() if you want to stream indefinitely.
+// LedgerHandler is a user-supplied function that is executed for each streamed ledger received.
+func (c *Client) StreamLedgers(ctx context.Context, request LedgerRequest, handler LedgerHandler) error {
+	return request.StreamLedgers(ctx, c, handler)
+}
+
 // ensure that the horizon client implements ClientInterface
 var _ ClientInterface = &Client{}
--- exp/clients/horizon/ledger_request.go
+++ exp/clients/horizon/ledger_request.go
@@ -40,22 +40,25 @@ func (lr LedgerRequest) BuildUrl() (endpoint string, err error) {
 	return endpoint, err
 }
 
-// Stream streams incoming ledgers. Use context.WithCancel to stop streaming or
-// context.Background() if you want to stream indefinitely.
-func (lr LedgerRequest) Stream(ctx context.Context, client *Client,
-	handler func(interface{})) (err error) {
+// LedgerHandler is a function that is called when a new ledger is received
+type LedgerHandler func(hProtocol.Ledger)
+
+// StreamLedgers streams stellar ledgers. It can be used to stream all ledgers. Use context.WithCancel
+// to stop streaming or context.Background() if you want to stream indefinitely.
+// LedgerHandler is a user-supplied function that is executed for each streamed ledger received.
+func (lr LedgerRequest) StreamLedgers(ctx context.Context, client *Client,
+	handler LedgerHandler) (err error) {
 	endpoint, err := lr.BuildUrl()
 	if err != nil {
-		return errors.Wrap(err, "Unable to build endpoint")
+		return errors.Wrap(err, "Unable to build endpoint for ledger request")
 	}
 
 	url := fmt.Sprintf("%s%s", client.getHorizonURL(), endpoint)
-
 	return client.stream(ctx, url, func(data []byte) error {
 		var ledger hProtocol.Ledger
 		err = json.Unmarshal(data, &ledger)
 		if err != nil {
-			return errors.Wrap(err, "Error unmarshaling data")
+			return errors.Wrap(err, "Error unmarshaling data for ledger request")
 		}
 		handler(ledger)
 		return nil
