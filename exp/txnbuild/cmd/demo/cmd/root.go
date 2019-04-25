a3d4867741221a852eaa7cc1aa3ce8261671c42b
ire-and-curses <eric.saunders@gmail.com>
2019-04-25 01:11:41 -0700

    clean up logging, add comments

--- exp/txnbuild/cmd/demo/cmd/root.go
+++ exp/txnbuild/cmd/demo/cmd/root.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"os"
 
+	log "github.com/sirupsen/logrus"
+
 	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var cfgFile string
+var verbose bool
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -42,10 +45,11 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.demo.yaml)")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose logging")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("verbose", "v", false, "Enable verbose logging")
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -71,5 +75,9 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+
+	if verbose {
+		log.SetLevel(log.DebugLevel)
 	}
 }
--- exp/txnbuild/cmd/demo/operations/demo.go
+++ exp/txnbuild/cmd/demo/operations/demo.go
@@ -5,8 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/stellar/go/clients/horizon"
 	horizonclient "github.com/stellar/go/exp/clients/horizon"
@@ -19,11 +20,11 @@ import (
 	"github.com/stellar/go/keypair"
 )
 
-// TODO:
-// 2) Clean up printing output
-// 3) Add missing operations
-
+// The account address of the TestNet "friendbot"
 const friendbotAddress = "GAIH3ULLFQ4DGSECF2AR555KZ4KNDGEKN4AFI4SU2M7B43MGK3QJZNSR"
+
+// The local file where your generated demo account keys will be stored
+// For convenience, the address is also stored so you can look up accounts on the network
 const accountsFile = "demo.keys"
 
 // Account represents a Stellar account for this demo.
@@ -39,12 +40,11 @@ type Account struct {
 // InitKeys reads the file instead to construct the keypairs (and ignores n).
 func InitKeys(n int) []Account {
 	var accounts []Account
-	// accounts := make([]Account, n)
-	fh, err := os.Open(accountsFile)
 
+	fh, err := os.Open(accountsFile)
 	if os.IsNotExist(err) {
 		// Create the accounts and record them in a file
-		log.Println("Accounts file not found...")
+		log.Info("Accounts file not found - creating new keypairs...")
 		for i := 0; i < n; i++ {
 			accounts[i] = createKeypair()
 		}
@@ -53,17 +53,19 @@ func InitKeys(n int) []Account {
 		dieIfError("problem marshalling json accounts", err)
 		err = ioutil.WriteFile(accountsFile, jsonAccounts, 0644)
 		dieIfError("problem writing json accounts file", err)
+		log.Info("Wrote keypairs to local file", accountsFile)
 
 		return accounts
 	}
 
 	// Read the file and create keypairs
-	log.Printf("Found accounts file %s...\n", accountsFile)
+	log.Infof("Found accounts file %s...", accountsFile)
 	defer fh.Close()
 	bytes, err := ioutil.ReadAll(fh)
 	dieIfError("problem converting json file to bytes", err)
 	json.Unmarshal(bytes, &accounts)
 
+	// Create the keypair objects
 	for i, k := range accounts {
 		kp, err := keypair.Parse(k.Seed)
 		dieIfError("keypair didn't parse!", err)
@@ -73,17 +75,18 @@ func InitKeys(n int) []Account {
 	return accounts
 }
 
-// Reset removes all test accounts created by this demo. All funds are transferred back to Friendbot.
+// Reset is a command that removes all test accounts created by this demo. All funds are
+// transferred back to Friendbot.
 func Reset(client *horizonclient.Client, keys []Account) {
 	keys = loadAccounts(client, keys)
 	for _, k := range keys {
 		if !k.Exists {
-			fmt.Printf("    Account %s not found - skipping further operations on it...\n", k.Address)
+			log.Infof("Account %s not found - skipping further operations on it...", k.Address)
 			continue
 		}
 
 		// It exists - so we will proceed to delete it
-		fmt.Println("\n    Found testnet account with ID:", k.HAccount.ID)
+		log.Info("Found testnet account with ID:", k.HAccount.ID)
 
 		// Find any offers that need deleting...
 		offerRequest := horizonclient.OfferRequest{
@@ -93,20 +96,20 @@ func Reset(client *horizonclient.Client, keys []Account) {
 		}
 		offers, err := client.Offers(offerRequest)
 		dieIfError("error while getting offers", err)
-		fmt.Printf("    Account %s has %v offers:\n", k.Address, len(offers.Embedded.Records))
+		log.Infof("Account %s has %v offers...", k.Address, len(offers.Embedded.Records))
 
 		// ...and delete them
 		for _, o := range offers.Embedded.Records {
-			fmt.Println("    ", o)
+			log.Info("    ", o)
 			txe, err := deleteOffer(k.HAccount, o.ID, k)
 			dieIfError("problem building deleteOffer op", err)
-			fmt.Printf("        Deleting offer %d...\n", o.ID)
+			log.Infof("Deleting offer %d...", o.ID)
 			resp := submit(client, txe)
-			fmt.Println(resp.TransactionSuccessToString())
+			log.Debug(resp.TransactionSuccessToString())
 		}
 
 		// Find any authorised trustlines on this account...
-		fmt.Printf("    Account %s has %d balances...\n", k.Address, len(k.HAccount.Balances))
+		log.Infof("Account %s has %d balances...", k.Address, len(k.HAccount.Balances))
 
 		// ...and delete them
 		for _, b := range k.HAccount.Balances {
@@ -120,68 +123,68 @@ func Reset(client *horizonclient.Client, keys []Account) {
 			}
 
 			// Send the asset back to the issuer...
-			fmt.Printf("        Sending %v of surplus asset %s:%s back to issuer...\n", b.Balance, asset.Code, asset.Issuer)
+			log.Infof("Sending %v of surplus asset %s:%s back to issuer...", b.Balance, asset.Code, asset.Issuer)
 			txe, err := payment(k.HAccount, asset.Issuer, b.Balance, asset, k)
 			dieIfError("problem building payment op", err)
 			resp := submit(client, txe)
-			fmt.Println(resp.TransactionSuccessToString())
+			log.Debug(resp.TransactionSuccessToString())
 
 			// Delete the now-empty trustline...
-			fmt.Printf("        Deleting trustline for asset %s:%s...\n", b.Code, b.Issuer)
+			log.Infof("Deleting trustline for asset %s:%s...", b.Code, b.Issuer)
 			txe, err = deleteTrustline(k.HAccount, asset, k)
 			dieIfError("problem building deleteTrustline op", err)
 			resp = submit(client, txe)
-			fmt.Println(resp.TransactionSuccessToString())
+			log.Debug(resp.TransactionSuccessToString())
 		}
 
 		// Find any data entries on this account...
-		fmt.Printf("    Account %s has %d data entries...\n", k.Address, len(k.HAccount.Data))
+		log.Infof("Account %s has %d data entries...", k.Address, len(k.HAccount.Data))
 		for dataKey := range k.HAccount.Data {
 			decodedV, _ := k.HAccount.GetData(dataKey)
-			fmt.Printf("    Deleting data entry '%s' -> '%s'...\n", dataKey, decodedV)
+			log.Infof("Deleting data entry '%s' -> '%s'...", dataKey, decodedV)
 			txe, err := deleteData(k.HAccount, dataKey, k)
 			dieIfError("problem building manageData op", err)
 			resp := submit(client, txe)
-			fmt.Println(resp.TransactionSuccessToString())
+			log.Debug(resp.TransactionSuccessToString())
 		}
 	}
 
-	// Merge the accounts...
+	// Finally, the accounts may be merged
 	for _, k := range keys {
 		if !k.Exists {
 			continue
 		}
-		fmt.Printf("    Merging account %s back to friendbot (%s)...\n", k.Address, friendbotAddress)
+		log.Infof("Merging account %s back to friendbot (%s)...", k.Address, friendbotAddress)
 		txe, err := mergeAccount(k.HAccount, friendbotAddress, k)
 		dieIfError("problem building mergeAccount op", err)
 		resp := submit(client, txe)
-		fmt.Println(resp.TransactionSuccessToString())
+		log.Debug(resp.TransactionSuccessToString())
 	}
 }
 
-// Initialise funds an initial set of accounts for use with other demo operations. The first account is
-// funded from Friendbot; subseqeuent accounts are created and funded from this first account.
+// Initialise is a command that funds an initial set of accounts for use with other demo operations.
+// The first account is funded from Friendbot; subseqeuent accounts are created and funded from this
+// first account.
 func Initialise(client *horizonclient.Client, keys []Account) {
 	// Fund the first account from friendbot
-	fmt.Printf("    Funding account %s from friendbot...\n", keys[0].Address)
+	log.Infof("Funding account %s from friendbot...", keys[0].Address)
 	_, err := client.Fund(keys[0].Address)
 	dieIfError(fmt.Sprintf("couldn't fund account %s from friendbot", keys[0].Address), err)
-
 	keys[0].HAccount = loadAccount(client, keys[0].Address)
 	keys[0].Exists = true
 
 	// Fund the others using the create account operation
 	for i := 1; i < len(keys); i++ {
-		fmt.Printf("    Funding account %s from account %s...\n", keys[i].Address, keys[0].Address)
+		log.Infof("Funding account %s from account %s...", keys[i].Address, keys[0].Address)
 		txe, err := createAccount(keys[0].HAccount, keys[i].Address, keys[0])
 		dieIfError("problem building createAccount op", err)
 		resp := submit(client, txe)
-		fmt.Println(resp.TransactionSuccessToString())
+		log.Debug(resp.TransactionSuccessToString())
 	}
 }
 
-// TXError deliberately creates a bad transaction to trigger an error response from Horizon. This code
-// demonstrates how to retrieve and inspect the error.
+// TXError is a command that deliberately creates a bad transaction to trigger an error response
+// from Horizon. This code demonstrates how to retrieve and inspect the error.
 func TXError(client *horizonclient.Client, keys []Account) {
 	keys = loadAccounts(client, keys)
 	// Create a bump seq operation
@@ -189,11 +192,15 @@ func TXError(client *horizonclient.Client, keys []Account) {
 	// Create the transaction
 	txe, err := bumpSequence(keys[0].HAccount, -1, keys[0])
 	dieIfError("problem building createAccount op", err)
-	resp := submit(client, txe)
+
 	// Submit
+	resp := submit(client, txe)
+
 	// Inspect and print error
-	fmt.Println(resp.TransactionSuccessToString())
+	log.Info(resp.TransactionSuccessToString())
 }
+
+/***** Examples of operation building follow *****/
 
 func bumpSequence(source *horizon.Account, seqNum int64, signer Account) (string, error) {
 	bumpSequenceOp := txnbuild.BumpSequence{
@@ -361,8 +368,8 @@ func createKeypair() Account {
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("Seed:", pair.Seed())
-	log.Println("Address:", pair.Address())
+	log.Info("Seed:", pair.Seed())
+	log.Info("Address:", pair.Address())
 
 	return Account{
 		Seed:    pair.Seed(),
@@ -371,15 +378,16 @@ func createKeypair() Account {
 	}
 }
 
-func loadAccounts(client *horizonclient.Client, keys []Account) []Account {
-	for i, k := range keys {
-		keys[i].HAccount = loadAccount(client, k.Address)
-		keys[i].Exists = true
+func loadAccounts(client *horizonclient.Client, accounts []Account) []Account {
+	for i, a := range accounts {
+		accounts[i].HAccount = loadAccount(client, a.Address)
+		accounts[i].Exists = true
 	}
 
-	return keys
+	return accounts
 }
 
+// loadAccount is an example of how to get an account's details from Horizon.
 func loadAccount(client *horizonclient.Client, address string) *horizon.Account {
 	accountRequest := horizonclient.AccountRequest{AccountID: address}
 	horizonSourceAccount, err := client.AccountDetail(accountRequest)
@@ -408,6 +416,7 @@ func dieIfError(desc string, err error) {
 	}
 }
 
+// printHorizonError is an example of how to inspect the error returned from Horizon.
 func printHorizonError(hError *horizonclient.Error) error {
 	problem := hError.Problem
 	log.Println("Error type:", problem.Type)
@@ -437,23 +446,13 @@ func printHorizonError(hError *horizonclient.Error) error {
 		return errors.Wrap(err, "Couldn't read Envelope")
 	}
 
-	// envelopeXDR, err := hError.EnvelopeXDR()
-	// if err != nil {
-	// 	return errors.Wrap(err, "Couldn't read Envelope XDR")
-	// }
-
-	log.Println("TransactionEnvelope XDR:", envelope)
-	// log.Println("TransactionEnvelope XDR 2:", envelopeXDR)
-
-	log.Println("***************")
 	txe := envelope.Tx
-	log.Println("Transaction:", txe)
 	aid := txe.SourceAccount.MustEd25519()
 	decodedAID, err := strkey.Encode(strkey.VersionByteAccountID, aid[:])
 	if err != nil {
 		log.Println("Couldn't decode account ID:", err)
 	} else {
-		log.Printf("SourceAccount (%s): %s\n", txe.SourceAccount.Type, decodedAID)
+		log.Printf("SourceAccount (%s): %s", txe.SourceAccount.Type, decodedAID)
 	}
 	log.Println("Fee:", txe.Fee)
 	log.Println("SequenceNumber:", txe.SeqNum)
@@ -468,27 +467,11 @@ func printHorizonError(hError *horizonclient.Error) error {
 	}
 	log.Println("Operations:", txe.Operations)
 
-	// Generalise printing for any operation type
 	for _, op := range txe.Operations {
 		log.Println("Operations.SourceAccount:", op.SourceAccount)
 		log.Println("Operations.Body.Type:", op.Body.Type)
-		// log.Println("Operations.Body.BumpSequenceOp.BumpTo:", op.Body.BumpSequenceOp.BumpTo)
 	}
 	log.Println("Ext:", txe.Ext)
-
-	// SourceAccount AccountId
-	// Fee           Uint32
-	// SeqNum        SequenceNumber
-	// TimeBounds    *TimeBounds
-	// Memo          Memo
-	// type Memo struct {
-	// 	Type    MemoType
-	// 	Text    *string `xdrmaxsize:"28"`
-	// 	Id      *Uint64
-	// 	Hash    *Hash
-	// 	RetHash *Hash
-	// Operations    []Operation `xdrmaxsize:"100"`
-	// Ext           TransactionExt
 
 	return nil
 }
