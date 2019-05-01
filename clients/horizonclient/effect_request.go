2965f99cd8dc20a287de5849658da0d12f3e1316
oliha <poliha2002@yahoo.com>
2019-05-01 08:40:33 +0100

    update tests

--- clients/horizonclient/effect_request.go
+++ clients/horizonclient/effect_request.go
@@ -11,59 +11,38 @@ import (
 )
 
 // EffectHandler is a function that is called when a new effect is received
-type EffectHandler func(effects.Base)
+type EffectHandler func(effects.Effect)
 
 // BuildURL creates the endpoint to be queried based on the data in the EffectRequest struct.
 // If no data is set, it defaults to the build the URL for all effects
 func (er EffectRequest) BuildURL() (endpoint string, err error) {
-
 	nParams := countParams(er.ForAccount, er.ForLedger, er.ForOperation, er.ForTransaction)
 
 	if nParams > 1 {
-		err = errors.New("invalid request: too many parameters")
-	}
-
-	if err != nil {
-		return endpoint, err
+		return endpoint, errors.New("invalid request: too many parameters")
 	}
 
 	endpoint = "effects"
 
 	if er.ForAccount != "" {
-		endpoint = fmt.Sprintf(
-			"accounts/%s/effects",
-			er.ForAccount,
-		)
+		endpoint = fmt.Sprintf("accounts/%s/effects", er.ForAccount)
 	}
 
 	if er.ForLedger != "" {
-		endpoint = fmt.Sprintf(
-			"ledgers/%s/effects",
-			er.ForLedger,
-		)
+		endpoint = fmt.Sprintf("ledgers/%s/effects", er.ForLedger)
 	}
 
 	if er.ForOperation != "" {
-		endpoint = fmt.Sprintf(
-			"operations/%s/effects",
-			er.ForOperation,
-		)
+		endpoint = fmt.Sprintf("operations/%s/effects", er.ForOperation)
 	}
 
 	if er.ForTransaction != "" {
-		endpoint = fmt.Sprintf(
-			"transactions/%s/effects",
-			er.ForTransaction,
-		)
+		endpoint = fmt.Sprintf("transactions/%s/effects", er.ForTransaction)
 	}
 
 	queryParams := addQueryParams(cursor(er.Cursor), limit(er.Limit), er.Order)
 	if queryParams != "" {
-		endpoint = fmt.Sprintf(
-			"%s?%s",
-			endpoint,
-			queryParams,
-		)
+		endpoint = fmt.Sprintf("%s?%s", endpoint, queryParams)
 	}
 
 	_, err = url.Parse(endpoint)
@@ -77,24 +56,27 @@ func (er EffectRequest) BuildURL() (endpoint string, err error) {
 // StreamEffects streams horizon effects. It can be used to stream all effects or account specific effects.
 // Use context.WithCancel to stop streaming or context.Background() if you want to stream indefinitely.
 // EffectHandler is a user-supplied function that is executed for each streamed effect received.
-func (er EffectRequest) StreamEffects(
-	ctx context.Context,
-	client *Client,
-	handler EffectHandler,
-) (err error) {
+func (er EffectRequest) StreamEffects(ctx context.Context, client *Client, handler EffectHandler) error {
 	endpoint, err := er.BuildURL()
 	if err != nil {
-		return errors.Wrap(err, "unable to build endpoint")
+		return errors.Wrap(err, "unable to build endpoint for effects request")
 	}
 
 	url := fmt.Sprintf("%s%s", client.fixHorizonURL(), endpoint)
 	return client.stream(ctx, url, func(data []byte) error {
-		var effect effects.Base
-		err = json.Unmarshal(data, &effect)
-		if err != nil {
-			return errors.Wrap(err, "error unmarshaling data")
+		var baseEffect effects.Base
+		// unmarshal into the base effect type
+		if err = json.Unmarshal(data, &baseEffect); err != nil {
+			return errors.Wrap(err, "error unmarshaling data for effects request")
 		}
-		handler(effect)
+
+		// unmarshal into the concrete effect type
+		effs, err := effects.UnmarshalEffect(baseEffect.GetType(), data)
+		if err != nil {
+			return errors.Wrap(err, "unmarshaling to the correct effect type")
+		}
+
+		handler(effs)
 		return nil
 	})
 }
