d36f096c095eaef0f34854f626b32bd33b11a465
Howard Chen <howard@chain.com>
2019-05-10 15:01:49 -0400

    add TestHandler

--- support/render/hal/handler_test.go
+++ support/render/hal/handler_test.go
@@ -2,9 +2,52 @@ package hal
 
 import (
 	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 )
+
+func TestHandler(t *testing.T) {
+	cases := []struct {
+		input   interface{}
+		output  string
+		f       interface{}
+		wantErr bool
+	}{
+		{`foo`, `"foo"`, func(ctx context.Context, s string) (string, error) { return s, nil }, false},
+		{struct{ Foo int }{1}, `1`, func(ctx context.Context, param struct{ Foo int }) (int, error) { return param.Foo, nil }, false},
+		{``, ``, func(ctx context.Context) (int, error) { return 0, errors.New("test") }, true},
+	}
+
+	for _, tc := range cases {
+		h, err := Handler(tc.f, tc.input)
+		if err != nil {
+			t.Errorf("Handler(%v) got err %v", tc.f, err)
+			continue
+		}
+
+		resp := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		h.ServeHTTP(resp, req.WithContext(context.Background()))
+		if tc.wantErr {
+			if resp.Code != 500 {
+				t.Errorf("%T response code = %d want 200", tc.f, resp.Code)
+			}
+			continue
+		}
+
+		if resp.Code != 200 {
+			t.Errorf("%T response code = %d want 200", tc.f, resp.Code)
+		}
+
+		got := resp.Body.String()
+		if got != tc.output {
+			t.Errorf("%T response body = %#q want %#q", tc.f, got, tc.output)
+		}
+	}
+}
 
 func TestFuncParamTypeError(t *testing.T) {
 	cases := []interface{}{
