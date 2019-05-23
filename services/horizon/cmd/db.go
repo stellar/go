56b4bd29ec29da47821e95f590a5ba651a44000e
Bartek Nowotarski <bartek@nowotarski.info>
2019-05-23 17:36:50 +0200

    Typo

--- services/horizon/cmd/db.go
+++ services/horizon/cmd/db.go
@@ -411,7 +411,7 @@ func reingestRange(i *ingest.System, from, to int32) error {
 
 		_, err := i.ReingestRange(lr.from, lr.to)
 		if err != nil {
-			localLog.WithField("err", err).Error("Worker failed range, work will processed again")
+			localLog.WithField("err", err).Error("Worker failed range, work will be processed again")
 			// Add the work again
 			pool.AddWork(lr)
 			return
