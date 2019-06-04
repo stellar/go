384e61ca45c918c2e4e1d9d3e505a0a2a67f92ae
Howard Chen <howard@chain.com>
2019-06-04 16:13:33 -0700

    update hal package

--- support/render/hal/handler_test.go
+++ support/render/hal/handler_test.go
@@ -50,7 +50,7 @@ func TestHandler(t *testing.T) {
 	}
 }
 
-func TestPostHandler(t *testing.T) {
+func TestReqBodyHandler(t *testing.T) {
 	cases := []struct {
 		input   string
 		output  string
@@ -95,7 +95,6 @@ func TestFuncParamTypeError(t *testing.T) {
 		"a string",                               // not a function
 		func() (int, error) { return 0, nil },    // no inputs
 		func(int) (int, error) { return 0, nil }, // first input is not context
-		func(context.Context) {},                 // not return values
 		func(context.Context, int, int) (int, error) { return 0, nil }, // too many inputs
 		func(context.Context, int) (int, int) { return 0, 0 },          // second return value is not an error
 		func() (int, int, error) { return 0, 0, nil },                  // too many return values
