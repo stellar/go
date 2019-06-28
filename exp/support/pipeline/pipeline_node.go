7ce506c68b6013dbe08e3012dc8929bf8f8bee64
Bartek Nowotarski <bartek@nowotarski.info>
2019-06-28 13:09:30 +0200

    Remove unused

--- exp/support/pipeline/pipeline_node.go
+++ exp/support/pipeline/pipeline_node.go
@@ -12,7 +12,6 @@ func (p *PipelineNode) Pipe(children ...*PipelineNode) *PipelineNode {
 func (p *PipelineNode) reset() {
 	p.Processor.Reset()
 
-	p.duration = time.Duration(0)
 	p.jobs = 0
 	p.readEntries = 0
 	p.readsPerSecond = 0
