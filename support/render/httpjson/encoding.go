bc680fe5020eb3acf17c9c3dcd45f5952a3c7647
Howard Chen <howard@stellar.org>
2019-07-02 14:23:16 -0400

    support/render/httpjson: add comments and tests for encoding  (#1467)
    This PR makes the intention of the manual type more clear. This PR makes the intention of the manual type more clear. It renames Object to RawObject as well as adds examples of the usage.

--- support/render/httpjson/encoding.go
+++ support/render/httpjson/encoding.go
@@ -2,21 +2,38 @@ package httpjson
 
 import "github.com/stellar/go/support/errors"
 
-// ErrNotObject is returned when Object.UnmarshalJSON is called
+// ErrNotJSONObject is returned when Object.UnmarshalJSON is called
 // with bytes not representing a valid json object.
 // A valid json object means it starts with `null` or `{`, not `[`.
-var ErrNotJsonObject = errors.New("input is not a json object")
+var ErrNotJSONObject = errors.New("input is not a json object")
 
-type Object []byte
+// RawObject can be used directly to make sure that what's in the request body
+// is not a json array:
+//
+// func example(ctx context.Context, in httpjson.RawObject)
+//
+// It can also be used as a field in a struct:
+//
+// type example struct {
+//  	name string
+//   	extra httpjson.RawObject
+// }
+//
+// In this case, Unmarshaler will check whether extra is a json object ot not.
+// It will error if extra is a json number/string/array/boolean.
+//
+// RawObject also implements Marshaler so that we would populate an empty json
+// object is extra is not set.
+type RawObject []byte
 
-func (o Object) MarshalJSON() ([]byte, error) {
+func (o RawObject) MarshalJSON() ([]byte, error) {
 	if len(o) == 0 {
 		return []byte("{}"), nil
 	}
 	return o, nil
 }
 
-func (o *Object) UnmarshalJSON(in []byte) error {
+func (o *RawObject) UnmarshalJSON(in []byte) error {
 	var first byte
 	for _, c := range in {
 		if !isSpace(c) {
@@ -26,7 +43,7 @@ func (o *Object) UnmarshalJSON(in []byte) error {
 	}
 	// input does not start with 'n' ("null") or '{'
 	if first != 'n' && first != '{' {
-		return ErrNotJsonObject
+		return ErrNotJSONObject
 	}
 
 	*o = in
