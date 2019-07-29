44687c847956dbd3a220e372f5bc5ff956629bf4
poliha <poliha2002@gmail.com>
2019-07-29 18:52:37 -0400

    use staticcheck-2019.2 (#1547)

--- staticcheck.sh
+++ staticcheck.sh
@@ -4,6 +4,15 @@ set -e
 # Check if staticcheck is installed, if not install it.
 command -v staticcheck >/dev/null 2>&1 || go get honnef.co/go/tools/cmd/staticcheck
 
+# use staticcheck stable version
+cd $GOPATH/src/honnef.co/go/tools/cmd/staticcheck
+git checkout 2019.2
+go get
+go install
+
+# go back to previous directory
+cd -
+
 printf "Running staticcheck...\n"
 
 ls -d */ \
