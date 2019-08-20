3d06f4f0ad7aeeadb2d26bc284c990bba6f860c1
Howard Chen <howard@chain.com>
2019-08-20 19:10:16 -0400

    update rubenv - too old

--- services/keystore/migrations/2019-08-20.0.update-encrypted-keys.sql
+++ services/keystore/migrations/2019-08-20.0.update-encrypted-keys.sql
@@ -2,16 +2,20 @@
 
 ALTER TABLE public.encrypted_keys
 	DROP COLUMN salt,
-	DROP COLUMN encrypter_name;
+	DROP COLUMN encrypter_name,
+	DROP COLUMN encrypted_keys_data;
+
+TRUNCATE public.encrypted_keys;
 
 ALTER TABLE public.encrypted_keys
-	ALTER COLUMN encrypted_keys_data TYPE jsonb;
+	ADD COLUMN encrypted_keys_data jsonb NOT NULL;
 
 -- +migrate Down
 
 ALTER TABLE public.encrypted_keys
-	ADD COLUMN salt text NOT NULL,
-	ADD COLUMN encrypter_name text NOT NULL;
+	DROP COLUMN encrypted_keys_data;
 
 ALTER TABLE public.encrypted_keys
-	ALTER COLUMN encrypted_keys_data TYPE bytea;
+	ADD COLUMN salt text NOT NULL,
+	ADD COLUMN encrypter_name text NOT NULL,
+	ADD COLUMN encrypted_keys_data bytea NOT NULL;
