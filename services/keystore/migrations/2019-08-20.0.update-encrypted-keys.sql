ae184b990558539078ae9b9458c6a2dad25779e5
Howard Chen <howard@chain.com>
2019-08-20 15:28:55 -0400

    fix migration

--- services/keystore/migrations/2019-08-20.0.update-encrypted-keys.sql
+++ services/keystore/migrations/2019-08-20.0.update-encrypted-keys.sql
@@ -2,18 +2,16 @@
 
 ALTER TABLE public.encrypted_keys
 	DROP COLUMN salt,
-	DROP COLUMN encrypter_name,
-
-TRUNCATE encrypted_keys;
+	DROP COLUMN encrypter_name;
 
 ALTER TABLE public.encrypted_keys
-	ALTER COLUMN encoded_keys_data TYPE jsonb NOT NULL;
+	ALTER COLUMN encrypted_keys_data TYPE jsonb;
 
 -- +migrate Down
 
 ALTER TABLE public.encrypted_keys
 	ADD COLUMN salt text NOT NULL,
-	ADD COLUMN encrypter_name text NOT NULL,
+	ADD COLUMN encrypter_name text NOT NULL;
 
 ALTER TABLE public.encrypted_keys
-	ALTER COLUMN encoded_keys_data TYPE bytea NOT NULL;
+	ALTER COLUMN encrypted_keys_data TYPE bytea;
