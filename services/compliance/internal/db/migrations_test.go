fc3421c5e851bacc1bd521d456f5daf0227c4328
Tamir Sen <tamir@stellar.org>
2019-08-23 10:51:32 +0200

    Add tests for bridge, compliance, and ticker assets

---
+++ services/compliance/internal/db/migrations_test.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+
+	assetfs "github.com/elazarl/go-bindata-assetfs"
+	"github.com/shurcooL/httpfs/filter"
+
+	supportHttp "github.com/stellar/go/support/http"
+)
+
+func TestGeneratedAssets(t *testing.T) {
+	localAssets := filter.Keep(http.Dir("."), func(path string, fi os.FileInfo) bool {
+		return fi.IsDir() || strings.HasSuffix(path, ".sql")
+	})
+	generatedAssets := &assetfs.AssetFS{Asset: Asset, AssetDir: AssetDir, AssetInfo: AssetInfo}
+	if !supportHttp.EqualFileSystems(localAssets, generatedAssets, "/") {
+		t.Fatalf("generated migrations does not match local migrations")
+	}
+}
