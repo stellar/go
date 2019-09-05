c3085447848668d401c73284b6abadcffdfa9c6a
Tamir Sen <tamir@stellar.org>
2019-09-05 11:52:57 +0200

    Remove go 1.11 price functions
    We are only supporting Go 1.12+ so it is no longer necessary to have price/mul_legacy.go

--- price/main.go
+++ price/main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math"
 	"math/big"
+	"math/bits"
 	"regexp"
 	"strconv"
 
@@ -155,6 +156,50 @@ func ConvertToBuyingUnits(sellingOfferAmount int64, sellingUnitsNeeded int64, pr
 	}
 
 	return result, sellingUnitsExtracted, nil
+}
+
+// MulFractionRoundDown sets x = (x * n) / d, which is a round-down operation
+// see https://github.com/stellar/stellar-core/blob/9af27ef4e20b66f38ab148d52ba7904e74fe502f/src/util/types.cpp#L201
+func MulFractionRoundDown(x int64, n int64, d int64) (int64, error) {
+	if d == 0 {
+		return 0, errors.New("division by 0")
+	}
+
+	hi, lo := bits.Mul64(uint64(x), uint64(n))
+
+	denominator := uint64(d)
+	if denominator <= hi {
+		return 0, errors.New("overflow")
+	}
+	q, _ := bits.Div64(hi, lo, denominator)
+	if q > math.MaxInt64 {
+		return 0, errors.New("overflow")
+	}
+
+	return int64(q), nil
+}
+
+// mulFractionRoundUp sets x = ((x * n) + d - 1) / d, which is a round-up operation
+// see https://github.com/stellar/stellar-core/blob/9af27ef4e20b66f38ab148d52ba7904e74fe502f/src/util/types.cpp#L201
+func mulFractionRoundUp(x int64, n int64, d int64) (int64, error) {
+	if d == 0 {
+		return 0, errors.New("division by 0")
+	}
+
+	hi, lo := bits.Mul64(uint64(x), uint64(n))
+	lo, carry := bits.Add64(lo, uint64(d-1), 0)
+	hi += carry
+
+	denominator := uint64(d)
+	if denominator <= hi {
+		return 0, errors.New("overflow")
+	}
+	q, _ := bits.Div64(hi, lo, denominator)
+	if q > math.MaxInt64 {
+		return 0, errors.New("overflow")
+	}
+
+	return int64(q), nil
 }
 
 // min impl for int64
