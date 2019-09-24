1a1a399331bfe584d475c2f499c3fbedd91c2c46
Leigh McCulloch <leigh@stellar.org>
2019-09-24 13:38:14 -0700

    all: simplify gofmt script

--- gofmt.sh
+++ gofmt.sh
@@ -2,9 +2,7 @@
 set -e
 
 printf "Running gofmt checks...\n"
-OUTPUT=$(ls -d */ \
-  | egrep -v '^vendor|^docs' \
-  | xargs -I {} -P 4 gofmt -d {})
+OUTPUT=$(gofmt -d .)
 
 if [[ $OUTPUT ]]; then
   printf "gofmt found unformatted files:\n\n"
