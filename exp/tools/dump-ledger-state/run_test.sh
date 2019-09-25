ceabf952bc57e46171f7e211a4e67665e5c96d3a
Tamir Sen <tamir@stellar.org>
2019-09-25 17:40:55 +0200

    Publish docker image to run state diff checks

--- exp/tools/dump-ledger-state/run_test.sh
+++ exp/tools/dump-ledger-state/run_test.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 set -e
 
-# Get latest ledger
-echo "Getting latest checkpoint ledger..."
-export LATEST_LEDGER=`curl -s http://history.stellar.org/prd/core-live/core_live_001/.well-known/stellar-history.json | jq -r '.currentLedger'`
-echo "Latest ledger: $LATEST_LEDGER"
+if [ -z ${LATEST_LEDGER+x} ]; then
+    # Get latest ledger
+    echo "Getting latest checkpoint ledger..."
+    export LATEST_LEDGER=`curl -s http://history.stellar.org/prd/core-live/core_live_001/.well-known/stellar-history.json | jq -r '.currentLedger'`
+    echo "Latest ledger: $LATEST_LEDGER"
+fi
 
 # Dump state using Golang
 echo "Dumping state using ingest..."
@@ -12,11 +14,11 @@ go run ./main.go
 echo "State dumped..."
 
 # Catchup core
-stellar-core catchup $LATEST_LEDGER/1
+stellar-core --conf ./stellar-core.cfg catchup $LATEST_LEDGER/1
 
 echo "Dumping state from stellar-core..."
-dump_core_db.sh
+./dump_core_db.sh
 echo "State dumped..."
 
 echo "Comparing state dumps..."
-diff_test.sh
+./diff_test.sh
