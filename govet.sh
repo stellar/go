fd7758156f861396d77c7bd3f7cbc36307444ae7
Bartek Nowotarski <bartek@nowotarski.info>
2019-09-26 17:41:30 +0200

    Test shadow issues

--- govet.sh
+++ govet.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 set -e
 
+cat -n ./keypair/main.go
+
 printf "Running go vet...\n"
 go vet -all -composites=false -unreachable=false -tests=false ./...
+
+cat -n ./keypair/main.go
 
 printf "Running go vet shadow...\n"
 command -v shadow >/dev/null 2>&1 || (
@@ -13,6 +17,8 @@ command -v shadow >/dev/null 2>&1 || (
   popd
 )
 
+cat -n ./keypair/main.go
+
 # The go vet -vettool option was added in 1.12, and broken in the initial 1.13
 # release. Until it is fixed we must call vettool's directly as a work around.
 # https://github.com/golang/go/issues/34053
@@ -21,3 +27,5 @@ if [[ $GOLANG_VERSION = 1.12.* ]]; then
 else
   shadow ./...
 fi
+
+cat -n ./keypair/main.go
