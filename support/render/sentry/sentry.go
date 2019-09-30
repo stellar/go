eeb29f7b7fd44b90a462e55ff55ba476abdadda5
Howard Chen <howard@chain.com>
2019-09-30 18:09:50 -0400

    make comments prettier

--- support/render/sentry/sentry.go
+++ support/render/sentry/sentry.go
@@ -7,13 +7,13 @@ import (
 )
 
 // AsyncCapture reports err to Sentry asynchronously.
-// Note that the Sentry Go SDK itself, provides two built-in transports. HTTPTransport,
-// which is non-blocking and is used by default. And HTTPSyncTransport which is
-// blocking.
-// To use HTTPSyncTransport, you would need to configure Sentry client with ClientOptions
-// during initialization.
+// Note that the Sentry Go SDK provides two built-in transports:
+// 1. HTTPTransport, which is non-blocking and is used by default.
+// 2. HTTPSyncTransport, which is blocking.
+// To use HTTPSyncTransport, you would need to configure the Sentry client with
+// ClientOptions during initialization.
 func AsyncCapture(ctx context.Context, err error) {
-	// Use Sentry Scope to capture other information in the context
+	// Use Sentry Scope to capture other information in the context.
 	// https://docs.sentry.io/enriching-error-data/context/?platform=go#tagging-events
 	sentry.CaptureException(err)
 }
