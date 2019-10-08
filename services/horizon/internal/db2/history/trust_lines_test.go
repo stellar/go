4ab814cbf44b00ccdf0c923e0fb5765b880f195e
Bartek Nowotarski <bartek@nowotarski.info>
2019-10-08 13:56:35 +0200

    Update tests

--- services/horizon/internal/db2/history/trust_lines_test.go
+++ services/horizon/internal/db2/history/trust_lines_test.go
@@ -3,11 +3,9 @@ package history
 import (
 	"testing"
 
-	"github.com/Masterminds/squirrel"
-	"github.com/stellar/go/support/db"
+	"github.com/stellar/go/services/horizon/internal/test"
 	"github.com/stellar/go/xdr"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
 )
 
 var (
@@ -48,141 +46,94 @@ var (
 	}
 )
 
-type sqlResult struct {
-	lastInsertId int64
-	rowsAffected int64
-}
-
-func (r sqlResult) LastInsertId() (int64, error) {
-	return r.lastInsertId, nil
-}
-
-func (r sqlResult) RowsAffected() (int64, error) {
-	return r.rowsAffected, nil
-}
-
 func TestInsertTrustLine(t *testing.T) {
-	mockSession := &db.MockSession{}
-	q := &Q{mockSession}
+	tt := test.Start(t).Scenario("base")
+	defer tt.Finish()
+	q := &Q{tt.HorizonSession()}
 
-	const expectedQuery = "INSERT INTO trust_lines (accountid,assetcode,assetissuer,assettype,balance,buyingliabilities,flags,last_modified_ledger,lkey,sellingliabilities,tlimit) VALUES (?,?,?,?,?,?,?,?,?,?,?)"
-
-	mockSession.
-		On("Exec", mock.Anything).
-		Run(func(args mock.Arguments) {
-			arg := args.Get(0).(squirrel.Sqlizer)
-			q, qargs, err := arg.ToSql()
-			assert.Equal(t, expectedQuery, q)
-			assert.Equal(t, []interface{}{
-				"GAOQJGUAB7NI7K7I62ORBXMN3J4SSWQUQ7FOEPSDJ322W2HMCNWPHXFB",
-				"EUR",
-				"GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H",
-				xdr.AssetTypeAssetTypeCreditAlphanum4, // assettype
-				xdr.Int64(20000),                      // balance
-				xdr.Int64(3),                          // buyinliabilities
-				xdr.Uint32(1),                         // flags
-				xdr.Uint32(1234),                      // last_modified_ledger
-				// lkey:
-				"AAAAAQAAAAAdBJqAD9qPq+j2nRDdjdp5KVoUh8riPkNO9ato7BNs8wAAAAFFVVIAAAAAAGL8HQvQkbK2HA3WVjRrKmjX00fG8sLI7m0ERwJW/AX3",
-				xdr.Int64(4),         // sellingliabilities
-				xdr.Int64(223456789), // tlimit
-			}, qargs)
-			assert.NoError(t, err)
-		}).
-		Return(sqlResult{rowsAffected: 1}, nil).
-		Once()
 	rows, err := q.InsertTrustLine(eurTrustLine, 1234)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), rows)
 
-	mockSession.
-		On("Exec", mock.Anything).
-		Run(func(args mock.Arguments) {
-			arg := args.Get(0).(squirrel.Sqlizer)
-			q, qargs, err := arg.ToSql()
-			assert.Equal(t, expectedQuery, q)
-			assert.Equal(t, []interface{}{
-				"GAOQJGUAB7NI7K7I62ORBXMN3J4SSWQUQ7FOEPSDJ322W2HMCNWPHXFB",
-				"USDUSD",
-				"GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H",
-				xdr.AssetTypeAssetTypeCreditAlphanum12, // assettype
-				xdr.Int64(10000),                       // balance
-				xdr.Int64(1),                           // buyinliabilities
-				xdr.Uint32(0),                          // flags
-				xdr.Uint32(1235),                       // last_modified_ledger
-				// lkey:
-				"AAAAAQAAAAAdBJqAD9qPq+j2nRDdjdp5KVoUh8riPkNO9ato7BNs8wAAAAJVU0RVU0QAAAAAAAAAAAAAYvwdC9CRsrYcDdZWNGsqaNfTR8bywsjubQRHAlb8Bfc=",
-				xdr.Int64(2),         // sellingliabilities
-				xdr.Int64(123456789), // tlimit
-			}, qargs)
-			assert.NoError(t, err)
-		}).
-		Return(sqlResult{rowsAffected: 1}, nil).
-		Once()
 	rows, err = q.InsertTrustLine(usdTrustLine, 1235)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), rows)
+
+	keys := []xdr.LedgerKeyTrustLine{
+		{Asset: eurTrustLine.Asset, AccountId: eurTrustLine.AccountId},
+		{Asset: usdTrustLine.Asset, AccountId: usdTrustLine.AccountId},
+	}
+
+	lines, err := q.GetTrustLinesByKeys(keys)
+	assert.NoError(t, err)
+	assert.Len(t, lines, 2)
 }
 
 func TestUpdateTrustLine(t *testing.T) {
-	mockSession := &db.MockSession{}
-	q := &Q{mockSession}
+	tt := test.Start(t).Scenario("base")
+	defer tt.Finish()
+	q := &Q{tt.HorizonSession()}
 
-	const expectedQuery = "UPDATE trust_lines SET accountid = ?, assetcode = ?, assetissuer = ?, assettype = ?, balance = ?, buyingliabilities = ?, flags = ?, last_modified_ledger = ?, sellingliabilities = ?, tlimit = ? WHERE lkey = ?"
-
-	mockSession.
-		On("Exec", mock.Anything).
-		Run(func(args mock.Arguments) {
-			arg := args.Get(0).(squirrel.Sqlizer)
-			q, qargs, err := arg.ToSql()
-			assert.Equal(t, expectedQuery, q)
-			assert.Equal(t, []interface{}{
-				"GAOQJGUAB7NI7K7I62ORBXMN3J4SSWQUQ7FOEPSDJ322W2HMCNWPHXFB",
-				"EUR",
-				"GBRPYHIL2CI3FNQ4BXLFMNDLFJUNPU2HY3ZMFSHONUCEOASW7QC7OX2H",
-				xdr.AssetTypeAssetTypeCreditAlphanum4, // assettype
-				xdr.Int64(20000),                      // balance
-				xdr.Int64(3),                          // buyinliabilities
-				xdr.Uint32(1),                         // flags
-				xdr.Uint32(1234),                      // last_modified_ledger
-				xdr.Int64(4),                          // sellingliabilities
-				xdr.Int64(223456789),                  // tlimit
-				// lkey:
-				"AAAAAQAAAAAdBJqAD9qPq+j2nRDdjdp5KVoUh8riPkNO9ato7BNs8wAAAAFFVVIAAAAAAGL8HQvQkbK2HA3WVjRrKmjX00fG8sLI7m0ERwJW/AX3",
-			}, qargs)
-			assert.NoError(t, err)
-		}).
-		Return(sqlResult{rowsAffected: 1}, nil).
-		Once()
-	rows, err := q.UpdateTrustLine(eurTrustLine, 1234)
+	rows, err := q.InsertTrustLine(eurTrustLine, 1234)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), rows)
+
+	modifiedTrustLine := eurTrustLine
+	modifiedTrustLine.Balance = 30000
+
+	rows, err = q.UpdateTrustLine(modifiedTrustLine, 1235)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), rows)
+
+	keys := []xdr.LedgerKeyTrustLine{
+		{Asset: eurTrustLine.Asset, AccountId: eurTrustLine.AccountId},
+	}
+	lines, err := q.GetTrustLinesByKeys(keys)
+	assert.NoError(t, err)
+	assert.Len(t, lines, 1)
+
+	expectedBinary, err := modifiedTrustLine.MarshalBinary()
+	assert.NoError(t, err)
+
+	dbEntry := xdr.TrustLineEntry{
+		AccountId: xdr.MustAddress(lines[0].AccountID),
+		Asset:     xdr.MustNewCreditAsset(lines[0].AssetCode, lines[0].AssetIssuer),
+		Balance:   xdr.Int64(lines[0].Balance),
+		Limit:     xdr.Int64(lines[0].Limit),
+		Flags:     xdr.Uint32(lines[0].Flags),
+		Ext: xdr.TrustLineEntryExt{
+			V: 1,
+			V1: &xdr.TrustLineEntryV1{
+				Liabilities: xdr.Liabilities{
+					Buying:  xdr.Int64(lines[0].BuyingLiabilities),
+					Selling: xdr.Int64(lines[0].SellingLiabilities),
+				},
+			},
+		},
+	}
+
+	actualBinary, err := dbEntry.MarshalBinary()
+	assert.NoError(t, err)
+	assert.Equal(t, expectedBinary, actualBinary)
+	assert.Equal(t, uint32(1235), lines[0].LastModifiedLedger)
 }
 
 func TestRemoveTrustLine(t *testing.T) {
-	mockSession := &db.MockSession{}
-	q := &Q{mockSession}
+	tt := test.Start(t).Scenario("base")
+	defer tt.Finish()
+	q := &Q{tt.HorizonSession()}
 
-	ledgerKeyTrustline := xdr.LedgerKeyTrustLine{
-		AccountId: eurTrustLine.AccountId,
-		Asset:     eurTrustLine.Asset,
-	}
-
-	mockSession.
-		On("Exec", mock.Anything).
-		Run(func(args mock.Arguments) {
-			arg := args.Get(0).(squirrel.Sqlizer)
-			q, qargs, err := arg.ToSql()
-			assert.Equal(t, "DELETE FROM trust_lines WHERE lkey = ?", q)
-			assert.Equal(t, []interface{}{
-				// lkey:
-				"AAAAAQAAAAAdBJqAD9qPq+j2nRDdjdp5KVoUh8riPkNO9ato7BNs8wAAAAFFVVIAAAAAAGL8HQvQkbK2HA3WVjRrKmjX00fG8sLI7m0ERwJW/AX3",
-			}, qargs)
-			assert.NoError(t, err)
-		}).
-		Return(sqlResult{rowsAffected: 1}, nil).
-		Once()
-	rows, err := q.RemoveTrustLine(ledgerKeyTrustline)
+	rows, err := q.InsertTrustLine(eurTrustLine, 1234)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), rows)
+
+	key := xdr.LedgerKeyTrustLine{Asset: eurTrustLine.Asset, AccountId: eurTrustLine.AccountId}
+	rows, err = q.RemoveTrustLine(key)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), rows)
+
+	// Doesn't exist anymore
+	rows, err = q.RemoveTrustLine(key)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(0), rows)
 }
