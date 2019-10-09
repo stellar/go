05f9c3d1d867e56f38f0fa6cab6533649346053b
Yakov Zaytsev <me@ysz.io>
2019-10-09 19:04:01 +0300

    how to fix failing tests

--- build/asset_test.go
+++ build/asset_test.go
@@ -55,7 +55,7 @@ func TestAsset_ToXDR(t *testing.T) {
 		{
 			Name:        "bad issuer",
 			Asset:       CreditAsset("USD", "FUNK"),
-			ExpectedErr: "base32 decode failed: illegal base32 data at input byte 0",
+			ExpectedErr: "length has to be a multiple of 8",
 		},
 		{
 			Name:        "bad code",
