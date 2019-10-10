0fc3bfe3dfa7aba69074e417e7dd47101b44e219
Bartek Nowotarski <bartek@nowotarski.info>
2019-10-10 22:56:48 +0200

    horizon/ingest: Fix ingesting failed path payment operations (#1826)
    In #1776 we changed code ingesting path payment operations to use helper
    methods (`DestAmount()` and `SendAmount()`) without checking if
    transaction is successful. The problem with that approach is that to
    call helper methods we need result structs fetched using
    `c.OperationResult()` however these may not exist if transaction was not
    successful or failed with `txFAILED` because for other error codes
    `results` slice is empty.
    
    This commit reverts the code in `operationDetails` to check if tx is
    successful before using helper methods. We also add a test to catch such
    case.

--- services/horizon/internal/db2/schema/bindata.go
+++ services/horizon/internal/db2/schema/bindata.go
@@ -305,7 +305,7 @@ func migrations1_initial_schemaSql() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "migrations/1_initial_schema.sql", size: 9977, mode: os.FileMode(0644), modTime: time.Unix(1570633826, 0)}
+	info := bindataFileInfo{name: "migrations/1_initial_schema.sql", size: 9977, mode: os.FileMode(0644), modTime: time.Unix(1570634484, 0)}
 	a := &asset{bytes: bytes, info: info, digest: [32]uint8{0x6d, 0xdd, 0xc3, 0x1d, 0x39, 0xb7, 0x5e, 0x6a, 0x64, 0x2b, 0xd5, 0x66, 0x83, 0x61, 0x79, 0xf7, 0x59, 0xce, 0x51, 0x66, 0x3c, 0xd0, 0xf3, 0x14, 0x35, 0xcd, 0x21, 0xc0, 0xae, 0x24, 0x84, 0x35}}
 	return a, nil
 }
