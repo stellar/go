5b64bbd2a792bee35301e18bc649811deb6d7175
Leigh McCulloch <leigh@stellar.org>
2019-10-24 14:01:48 -0700

    support/env: add tests for existing funcs

---
+++ support/env/env_test.go
@@ -0,0 +1,63 @@
+package env_test
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"os"
+	"testing"
+
+	"github.com/stellar/go/support/env"
+	"github.com/stellar/go/support/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func randomStr(length int) string {
+	raw := make([]byte, (length+1)/2)
+	_, err := rand.Read(raw)
+	if err != nil {
+		err = errors.Wrap(err, "read from crypto/rand failed")
+		panic(err)
+	}
+	return hex.EncodeToString(raw)[:length]
+}
+
+// TestString_set tests that env.String will return the value of the
+// environment variable when the environment variable is set.
+func TestString_set(t *testing.T) {
+	envVar := "TestString_set_" + randomStr(10)
+	err := os.Setenv(envVar, "value")
+	require.NoError(t, err)
+	defer os.Unsetenv(envVar)
+
+	value := env.String(envVar, "default")
+	assert.Equal(t, "value", value)
+}
+
+// TestString_set tests that env.String will return the default value given
+// when the environment variable is not set.
+func TestString_notSet(t *testing.T) {
+	envVar := "TestString_notSet_" + randomStr(10)
+	value := env.String(envVar, "default")
+	assert.Equal(t, "default", value)
+}
+
+// TestInt_set tests that env.Int will return the value of the environment
+// variable as an int when the environment variable is set.
+func TestInt_set(t *testing.T) {
+	envVar := "TestInt_set_" + randomStr(10)
+	err := os.Setenv(envVar, "12345")
+	require.NoError(t, err)
+	defer os.Unsetenv(envVar)
+
+	value := env.Int(envVar, 67890)
+	assert.Equal(t, 12345, value)
+}
+
+// TestInt_set tests that env.Int will return the default value given when the
+// environment variable is not set.
+func TestInt_notSet(t *testing.T) {
+	envVar := "TestInt_notSet_" + randomStr(10)
+	value := env.Int(envVar, 67890)
+	assert.Equal(t, 67890, value)
+}
