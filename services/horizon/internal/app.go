168dd5d480f027a3b6b52b2a600147aeefed2eb7
Leigh McCulloch <leigh@stellar.org>
2019-11-15 08:48:13 -0800

    services/horizon/internal: remove unused layer of router

--- services/horizon/internal/app.go
+++ services/horizon/internal/app.go
@@ -78,9 +78,6 @@ func NewApp(config Config) *App {
 // Serve starts the horizon web server, binding it to a socket, setting up
 // the shutdown signals.
 func (a *App) Serve() {
-	mux := http.NewServeMux()
-	mux.Handle("/", a.web.router)
-
 	addr := fmt.Sprintf(":%d", a.config.Port)
 
 	srv := &graceful.Server{
@@ -88,7 +85,7 @@ func (a *App) Serve() {
 
 		Server: &http.Server{
 			Addr:              addr,
-			Handler:           mux,
+			Handler:           a.web.router,
 			ReadHeaderTimeout: 5 * time.Second,
 		},
 
