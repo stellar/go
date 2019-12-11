92cba56a517156768493fdb115ea74b23cfb5205
Adolfo Builes <builes.adolfo@gmail.com>
2019-12-11 11:30:52 -0500

    Implement functions for transaction_operation.

--- services/horizon/internal/db2/history/transaction_operation.go
+++ services/horizon/internal/db2/history/transaction_operation.go
@@ -8,41 +8,41 @@ import (
 
 // TransactionOperation represents the data for a single operation within a transaction
 type TransactionOperation struct {
-	Index       uint32
-	Transaction io.LedgerTransaction
-	Operation   xdr.Operation
+	Index          uint32
+	Transaction    io.LedgerTransaction
+	Operation      xdr.Operation
+	LedgerSequence uint32
 }
 
 // ID returns the ID for the operation.
-func (op *TransactionOperation) ID(sequence uint32) int64 {
+func (op *TransactionOperation) ID() int64 {
 	return toid.New(
-		int32(sequence),
+		int32(op.LedgerSequence),
 		int32(op.Transaction.Index),
 		int32(op.Index),
 	).ToInt64()
 }
 
-// TxID returns the id for the transaction related with this operation.
-func (op *TransactionOperation) TxID() int64 {
-	return 0
+// TransactionID returns the id for the transaction related with this operation.
+func (op *TransactionOperation) TransactionID() int64 {
+	return toid.New(int32(op.LedgerSequence), int32(op.Transaction.Index), 0).ToInt64()
 }
 
-// Order returns the order of this operation within the transaction's operations.
-func (op *TransactionOperation) Order() int32 {
-	return int32(op.Index)
+// SourceAccount returns the operation's source account.
+func (op *TransactionOperation) SourceAccount() *xdr.AccountId {
+	sourceAccount := op.Operation.SourceAccount
+	if sourceAccount != nil {
+		return sourceAccount
+	}
+
+	return &op.Transaction.Envelope.Tx.SourceAccount
 }
 
-// // SourceAccount returns the operation's source account.
-// func (op *TransactionOperation) SourceAccount() xdr.AccountId {
-// 	return nil
-// }
+// OperationType returns the operation type.
+func (op *TransactionOperation) OperationType() xdr.OperationType {
+	return op.Operation.Body.Type
+}
 
-// // OperationType returns the operation type.
-// func (op *TransactionOperation) OperationType() xdr.OperationType {
-// 	return nil
-
-// }
-
-// // Details returns the operation details as a map which can be stored as JSON.
+// Details returns the operation details as a map which can be stored as JSON.
 // func (op *TransactionOperation) Details() map[string]interface{} {
 // }
--- services/horizon/internal/db2/history/transaction_operation_test.go
+++ services/horizon/internal/db2/history/transaction_operation_test.go
@@ -8,27 +8,121 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var ledger = int64(4294967296) // ledger sequence 1
-var tx = int64(4096)           // tx index 1
-var op = int64(1)              // op index 1
+var (
+	ledger = int64(4294967296) // ledger sequence 1
+	tx     = int64(4096)       // tx index 1
+	op     = int64(1)          // op index 1
+)
 
 func TestTransactionOperationID(t *testing.T) {
 	tt := assert.New(t)
+	transaction, err := buildTransaction(
+		1,
+		"AAAAACiSTRmpH6bHC6Ekna5e82oiGY5vKDEEUgkq9CB//t+rAAAAZAEXUhsAADDGAAAAAQAAAAAAAAAAAAAAAF3v3WAAAAABAAAACjEwOTUzMDMyNTAAAAAAAAEAAAAAAAAAAQAAAAAOr5CG1ax6qG2fBEgXJlF0sw5W0irOS6N/NRDbavBm4QAAAAAAAAAAE32fwAAAAAAAAAABf/7fqwAAAEAkWgyAgV5tF3m1y1TIDYkNXP8pZLAwcxhWEi4f3jcZJK7QrKSXhKoawVGrp5NNs4y9dgKt8zHZ8KbJreFBUsIB",
+	)
+	tt.NoError(err)
+
+	operation := TransactionOperation{
+		Index:          1,
+		Transaction:    transaction,
+		Operation:      transaction.Envelope.Tx.Operations[0],
+		LedgerSequence: 1,
+	}
+
+	tt.Equal(ledger+tx+op, operation.ID())
+}
+
+func TestTransactionOperationTransactionID(t *testing.T) {
+	tt := assert.New(t)
+	transaction, err := buildTransaction(
+		1,
+		"AAAAACiSTRmpH6bHC6Ekna5e82oiGY5vKDEEUgkq9CB//t+rAAAAZAEXUhsAADDGAAAAAQAAAAAAAAAAAAAAAF3v3WAAAAABAAAACjEwOTUzMDMyNTAAAAAAAAEAAAAAAAAAAQAAAAAOr5CG1ax6qG2fBEgXJlF0sw5W0irOS6N/NRDbavBm4QAAAAAAAAAAE32fwAAAAAAAAAABf/7fqwAAAEAkWgyAgV5tF3m1y1TIDYkNXP8pZLAwcxhWEi4f3jcZJK7QrKSXhKoawVGrp5NNs4y9dgKt8zHZ8KbJreFBUsIB",
+	)
+	tt.NoError(err)
+
+	operation := TransactionOperation{
+		Index:          1,
+		Transaction:    transaction,
+		Operation:      transaction.Envelope.Tx.Operations[0],
+		LedgerSequence: 1,
+	}
+
+	tt.Equal(ledger+tx, operation.TransactionID())
+}
+
+func TestOperationTransactionSourceAccount(t *testing.T) {
+	testCases := []struct {
+		desc          string
+		sourceAccount string
+		expected      string
+	}{
+		{
+			desc:          "Source account is same as transaction",
+			sourceAccount: "",
+			expected:      "GAUJETIZVEP2NRYLUESJ3LS66NVCEGMON4UDCBCSBEVPIID773P2W6AY",
+		},
+		{
+			desc:          "Source account is different to transaction",
+			sourceAccount: "GDMQUXK7ZUCWM5472ZU3YLDP4BMJLQQ76DEMNYDEY2ODEEGGRKLEWGW2",
+			expected:      "GDMQUXK7ZUCWM5472ZU3YLDP4BMJLQQ76DEMNYDEY2ODEEGGRKLEWGW2",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			tt := assert.New(t)
+			transaction, err := buildTransaction(
+				1,
+				"AAAAACiSTRmpH6bHC6Ekna5e82oiGY5vKDEEUgkq9CB//t+rAAAAZAEXUhsAADDGAAAAAQAAAAAAAAAAAAAAAF3v3WAAAAABAAAACjEwOTUzMDMyNTAAAAAAAAEAAAAAAAAAAQAAAAAOr5CG1ax6qG2fBEgXJlF0sw5W0irOS6N/NRDbavBm4QAAAAAAAAAAE32fwAAAAAAAAAABf/7fqwAAAEAkWgyAgV5tF3m1y1TIDYkNXP8pZLAwcxhWEi4f3jcZJK7QrKSXhKoawVGrp5NNs4y9dgKt8zHZ8KbJreFBUsIB",
+			)
+			tt.NoError(err)
+			op := transaction.Envelope.Tx.Operations[0]
+			if len(tc.sourceAccount) > 0 {
+				sourceAccount := xdr.MustAddress(tc.sourceAccount)
+				op.SourceAccount = &sourceAccount
+			}
+
+			operation := TransactionOperation{
+				Index:          1,
+				Transaction:    transaction,
+				Operation:      op,
+				LedgerSequence: 1,
+			}
+
+			tt.Equal(tc.expected, operation.SourceAccount().Address())
+		})
+	}
+}
+
+func TestTransactionOperationType(t *testing.T) {
+	tt := assert.New(t)
+	transaction, err := buildTransaction(
+		1,
+		"AAAAACiSTRmpH6bHC6Ekna5e82oiGY5vKDEEUgkq9CB//t+rAAAAZAEXUhsAADDGAAAAAQAAAAAAAAAAAAAAAF3v3WAAAAABAAAACjEwOTUzMDMyNTAAAAAAAAEAAAAAAAAAAQAAAAAOr5CG1ax6qG2fBEgXJlF0sw5W0irOS6N/NRDbavBm4QAAAAAAAAAAE32fwAAAAAAAAAABf/7fqwAAAEAkWgyAgV5tF3m1y1TIDYkNXP8pZLAwcxhWEi4f3jcZJK7QrKSXhKoawVGrp5NNs4y9dgKt8zHZ8KbJreFBUsIB",
+	)
+	tt.NoError(err)
+
+	operation := TransactionOperation{
+		Index:          1,
+		Transaction:    transaction,
+		Operation:      transaction.Envelope.Tx.Operations[0],
+		LedgerSequence: 1,
+	}
+
+	tt.Equal(xdr.OperationTypePayment, operation.OperationType())
+}
+
+func buildTransaction(index uint32, envelope string) (io.LedgerTransaction, error) {
 	transaction := io.LedgerTransaction{
 		Index:    1,
 		Envelope: xdr.TransactionEnvelope{},
 	}
 	err := xdr.SafeUnmarshalBase64(
-		"AAAAACiSTRmpH6bHC6Ekna5e82oiGY5vKDEEUgkq9CB//t+rAAAAZAEXUhsAADDGAAAAAQAAAAAAAAAAAAAAAF3v3WAAAAABAAAACjEwOTUzMDMyNTAAAAAAAAEAAAAAAAAAAQAAAAAOr5CG1ax6qG2fBEgXJlF0sw5W0irOS6N/NRDbavBm4QAAAAAAAAAAE32fwAAAAAAAAAABf/7fqwAAAEAkWgyAgV5tF3m1y1TIDYkNXP8pZLAwcxhWEi4f3jcZJK7QrKSXhKoawVGrp5NNs4y9dgKt8zHZ8KbJreFBUsIB",
+		envelope,
 		&transaction.Envelope,
 	)
-	tt.NoError(err)
-
-	operation := TransactionOperation{
-		Index:       1,
-		Transaction: transaction,
-		Operation:   transaction.Envelope.Tx.Operations[0],
+	if err != nil {
+		return transaction, err
 	}
 
-	tt.Equal(ledger+tx+op, operation.ID(1))
+	return transaction, nil
 }
