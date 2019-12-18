ac74452b7e9917ef6ccaca77ff2a034b3d5ae245
Leigh McCulloch <leigh@stellar.org>
2019-12-17 17:57:29 -0800

    Implement changes required by SEP-10 proposal

--- exp/services/webauth/internal/serve/token_test.go
+++ exp/services/webauth/internal/serve/token_test.go
@@ -227,7 +227,7 @@ func TestToken_jsonInputSuccess(t *testing.T) {
 	assert.Equal(t, exp.Sub(iat), time.Minute)
 }
 
-func TestToken_jsonInputValidMultipleSigners(t *testing.T) {
+func TestToken_jsonInputSuccessMultipleSigners(t *testing.T) {
 	serverKey := keypair.MustRandom()
 	t.Logf("Server signing key: %s", serverKey.Address())
 
@@ -313,13 +313,35 @@ func TestToken_jsonInputValidMultipleSigners(t *testing.T) {
 	h.ServeHTTP(w, r)
 	resp := w.Result()
 
-	require.Equal(t, 401, resp.StatusCode)
+	require.Equal(t, 200, resp.StatusCode)
 	assert.Equal(t, "application/json; charset=utf-8", resp.Header.Get("Content-Type"))
 
-	respBodyBytes, err := ioutil.ReadAll(resp.Body)
+	res := struct {
+		Token string `json:"token"`
+	}{}
+	err = json.NewDecoder(resp.Body).Decode(&res)
 	require.NoError(t, err)
 
-	assert.JSONEq(t, `{"error":"The request could not be authenticated."}`, string(respBodyBytes))
+	t.Logf("JWT: %s", res.Token)
+
+	token, err := jwt.Parse(res.Token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return &jwtPrivateKey.PublicKey, nil
+	})
+	require.NoError(t, err)
+
+	claims := token.Claims.(jwt.MapClaims)
+	assert.Equal(t, serverKey.Address(), claims["iss"])
+	assert.Equal(t, account.Address(), claims["sub"])
+	assert.Equal(t, account.Address(), claims["sub"])
+	iat := time.Unix(int64(claims["iat"].(float64)), 0)
+	exp := time.Unix(int64(claims["exp"].(float64)), 0)
+	assert.True(t, iat.Before(time.Now()))
+	assert.True(t, exp.After(time.Now()))
+	assert.True(t, time.Now().Add(time.Minute).After(exp))
+	assert.Equal(t, exp.Sub(iat), time.Minute)
 }
 
 func TestToken_jsonInputNotEnoughWeight(t *testing.T) {
@@ -395,33 +417,11 @@ func TestToken_jsonInputNotEnoughWeight(t *testing.T) {
 	h.ServeHTTP(w, r)
 	resp := w.Result()
 
-	require.Equal(t, 200, resp.StatusCode)
+	require.Equal(t, 401, resp.StatusCode)
 	assert.Equal(t, "application/json; charset=utf-8", resp.Header.Get("Content-Type"))
 
-	res := struct {
-		Token string `json:"token"`
-	}{}
-	err = json.NewDecoder(resp.Body).Decode(&res)
+	respBodyBytes, err := ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
 
-	t.Logf("JWT: %s", res.Token)
-
-	token, err := jwt.Parse(res.Token, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return &jwtPrivateKey.PublicKey, nil
-	})
-	require.NoError(t, err)
-
-	claims := token.Claims.(jwt.MapClaims)
-	assert.Equal(t, serverKey.Address(), claims["iss"])
-	assert.Equal(t, account.Address(), claims["sub"])
-	assert.Equal(t, account.Address(), claims["sub"])
-	iat := time.Unix(int64(claims["iat"].(float64)), 0)
-	exp := time.Unix(int64(claims["exp"].(float64)), 0)
-	assert.True(t, iat.Before(time.Now()))
-	assert.True(t, exp.After(time.Now()))
-	assert.True(t, time.Now().Add(time.Minute).After(exp))
-	assert.Equal(t, exp.Sub(iat), time.Minute)
+	assert.JSONEq(t, `{"error":"The request could not be authenticated."}`, string(respBodyBytes))
 }
