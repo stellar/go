43729470fd6577e93b6b9943c74c24998e99ed09
Leigh McCulloch <leigh@stellar.org>
2019-12-19 15:34:25 -0800

    support/http/httpdecode: add funcs for form and form+json decoding (#2078)
    Add functions `DecodeForm` that decodes form URL encoded post bodies and `Decode` for combined decoding of requests that contain either form URL encoded or JSON bodies.
    
    In some of our SEPs we are required to parse requests that can be either form URL encoded or JSON encoded. I'm writing two applications that will need to contain this same parsing logic. Writing this logic once has some benefits:
    
    - This logic has subtleties that can make it difficult to get correct every time and writing it as a library in isolation to general application code means we're more likely to write extensive tests that cover the edge cases. The reasons this code can be hard to get correct:
       - The default form parsing built into the Go stdlib silently fails some error conditions. This makes it easy to write a http handler that looks like it receives form URL encoded data and decodes it correctly, but the code may continue on with invalid input unless it specifically checks the state of the request.
       - Parsing the `Content-Type` header is not as simple as comparing two strings because the header can contain optional parameters, like `charset=utf-8`.
    - This will also mean when we find bugs with this logic we can fix it in one place and improve both applications.
    - We can set some common defaults for consistency across all our applications that parse form URL encoded data. For example: we can configure decoding not to fail on unrecognized keys which is not the default in the most popular form parsing library, but that makes our form decoding consistent with our JSON decoding.

--- support/http/httpdecode/httpdecode_test.go
+++ support/http/httpdecode/httpdecode_test.go
@@ -32,3 +32,208 @@ func TestDecodeJSON_invalid(t *testing.T) {
 	err := DecodeJSON(r, &bodyDecoded)
 	require.EqualError(t, err, "invalid character 'b' after object key")
 }
+
+func TestDecodeForm_valid(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	bodyDecoded := struct {
+		Foo string
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.Foo)
+}
+
+func TestDecodeForm_validTags(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	bodyDecoded := struct {
+		FooName string `form:"foo"`
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.FooName)
+}
+
+func TestDecodeForm_validIgnoresUnkownKeys(t *testing.T) {
+	body := `foo=bar&foz=baz`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+
+	bodyDecoded := struct {
+		Foo string
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.Foo)
+}
+
+func TestDecodeForm_validContentTypeWithOptions(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+
+	bodyDecoded := struct {
+		Foo string
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.Foo)
+}
+
+func TestDecodeForm_invalidBody(t *testing.T) {
+	body := `foo=%=`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	bodyDecoded := struct {
+		Foo string
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.EqualError(t, err, `invalid URL escape "%="`)
+	assert.Equal(t, "", bodyDecoded.Foo)
+}
+
+func TestDecodeForm_invalidNoContentType(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+
+	bodyDecoded := struct {
+		Foo string
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.EqualError(t, err, `content type application/x-www-form-urlencoded required for form decoding: mime: no media type`)
+	assert.Equal(t, "", bodyDecoded.Foo)
+}
+
+func TestDecodeForm_invalidUnrecognizedContentType(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/xwwwformurlencoded")
+
+	bodyDecoded := struct {
+		Foo string
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.EqualError(t, err, `content type application/x-www-form-urlencoded required for form decoding: received content type "application/xwwwformurlencoded"`)
+	assert.Equal(t, "", bodyDecoded.Foo)
+}
+
+func TestDecodeForm_invalidMethodType(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("GET", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	bodyDecoded := struct {
+		Foo string
+	}{}
+	err := DecodeForm(r, &bodyDecoded)
+	assert.EqualError(t, err, `method POST, PUT, or PATCH required for form decoding: request has method "GET"`)
+	assert.Equal(t, "", bodyDecoded.Foo)
+}
+
+func TestDecode_validJSONNoContentType(t *testing.T) {
+	body := `{"foo":"bar"}`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.FooName)
+}
+
+func TestDecode_validJSONWithContentType(t *testing.T) {
+	body := `{"foo":"bar"}`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/json")
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.FooName)
+}
+
+func TestDecode_validJSONWithContentTypeOptions(t *testing.T) {
+	body := `{"foo":"bar"}`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/json; charset=utf-8")
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.FooName)
+}
+
+func TestDecode_validForm(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.FooName)
+}
+
+func TestDecode_validFormWithContentTypeOptions(t *testing.T) {
+	body := `foo=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", bodyDecoded.FooName)
+}
+
+func TestDecode_cannotParseContentType(t *testing.T) {
+	body := `{"foo":"bar"}`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application=json")
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.EqualError(t, err, "content type could not be parsed: mime: expected slash after first token")
+	assert.Equal(t, "", bodyDecoded.FooName)
+}
+
+func TestDecode_invalidJSON(t *testing.T) {
+	body := `{"foo""bar"}`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.EqualError(t, err, `invalid character '"' after object key`)
+	assert.Equal(t, "", bodyDecoded.FooName)
+}
+
+func TestDecode_invalidForm(t *testing.T) {
+	body := `foo=%=bar`
+	r, _ := http.NewRequest("POST", "/", strings.NewReader(body))
+	r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	bodyDecoded := struct {
+		FooName string `json:"foo" form:"foo"`
+	}{}
+	err := Decode(r, &bodyDecoded)
+	assert.EqualError(t, err, `invalid URL escape "%=b"`)
+	assert.Equal(t, "", bodyDecoded.FooName)
+}
