e981410234e7336c1b0cd6cce8fb9ef1ee18658e
Leigh McCulloch <leigh@stellar.org>
2020-01-23 13:45:02 -0800

    Add a doc reference

--- support/render/healthjson/handler.go
+++ support/render/healthjson/handler.go
@@ -6,6 +6,8 @@ import (
 	"github.com/stellar/go/support/render/httpjson"
 )
 
+// PassHandler implements a simple handler that returns the most basic health
+// response with a status of 'pass'.
 type PassHandler struct{}
 
 func (h PassHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
--- support/render/healthjson/status.go
+++ support/render/healthjson/status.go
@@ -1,7 +1,9 @@
 package healthjson
 
+// Status indicates whether the service is health or not.
 type Status string
 
 const (
+	// StatusPass indicates that the service is health.
 	StatusPass Status = "pass"
 )
