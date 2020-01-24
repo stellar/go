ed91313a6d412f71424c84b61b0640e102c6d92c
Leigh McCulloch <leigh@stellar.org>
2020-01-23 21:17:12 -0800

    Add another example

--- support/render/health/example_test.go
+++ support/render/health/example_test.go
@@ -3,14 +3,48 @@ package health_test
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 
-	"github.com/stellar/go/support/http"
+	supporthttp "github.com/stellar/go/support/http"
 	"github.com/stellar/go/support/render/health"
+	"github.com/stellar/go/support/render/httpjson"
 )
 
+func ExampleResponse() {
+	mux := supporthttp.NewAPIMux()
+
+	mux.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		healthCheckResult := false
+		response := health.Response{}
+		if healthCheckResult {
+			response.Status = health.StatusPass
+		} else {
+			response.Status = health.StatusFail
+		}
+		httpjson.Render(w, response, httpjson.HEALTHJSON)
+	})
+
+	r := httptest.NewRequest("GET", "/health", nil)
+	w := httptest.NewRecorder()
+	mux.ServeHTTP(w, r)
+	resp := w.Result()
+
+	fmt.Println("Content Type:", resp.Header.Get("Content-Type"))
+	fmt.Println("Status Code:", resp.StatusCode)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("Body:", string(body))
+
+	// Output:
+	// Content Type: application/health+json; charset=utf-8
+	// Status Code: 200
+	// Body: {
+	//   "status": "fail"
+	// }
+}
+
 func ExampleHandler() {
-	mux := http.NewAPIMux()
+	mux := supporthttp.NewAPIMux()
 
 	mux.Get("/health", health.PassHandler{}.ServeHTTP)
 
