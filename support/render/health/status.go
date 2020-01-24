a6803e80952d6ebd7ee53c32554e7744fa844280
Leigh McCulloch <leigh@stellar.org>
2020-01-23 20:45:03 -0800

    Update enums

--- support/render/health/status.go
+++ support/render/health/status.go
@@ -4,6 +4,8 @@ package health
 type Status string
 
 const (
-	// StatusPass indicates that the service is health.
+	// StatusPass indicates that the service is healthy.
 	StatusPass Status = "pass"
+	// StatusFail indicates that the service is unhealthy.
+	StatusFail Status = "fail"
 )
