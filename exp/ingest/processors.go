cb22fd97158660bd19274770503f1aa4c7327566
Tamir Sen <tamir@stellar.org>
2020-01-28 00:00:29 +0100

    Add ingestion primitives

--- exp/ingest/processors.go
+++ exp/ingest/processors.go
@@ -1,7 +1,13 @@
 package ingest
 
 import (
+	"context"
+	stdio "io"
+
 	"github.com/stellar/go/exp/ingest/io"
+	"github.com/stellar/go/exp/ingest/ledgerbackend"
+	"github.com/stellar/go/support/errors"
+	"github.com/stellar/go/support/historyarchive"
 	"github.com/stellar/go/xdr"
 )
 
@@ -85,4 +91,177 @@ func GroupLedgerTransactionProcessors(
 	processors ...LedgerTransactionProcessor,
 ) LedgerTransactionProcessor {
 	return ltGroup(processors)
+}
+
+func RunTransactionProcessorOnLedger(
+	ctx context.Context,
+	ltProcessor LedgerTransactionProcessor,
+	ledgerBackend ledgerbackend.LedgerBackend,
+	ledger uint32,
+) error {
+	ledgerReader, err := io.NewDBLedgerReader(ledger, ledgerBackend)
+	if err != nil {
+		return errors.Wrapf(
+			err,
+			"could not get ledger reader for ledger %v",
+			ledger,
+		)
+	}
+
+	if err := ltProcessor.Init(ledgerReader.GetHeader().Header); err != nil {
+		return errors.Wrapf(
+			err,
+			"could not initialize sequence processor for ledger %v",
+			ledger,
+		)
+	}
+
+	for {
+		tx, err := ledgerReader.Read()
+		if err == stdio.EOF {
+			break
+		}
+		if err != nil {
+			return errors.Wrapf(err, "could not read transaction in ledger %v", ledger)
+		}
+		if err = ltProcessor.ProcessTransaction(tx); err != nil {
+			return errors.Wrapf(
+				err,
+				"could not process transaction %v in ledger %v",
+				tx.Index,
+				ledger,
+			)
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			continue
+		}
+	}
+
+	if err := ltProcessor.Commit(); err != nil {
+		return errors.Wrapf(err, "could not commit processor for ledger %v", ledger)
+	}
+
+	return nil
+}
+
+func RunChangeProcessorOnHAS(
+	ctx context.Context,
+	changeProcessor ChangeProcessor,
+	archive historyarchive.ArchiveInterface,
+	tempSet io.TempSet,
+	maxStreamRetries int,
+	ledger uint32,
+) error {
+	reader, err := io.MakeSingleLedgerStateReader(
+		archive,
+		tempSet,
+		ledger,
+		maxStreamRetries,
+	)
+	if err != nil {
+		return errors.Wrap(err, "could not create state reader")
+	}
+
+	if err := changeProcessor.Init(ledger); err != nil {
+		return errors.Wrapf(
+			err,
+			"could not initialize cumulative processor for ledger %v",
+			ledger,
+		)
+	}
+
+	for {
+		entryChange, err := reader.Read()
+		if err == stdio.EOF {
+			break
+		}
+		if err != nil {
+			return errors.Wrap(err, "could not read from state reader")
+		}
+
+		if err = changeProcessor.ProcessChange(io.Change{
+			Type: entryChange.EntryType(),
+			Pre:  nil,
+			Post: entryChange.Created,
+		}); err != nil {
+			return errors.Wrapf(
+				err,
+				"could not process change in ledger %v",
+				ledger,
+			)
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			continue
+		}
+	}
+
+	if err := changeProcessor.Commit(); err != nil {
+		return errors.Wrapf(err, "could not commit processor for sequence %v", ledger)
+	}
+
+	return nil
+}
+
+func RunChangeProcessorOnLedger(
+	ctx context.Context,
+	changeProcessor ChangeProcessor,
+	ledgerBackend ledgerbackend.LedgerBackend,
+	ledger uint32,
+) error {
+	changeReader, err := io.NewChangeReader(ledger, ledgerBackend)
+	if err != nil {
+		return errors.Wrapf(
+			err,
+			"could not get ledger reader for ledger %v",
+			ledger,
+		)
+	}
+
+	if err := changeProcessor.Init(ledger); err != nil {
+		return errors.Wrapf(
+			err,
+			"could not initialize sequence processor for ledger %v",
+			ledger,
+		)
+	}
+
+	for {
+		var change io.Change
+		change, err = changeReader.Read()
+		if err == stdio.EOF {
+			break
+		}
+		if err != nil {
+			return errors.Wrap(err, "could not read from change reader")
+		}
+
+		if err = changeProcessor.ProcessChange(change); err != nil {
+			return errors.Wrapf(
+				err,
+				"could not process change in ledger %v",
+				ledger,
+			)
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+			continue
+		}
+	}
+
+	if err := changeProcessor.Commit(); err != nil {
+		return errors.Wrapf(err, "could not commit processor for ledger %v", ledger)
+	}
+
+	return nil
 }
