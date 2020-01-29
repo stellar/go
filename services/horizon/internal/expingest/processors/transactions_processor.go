0891609d84b8bfa3d59d6e495bd256b09cba0027
Adolfo Builes <builes.adolfo@gmail.com>
2020-01-29 11:24:50 -0500

    Remove new_transaction_processor and use transaction_processor.

--- services/horizon/internal/expingest/processors/transactions_processor.go
+++ services/horizon/internal/expingest/processors/transactions_processor.go
@@ -1,75 +1,40 @@
 package processors
 
 import (
-	"context"
-	stdio "io"
-
 	"github.com/stellar/go/exp/ingest/io"
-	ingestpipeline "github.com/stellar/go/exp/ingest/pipeline"
-	"github.com/stellar/go/exp/support/pipeline"
 	"github.com/stellar/go/services/horizon/internal/db2/history"
 	"github.com/stellar/go/support/errors"
 )
 
 type TransactionProcessor struct {
-	TransactionsQ history.QTransactions
+	transactionsQ history.QTransactions
+	sequence      uint32
+	batch         history.TransactionBatchInsertBuilder
 }
 
-func (p *TransactionProcessor) ProcessLedger(ctx context.Context, store *pipeline.Store, r io.LedgerReader, w io.LedgerWriter) (err error) {
-	defer func() {
-		// io.LedgerReader.Close() returns error if upgrade changes have not
-		// been processed so it's worth checking the error.
-		closeErr := r.Close()
-		// Do not overwrite the previous error
-		if err == nil {
-			err = closeErr
-		}
-	}()
-	defer w.Close()
-	r.IgnoreUpgradeChanges()
+func NewTransactionProcessor(transactionsQ history.QTransactions, sequence uint32) *TransactionProcessor {
+	return &TransactionProcessor{
+		transactionsQ: transactionsQ,
+		sequence:      sequence,
+		batch:         transactionsQ.NewTransactionBatchInsertBuilder(maxBatchSize),
+	}
+}
 
-	// Exit early if not ingesting into a DB
-	if v := ctx.Value(IngestUpdateDatabase); !(v != nil && v.(bool)) {
-		return nil
+func (p *TransactionProcessor) ProcessTransaction(transaction io.LedgerTransaction) error {
+	if err := p.batch.Add(transaction, p.sequence); err != nil {
+		return errors.Wrap(err, "Error batch inserting transaction rows")
 	}
 
-	transactionBatch := p.TransactionsQ.NewTransactionBatchInsertBuilder(maxBatchSize)
-	sequence := r.GetSequence()
+	return nil
+}
 
-	for {
-		var transaction io.LedgerTransaction
-		transaction, err = r.Read()
-		if err != nil {
-			if err == stdio.EOF {
-				break
-			} else {
-				return err
-			}
-		}
-
-		if err = transactionBatch.Add(transaction, sequence); err != nil {
-			return errors.Wrap(err, "Error batch inserting transaction rows")
-		}
-
-		select {
-		case <-ctx.Done():
-			return nil
-		default:
-			continue
-		}
-	}
-
-	if err = transactionBatch.Exec(); err != nil {
+func (p *TransactionProcessor) Commit() error {
+	if err := p.batch.Exec(); err != nil {
 		return errors.Wrap(err, "Error flushing transaction batch")
 	}
 
 	return nil
 }
 
-func (p *TransactionProcessor) Name() string {
-	return "TransactionProcessor"
-}
-
-func (p *TransactionProcessor) Reset() {}
-
-var _ ingestpipeline.LedgerProcessor = &TransactionProcessor{}
+// TODO: remove comment after https://github.com/stellar/go/pull/2172/files is merged
+// var _ io.ChangeReader = &TransactionProcessor{}
