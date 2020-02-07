94e2c6e799fb8eb2fc8ad93bd239d65dc6d3e0f9
Bartek Nowotarski <bartek@nowotarski.info>
2020-02-07 13:17:13 +0100

    services/horizon/expingest: Wrap errors return by processors

--- services/horizon/internal/expingest/group_processors.go
+++ services/horizon/internal/expingest/group_processors.go
@@ -1,6 +1,9 @@
 package expingest
 
-import "github.com/stellar/go/exp/ingest/io"
+import (
+	"github.com/stellar/go/exp/ingest/io"
+	"github.com/stellar/go/support/errors"
+)
 
 type horizonChangeProcessor interface {
 	io.ChangeProcessor
@@ -33,7 +36,7 @@ type groupTransactionProcessors []horizonTransactionProcessor
 func (g groupTransactionProcessors) ProcessTransaction(tx io.LedgerTransaction) error {
 	for _, p := range g {
 		if err := p.ProcessTransaction(tx); err != nil {
-			return err
+			return errors.Wrapf(err, "error in %T.ProcessTransaction", p)
 		}
 	}
 	return nil
@@ -42,7 +45,7 @@ func (g groupTransactionProcessors) ProcessTransaction(tx io.LedgerTransaction) 
 func (g groupTransactionProcessors) Commit() error {
 	for _, p := range g {
 		if err := p.Commit(); err != nil {
-			return err
+			return errors.Wrapf(err, "error in %T.Commit", p)
 		}
 	}
 	return nil
