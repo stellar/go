4c70d5c80abbd58f20fc34f56b53754cd5310971
Adolfo Builes <builes.adolfo@gmail.com>
2020-02-10 12:20:53 -0500

    Remove withIngestion variant.

--- services/horizon/internal/actions_root_test.go
+++ services/horizon/internal/actions_root_test.go
@@ -40,35 +40,8 @@ func TestRootAction(t *testing.T) {
 		ht.Assert.Equal("test-core", actual.StellarCoreVersion)
 		ht.Assert.Equal(int32(4), actual.CoreSupportedProtocolVersion)
 		ht.Assert.Equal(int32(3), actual.CurrentProtocolVersion)
-	}
-}
 
-func TestRootActionWithIngestion(t *testing.T) {
-	ht := StartHTTPTest(t, "base")
-	defer ht.Finish()
-
-	server := test.NewStaticMockServer(`{
-			"info": {
-				"network": "test",
-				"build": "test-core",
-				"ledger": {
-					"version": 3
-				},
-				"protocol_version": 4
-			}
-		}`)
-	defer server.Close()
-
-	ht.App.horizonVersion = "test-horizon"
-	ht.App.config.StellarCoreURL = server.URL
-	ht.App.config.NetworkPassphrase = "test"
-	ht.App.UpdateStellarCoreInfo()
-
-	w := ht.Get("/")
-
-	if ht.Assert.Equal(200, w.Code) {
-		var actual horizon.Root
-		err := json.Unmarshal(w.Body.Bytes(), &actual)
+		err = json.Unmarshal(w.Body.Bytes(), &actual)
 		ht.Require.NoError(err)
 		ht.Assert.Equal(
 			"http://localhost/accounts{?signer,asset,cursor,limit,order}",
