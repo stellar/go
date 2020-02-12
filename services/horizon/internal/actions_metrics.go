6a2b7fc75d4c0fdbcaf98bf7467a3034d2624296
Bartek Nowotarski <bartek@nowotarski.info>
2020-02-12 14:48:35 +0100

    services/horizon/actions: Add Prometheus text exposition format in /metrics (#2265)
    This commit adds support for Prometheus text exposition format response
    in `/metrics`. To receive the response in Prometheus format,
    `prometheus_format=true` GET parameter must be set.
    
    Known limitations:
    * Metrics appear in random order because they are stored in map. Since
    this will only be read by machines, it seems fine.
    * `PrometheusResponder` is a temporary interface that is still part of
    the old actions package. The code will be improved when moved to the new
    system.

--- services/horizon/internal/actions_metrics.go
+++ services/horizon/internal/actions_metrics.go
@@ -1,6 +1,7 @@
 package horizon
 
 import (
+	"fmt"
 	"time"
 
 	metrics "github.com/rcrowley/go-metrics"
@@ -26,6 +27,63 @@ func (action *MetricsAction) JSON() error {
 	}
 
 	hal.Render(action.W, action.Snapshot)
+	return action.Err
+}
+
+// PrometheusFormat is a method for actions.PrometheusResponder
+func (action *MetricsAction) PrometheusFormat() error {
+	action.App.metrics.Each(func(name string, i interface{}) {
+		switch metric := i.(type) {
+		case metrics.Counter:
+			fmt.Fprintf(action.W, "%s %d\n", name, metric.Count())
+		case metrics.Gauge:
+			fmt.Fprintf(action.W, "%s %d\n", name, metric.Value())
+		case metrics.GaugeFloat64:
+			fmt.Fprintf(action.W, "%s %f\n", name, metric.Value())
+		case metrics.Histogram:
+			h := metric.Snapshot()
+			ps := h.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+
+			fmt.Fprintf(action.W, "%s_count %d\n", name, h.Count())
+			fmt.Fprintf(action.W, "%s_min %d\n", name, h.Min())
+			fmt.Fprintf(action.W, "%s_max %d\n", name, h.Max())
+			fmt.Fprintf(action.W, "%s_mean %f\n", name, h.Mean())
+			fmt.Fprintf(action.W, "%s_stddev %f\n", name, h.StdDev())
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.50\"} %f\n", name, ps[0])
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.75\"} %f\n", name, ps[1])
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.95\"} %f\n", name, ps[2])
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.99\"} %f\n", name, ps[3])
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.999\"} %f\n", name, ps[4])
+		case metrics.Meter:
+			m := metric.Snapshot()
+
+			fmt.Fprintf(action.W, "%s_count %d\n", name, m.Count())
+			fmt.Fprintf(action.W, "%s_1m_rate %f\n", name, m.Rate1())
+			fmt.Fprintf(action.W, "%s_5m_rate %f\n", name, m.Rate5())
+			fmt.Fprintf(action.W, "%s_15m_rate %f\n", name, m.Rate15())
+			fmt.Fprintf(action.W, "%s_mean_rate %f\n", name, m.RateMean())
+		case metrics.Timer:
+			t := metric.Snapshot()
+			ps := t.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999})
+
+			fmt.Fprintf(action.W, "%s_count %d\n", name, t.Count())
+			fmt.Fprintf(action.W, "%s_min %f\n", name, time.Duration(t.Min()).Seconds())
+			fmt.Fprintf(action.W, "%s_max %f\n", name, time.Duration(t.Max()).Seconds())
+			fmt.Fprintf(action.W, "%s_mean %f\n", name, time.Duration(t.Mean()).Seconds())
+			fmt.Fprintf(action.W, "%s_stddev %f\n", name, time.Duration(t.StdDev()).Seconds())
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.50\"} %f\n", name, time.Duration(ps[0]).Seconds())
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.75\"} %f\n", name, time.Duration(ps[1]).Seconds())
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.95\"} %f\n", name, time.Duration(ps[2]).Seconds())
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.99\"} %f\n", name, time.Duration(ps[3]).Seconds())
+			fmt.Fprintf(action.W, "%s_bucket{quantile=\"0.999\"} %f\n", name, time.Duration(ps[4]).Seconds())
+			fmt.Fprintf(action.W, "%s_1m_rate %f\n", name, t.Rate1())
+			fmt.Fprintf(action.W, "%s_5m_rate %f\n", name, t.Rate5())
+			fmt.Fprintf(action.W, "%s_15m_rate %f\n", name, t.Rate15())
+			fmt.Fprintf(action.W, "%s_mean_rate %f\n", name, t.RateMean())
+		}
+		fmt.Fprintf(action.W, "\n")
+	})
+
 	return action.Err
 }
 
