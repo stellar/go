6529292a42e05a6b4038acbfcfad762da48abdb3
Leigh McCulloch <leigh@stellar.org>
2020-02-13 17:00:25 -0800

    exp/services/recoverysigner: remove response not used in this endpoint

--- exp/services/recoverysigner/internal/serve/account_response.go
+++
@@ -1,8 +0,0 @@
-package serve
-
-type accountResponse struct {
-	Address  string `json:"address"`
-	Type     string `json:"type"`
-	Identity string `json:"identity"`
-	Signer   string `json:"signer"`
-}
