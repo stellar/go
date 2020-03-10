569eff51df8c9cab52b30a9ba1e6ae03fd10ce1e
Leigh McCulloch <leigh@stellar.org>
2020-03-10 09:14:11 -0700

    exp/services/recoverysigner: add test for open and dbmigrate

--- exp/services/recoverysigner/internal/db/dbmigrate/dbmigrate_test.go
+++ exp/services/recoverysigner/internal/db/dbmigrate/dbmigrate_test.go
@@ -9,7 +9,8 @@ import (
 	assetfs "github.com/elazarl/go-bindata-assetfs"
 	migrate "github.com/rubenv/sql-migrate"
 	"github.com/shurcooL/httpfs/filter"
-	"github.com/stellar/go/exp/services/recoverysigner/internal/db/dbtest"
+	dbpkg "github.com/stellar/go/exp/services/recoverysigner/internal/db"
+	"github.com/stellar/go/support/db/dbtest"
 	supportHttp "github.com/stellar/go/support/http"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -26,40 +27,213 @@ func TestGeneratedAssets(t *testing.T) {
 	}
 }
 
-func TestPlanMigration_noneToApply(t *testing.T) {
-	db := dbtest.Open(t)
-	session := db.Open()
+func TestPlanMigration_upApplyOne(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	migrations, err := PlanMigration(session, migrate.Up, 1)
+	require.NoError(t, err)
+	wantMigrations := []string{"20200309000000-initial-1.sql"}
+	assert.Equal(t, wantMigrations, migrations)
+}
+
+func TestPlanMigration_upApplyAll(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
 
 	migrations, err := PlanMigration(session, migrate.Up, 0)
 	require.NoError(t, err)
-	wantMigrations := []string{}
-	assert.Equal(t, wantMigrations, migrations)
+	require.Greater(t, len(migrations), 1)
+	assert.Equal(t, "20200309000000-initial-1.sql", migrations[0])
 }
 
-func TestPlanMigration_someToApply(t *testing.T) {
-	db := dbtest.Open(t)
-	session := db.Open()
-
-	migrations, err := PlanMigration(session, migrate.Down, 0)
+func TestPlanMigration_upApplyNone(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
 	require.NoError(t, err)
-	wantMigrations := []string{"20200309000000-initial.sql"}
-	assert.Equal(t, wantMigrations, migrations)
-}
-
-func TestMigrate_noneToApply(t *testing.T) {
-	db := dbtest.Open(t)
-	session := db.Open()
 
 	n, err := Migrate(session, migrate.Up, 0)
 	require.NoError(t, err)
-	assert.Equal(t, 0, n)
+	require.Greater(t, n, 1)
+
+	migrations, err := PlanMigration(session, migrate.Up, 0)
+	require.NoError(t, err)
+	require.Empty(t, migrations)
 }
 
-func TestMigrate_someToApply(t *testing.T) {
-	db := dbtest.Open(t)
-	session := db.Open()
+func TestPlanMigration_downApplyOne(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
 
-	n, err := Migrate(session, migrate.Down, 0)
+	n, err := Migrate(session, migrate.Up, 2)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	migrations, err := PlanMigration(session, migrate.Down, 1)
+	require.NoError(t, err)
+	wantMigrations := []string{"20200309000001-initial-2.sql"}
+	assert.Equal(t, wantMigrations, migrations)
+}
+
+func TestPlanMigration_downApplyAll(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 2)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	migrations, err := PlanMigration(session, migrate.Down, 0)
+	require.NoError(t, err)
+	wantMigrations := []string{
+		"20200309000001-initial-2.sql",
+		"20200309000000-initial-1.sql",
+	}
+	assert.Equal(t, wantMigrations, migrations)
+}
+
+func TestPlanMigration_downApplyNone(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 2)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	n, err = Migrate(session, migrate.Down, 0)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	migrations, err := PlanMigration(session, migrate.Down, 0)
+	require.NoError(t, err)
+	assert.Empty(t, migrations)
+}
+
+func TestMigrate_upApplyOne(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 1)
 	require.NoError(t, err)
 	assert.Equal(t, 1, n)
+
+	ids := []string{}
+	err = session.Select(&ids, `SELECT id FROM gorp_migrations`)
+	require.NoError(t, err)
+	wantIDs := []string{
+		"20200309000000-initial-1.sql",
+	}
+	assert.Equal(t, wantIDs, ids)
+}
+
+func TestMigrate_upApplyAll(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 0)
+	require.NoError(t, err)
+	require.Greater(t, n, 1)
+
+	ids := []string{}
+	err = session.Select(&ids, `SELECT id FROM gorp_migrations`)
+	require.NoError(t, err)
+	wantIDs := []string{
+		"20200309000000-initial-1.sql",
+		"20200309000001-initial-2.sql",
+	}
+	assert.Equal(t, wantIDs, ids)
+}
+
+func TestMigrate_upApplyNone(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 0)
+	require.NoError(t, err)
+	require.Greater(t, n, 1)
+
+	n, err = Migrate(session, migrate.Up, 0)
+	require.NoError(t, err)
+	require.Zero(t, n)
+
+	ids := []string{}
+	err = session.Select(&ids, `SELECT id FROM gorp_migrations`)
+	require.NoError(t, err)
+	wantIDs := []string{
+		"20200309000000-initial-1.sql",
+		"20200309000001-initial-2.sql",
+	}
+	assert.Equal(t, wantIDs, ids)
+}
+
+func TestMigrate_downApplyOne(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 2)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	n, err = Migrate(session, migrate.Down, 1)
+	require.NoError(t, err)
+	require.Equal(t, 1, n)
+
+	ids := []string{}
+	err = session.Select(&ids, `SELECT id FROM gorp_migrations`)
+	require.NoError(t, err)
+	wantIDs := []string{
+		"20200309000000-initial-1.sql",
+	}
+	assert.Equal(t, wantIDs, ids)
+}
+
+func TestMigrate_downApplyAll(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 2)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	n, err = Migrate(session, migrate.Down, 0)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	ids := []string{}
+	err = session.Select(&ids, `SELECT id FROM gorp_migrations`)
+	require.NoError(t, err)
+	assert.Empty(t, ids)
+}
+
+func TestMigrate_downApplyNone(t *testing.T) {
+	db := dbtest.Postgres(t)
+	session, err := dbpkg.Open(db.DSN)
+	require.NoError(t, err)
+
+	n, err := Migrate(session, migrate.Up, 2)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	n, err = Migrate(session, migrate.Down, 0)
+	require.NoError(t, err)
+	require.Equal(t, 2, n)
+
+	n, err = Migrate(session, migrate.Down, 0)
+	require.NoError(t, err)
+	require.Equal(t, 0, n)
+
+	ids := []string{}
+	err = session.Select(&ids, `SELECT id FROM gorp_migrations`)
+	require.NoError(t, err)
+	assert.Empty(t, ids)
 }
