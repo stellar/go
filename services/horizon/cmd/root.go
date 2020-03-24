5974a25d94121474d61914b590b38b8cb0ae497d
Alfonso Acosta <alfonso@stellar.org>
2020-03-24 19:35:46 +0100

    Reinstate Redis flags and mark as deprecated
    We should deprecate them first, to give room to remove them
    (in case someone is using them)

--- services/horizon/cmd/root.go
+++ services/horizon/cmd/root.go
@@ -218,6 +218,16 @@ var configOpts = support.ConfigOptions{
 		},
 		Usage: "max count of requests allowed in a one hour period, by remote ip address",
 	},
+	&support.ConfigOption{ // To be removed in Horizon 2.0.0
+		Name:    "rate-limit-redis-key",
+		OptType: types.String,
+		Usage:   "deprecated, do not use",
+	},
+	&support.ConfigOption{ // To be removed in horizon 2.0.0
+		Name:    "redis-url",
+		OptType: types.String,
+		Usage:   "deprecated, do not use",
+	},
 	&support.ConfigOption{
 		Name:           "friendbot-url",
 		ConfigKey:      &config.FriendbotURL,
