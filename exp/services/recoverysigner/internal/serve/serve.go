d561424d2e69d7b63ca9cf26288786e51545fb9f
Howard Chen <howard@stellar.org>
2020-04-07 18:22:45 -0400

    exp/recoverysigner: add GET /accounts/{address} endpoint (#2463)
    What
    This PR adds GET /accounts/{address} endpoint specified in the spec. This PR also adds notPermitted error for the case where a user is authenticated, but her credential does not match any of the identities in the requested account.
    
    Why
    This PR implements the missing endpoint.
    
    Known limitations
    N/A

--- exp/services/recoverysigner/internal/serve/serve.go
+++ exp/services/recoverysigner/internal/serve/serve.go
@@ -122,7 +122,11 @@ func handler(deps handlerDeps) http.Handler {
 				AccountStore:   deps.AccountStore,
 			}.ServeHTTP)
 			// TODO: mux.Put("/", accountPutHandler{}.ServeHTTP)
-			// TODO: mux.Get("/", accountGetHandler{}.ServeHTTP)
+			mux.Get("/", accountGetHandler{
+				Logger:         deps.Logger,
+				SigningAddress: deps.SigningKey.FromAddress(),
+				AccountStore:   deps.AccountStore,
+			}.ServeHTTP)
 			// TODO: mux.Delete("/", accountDeleteHandler{}.ServeHTTP)
 			mux.Post("/sign", accountSignHandler{
 				Logger:            deps.Logger,
