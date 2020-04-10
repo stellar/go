9bdc421e7239dc2e31bf8c4d2d4fcf8a370e9396
Leigh McCulloch <leigh@stellar.org>
2020-04-10 14:56:19 -0700

    exp/services/recoverysigner: add JWT exp, iat as required

--- exp/services/recoverysigner/internal/serve/auth/sep10.go
+++ exp/services/recoverysigner/internal/serve/auth/sep10.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"net/http"
 	"time"
 
@@ -31,8 +32,13 @@ type sep10JWTClaims struct {
 }
 
 func (c sep10JWTClaims) Validate() error {
-	// TODO: Verify that iat and exp are present.
-	// TODO: Verify that sub is a G... strkey.
+	if c.Claims.IssuedAt == nil {
+		return errors.New("validation failed, no issued at (iat) in token")
+	}
+	if c.Claims.Expiry == nil {
+		return errors.New("validation failed, no expiry (exp) in token")
+	}
+	// TODO: Verify that sub is a G... strkey
 	// TODO: Verify that iss is as expected.
 	return c.Claims.Validate(jwt.Expected{Time: time.Now()})
 }
