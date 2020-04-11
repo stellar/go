fac6698bf69fab38e17391ae7e27676bbde790c2
Howard Chen <howard@chain.com>
2020-04-10 21:16:15 -0400

    add Update to Store interface

--- exp/services/recoverysigner/internal/account/store.go
+++ exp/services/recoverysigner/internal/account/store.go
@@ -6,6 +6,7 @@ type Store interface {
 	Add(a Account) error
 	Delete(address string) error
 	Get(address string) (Account, error)
+	Update(a Account) error
 	FindWithIdentityAddress(address string) ([]Account, error)
 	FindWithIdentityPhoneNumber(phoneNumber string) ([]Account, error)
 	FindWithIdentityEmail(email string) ([]Account, error)
