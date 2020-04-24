00bbdc124f202b9a7ce812dd63b61179274256bc
Leigh McCulloch <leigh@stellar.org>
2020-04-24 16:10:56 -0700

    services/{bridge,compliance}: delete
    ### What
    Delete the `bridge` and `compliance` services, and the shared internal package used only by them.
    
    ### Why
    The bridge and compliance services were built to facilitate [SEP-3]. The SEP was deprecated in December 2019. We don't seem to be making changes to bridge and compliance and from what I can tell they aren't being actively supported anymore.
    
    We're making changes to the SDKs in the monorepo and bridge and
    compliance both need updating, but that effort seems unnecessary
    given the services aren't otherwise being maintained.

--- services/internal/bridge-compliance-shared/protocols/compliance/callback.go
+++
@@ -1,8 +0,0 @@
-package compliance
-
-// CallbackResponse is a response from Sanctions and AskUser callbacks when they return 202 Accepted or 400 Bad Requests statuses
-type CallbackResponse struct {
-	// Estimated number of seconds utill the sender can check back for a change in status.
-	Pending int    `json:"pending"`
-	Error   string `json:"error"`
-}
