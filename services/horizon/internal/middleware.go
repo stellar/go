8db499bf0cca3f89bacc15e553378bea3603a27d
Adolfo Builes <builes.adolfo@gmail.com>
2020-05-19 11:13:13 -0500

    Add checkHistoryStaleMiddleware.

--- services/horizon/internal/middleware.go
+++ services/horizon/internal/middleware.go
@@ -17,6 +17,7 @@ import (
 	"github.com/stellar/go/services/horizon/internal/expingest"
 	"github.com/stellar/go/services/horizon/internal/hchi"
 	"github.com/stellar/go/services/horizon/internal/httpx"
+	"github.com/stellar/go/services/horizon/internal/ledger"
 	"github.com/stellar/go/services/horizon/internal/render"
 	hProblem "github.com/stellar/go/services/horizon/internal/render/problem"
 	"github.com/stellar/go/support/db"
@@ -238,6 +239,30 @@ func requestMetricsMiddleware(h http.Handler) http.Handler {
 			app.web.failureMeter.Mark(1)
 		}
 	})
+}
+
+// checkHistoryStaleMiddleware ensures the difference between latest core ledger and
+// latest history ledger is not higher than the given threshold
+func checkHistoryStaleMiddleware(staleThreshold int32) func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if staleThreshold > 0 {
+				ls := ledger.CurrentState()
+				isStale := (ls.CoreLatest - ls.HistoryLatest) > int32(staleThreshold)
+				if isStale {
+					err := hProblem.StaleHistory
+					err.Extras = map[string]interface{}{
+						"history_latest_ledger": ls.HistoryLatest,
+						"core_latest_ledger":    ls.CoreLatest,
+					}
+					problem.Render(r.Context(), w, err)
+					return
+				}
+			}
+
+			h.ServeHTTP(w, r)
+		})
+	}
 }
 
 // StateMiddleware is a middleware which enables a state handler if the state
