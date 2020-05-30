b84a859056ca0597401c1de12918cbb26432dcdc
Tamir Sen <tamir@stellar.org>
2020-05-30 13:12:55 +0200

    Fix indexes

--- services/horizon/internal/db2/schema/migrations/36_deleted_offers.sql
+++ services/horizon/internal/db2/schema/migrations/36_deleted_offers.sql
@@ -3,22 +3,20 @@
 ALTER TABLE offers ADD deleted boolean DEFAULT false;
 
 CREATE INDEX best_offer ON offers USING BTREE (selling_asset, buying_asset, deleted, price);
-CREATE INDEX live_offers ON offers USING BTREE (deleted);
+CREATE INDEX live_offers ON offers USING BTREE (deleted, last_modified_ledger);
 
-DROP INDEX offers_by_seller, offers_by_selling_asset, offers_by_buying_asset, offers_by_last_modified_ledger;
+DROP INDEX offers_by_seller, offers_by_selling_asset, offers_by_buying_asset;
 
 CREATE INDEX offers_by_seller ON offers USING BTREE(seller_id, deleted);
 CREATE INDEX offers_by_selling_asset ON offers USING BTREE(selling_asset, deleted);
 CREATE INDEX offers_by_buying_asset ON offers USING BTREE(buying_asset, deleted);
-CREATE INDEX offers_by_last_modified_ledger ON offers USING BTREE(last_modified_ledger, deleted);
 
 -- +migrate Down
 
-DROP INDEX offers_by_seller, offers_by_selling_asset, offers_by_buying_asset, offers_by_last_modified_ledger;
+DROP INDEX offers_by_seller, offers_by_selling_asset, offers_by_buying_asset;
 
 ALTER TABLE offers DROP COLUMN deleted;
 
 CREATE INDEX offers_by_seller ON offers USING BTREE(seller_id);
 CREATE INDEX offers_by_selling_asset ON offers USING BTREE(selling_asset);
 CREATE INDEX offers_by_buying_asset ON offers USING BTREE(buying_asset);
-CREATE INDEX offers_by_last_modified_ledger ON offers USING BTREE(last_modified_ledger);
