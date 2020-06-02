9a400f83b8e7724a413de19634d117f8754a9d04
Howard Chen <howard@stellar.org>
2020-06-02 15:54:57 -0400

    rename and add encryption_keyid

--- exp/services/recoverysigner/internal/db/dbmigrate/migrations/20200601000000-create-signing-keys.sql
+++ exp/services/recoverysigner/internal/db/dbmigrate/migrations/20200601000000-create-signing-keys.sql
@@ -4,15 +4,16 @@ CREATE TABLE signing_keys (
   account_id BIGINT NOT NULL REFERENCES accounts (id) ON DELETE CASCADE,
   id BIGINT NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 
-  key TEXT NOT NULL,
-  encrypted_seed BYTEA NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
 
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+  public_key TEXT NOT NULL,
+  encrypted_secret_key BYTEA NOT NULL,
+  encryption_keyid TEXT NOT NULL
 );
 
 
 CREATE INDEX ON signing_keys (account_id);
-CREATE INDEX ON signing_keys (key);
+CREATE INDEX ON signing_keys (public_key);
 
 -- +migrate Down
 
