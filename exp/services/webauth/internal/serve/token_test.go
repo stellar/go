f36941341a926eb16275283c6036fb95bd9efda2
Leigh McCulloch <leigh@stellar.org>
2020-06-05 20:19:50 -0700

    Rewrite test so that it tests both server keys

--- exp/services/webauth/internal/serve/token_test.go
+++ exp/services/webauth/internal/serve/token_test.go
@@ -227,11 +227,17 @@ func TestToken_jsonInputSuccess(t *testing.T) {
 	assert.Equal(t, exp.Sub(iat), time.Minute)
 }
 
+// This test ensures that when multiple server keys are configured on the
+// server that a challenge transaction is accepted if it was signed with either
+// key, along with the accounts signing keys.
 func TestToken_jsonInputValidRotatingServerSigners(t *testing.T) {
-	serverKey1 := keypair.MustRandom()
-	t.Logf("Server signing key 1: %s", serverKey1.Address())
-	serverKey2 := keypair.MustRandom()
-	t.Logf("Server signing key 2: %s", serverKey2.Address())
+	serverKeys := []*keypair.Full{keypair.MustRandom(), keypair.MustRandom()}
+	serverKeyAddresses := []*keypair.FromAddress{}
+	for i, serverKey := range serverKeys {
+		serverKeyAddress := serverKey.FromAddress()
+		serverKeyAddresses = append(serverKeyAddresses, serverKeyAddress)
+		t.Logf("Server signing key %d: %v", i, serverKeyAddress)
+	}
 
 	jwtPrivateKey, err := jwtkey.GenerateKey()
 	require.NoError(t, err)
@@ -245,25 +251,6 @@ func TestToken_jsonInputValidRotatingServerSigners(t *testing.T) {
 
 	accountSigner2 := keypair.MustRandom()
 	t.Logf("Client account signer 2: %s", accountSigner2.Address())
-
-	tx, err := txnbuild.BuildChallengeTx(
-		serverKey2.Seed(),
-		account.Address(),
-		"testserver",
-		network.TestNetworkPassphrase,
-		time.Minute,
-	)
-	require.NoError(t, err)
-
-	chTx, err := tx.Base64()
-	require.NoError(t, err)
-	t.Logf("Tx: %s", chTx)
-
-	tx, err = tx.Sign(network.TestNetworkPassphrase, accountSigner1, accountSigner2)
-	require.NoError(t, err)
-	txSigned, err := tx.Base64()
-	require.NoError(t, err)
-	t.Logf("Signed: %s", txSigned)
 
 	horizonClient := &horizonclient.MockClient{}
 	horizonClient.
@@ -292,53 +279,81 @@ func TestToken_jsonInputValidRotatingServerSigners(t *testing.T) {
 		Logger:            supportlog.DefaultLogger,
 		HorizonClient:     horizonClient,
 		NetworkPassphrase: network.TestNetworkPassphrase,
-		SigningAddresses:  []*keypair.FromAddress{serverKey1.FromAddress(), serverKey2.FromAddress()},
+		SigningAddresses:  serverKeyAddresses,
 		JWK:               jwk,
 		JWTIssuer:         "https://example.com",
 		JWTExpiresIn:      time.Minute,
 	}
 
-	body := struct {
-		Transaction string `json:"transaction"`
-	}{
-		Transaction: txSigned,
+	for i, serverKey := range serverKeys {
+		t.Run(fmt.Sprintf("signed with server key %d", i), func(t *testing.T) {
+			// Build challenge transaction using one server signing key
+			tx, err := txnbuild.BuildChallengeTx(
+				serverKey.Seed(),
+				account.Address(),
+				"testserver",
+				network.TestNetworkPassphrase,
+				time.Minute,
+			)
+			require.NoError(t, err)
+
+			// Sign the challenge transaction with the accounts signers
+			chTx, err := tx.Base64()
+			require.NoError(t, err)
+			t.Logf("Tx: %s", chTx)
+
+			tx, err = tx.Sign(network.TestNetworkPassphrase, accountSigner1, accountSigner2)
+			require.NoError(t, err)
+			txSigned, err := tx.Base64()
+			require.NoError(t, err)
+			t.Logf("Signed: %s", txSigned)
+
+			// Post the signed challenge transaction back to the server's token endpoint
+			body := struct {
+				Transaction string `json:"transaction"`
+			}{
+				Transaction: txSigned,
+			}
+			bodyBytes, err := json.Marshal(body)
+			require.NoError(t, err)
+			r := httptest.NewRequest("POST", "/", bytes.NewReader(bodyBytes))
+			r.Header.Set("Content-Type", "application/json")
+			w := httptest.NewRecorder()
+			h.ServeHTTP(w, r)
+			resp := w.Result()
+
+			// Check that we get back an ok response
+			require.Equal(t, http.StatusOK, resp.StatusCode)
+			assert.Equal(t, "application/json; charset=utf-8", resp.Header.Get("Content-Type"))
+
+			// Check that we get back the valid JWT token
+			res := struct {
+				Token string `json:"token"`
+			}{}
+			err = json.NewDecoder(resp.Body).Decode(&res)
+			require.NoError(t, err)
+
+			t.Logf("JWT: %s", res.Token)
+
+			token, err := jwt.Parse(res.Token, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+				}
+				return &jwtPrivateKey.PublicKey, nil
+			})
+			require.NoError(t, err)
+
+			claims := token.Claims.(jwt.MapClaims)
+			assert.Equal(t, "https://example.com", claims["iss"])
+			assert.Equal(t, account.Address(), claims["sub"])
+			iat := time.Unix(int64(claims["iat"].(float64)), 0)
+			exp := time.Unix(int64(claims["exp"].(float64)), 0)
+			assert.True(t, iat.Before(time.Now()))
+			assert.True(t, exp.After(time.Now()))
+			assert.True(t, time.Now().Add(time.Minute).After(exp))
+			assert.Equal(t, exp.Sub(iat), time.Minute)
+		})
 	}
-	bodyBytes, err := json.Marshal(body)
-	require.NoError(t, err)
-	r := httptest.NewRequest("POST", "/", bytes.NewReader(bodyBytes))
-	r.Header.Set("Content-Type", "application/json")
-	w := httptest.NewRecorder()
-	h.ServeHTTP(w, r)
-	resp := w.Result()
-
-	require.Equal(t, http.StatusOK, resp.StatusCode)
-	assert.Equal(t, "application/json; charset=utf-8", resp.Header.Get("Content-Type"))
-
-	res := struct {
-		Token string `json:"token"`
-	}{}
-	err = json.NewDecoder(resp.Body).Decode(&res)
-	require.NoError(t, err)
-
-	t.Logf("JWT: %s", res.Token)
-
-	token, err := jwt.Parse(res.Token, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return &jwtPrivateKey.PublicKey, nil
-	})
-	require.NoError(t, err)
-
-	claims := token.Claims.(jwt.MapClaims)
-	assert.Equal(t, "https://example.com", claims["iss"])
-	assert.Equal(t, account.Address(), claims["sub"])
-	iat := time.Unix(int64(claims["iat"].(float64)), 0)
-	exp := time.Unix(int64(claims["exp"].(float64)), 0)
-	assert.True(t, iat.Before(time.Now()))
-	assert.True(t, exp.After(time.Now()))
-	assert.True(t, time.Now().Add(time.Minute).After(exp))
-	assert.Equal(t, exp.Sub(iat), time.Minute)
 }
 
 func TestToken_jsonInputValidMultipleSigners(t *testing.T) {
