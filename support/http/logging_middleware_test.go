8beeb650444b87f1c8d039a0a051c01cd273acc3
Leigh McCulloch <leigh@stellar.org>
2020-06-08 14:36:08 -0700

    Add test to ensure it works okay with non-chi handlers

--- support/http/logging_middleware_test.go
+++ support/http/logging_middleware_test.go
@@ -82,6 +82,73 @@ func TestHTTPMiddleware(t *testing.T) {
 	}
 }
 
+func TestHTTPMiddleware_stdlibServeMux(t *testing.T) {
+	done := log.DefaultLogger.StartTest(log.InfoLevel)
+
+	mux := stdhttp.ServeMux{}
+	mux.Handle(
+		"/path/1234",
+		middleware.RequestID(
+			LoggingMiddleware(
+				stdhttp.HandlerFunc(func(w stdhttp.ResponseWriter, r *stdhttp.Request) {
+					log.Ctx(r.Context()).Info("handler log line")
+				}),
+			),
+		),
+	)
+	mux.Handle(
+		"/not_found",
+		middleware.RequestID(
+			LoggingMiddleware(
+				stdhttp.NotFoundHandler(),
+			),
+		),
+	)
+
+	src := httptest.NewServer(t, &mux)
+	src.GET("/path/1234").Expect().Status(stdhttp.StatusOK)
+	src.GET("/not_found").Expect().Status(stdhttp.StatusNotFound)
+	src.GET("/really_not_found").Expect().Status(stdhttp.StatusNotFound)
+
+	// get the log buffer and ensure it has both the start and end log lines for
+	// each request
+	logged := done()
+	if assert.Len(t, logged, 5, "unexpected log line count") {
+		assert.Equal(t, "starting request", logged[0].Message)
+		assert.Equal(t, "http", logged[0].Data["subsys"])
+		assert.Equal(t, "GET", logged[0].Data["method"])
+		assert.NotEmpty(t, logged[0].Data["req"])
+		assert.Equal(t, "/path/1234", logged[0].Data["path"])
+		assert.Equal(t, "Go-http-client/1.1", logged[0].Data["useragent"])
+		req1 := logged[0].Data["req"]
+
+		assert.Equal(t, "handler log line", logged[1].Message)
+		assert.Equal(t, req1, logged[1].Data["req"])
+
+		assert.Equal(t, "finished request", logged[2].Message)
+		assert.Equal(t, "http", logged[2].Data["subsys"])
+		assert.Equal(t, "GET", logged[2].Data["method"])
+		assert.Equal(t, req1, logged[2].Data["req"])
+		assert.Equal(t, "/path/1234", logged[2].Data["path"])
+		assert.Equal(t, nil, logged[2].Data["route"])
+
+		assert.Equal(t, "starting request", logged[3].Message)
+		assert.Equal(t, "http", logged[3].Data["subsys"])
+		assert.Equal(t, "GET", logged[3].Data["method"])
+		assert.NotEmpty(t, logged[3].Data["req"])
+		assert.NotEmpty(t, logged[3].Data["path"])
+		assert.Equal(t, "Go-http-client/1.1", logged[3].Data["useragent"])
+		req2 := logged[3].Data["req"]
+
+		assert.Equal(t, "finished request", logged[4].Message)
+		assert.Equal(t, "http", logged[4].Data["subsys"])
+		assert.Equal(t, "GET", logged[4].Data["method"])
+		assert.Equal(t, req2, logged[4].Data["req"])
+		assert.Equal(t, "/not_found", logged[4].Data["path"])
+		assert.Equal(t, nil, logged[4].Data["route"])
+	}
+}
+
 func TestHTTPMiddlewareWithLoggerSet(t *testing.T) {
 	logger := log.New()
 	done := logger.StartTest(log.InfoLevel)
