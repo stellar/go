f67623aa5ed9d64afe7a38b0f8aab6096b4eb392
Bartek Nowotarski <bartek@nowotarski.info>
2020-06-11 19:55:10 +0200

    exp/ingest/io: Refactor readers (#2644)
    * Rename `DBLedgerReader` to `LedgerTransactionReader` because it works
    with any ledger backend.
    * `LedgerTransactionReader` gets `LedgerCloseMeta` from a provided
    ledger backend.
    * `LedgerChangeReader` has been refactored and now embeds
    `LedgerTransactionReader`.
    * Updated `GetChangesFromLedgerEntryChanges` to be a public function.
    * Removed `ctx.Context` from readers because it's not idiomatic.
    * Removed unused interfaces and mocks.
    
    Part of #2187.

--- exp/ingest/io/ledger_transaction.go
+++ exp/ingest/io/ledger_transaction.go
@@ -24,7 +24,7 @@ func (t *LedgerTransaction) txInternalError() bool {
 // GetFeeChanges returns a developer friendly representation of LedgerEntryChanges
 // connected to fees.
 func (t *LedgerTransaction) GetFeeChanges() []Change {
-	return getChangesFromLedgerEntryChanges(t.FeeChanges)
+	return GetChangesFromLedgerEntryChanges(t.FeeChanges)
 }
 
 // GetChanges returns a developer friendly representation of LedgerEntryChanges.
@@ -40,7 +40,7 @@ func (t *LedgerTransaction) GetChanges() ([]Change, error) {
 		return changes, errors.New("TransactionMeta.V=0 not supported")
 	case 1:
 		v1Meta := t.Meta.MustV1()
-		txChanges := getChangesFromLedgerEntryChanges(v1Meta.TxChanges)
+		txChanges := GetChangesFromLedgerEntryChanges(v1Meta.TxChanges)
 		changes = append(changes, txChanges...)
 
 		// Ignore operations meta if txInternalError https://github.com/stellar/go/issues/2111
@@ -49,7 +49,7 @@ func (t *LedgerTransaction) GetChanges() ([]Change, error) {
 		}
 
 		for _, operationMeta := range v1Meta.Operations {
-			opChanges := getChangesFromLedgerEntryChanges(
+			opChanges := GetChangesFromLedgerEntryChanges(
 				operationMeta.Changes,
 			)
 			changes = append(changes, opChanges...)
@@ -57,7 +57,7 @@ func (t *LedgerTransaction) GetChanges() ([]Change, error) {
 
 	case 2:
 		v2Meta := t.Meta.MustV2()
-		txChangesBefore := getChangesFromLedgerEntryChanges(v2Meta.TxChangesBefore)
+		txChangesBefore := GetChangesFromLedgerEntryChanges(v2Meta.TxChangesBefore)
 		changes = append(changes, txChangesBefore...)
 
 		// Ignore operations meta and txChangesAfter if txInternalError
@@ -67,13 +67,13 @@ func (t *LedgerTransaction) GetChanges() ([]Change, error) {
 		}
 
 		for _, operationMeta := range v2Meta.Operations {
-			opChanges := getChangesFromLedgerEntryChanges(
+			opChanges := GetChangesFromLedgerEntryChanges(
 				operationMeta.Changes,
 			)
 			changes = append(changes, opChanges...)
 		}
 
-		txChangesAfter := getChangesFromLedgerEntryChanges(v2Meta.TxChangesAfter)
+		txChangesAfter := GetChangesFromLedgerEntryChanges(v2Meta.TxChangesAfter)
 		changes = append(changes, txChangesAfter...)
 	default:
 		return changes, errors.New("Unsupported TransactionMeta version")
@@ -120,55 +120,7 @@ func operationChanges(ops []xdr.OperationMeta, index uint32) []Change {
 	}
 
 	operationMeta := ops[index]
-	return getChangesFromLedgerEntryChanges(
+	return GetChangesFromLedgerEntryChanges(
 		operationMeta.Changes,
 	)
-}
-
-// getChangesFromLedgerEntryChanges transforms LedgerEntryChanges to []Change.
-// Each `update` and `removed` is preceded with `state` and `create` changes
-// are alone, without `state`. The transformation we're doing is to move each
-// change (state/update, state/removed or create) to an array of pre/post pairs.
-// Then:
-// - for create, pre is null and post is a new entry,
-// - for update, pre is previous state and post is the current state,
-// - for removed, pre is previous state and post is null.
-//
-// stellar-core source:
-// https://github.com/stellar/stellar-core/blob/e584b43/src/ledger/LedgerTxn.cpp#L582
-func getChangesFromLedgerEntryChanges(ledgerEntryChanges xdr.LedgerEntryChanges) []Change {
-	changes := []Change{}
-
-	for i, entryChange := range ledgerEntryChanges {
-		switch entryChange.Type {
-		case xdr.LedgerEntryChangeTypeLedgerEntryCreated:
-			created := entryChange.MustCreated()
-			changes = append(changes, Change{
-				Type: created.Data.Type,
-				Pre:  nil,
-				Post: &created,
-			})
-		case xdr.LedgerEntryChangeTypeLedgerEntryUpdated:
-			state := ledgerEntryChanges[i-1].MustState()
-			updated := entryChange.MustUpdated()
-			changes = append(changes, Change{
-				Type: state.Data.Type,
-				Pre:  &state,
-				Post: &updated,
-			})
-		case xdr.LedgerEntryChangeTypeLedgerEntryRemoved:
-			state := ledgerEntryChanges[i-1].MustState()
-			changes = append(changes, Change{
-				Type: state.Data.Type,
-				Pre:  &state,
-				Post: nil,
-			})
-		case xdr.LedgerEntryChangeTypeLedgerEntryState:
-			continue
-		default:
-			panic("Invalid LedgerEntryChangeType")
-		}
-	}
-
-	return changes
 }
