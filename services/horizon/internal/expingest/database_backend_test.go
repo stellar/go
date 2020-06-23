e2f15e43b2aa41b7da7de86570171ba12b928240
Adolfo Builes <builes.adolfo@gmail.com>
2020-06-23 12:00:10 -0500

    Remove db2/core.

--- services/horizon/internal/expingest/database_backend_test.go
+++ services/horizon/internal/expingest/database_backend_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/stellar/go/exp/ingest/ledgerbackend"
-	"github.com/stellar/go/services/horizon/internal/db2/core"
 	"github.com/stellar/go/services/horizon/internal/test"
 )
 
@@ -22,12 +21,11 @@ func TestGetLatestLedger(t *testing.T) {
 func TestGetLatestLedgerNotFound(t *testing.T) {
 	tt := test.Start(t).ScenarioWithoutHorizon("base")
 	defer tt.Finish()
-	q := &core.Q{tt.CoreSession()}
 
 	_, err := tt.CoreDB.Exec(`DELETE FROM ledgerheaders`)
 	tt.Assert.NoError(err, "failed to remove ledgerheaders")
 
-	backend, err := ledgerbackend.NewDatabaseBackendFromSession(q.Session)
+	backend, err := ledgerbackend.NewDatabaseBackendFromSession(tt.CoreSession())
 	tt.Assert.NoError(err)
 	_, err = backend.GetLatestLedgerSequence()
 	tt.Assert.EqualError(err, "no ledgers exist in ledgerheaders table")
