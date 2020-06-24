295a506f48432c5eac3f5ab9001dea608cdc3ad3
Alfonso Acosta <alfonso@stellar.org>
2020-06-24 15:11:54 +0200

    Revert "Remove job size (the larger, the faster it is)"
    This reverts commit 2cd8e4ee41623150b3a631c2f3df766df1428bd2.

--- services/horizon/cmd/db.go
+++ services/horizon/cmd/db.go
@@ -126,6 +126,7 @@ var dbReingestCmd = &cobra.Command{
 var (
 	reingestForce   bool
 	parallelWorkers uint
+	parallelJobSize uint32
 )
 var reingestRangeCmdOpts = []*support.ConfigOption{
 	{
@@ -144,6 +145,14 @@ var reingestRangeCmdOpts = []*support.ConfigOption{
 		Required:    false,
 		FlagDefault: uint(1),
 		Usage:       "[optional] if this flag is set to > 1, horizon will parallelize reingestion using the supplied number of workers",
+	},
+	{
+		Name:        "parallel-job-size",
+		ConfigKey:   &parallelJobSize,
+		OptType:     types.Uint32,
+		Required:    false,
+		FlagDefault: uint32(256),
+		Usage:       "[optional] parallel workers will run jobs processing ledger batches of the supplied size",
 	},
 }
 
@@ -218,7 +227,7 @@ var dbReingestRangeCmd = &cobra.Command{
 			err = system.ReingestRange(
 				argsInt32[0],
 				argsInt32[1],
-				argsInt32[1]-argsInt32[0],
+				parallelJobSize,
 			)
 		}
 
