51656b3c9a6f376ee5c32798f9dd3afcc8219b51
Howard Tinghao Chen <howard@stellar.org>
2020-06-25 19:38:29 -0400

    exp/services/recoverysigner: add tests for starting with a rotated keyset (#2744)
    What
    This PR adds tests to make sure that the system is able to be booted with a keyset that has been rotated, is able to do encryption and decryption correctly, even if the keyset contains the keys from different tink hybrid encryption templates.
    
    This can reassure us that if we decide to switch to ECIESHKDFAES128CTRHMACSHA256KeyTemplate, the system will continue to work and is backward compatible.
    
    This PR does a little refactoring to our test helper functions used to generate tink keyset to make it more versatile.

--- exp/services/recoverysigner/internal/crypto/keygen_test.go
+++ exp/services/recoverysigner/internal/crypto/keygen_test.go
@@ -4,14 +4,14 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/google/tink/go/hybrid"
 	"github.com/google/tink/go/insecurecleartextkeyset"
 	"github.com/google/tink/go/keyset"
+	tinkpb "github.com/google/tink/go/proto/tink_go_proto"
 	"github.com/stretchr/testify/require"
 )
 
-func generateHybridKeysetCleartext(t *testing.T) string {
-	khPriv, err := keyset.NewHandle(hybrid.ECIESHKDFAES128CTRHMACSHA256KeyTemplate())
+func generateKeysetCleartext(t *testing.T, keyTemplate *tinkpb.KeyTemplate) string {
+	khPriv, err := keyset.NewHandle(keyTemplate)
 	require.NoError(t, err)
 
 	keysetPrivateCleartext := strings.Builder{}
@@ -21,8 +21,44 @@ func generateHybridKeysetCleartext(t *testing.T) string {
 	return keysetPrivateCleartext.String()
 }
 
-func generateHybridKeysetEncrypted(t *testing.T) string {
-	khPriv, err := keyset.NewHandle(hybrid.ECIESHKDFAES128CTRHMACSHA256KeyTemplate())
+func generateKeysetEncrypted(t *testing.T, keyTemplate *tinkpb.KeyTemplate) string {
+	khPriv, err := keyset.NewHandle(keyTemplate)
+	require.NoError(t, err)
+
+	keysetPrivateEncrypted := strings.Builder{}
+	err = khPriv.Write(keyset.NewJSONWriter(&keysetPrivateEncrypted), mockAEAD{})
+	require.NoError(t, err)
+
+	return keysetPrivateEncrypted.String()
+}
+
+func rotateKeysetCleartext(t *testing.T, keysetCleartextJSON string, keyTemplate *tinkpb.KeyTemplate) string {
+	khPriv, err := insecurecleartextkeyset.Read(keyset.NewJSONReader(strings.NewReader(keysetCleartextJSON)))
+	require.NoError(t, err)
+
+	m := keyset.NewManagerFromHandle(khPriv)
+	err = m.Rotate(keyTemplate)
+	require.NoError(t, err)
+
+	khPriv, err = m.Handle()
+	require.NoError(t, err)
+
+	keysetPrivateCleartext := strings.Builder{}
+	err = insecurecleartextkeyset.Write(khPriv, keyset.NewJSONWriter(&keysetPrivateCleartext))
+	require.NoError(t, err)
+
+	return keysetPrivateCleartext.String()
+}
+
+func rotateKeysetEncrypted(t *testing.T, keysetEncryptedJSON string, keyTemplate *tinkpb.KeyTemplate) string {
+	khPriv, err := keyset.Read(keyset.NewJSONReader(strings.NewReader(keysetEncryptedJSON)), mockAEAD{})
+	require.NoError(t, err)
+
+	m := keyset.NewManagerFromHandle(khPriv)
+	err = m.Rotate(keyTemplate)
+	require.NoError(t, err)
+
+	khPriv, err = m.Handle()
 	require.NoError(t, err)
 
 	keysetPrivateEncrypted := strings.Builder{}
