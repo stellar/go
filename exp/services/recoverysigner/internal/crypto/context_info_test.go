2accc92e52b52eedeefa800c00f6ee9385f553c0
Leigh McCulloch <leigh@stellar.org>
2020-07-01 18:09:35 -0700

    Add context info

--- exp/services/recoverysigner/internal/crypto/context_info_test.go
+++ exp/services/recoverysigner/internal/crypto/context_info_test.go
@@ -10,7 +10,7 @@ func TestContextInfo(t *testing.T) {
 	const accountAddress = "GAEGZCXGFXBSI77L2YQ6WFGLGZYM3Q6USE2YII5JQFIWVJUGALHIYLJK"
 	const signingAddress = "GBJLCILAGXNXRU7BFNOQZG3AYN4CRSTQLKU42MLII7XJW4LR56SY6Y6T"
 
-	contextInfo := ContextInfo(accountAddress)
+	contextInfo := ContextInfo(accountAddress, signingAddress)
 
 	wantContextInfo := []byte("GAEGZCXGFXBSI77L2YQ6WFGLGZYM3Q6USE2YII5JQFIWVJUGALHIYLJK,GBJLCILAGXNXRU7BFNOQZG3AYN4CRSTQLKU42MLII7XJW4LR56SY6Y6T")
 	assert.Equal(t, wantContextInfo, contextInfo)
