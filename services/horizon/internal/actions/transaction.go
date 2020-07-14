048c61fc4eda217c0edab2a08086ac7a73917230
Adolfo Builes <builes.adolfo@gmail.com>
2020-07-14 16:42:35 -0500

    Use new actions architecture for transactions.

--- services/horizon/internal/actions/transaction.go
+++ services/horizon/internal/actions/transaction.go
@@ -2,46 +2,17 @@ package actions
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/stellar/go/protocols/horizon"
 	"github.com/stellar/go/services/horizon/internal/db2"
 	"github.com/stellar/go/services/horizon/internal/db2/history"
-	"github.com/stellar/go/services/horizon/internal/render/sse"
 	"github.com/stellar/go/services/horizon/internal/resourceadapter"
 	"github.com/stellar/go/support/errors"
 	"github.com/stellar/go/support/render/hal"
+	supportProblem "github.com/stellar/go/support/render/problem"
 	"github.com/stellar/go/xdr"
 )
-
-// TransactionPage returns a page containing the transaction records of an
-// account/ledger identified by accountID/ledgerID into a page based on pq and
-// includeFailedTx.
-func TransactionPage(ctx context.Context, hq *history.Q, accountID string, ledgerID int32, includeFailedTx bool, pq db2.PageQuery) (hal.Page, error) {
-	records, err := loadTransactionRecords(hq, accountID, ledgerID, includeFailedTx, pq)
-	if err != nil {
-		return hal.Page{}, errors.Wrap(err, "loading transaction records")
-	}
-
-	page := hal.Page{
-		Cursor: pq.Cursor,
-		Order:  pq.Order,
-		Limit:  pq.Limit,
-	}
-
-	for _, record := range records {
-		// TODO: make PopulateTransaction return horizon.Transaction directly.
-		var res horizon.Transaction
-		err = resourceadapter.PopulateTransaction(ctx, record.TransactionHash, &res, record)
-		if err != nil {
-			return hal.Page{}, errors.Wrap(err, "could not populate transaction")
-		}
-		page.Add(res)
-	}
-
-	page.FullURL = FullURL(ctx)
-	page.PopulateLinks()
-	return page, nil
-}
 
 // loadTransactionRecords returns a slice of transaction records of an
 // account/ledger identified by accountID/ledgerID based on pq and
@@ -91,28 +62,6 @@ func loadTransactionRecords(hq *history.Q, accountID string, ledgerID int32, inc
 	return records, nil
 }
 
-// StreamTransactions streams transaction records of an account/ledger
-// identified by accountID/ledgerID based on pq and includeFailedTx.
-func StreamTransactions(ctx context.Context, s *sse.Stream, hq *history.Q, accountID string, ledgerID int32, includeFailedTx bool, pq db2.PageQuery) error {
-	allRecords, err := loadTransactionRecords(hq, accountID, ledgerID, includeFailedTx, pq)
-	if err != nil {
-		return errors.Wrap(err, "loading transaction records")
-	}
-
-	s.SetLimit(int(pq.Limit))
-	records := allRecords[s.SentCount():]
-	for _, record := range records {
-		var res horizon.Transaction
-		err = resourceadapter.PopulateTransaction(ctx, record.TransactionHash, &res, record)
-		if err != nil {
-			return errors.Wrap(err, "could not populate transaction")
-		}
-		s.Send(sse.Event{ID: res.PagingToken(), Data: res})
-	}
-
-	return nil
-}
-
 // TransactionResource returns a single transaction resource identified by txHash.
 func TransactionResource(ctx context.Context, hq *history.Q, txHash string) (horizon.Transaction, error) {
 	var (
@@ -128,4 +77,80 @@ func TransactionResource(ctx context.Context, hq *history.Q, txHash string) (hor
 		return resource, errors.Wrap(err, "could not populate transaction")
 	}
 	return resource, nil
+}
+
+// TransactionsQuery query struct for transactions end-points
+type TransactionsQuery struct {
+	AccountID                 string `schema:"account_id" valid:"accountID,optional"`
+	IncludeFailedTransactions bool   `schema:"include_failed" valid:"-"`
+	LedgerID                  uint32 `schema:"ledger_id" valid:"-"`
+}
+
+// Validate runs extra validations on query parameters
+func (qp TransactionsQuery) Validate() error {
+	filters, err := countNonEmpty(
+		qp.AccountID,
+		qp.LedgerID,
+	)
+
+	if err != nil {
+		return supportProblem.BadRequest
+	}
+
+	if filters > 1 {
+		return supportProblem.MakeInvalidFieldProblem(
+			"filters",
+			errors.New("Use a single filter for transaction, you can only use one of account_id or ledger_id"),
+		)
+	}
+
+	return nil
+}
+
+// GetTransactionsHandler is the action handler for all end-points returning a list of transactions.
+type GetTransactionsHandler struct {
+}
+
+// GetResourcePage returns a page of operations.
+func (handler GetTransactionsHandler) GetResourcePage(w HeaderWriter, r *http.Request) ([]hal.Pageable, error) {
+	ctx := r.Context()
+
+	pq, err := GetPageQuery(r)
+	if err != nil {
+		return nil, err
+	}
+
+	err = ValidateCursorWithinHistory(pq)
+	if err != nil {
+		return nil, err
+	}
+
+	qp := TransactionsQuery{}
+	err = GetParams(&qp, r)
+	if err != nil {
+		return nil, err
+	}
+
+	historyQ, err := HistoryQFromRequest(r)
+	if err != nil {
+		return nil, err
+	}
+
+	records, err := loadTransactionRecords(historyQ, qp.AccountID, int32(qp.LedgerID), qp.IncludeFailedTransactions, pq)
+	if err != nil {
+		return nil, errors.Wrap(err, "loading transaction records")
+	}
+
+	var response []hal.Pageable
+
+	for _, record := range records {
+		var res horizon.Transaction
+		err = resourceadapter.PopulateTransaction(ctx, record.TransactionHash, &res, record)
+		if err != nil {
+			return nil, errors.Wrap(err, "could not populate transaction")
+		}
+		response = append(response, res)
+	}
+
+	return response, nil
 }
