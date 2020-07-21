b057c50182ccddd28da4f4b6c7936fee0f41f297
Tamir Sen <tamir@stellar.org>
2020-07-21 10:31:18 +0200

    Get rid of NotImplemented, NotFound, and RateLimitExceeded actions

--- services/horizon/internal/actions.go
+++ services/horizon/internal/actions.go
@@ -4,24 +4,6 @@ import (
 	"net/http"
 )
 
-func (action NotFoundAction) Handle(w http.ResponseWriter, r *http.Request) {
-	ap := &action.Action
-	ap.Prepare(w, r)
-	ap.Execute(&action)
-}
-
-func (action NotImplementedAction) Handle(w http.ResponseWriter, r *http.Request) {
-	ap := &action.Action
-	ap.Prepare(w, r)
-	ap.Execute(&action)
-}
-
-func (action RateLimitExceededAction) Handle(w http.ResponseWriter, r *http.Request) {
-	ap := &action.Action
-	ap.Prepare(w, r)
-	ap.Execute(&action)
-}
-
 func (action RootAction) Handle(w http.ResponseWriter, r *http.Request) {
 	ap := &action.Action
 	ap.Prepare(w, r)
