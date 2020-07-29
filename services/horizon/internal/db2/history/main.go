c8c60f4ee409d5a9411cfb3dbdae3ee050d00165
Bartek Nowotarski <bartek@nowotarski.info>
2020-07-29 20:10:55 +0200

    services/horizon/db2/history: Improve /trades DB queries performance

--- services/horizon/internal/db2/history/main.go
+++ services/horizon/internal/db2/history/main.go
@@ -547,9 +547,19 @@ type Trade struct {
 // TradesQ is a helper struct to aid in configuring queries that loads
 // slices of trade structs.
 type TradesQ struct {
-	Err    error
-	parent *Q
-	sql    sq.SelectBuilder
+	Err        error
+	parent     *Q
+	sql        sq.SelectBuilder
+	pageCalled bool
+
+	// For queries for account and offer we construct UNION query. The alternative
+	// is to use (base = X OR counter = X) query but it's costly.
+	forAccountID int64
+	forOfferID   int64
+
+	// rawSQL will be executed if present (instead of sql - sq.SelectBuilder).
+	rawSQL  string
+	rawArgs []interface{}
 }
 
 // Transaction is a row of data from the `history_transactions` table
