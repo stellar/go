1c3f371c0cb404666be6c80a79366b31a2f3cf14
Adolfo Builes <builes.adolfo@gmail.com>
2020-08-06 20:25:35 -0500

    Implement ClaimableBalancesBatchInsertBuilder.

--- services/horizon/internal/db2/history/claimable_balances.go
+++ services/horizon/internal/db2/history/claimable_balances.go
@@ -1,18 +1,24 @@
 package history
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+
+	sq "github.com/Masterminds/squirrel"
 	"github.com/guregu/null"
+	"github.com/stellar/go/support/db"
+	"github.com/stellar/go/support/errors"
 	"github.com/stellar/go/xdr"
 )
 
 // ClaimableBalance is a row of data from the `claimable_balances` table.
 type ClaimableBalance struct {
-	ID                 string                 `db:"id"`
-	BalanceID          xdr.ClaimableBalanceId `db:"balance_id"`
-	Asset              xdr.Asset              `db:"asset"`
-	Amount             xdr.Int64              `db:"amount"`
-	Sponsor            null.String            `db:"sponsor"`
-	LastModifiedLedger uint32                 `db:"last_modified_ledger"`
+	ID                 string      `db:"id"`
+	BalanceID          string      `db:"balance_id"` // this should be an xdr.BalanceID
+	Asset              xdr.Asset   `db:"asset"`
+	Amount             xdr.Int64   `db:"amount"`
+	Sponsor            null.String `db:"sponsor"`
+	LastModifiedLedger uint32      `db:"last_modified_ledger"`
 }
 
 // Claimant is a row of data from the `claimable_balances_claimants` table
@@ -33,3 +39,56 @@ type QClaimableBalances interface {
 	UpdateClaimableBalance(entry *xdr.LedgerEntry) (int64, error)
 	RemoveClaimableBalance(key xdr.LedgerKeyClaimableBalance) (int64, error)
 }
+
+// NewClaimableBalancesBatchInsertBuilder constructs a new ClaimableBalancesBatchInsertBuilder instance
+func (q *Q) NewClaimableBalancesBatchInsertBuilder(maxBatchSize int) ClaimableBalancesBatchInsertBuilder {
+	return &claimableBalancesBatchInsertBuilder{
+		builder: db.BatchInsertBuilder{
+			Table:        q.GetTable("claimable_balances"),
+			MaxBatchSize: maxBatchSize,
+		},
+	}
+}
+
+type claimableBalancesBatchInsertBuilder struct {
+	builder db.BatchInsertBuilder
+}
+
+func balanceIDToHex(balanceID xdr.ClaimableBalanceId) (string, error) {
+	b, err := balanceID.MarshalBinary()
+	if err != nil {
+		return "", err
+	}
+	sum := sha256.Sum256(b)
+	return hex.EncodeToString(sum[:]), nil
+}
+
+func (i *claimableBalancesBatchInsertBuilder) Add(entry *xdr.LedgerEntry) error {
+	cBalance := entry.Data.MustClaimableBalance()
+	id, err := balanceIDToHex(cBalance.BalanceId)
+	if err != nil {
+		return errors.Wrap(err, "encoding balanceID")
+	}
+	row := ClaimableBalance{
+		ID:                 id,
+		BalanceID:          id[:48], // temporary solution while we can store the actual xdr
+		Asset:              cBalance.Asset,
+		Amount:             cBalance.Amount,
+		Sponsor:            null.StringFromPtr(nil), // TDB - we can add this later since there might be code from Bartek's PR which we can use to pull the sponsor,
+		LastModifiedLedger: uint32(entry.LastModifiedLedgerSeq),
+	}
+	return i.builder.RowStruct(row)
+}
+
+func (i *claimableBalancesBatchInsertBuilder) Exec() error {
+	return i.builder.Exec()
+}
+
+var selectClaimableBalances = sq.Select(
+	"cb.id, " +
+		"cb.balance_id, " +
+		"cb.asset, " +
+		"cb.amount, " +
+		"cb.sponsor, " +
+		"cb.last_modified_ledger").
+	From("claimable_balances cb")
---
+++ services/horizon/internal/db2/history/claimable_balances_batch_insert_builder_test.go
@@ -0,0 +1,58 @@
+package history
+
+import (
+	"testing"
+
+	"github.com/guregu/null"
+	"github.com/stellar/go/services/horizon/internal/test"
+	"github.com/stellar/go/xdr"
+)
+
+func TestAddClaimableBalance(t *testing.T) {
+	tt := test.Start(t)
+	defer tt.Finish()
+	test.ResetHorizonDB(t, tt.HorizonDB)
+	q := &Q{tt.HorizonSession()}
+
+	lastModifiedLedgerSeq := xdr.Uint32(123)
+	asset := xdr.MustNewCreditAsset("USD", "GC3C4AKRBQLHOJ45U4XG35ESVWRDECWO5XLDGYADO6DPR3L7KIDVUMML")
+	cBalance := xdr.ClaimableBalanceEntry{
+		BalanceId: xdr.ClaimableBalanceId{
+			Type: xdr.ClaimableBalanceIdTypeClaimableBalanceIdTypeV0,
+			V0:   &xdr.Hash{1, 2, 3},
+		},
+		Claimants: []xdr.Claimant{},
+		Asset:     asset,
+		Amount:    10,
+	}
+	entry := xdr.LedgerEntry{
+		Data: xdr.LedgerEntryData{
+			Type:             xdr.LedgerEntryTypeClaimableBalance,
+			ClaimableBalance: &cBalance,
+		},
+		LastModifiedLedgerSeq: lastModifiedLedgerSeq,
+	}
+
+	builder := q.NewClaimableBalancesBatchInsertBuilder(2)
+
+	err := builder.Add(&entry)
+	tt.Assert.NoError(err)
+
+	err = builder.Exec()
+	tt.Assert.NoError(err)
+
+	cbs := []ClaimableBalance{}
+	err = q.Select(&cbs, selectClaimableBalances)
+
+	if tt.Assert.NoError(err) {
+		tt.Assert.Len(cbs, 1)
+
+		cb := cbs[0]
+		tt.Assert.Equal("8f2fb9b32d46b3357f6d11ded015f470520ab81dd5386fc18ab7d33f5334c45b", cb.ID)
+		//  this is temporal while I figure how to deserialize/serialize the XDR
+		tt.Assert.Equal("8f2fb9b32d46b3357f6d11ded015f470520ab81dd5386fc1", cb.BalanceID)
+		tt.Assert.Equal(asset, cb.Asset)
+		tt.Assert.Equal(null.StringFromPtr(nil), cb.Sponsor)
+		tt.Assert.Equal(uint32(lastModifiedLedgerSeq), cb.LastModifiedLedger)
+	}
+}
