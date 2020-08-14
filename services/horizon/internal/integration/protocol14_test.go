6d0773ae1bb9c6fa54f589be668b97a134929870
Bartek Nowotarski <bartek@nowotarski.info>
2020-08-14 18:09:07 +0200

    Add P14 test

--- services/horizon/internal/integration/protocol14_test.go
+++ services/horizon/internal/integration/protocol14_test.go
@@ -5,7 +5,8 @@ import (
 
 	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/services/horizon/internal/test"
-	"github.com/stellar/go/txnbuild"
+	"github.com/stellar/go/services/horizon/internal/txnbuild"
+	"github.com/stellar/go/xdr"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -42,8 +43,57 @@ func TestProtocol14Sample(t *testing.T) {
 	tx, err = tx.Sign(test.IntegrationNetworkPassphrase, itest.Master().(*keypair.Full))
 	assert.NoError(t, err)
 
-	txResp, err := itest.Client().SubmitTransaction(tx)
+	txb64, err := tx.Base64()
+	assert.NoError(t, err)
+
+	txResp, err := itest.Client().SubmitTransactionXDR(txb64)
 	assert.NoError(t, err)
 	assert.Equal(t, itest.Master().Address(), txResp.Account)
 	assert.Equal(t, "1", txResp.AccountSequence)
+}
+
+func TestCreateClaimableBalance(t *testing.T) {
+	itest := test.NewIntegrationTest(t, protocol14Config)
+	defer itest.Close()
+
+	// Submit a simple tx
+	master := itest.Master()
+	op := txnbuild.CreateClaimableBalance{
+		Destinations: []string{master.Address()},
+		Amount:       "10",
+		Asset:        txnbuild.NativeAsset{},
+	}
+	tx, err := txnbuild.NewTransaction(
+		txnbuild.TransactionParams{
+			SourceAccount: &txnbuild.SimpleAccount{
+				AccountID: master.Address(),
+				Sequence:  1,
+			},
+			Operations: []txnbuild.Operation{&op},
+			BaseFee:    txnbuild.MinBaseFee,
+			Timebounds: txnbuild.NewInfiniteTimeout(),
+		},
+	)
+	assert.NoError(t, err)
+	tx, err = tx.Sign(test.IntegrationNetworkPassphrase, itest.Master().(*keypair.Full))
+	assert.NoError(t, err)
+
+	txb64, err := tx.Base64()
+	assert.NoError(t, err)
+
+	txResp, err := itest.Client().SubmitTransactionXDR(txb64)
+	assert.NoError(t, err)
+
+	var txResult xdr.TransactionResult
+	err = xdr.SafeUnmarshalBase64(txResp.ResultXdr, &txResult)
+	assert.NoError(t, err)
+
+	assert.Equal(t, xdr.TransactionResultCodeTxSuccess, txResult.Result.Code)
+	opsResults := *txResult.Result.Results
+	opResult := opsResults[0].MustTr().MustCreateClaimableBalanceResult()
+	assert.Equal(t,
+		xdr.CreateClaimableBalanceResultCodeCreateClaimableBalanceSuccess,
+		opResult.Code,
+	)
+	assert.NotNil(t, opResult.BalanceId)
 }
