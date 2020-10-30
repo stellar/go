4b599bf528da81bddd47c3e6bb2841f50668d042
Tamir Sen <tamir@stellar.org>
2020-10-30 14:45:37 +0100

    Fix dump ledger state script
    Stellar Core recently modified its database schema which consequently broke the SQL queries used in dump_core_db.sh.
    This commit fixes the SQL queries so that it works with the latest version of Stellar Core.
    This commit also adds support for claimable balances which were introduced in Protocol 14/15.

--- exp/tools/dump-ledger-state/dump_core_db.sh
+++ exp/tools/dump-ledger-state/dump_core_db.sh
@@ -3,17 +3,21 @@
 # https://dba.stackexchange.com/a/101510
 
 echo "Fetching accounts from stellar-core DB..."
-psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select accountid, balance, seqnum, numsubentries, inflationdest, homedomain, thresholds, flags, COALESCE(buyingliabilities, 0), COALESCE(sellingliabilities, 0), signers from accounts" > accounts_core.csv
+psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select accountid, balance, seqnum, numsubentries, inflationdest, homedomain, thresholds, flags, COALESCE(extension, 'AAAAAA=='), signers, ledgerext from accounts" > accounts_core.csv
 rm accounts_core_sorted.csv || true # Remove if exist in case original files are rebuilt
 
 echo "Fetching accountdata from stellar-core DB..."
-psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select accountid, dataname, datavalue from accountdata" > accountdata_core.csv
+psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select accountid, dataname, datavalue, COALESCE(extension, 'AAAAAA=='), ledgerext from accountdata" > accountdata_core.csv
 rm accountdata_core_sorted.csv || true # Remove if exist in case original files are rebuilt
 
 echo "Fetching offers from stellar-core DB..."
-psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select sellerid, offerid, sellingasset, buyingasset, amount, pricen, priced, flags from offers" > offers_core.csv
+psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select sellerid, offerid, sellingasset, buyingasset, amount, pricen, priced, flags, COALESCE(extension, 'AAAAAA=='), ledgerext from offers" > offers_core.csv
 rm offers_core_sorted.csv || true # Remove if exist in case original files are rebuilt
 
 echo "Fetching trustlines from stellar-core DB..."
-psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select accountid, assettype, issuer, assetcode, tlimit, balance, flags, COALESCE(buyingliabilities, 0), COALESCE(sellingliabilities, 0) from trustlines" > trustlines_core.csv
+psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select accountid, assettype, issuer, assetcode, tlimit, balance, flags, COALESCE(extension, 'AAAAAA=='), ledgerext from trustlines" > trustlines_core.csv
 rm trustlines_core_sorted.csv || true # Remove if exist in case original files are rebuilt
+
+echo "Fetching claimable balances from stellar-core DB..."
+psql -d core -t -A -F"," --variable="FETCH_COUNT=10000" -c "select balanceid, ledgerentry from claimablebalance" > claimablebalances_core.csv
+rm claimablebalances_core_sorted.csv || true # Remove if exist in case original files are rebuilt
