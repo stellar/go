39404c57362e7081c6f367fef29ecdb5d6b072e8
Tamir Sen <tamir@stellar.org>
2020-11-12 11:02:36 +0100

    Fix header

--- services/horizon/internal/httpx/middleware.go
+++ services/horizon/internal/httpx/middleware.go
@@ -33,7 +33,7 @@ func requestCacheHeadersMiddleware(h http.Handler) http.Handler {
 		// Before changing this read Stack Overflow answer about staled request
 		// in older versions of Chrome:
 		// https://stackoverflow.com/questions/27513994/chrome-stalls-when-making-multiple-requests-to-same-resource
-		w.Header().Set("State-Control", "no-cache, no-store, max-age=0")
+		w.Header().Set("Cache-Control", "no-cache, no-store, max-age=0")
 		h.ServeHTTP(w, r)
 	})
 }
