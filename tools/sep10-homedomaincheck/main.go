e724c989e113308337ec447ec451b30f2810fee2
Jake Urban <jake@stellar.org>
2020-11-20 10:32:35 -0800

    added more tests, refactored for readability

--- tools/sep10-homedomaincheck/main.go
+++ tools/sep10-homedomaincheck/main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 
 	"github.com/stellar/go/clients/stellartoml"
-	"github.com/stellar/go/network"
+	"github.com/stellar/go/keypair"
 	"github.com/stellar/go/txnbuild"
 )
 
@@ -18,7 +18,10 @@ func main() {
 	homeDomains := os.Args[1:]
 
 	w := csv.NewWriter(os.Stdout)
-	if err := w.Write([]string{"Home Domain", "Success?", "Error from Go SDK's ReadChallengeTx", "Challenge Transaction Returned"}); err != nil {
+	err := w.Write(
+		[]string{"Home Domain", "Success?", "Error from Go SDK's ReadChallengeTx", "Challenge Transaction Returned"},
+	)
+	if err != nil {
 		log.Fatal("error writing csv headers:", err)
 	}
 
@@ -42,7 +45,7 @@ func main() {
 			challengeTx,
 		}
 
-		if err := w.Write(record); err != nil {
+		if err = w.Write(record); err != nil {
 			log.Fatal("error writing record to csv:", err)
 		}
 		w.Flush()
@@ -55,25 +58,74 @@ func check(homeDomain string) (string, error) {
 		return "", err
 	}
 	if tomlResp.WebAuthEndpoint == "" {
-		return "", errors.New("sep-10 not supported")
+		return "", errors.New("SEP-10 not supported")
 	}
+
 	queryParams := url.Values{}
 	queryParams.Set("account", "GAAZI4TCR3TY5OJHCTJC2A4QSY6CJWJH5IAJTGKIN2ER7LBNVKOCCWN7")
-	queryParams.Set("home_domain", homeDomain)
 	webAuthResp, err := http.Get(tomlResp.WebAuthEndpoint + "?" + queryParams.Encode())
 	if err != nil {
-		return "", err
+		return "", errors.New("Unable to get SEP-10 challenge")
 	}
 	webAuthRespMsg := struct {
 		Transaction string
 	}{}
-	err = json.NewDecoder(webAuthResp.Body).Decode(&webAuthRespMsg)
+	tx, err := decodeAndRead(
+		webAuthResp,
+		webAuthRespMsg,
+		tomlResp.SigningKey,
+		tomlResp.NetworkPassphrase,
+		homeDomain,
+	)
 	if err != nil {
-		return "", err
+		return tx, err
 	}
-	_, _, _, err = txnbuild.ReadChallengeTx(webAuthRespMsg.Transaction, tomlResp.SigningKey, network.PublicNetworkPassphrase, []string{homeDomain})
+
+	kp, _ := keypair.Random()
+	queryParams.Del("account")
+	queryParams.Set("account", kp.Address())
+	webAuthResp, err = http.Get(tomlResp.WebAuthEndpoint + "?" + queryParams.Encode())
 	if err != nil {
-		return webAuthRespMsg.Transaction, err
+		return "", errors.New("Unable to specify account that does not exist")
+	}
+	tx, err = decodeAndRead(
+		webAuthResp,
+		webAuthRespMsg,
+		tomlResp.SigningKey,
+		tomlResp.NetworkPassphrase,
+		homeDomain,
+	)
+	if err != nil {
+		return tx, err
+	}
+
+	queryParams.Set("home_domain", homeDomain)
+	webAuthResp, err = http.Get(tomlResp.WebAuthEndpoint + "?" + queryParams.Encode())
+	if err != nil {
+		return "", errors.New("Unable to specify home_domain parameter")
+	}
+	tx, err = decodeAndRead(
+		webAuthResp,
+		webAuthRespMsg,
+		tomlResp.SigningKey,
+		tomlResp.NetworkPassphrase,
+		homeDomain,
+	)
+	if err != nil {
+		return tx, err
+	}
+
+	return tx, nil
+}
+
+func decodeAndRead(webAuthResp *http.Response, webAuthRespMsg struct{ Transaction string }, signingKey, passphrase, homeDomain string) (string, error) {
+	err := json.NewDecoder(webAuthResp.Body).Decode(&webAuthRespMsg)
+	if err != nil {
+		return "", errors.New("GET response does not match expectation")
+	}
+	_, _, _, err = txnbuild.ReadChallengeTx(webAuthRespMsg.Transaction, signingKey, passphrase, []string{homeDomain})
+	if err != nil {
+		return webAuthRespMsg.Transaction, errors.New("Unable to read challenge: " + err.Error())
 	}
 	return webAuthRespMsg.Transaction, nil
 }
