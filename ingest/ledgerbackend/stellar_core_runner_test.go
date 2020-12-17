8ec99772b8810bb59e8a693f604d5f3daeff8c09
tamirms <tamir@stellar.org>
2020-12-17 18:36:52 +0100

    ingest/ledgerbackend: Fix generation of captive core config (#3286)
    In some cases there were no new lines separating the history url section from the rest of the captive core configuration. This resulted in invalid stellar core configuration files which produced errors like:
    
    Config from /tmp/captive-stellar-core-2525239294c6527a/stellar-core.conf
    default: Got an exception: Failed to parse '/tmp/captive-stellar-core-2525239294c6527a/stellar-core.conf' :Unidentified trailing character '['---did you forget a '#'? at line 11 [CommandLine.cpp:1523]
    
    This commit adds trailing new lines to each section of the captive core configuration preceding the history urls.

--- ingest/ledgerbackend/stellar_core_runner_test.go
+++ ingest/ledgerbackend/stellar_core_runner_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -12,27 +13,57 @@ import (
 )
 
 func TestGenerateConfig(t *testing.T) {
-	tmpFile, err := ioutil.TempFile("", "test-generate-config")
-	assert.NoError(t, err)
-	defer tmpFile.Close()
-	defer os.Remove(tmpFile.Name())
+	for _, testCase := range []struct {
+		name         string
+		appendPath   string
+		mode         stellarCoreRunnerMode
+		expectedPath string
+	}{
+		{
+			name:         "offline config with no appendix",
+			mode:         stellarCoreRunnerModeOffline,
+			appendPath:   "",
+			expectedPath: filepath.Join("testdata", "expected-offline-core.cfg"),
+		},
+		{
+			name:         "online config with appendix",
+			mode:         stellarCoreRunnerModeOnline,
+			appendPath:   filepath.Join("testdata", "sample-appendix.cfg"),
+			expectedPath: filepath.Join("testdata", "expected-online-core.cfg"),
+		},
+		{
+			name:         "offline config with appendix",
+			mode:         stellarCoreRunnerModeOffline,
+			appendPath:   filepath.Join("testdata", "sample-appendix.cfg"),
+			expectedPath: filepath.Join("testdata", "expected-offline-with-appendix-core.cfg"),
+		},
+	} {
+		t.Run(testCase.name, func(t *testing.T) {
+			stellarCoreRunner, err := newStellarCoreRunner(CaptiveCoreConfig{
+				HTTPPort:           6789,
+				HistoryArchiveURLs: []string{"http://localhost:1170"},
+				Log:                log.New(),
+				ConfigAppendPath:   testCase.appendPath,
+				Context:            context.Background(),
+				NetworkPassphrase:  "Public Global Stellar Network ; September 2015",
+			}, testCase.mode)
+			assert.NoError(t, err)
 
-	r := stellarCoreRunner{
-		configAppendPath: tmpFile.Name(),
+			tempDir := stellarCoreRunner.tempDir
+			stellarCoreRunner.tempDir = "/test-temp-dir"
+
+			config, err := stellarCoreRunner.generateConfig()
+			assert.NoError(t, err)
+
+			expectedByte, err := ioutil.ReadFile(testCase.expectedPath)
+			assert.NoError(t, err)
+
+			assert.Equal(t, config, string(expectedByte))
+
+			stellarCoreRunner.tempDir = tempDir
+			assert.NoError(t, stellarCoreRunner.close())
+		})
 	}
-
-	tmpFile.WriteString(`[[HOME_DOMAINS]]
-HOME_DOMAIN="testnet.stellar.org"
-QUALITY="HIGH"
-
-[[VALIDATORS]]
-NAME="sdf_testnet_1"
-HOME_DOMAIN="testnet.stellar.org"
-PUBLIC_KEY="GDKXE2OZMJIPOSLNA6N6F2BVCI3O777I2OOC4BV7VOYUEHYX7RTRYA7Y"
-ADDRESS="core-testnet1.stellar.org"
-`)
-	_, err = r.generateConfig()
-	assert.NoError(t, err)
 }
 
 func TestCloseBeforeStart(t *testing.T) {
