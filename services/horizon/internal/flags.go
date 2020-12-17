99a29ddf3a67630650a0b7f6dbf76b1873915899
Alfonso Acosta <alfonso@stellar.org>
2020-12-17 17:33:44 +0100

    Remove deprecated Redis flags

--- services/horizon/internal/flags.go
+++ services/horizon/internal/flags.go
@@ -227,18 +227,6 @@ func Flags() (*Config, support.ConfigOptions) {
 			},
 			Usage: "max count of requests allowed in a one hour period, by remote ip address",
 		},
-		&support.ConfigOption{ // Action needed in release: horizon-v2.0.0
-			// remove deprecated flag
-			Name:    "rate-limit-redis-key",
-			OptType: types.String,
-			Usage:   "deprecated, do not use",
-		},
-		&support.ConfigOption{ // Action needed in release: horizon-v2.0.0
-			// remove deprecated flag
-			Name:    "redis-url",
-			OptType: types.String,
-			Usage:   "deprecated, do not use",
-		},
 		&support.ConfigOption{
 			Name:           "friendbot-url",
 			ConfigKey:      &config.FriendbotURL,
