b6a225333f3074a45f6a329a12889d918a8ca82e
Bartek Nowotarski <bartek@nowotarski.info>
2021-01-06 16:57:31 +0100

    gofmt

--- keypair/full.go
+++ keypair/full.go
@@ -2,12 +2,11 @@ package keypair
 
 import (
 	"bytes"
+	"crypto/ed25519"
 	"encoding/base64"
 
 	"github.com/stellar/go/strkey"
 	"github.com/stellar/go/xdr"
-
-	"crypto/ed25519"
 )
 
 type Full struct {
