cd8b17699547f6ad569c31eb98fa6224e9cbcf05
Bartek Nowotarski <bartek@nowotarski.info>
2021-01-11 13:40:01 +0100

    Fix bindata

--- services/horizon/internal/test/scenarios/bindata.go
+++ services/horizon/internal/test/scenarios/bindata.go
@@ -1525,9 +1525,6 @@ var _bindata = map[string]func() (*asset, error){
 	"trades-horizon.sql":                      tradesHorizonSql,
 }
 
-// AssetDebug is true if the assets were built with the debug flag enabled.
-const AssetDebug = false
-
 // AssetDir returns the file names below a certain
 // directory embedded in the file by go-bindata.
 // For example if you run go-bindata on data/... and data contains the
