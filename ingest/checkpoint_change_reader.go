e72a6e87805abe5a66a1cd0d5f722567d7d94ae4
George Kudrayvtsev <george@stellar.org>
2021-01-15 14:23:33 -0800

    Simplify error to just be a dynamic message helping the caller

--- ingest/checkpoint_change_reader.go
+++ ingest/checkpoint_change_reader.go
@@ -81,8 +81,14 @@ func NewCheckpointChangeReader(
 	archive historyarchive.ArchiveInterface,
 	sequence uint32,
 ) (*CheckpointChangeReader, error) {
-	if !archive.GetCheckpointManager().IsCheckpoint(sequence) {
-		return nil, NewCheckpointError(sequence, archive.GetCheckpointManager())
+	manager := archive.GetCheckpointManager()
+
+	// The nth ledger is a checkpoint ledger iff: n+1 mod f == 0, where f is the
+	// checkpoint frequency (64 by default).
+	if !manager.IsCheckpoint(sequence) {
+		return nil, errors.Errorf("%d is not a checkpoint ledger, try %d or %d",
+			sequence, manager.PrevCheckpoint(sequence),
+			manager.NextCheckpoint(sequence))
 	}
 
 	has, err := archive.GetCheckpointHAS(sequence)
--- ingest/errors.go
+++ ingest/errors.go
@@ -1,36 +1,11 @@
 package ingest
 
 import (
-	"github.com/stellar/go/historyarchive"
 	"github.com/stellar/go/support/errors"
 )
 
 // ErrNotFound is returned when the requested ledger is not found
 var ErrNotFound = errors.New("ledger not found")
-
-// ErrNotCheckpoint is returned when the requested ledger sequence number does
-// not correspond to a checkpoint ledger.
-//
-// You can use the fields within the parent `CheckpointError` object (below) to
-// resolve to an accurate checkpoint.
-//
-// The nth ledger n is a checkpoint ledger iff: n+1 mod f == 0, where f is the
-// checkpoint frequency (64 by default).
-var ErrNotCheckpoint = errors.New("sequence number is not a checkpoint ledger")
-
-type CheckpointError struct {
-	error
-	precedingCheckpoint uint32
-	followingCheckpoint uint32
-}
-
-func NewCheckpointError(sequence uint32, checkpointMgr historyarchive.CheckpointManager) CheckpointError {
-	return CheckpointError{
-		ErrNotCheckpoint,
-		checkpointMgr.PrevCheckpoint(sequence),
-		checkpointMgr.NextCheckpoint(sequence),
-	}
-}
 
 // StateError is a fatal error indicating that the Change stream
 // produced a result which violates fundamental invariants (e.g. an account
