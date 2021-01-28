621978b95e07ca15b266c9cc6077c3c0a95f8934
Alfonso Acosta <alfonso@stellar.org>
2021-01-28 01:31:03 +0100

    Add support for new result codes in CAP35

--- services/horizon/internal/codes/main.go
+++ services/horizon/internal/codes/main.go
@@ -33,6 +33,8 @@ const (
 	OpNoTrust = "op_no_trust"
 	// OpNotAuthorized occurs when a trust line is not authorized
 	OpNotAuthorized = "op_not_authorized"
+	// OpDoesNotExist occurs when claimable balance or sponsorship does not exist
+	OpDoesNotExist = "op_does_not_exist"
 )
 
 //String returns the appropriate string representation of the provided result code
@@ -357,7 +359,7 @@ func String(code interface{}) (string, error) {
 		case xdr.ClaimClaimableBalanceResultCodeClaimClaimableBalanceSuccess:
 			return OpSuccess, nil
 		case xdr.ClaimClaimableBalanceResultCodeClaimClaimableBalanceDoesNotExist:
-			return "op_does_not_exist", nil
+			return OpDoesNotExist, nil
 		case xdr.ClaimClaimableBalanceResultCodeClaimClaimableBalanceCannotClaim:
 			return "op_cannot_claim", nil
 		case xdr.ClaimClaimableBalanceResultCodeClaimClaimableBalanceLineFull:
@@ -390,13 +392,50 @@ func String(code interface{}) (string, error) {
 		case xdr.RevokeSponsorshipResultCodeRevokeSponsorshipSuccess:
 			return OpSuccess, nil
 		case xdr.RevokeSponsorshipResultCodeRevokeSponsorshipDoesNotExist:
-			return "op_does_not_exist", nil
+			return OpDoesNotExist, nil
 		case xdr.RevokeSponsorshipResultCodeRevokeSponsorshipNotSponsor:
 			return "op_not_sponsor", nil
 		case xdr.RevokeSponsorshipResultCodeRevokeSponsorshipLowReserve:
 			return OpLowReserve, nil
 		case xdr.RevokeSponsorshipResultCodeRevokeSponsorshipOnlyTransferable:
 			return "op_only_transferable", nil
+		}
+	case xdr.ClawbackResultCode:
+		switch code {
+		case xdr.ClawbackResultCodeClawbackSuccess:
+			return OpSuccess, nil
+		case xdr.ClawbackResultCodeClawbackMalformed:
+			return OpMalformed, nil
+		case xdr.ClawbackResultCodeClawbackNotClawbackEnabled:
+			return "op_not_clawback_enabled", nil
+		case xdr.ClawbackResultCodeClawbackNoTrust:
+			return OpNoTrust, nil
+		case xdr.ClawbackResultCodeClawbackUnderfunded:
+			return OpUnderfunded, nil
+		}
+	case xdr.ClawbackClaimableBalanceResultCode:
+		switch code {
+		case xdr.ClawbackClaimableBalanceResultCodeClawbackClaimableBalanceSuccess:
+			return OpSuccess, nil
+		case xdr.ClawbackClaimableBalanceResultCodeClawbackClaimableBalanceDoesNotExist:
+			return OpDoesNotExist, nil
+		case xdr.ClawbackClaimableBalanceResultCodeClawbackClaimableBalanceNotIssuer:
+			return OpNoIssuer, nil
+		case xdr.ClawbackClaimableBalanceResultCodeClawbackClaimableBalanceNotClawbackEnabled:
+			return "op_not_clawback_enabled", nil
+		}
+	case xdr.SetTrustLineFlagsResultCode:
+		switch code {
+		case xdr.SetTrustLineFlagsResultCodeSetTrustLineFlagsSuccess:
+			return OpSuccess, nil
+		case xdr.SetTrustLineFlagsResultCodeSetTrustLineFlagsMalformed:
+			return OpMalformed, nil
+		case xdr.SetTrustLineFlagsResultCodeSetTrustLineFlagsNoTrustLine:
+			return OpNoTrust, nil
+		case xdr.SetTrustLineFlagsResultCodeSetTrustLineFlagsCantRevoke:
+			return "op_cant_revoke", nil
+		case xdr.SetTrustLineFlagsResultCodeSetTrustLineFlagsInvalidState:
+			return "op_invalid_state", nil
 		}
 	}
 
