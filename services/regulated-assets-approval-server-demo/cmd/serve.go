d8264dc744224e677a2bc1749b8be3ca9d4f4b76
Marcelo Salloum <marcelo@stellar.org>
2021-03-08 19:24:53 -0300

    Bootstrap SEP-8 approval server.

---
+++ services/regulated-assets-approval-server-demo/cmd/serve.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"go/types"
+
+	"github.com/spf13/cobra"
+	"github.com/stellar/go/clients/horizonclient"
+	"github.com/stellar/go/network"
+	"github.com/stellar/go/services/regulated-assets-approval-server-demo/serve"
+	"github.com/stellar/go/support/config"
+)
+
+type ServeCommand struct{}
+
+func (c *ServeCommand) Command() *cobra.Command {
+	opts := serve.Options{}
+	configOpts := config.ConfigOptions{
+		{
+			Name:        "port",
+			Usage:       "Port to listen and serve on",
+			OptType:     types.Int,
+			ConfigKey:   &opts.Port,
+			FlagDefault: 8000,
+			Required:    true,
+		},
+		{
+			Name:        "horizon-url",
+			Usage:       "Horizon URL used for looking up account details",
+			OptType:     types.String,
+			ConfigKey:   &opts.HorizonURL,
+			FlagDefault: horizonclient.DefaultTestNetClient.HorizonURL,
+			Required:    true,
+		},
+		{
+			Name:        "network-passphrase",
+			Usage:       "Network passphrase of the Stellar network transactions should be signed for",
+			OptType:     types.String,
+			ConfigKey:   &opts.NetworkPassphrase,
+			FlagDefault: network.TestNetworkPassphrase,
+			Required:    true,
+		},
+	}
+	cmd := &cobra.Command{
+		Use:   "serve",
+		Short: "Run the SEP-8 Approval Server Demo",
+		Run: func(_ *cobra.Command, _ []string) {
+			configOpts.Require()
+			configOpts.SetValues()
+			c.Run(opts)
+		},
+	}
+	configOpts.Init(cmd)
+	return cmd
+}
+
+func (c *ServeCommand) Run(opts serve.Options) {
+	serve.Serve(opts)
+}
---
+++ services/regulated-assets-approval-server-demo/serve/handlers.go
@@ -0,0 +1,13 @@
+package serve
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func stellarTOMLHandler(cfg Options) http.HandlerFunc {
+	return http.HandlerFunc(func(rw http.ResponseWriter, _ *http.Request) {
+		fmt.Fprintf(rw, "NETWORK_PASSPHRASE=%q\n", cfg.NetworkPassphrase)
+		return
+	})
+}
