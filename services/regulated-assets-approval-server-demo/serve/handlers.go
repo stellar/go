553e6c2678554f0f6b5d3c6ebd4b4724a3d544d8
Marcelo Salloum <marcelo@stellar.org>
2021-03-08 19:39:42 -0300

    Fix error when running `./staticcheck.sh` in Go 1.16.

--- services/regulated-assets-approval-server-demo/serve/handlers.go
+++ services/regulated-assets-approval-server-demo/serve/handlers.go
@@ -8,6 +8,5 @@ import (
 func stellarTOMLHandler(cfg Options) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, _ *http.Request) {
 		fmt.Fprintf(rw, "NETWORK_PASSPHRASE=%q\n", cfg.NetworkPassphrase)
-		return
 	})
 }
