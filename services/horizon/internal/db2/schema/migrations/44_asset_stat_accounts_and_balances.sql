0c9df52411028d466449e821031cbd250e6c483b
Paul Bellamy <paul@stellar.org>
2021-03-18 14:49:19 +0000

    Rename migration to be after previous migrations

--- services/horizon/internal/db2/schema/migrations/44_asset_stat_accounts_and_balances.sql
+++ services/horizon/internal/db2/schema/migrations/44_asset_stat_accounts_and_balances.sql
@@ -1,16 +1,16 @@
 -- +migrate Up
 
-ALTER TABLE asset_stats
+ALTER TABLE exp_asset_stats
 ADD COLUMN accounts JSONB;
 ADD COLUMN balances JSONB;
--- Previously, all the asset_stats we stored were authorized.
-UPDATE asset_stats
+-- Previously, all the exp_asset_stats we stored were authorized.
+UPDATE exp_asset_stats
   SET
     accounts = jsonb_build_object('authorized', num_accounts),
     balances = jsonb_build_object('authorized', amount);
 
 -- +migrate Down
 
-ALTER TABLE asset_stats
+ALTER TABLE exp_asset_stats
 DROP COLUMN accounts;
 DROP COLUMN balances;
