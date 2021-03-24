29da5691fdd09b1c93d29941d6c0ec13e88261bc
Lawrence Lawson <lawrence@stellar.org>
2021-03-24 16:29:53 -0700

    correct Options var name for stellarTOMLHandler
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/serve/handlers.go
+++ services/regulated-assets-approval-server/serve/handlers.go
@@ -7,17 +7,17 @@ import (
 	"github.com/stellar/go/keypair"
 )
 
-func stellarTOMLHandler(cfg Options) http.HandlerFunc {
+func stellarTOMLHandler(opts Options) http.HandlerFunc {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		issuerKP, err := keypair.Parse(cfg.IssuerAccountSecret)
+		issuerKP, err := keypair.Parse(opts.IssuerAccountSecret)
 		if err != nil {
 			return
 		}
-		approvalCriteria := fmt.Sprintf("Currently %q is not approving any %q transactions", r.Host, cfg.AssetCode)
+		approvalCriteria := fmt.Sprintf("Currently %q is not approving any %q transactions", r.Host, opts.AssetCode)
 		approvalServer := fmt.Sprintf("%s/tx_approve", r.Host)
-		fmt.Fprintf(rw, "NETWORK_PASSPHRASE=%q\n", cfg.NetworkPassphrase)
+		fmt.Fprintf(rw, "NETWORK_PASSPHRASE=%q\n", opts.NetworkPassphrase)
 		fmt.Fprintf(rw, "[[CURRENCIES]]\n")
-		fmt.Fprintf(rw, "code=%q\n", cfg.AssetCode)
+		fmt.Fprintf(rw, "code=%q\n", opts.AssetCode)
 		fmt.Fprintf(rw, "issuer=%q\n", issuerKP.Address())
 		fmt.Fprintf(rw, "regulated=true\n")
 		fmt.Fprintf(rw, "approval_server=%q\n", approvalServer)
