fbbda87ece240b8cf58219732c7c3add0d6c2989
Paul Bellamy <paul@stellar.org>
2021-04-09 18:17:15 +0100

    Fixing up integration scripts

--- integration.sh
+++ integration.sh
@@ -4,11 +4,11 @@ set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 export HORIZON_INTEGRATION_TESTS=true
-export HORIZON_INTEGRATION_ENABLE_CAP_35=true
-export HORIZON_INTEGRATION_ENABLE_CAPTIVE_CORE=false
-export CAPTIVE_CORE_BIN=/usr/bin/stellar-core
-export TRACY_NO_INVARIANT_CHECK=1
+export HORIZON_INTEGRATION_ENABLE_CAP_35=${HORIZON_INTEGRATION_ENABLE_CAP_35:-}
+export HORIZON_INTEGRATION_ENABLE_CAPTIVE_CORE=${HORIZON_INTEGRATION_ENABLE_CAPTIVE_CORE:-}
+export CAPTIVE_CORE_BIN=${CAPTIVE_CORE_BIN:-/usr/bin/stellar-core}
 
+# launch postgres if it's not already.
 if [[ "$(docker inspect integration_postgres -f '{{.State.Running}}')" != "true" ]]; then
   docker rm -f integration_postgres || true;
   docker run -d --name integration_postgres --env POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 circleci/postgres:9.6.5-alpine
