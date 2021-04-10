ca97b071b476a1e2ce4f61ad2681d3e9abf6692a
tamirms <tamirms@gmail.com>
2021-04-10 09:10:08 +0200

    Generate XDR in gogenerate.sh

--- checkgoxdr.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-set -e
-
-printf "Running make gxdr/xdr_generated.go...\n"
-make gxdr/xdr_generated.go
-
-printf "Checking for no diff...\n"
-git diff --exit-code || (echo "Files changed after running make gxdr/xdr_generated.go. Run make gxdr/xdr_generated.go locally and update generated files." && exit 1)
