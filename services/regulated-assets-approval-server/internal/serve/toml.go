e2503878d7e72683ec454dfd7dac15f3ef212e66
Lawrence Lawson <lawrence@stellar.org>
2021-05-05 14:40:58 -0700

    update toml.go to support BASE_URL and APPROVAL_SERVER_PAYMENT_THRESHOLD cli args
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/serve/toml.go
+++ services/regulated-assets-approval-server/internal/serve/toml.go
@@ -7,19 +7,18 @@ import (
 
 type stellarTOMLHandler struct {
 	assetCode         string
+	approvalServer    string
 	issuerAddress     string
 	networkPassphrase string
+	kycThreshold      float64
 }
 
 func (h stellarTOMLHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	approvalServer := fmt.Sprintf("%s/tx_approve", r.Host)
-	approvalCriteria := fmt.Sprintf("Currently %s is not approving any %s transactions", approvalServer, h.assetCode)
 	fmt.Fprintf(rw, "NETWORK_PASSPHRASE=%q\n", h.networkPassphrase)
 	fmt.Fprintf(rw, "[[CURRENCIES]]\n")
 	fmt.Fprintf(rw, "code=%q\n", h.assetCode)
 	fmt.Fprintf(rw, "issuer=%q\n", h.issuerAddress)
 	fmt.Fprintf(rw, "regulated=true\n")
-	fmt.Fprintf(rw, "approval_server=%q\n", approvalServer)
-	fmt.Fprintf(rw, "approval_criteria=%q\n", approvalCriteria)
-
+	fmt.Fprintf(rw, "approval_server=%q\n", h.approvalServer)
+	fmt.Fprintf(rw, `approval_criteria="The approval server currently only accepts payments and the transaction must have exactly one operation and it must be of type payment. If an account is registered in the internal list of unauthorized addresses, all payments to/from that account will be rejected. Also, if the payment amount exceeds %.2f %s it will need KYC approval."`, h.kycThreshold, h.assetCode)
 }
