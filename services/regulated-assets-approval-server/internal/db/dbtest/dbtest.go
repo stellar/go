44b12105648c15687722cd68f27dc5ce86fb06c7
Marcelo Salloum <marcelo@stellar.org>
2021-05-05 16:13:23 -0300

    Add a DB to the sep8-regulated-assets service.

---
+++ services/regulated-assets-approval-server/internal/db/dbtest/dbtest.go
@@ -0,0 +1,34 @@
+package dbtest
+
+import (
+	"path"
+	"runtime"
+	"testing"
+
+	migrate "github.com/rubenv/sql-migrate"
+	"github.com/stellar/go/support/db/dbtest"
+)
+
+func Open(t *testing.T) *dbtest.DB {
+	db := dbtest.Postgres(t)
+
+	// Get the folder holding the migrations relative to this file. We cannot
+	// hardcode "../migrations" because Open is called from tests in multiple
+	// packages and tests are executed with the current working directory set
+	// to the package the test lives in.
+	_, filename, _, _ := runtime.Caller(0)
+	migrationsDir := path.Join(path.Dir(filename), "..", "dbmigrate", "migrations")
+
+	migrations := &migrate.FileMigrationSource{
+		Dir: migrationsDir,
+	}
+
+	conn := db.Open()
+	defer conn.Close()
+
+	_, err := migrate.Exec(conn.DB, "postgres", migrations, migrate.Up)
+	if err != nil {
+		t.Fatal(err)
+	}
+	return db
+}
