0da586ef45cec8e8c32086ae3338a6e40b293c02
Lawrence Lawson <lawrence@stellar.org>
2021-05-06 08:40:51 -0700

    return the previous approval_criteria msg
    - TODO: we should make this an env var
    
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/serve/toml.go
+++ services/regulated-assets-approval-server/internal/serve/toml.go
@@ -14,11 +14,12 @@ type stellarTOMLHandler struct {
 }
 
 func (h stellarTOMLHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	approvalCriteria := fmt.Sprintf("Currently %s is not approving any %s transactions", h.approvalServer, h.assetCode)
 	fmt.Fprintf(rw, "NETWORK_PASSPHRASE=%q\n", h.networkPassphrase)
 	fmt.Fprintf(rw, "[[CURRENCIES]]\n")
 	fmt.Fprintf(rw, "code=%q\n", h.assetCode)
 	fmt.Fprintf(rw, "issuer=%q\n", h.issuerAddress)
 	fmt.Fprintf(rw, "regulated=true\n")
 	fmt.Fprintf(rw, "approval_server=%q\n", h.approvalServer)
-	fmt.Fprintf(rw, `approval_criteria="The approval server currently only accepts payments and the transaction must have exactly one operation and it must be of type payment. If an account is registered in the internal list of unauthorized addresses, all payments to/from that account will be rejected. Also, if the payment amount exceeds %.2f %s it will need KYC approval."`, h.kycThreshold, h.assetCode)
+	fmt.Fprintf(rw, "approval_criteria=%q\n", approvalCriteria)
 }
