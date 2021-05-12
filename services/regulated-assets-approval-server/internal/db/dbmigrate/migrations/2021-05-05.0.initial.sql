db53eaebc5bbb1908cf9b9de6371bcfbda79393f
Lawrence Lawson <lawrence@stellar.org>
2021-05-12 08:21:13 -0700

    add migrate up sql
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2021-05-05.0.initial.sql
+++ services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2021-05-05.0.initial.sql
@@ -1,7 +1,12 @@
 -- This migration file is intentionally empty and is a first starting point for
 -- our migrations before we yet have a schema.
-
 -- +migrate Up
-
+CREATE TABLE public.accounts_kyc_status (
+    stellar_address text NOT NULL PRIMARY KEY,
+    email_address text,
+    created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    kyc_submitted_at timestamp with time zone,
+    approved_at timestamp with time zone
+);
 -- +migrate Down
-
+DROP TABLE public.accounts_kyc_status;
