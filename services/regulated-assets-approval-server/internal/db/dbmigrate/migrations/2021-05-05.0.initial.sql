2e7e5eb622e694e00c969296298ab1533502aba0
Lawrence Lawson <lawrence@stellar.org>
2021-05-12 10:31:29 -0700

    add callback_id
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2021-05-05.0.initial.sql
+++ services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2021-05-05.0.initial.sql
@@ -3,6 +3,7 @@
 -- +migrate Up
 CREATE TABLE public.accounts_kyc_status (
     stellar_address text NOT NULL PRIMARY KEY,
+    callback_id text NOT NULL,
     email_address text,
     created_at timestamp with time zone NOT NULL DEFAULT NOW(),
     kyc_submitted_at timestamp with time zone,
