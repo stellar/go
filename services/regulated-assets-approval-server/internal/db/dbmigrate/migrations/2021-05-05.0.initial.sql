ff4831ce3a634f694634acaf4249c2885861c0ee
Lawrence Lawson <lawrence@stellar.org>
2021-05-12 10:26:30 -0700

    update sql
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2021-05-05.0.initial.sql
+++ services/regulated-assets-approval-server/internal/db/dbmigrate/migrations/2021-05-05.0.initial.sql
@@ -6,7 +6,8 @@ CREATE TABLE public.accounts_kyc_status (
     email_address text,
     created_at timestamp with time zone NOT NULL DEFAULT NOW(),
     kyc_submitted_at timestamp with time zone,
-    approved_at timestamp with time zone
+    approved_at timestamp with time zone,
+    rejected_at timestamp with time zone
 );
 -- +migrate Down
 DROP TABLE public.accounts_kyc_status;
