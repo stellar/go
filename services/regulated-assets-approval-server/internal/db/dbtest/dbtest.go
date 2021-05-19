34491953aab78ee23f7e42f3e34e16bd073c9907
Lawrence Lawson <lawrence@stellar.org>
2021-05-18 22:07:54 -0700

    add dbmigrate tests, borrowered from recovery signer service
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/db/dbtest/dbtest.go
+++ services/regulated-assets-approval-server/internal/db/dbtest/dbtest.go
@@ -9,13 +9,18 @@ import (
 	"github.com/stellar/go/support/db/dbtest"
 )
 
-func Open(t *testing.T) *dbtest.DB {
+func OpenWithoutMigrations(t *testing.T) *dbtest.DB {
 	db := dbtest.Postgres(t)
+	return db
+}
+
+func Open(t *testing.T) *dbtest.DB {
+	db := OpenWithoutMigrations(t)
 
 	// Get the folder holding the migrations relative to this file. We cannot
-	// hardcode "../dbmigrate/migrations" because Open is called from tests in
-	// multiple packages and tests are executed with the current working
-	// directory set to the package the test lives in.
+	// hardcode "../migrations" because Open is called from tests in multiple
+	// packages and tests are executed with the current working directory set
+	// to the package the test lives in.
 	_, filename, _, _ := runtime.Caller(0)
 	migrationsDir := path.Join(path.Dir(filename), "..", "dbmigrate", "migrations")
 
