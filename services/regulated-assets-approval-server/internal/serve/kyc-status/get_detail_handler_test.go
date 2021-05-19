ed0deb6514c948a03707d54d8979af9c58d0e88b
Lawrence Lawson <lawrence@stellar.org>
2021-05-19 16:51:40 -0700

    add TestGetDetailHandlerTimePointerIfValid
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/serve/kyc-status/get_detail_handler_test.go
+++ services/regulated-assets-approval-server/internal/serve/kyc-status/get_detail_handler_test.go
@@ -1,9 +1,12 @@
 package kycstatus
 
 import (
+	"database/sql"
 	"testing"
+	"time"
 
 	"github.com/stellar/go/services/regulated-assets-approval-server/internal/db/dbtest"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -22,4 +25,25 @@ func TestGetDetailHandlerValidate(t *testing.T) {
 	}
 	err = h.validate()
 	require.NoError(t, err)
+}
+
+func TestGetDetailHandlerTimePointerIfValid(t *testing.T) {
+	// Prepare NULL nullTimePtr.
+	var nullTimePtr sql.NullTime
+
+	// Send a NullTime Pointer to timePointerIfValid.
+	// TEST if timePointer is null; timePointerIfValid will return nil in this case.
+	timePointer := timePointerIfValid(nullTimePtr)
+	require.Nil(t, timePointer)
+
+	// Prepare a valid nullTimePtr with a time set.
+	nullTimePtr.Valid = true
+	timeNow := time.Now()
+	nullTimePtr.Time = timeNow
+
+	// Send a valid Pointer to timePointerIfValid.
+	// TEST if timePointer is valid; timePointerIfValid will return the time.Time ptr.
+	timePointer = timePointerIfValid(nullTimePtr)
+	require.NotNil(t, timePointer)
+	assert.Equal(t, &timeNow, timePointer)
 }
