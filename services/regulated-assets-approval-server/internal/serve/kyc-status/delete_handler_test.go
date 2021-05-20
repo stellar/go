a06658595c3149afcd36092a1177272ad769655f
Lawrence Lawson <lawrence@stellar.org>
2021-05-20 07:20:30 -0700

    remove redundant query test
    Signed-off-by: Lawrence Lawson <lawrence@stellar.org>

--- services/regulated-assets-approval-server/internal/serve/kyc-status/delete_handler_test.go
+++ services/regulated-assets-approval-server/internal/serve/kyc-status/delete_handler_test.go
@@ -63,7 +63,13 @@ func TestDeleteHandlerHandle(t *testing.T) {
 	_, err = h.DB.ExecContext(ctx, insertNewAccountQuery, accountKP.Address(), callbackID, emailAddress)
 	require.NoError(t, err)
 
-	// Prepare and execute SELECT query for account was added, ensures its in db.
+	// Send deleteRequest to an account in the db.
+	err = h.handle(ctx, in)
+
+	// TEST if nil error returned (success).
+	require.NoError(t, err)
+
+	// Prepare and execute SELECT query for account that was deleted; ensure its no longer in db
 	existQuery := `
 		SELECT EXISTS(
 			SELECT stellar_address
@@ -71,17 +77,6 @@ func TestDeleteHandlerHandle(t *testing.T) {
 			WHERE stellar_address = $1
 		)`
 	var exists bool
-	err = h.DB.QueryRowContext(ctx, existQuery, accountKP.Address()).Scan(&exists)
-	require.NoError(t, err)
-	assert.True(t, exists)
-
-	// Send deleteRequest to an account in the db.
-	err = h.handle(ctx, in)
-
-	// TEST if nil error returned (success).
-	require.NoError(t, err)
-
-	// Execute SELECT query for account that was deleted; ensure its no longer in db
 	err = h.DB.QueryRowContext(ctx, existQuery, accountKP.Address()).Scan(&exists)
 	require.NoError(t, err)
 	assert.False(t, exists)
