d62d4fc056a5992a633025cfb2b1f1e27795e5d7
Leigh McCulloch <leigh@mcchouse.com>
2021-05-20 14:56:49 +0000

    undo ingest

--- ingest/genesis.go
+++ ingest/genesis.go
@@ -8,14 +8,14 @@ import (
 
 // GenesisChange returns the Change occurring at the genesis ledger (ledgerseq = 1)..
 func GenesisChange(networkPassPhrase string) Change {
-	rootKeyPair := keypair.Root(networkPassPhrase)
+	masterKeyPair := keypair.Master(networkPassPhrase)
 
-	rootAccountEntry := xdr.LedgerEntry{
+	masterAccountEntry := xdr.LedgerEntry{
 		LastModifiedLedgerSeq: 1,
 		Data: xdr.LedgerEntryData{
 			Type: xdr.LedgerEntryTypeAccount,
 			Account: &xdr.AccountEntry{
-				AccountId: xdr.MustAddress(rootKeyPair.Address()),
+				AccountId: xdr.MustAddress(masterKeyPair.Address()),
 				// 100B
 				Balance:    amount.MustParse("100000000000"),
 				SeqNum:     0,
@@ -25,7 +25,7 @@ func GenesisChange(networkPassPhrase string) Change {
 	}
 
 	return Change{
-		Type: rootAccountEntry.Data.Type,
-		Post: &rootAccountEntry,
+		Type: masterAccountEntry.Data.Type,
+		Post: &masterAccountEntry,
 	}
 }
