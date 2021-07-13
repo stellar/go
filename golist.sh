add99c309237cba340b47c6e7d8d57e39ec14a71
Leigh McCulloch <leigh@stellar.org>
2021-07-13 22:34:46 +0000

    all: make sort order of go.list stable regardless of locale

--- golist.sh
+++ golist.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-go list -deps -test -f '{{with .Module}}{{.Path}} {{.Version}}{{end}}' ./... | sort -u
+go list -deps -test -f '{{with .Module}}{{.Path}} {{.Version}}{{end}}' ./... | LC_ALL=C sort -u
