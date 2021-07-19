1a3d51a02f3445eee9cada9f63b584c9b907390b
tamirms <tamir@stellar.org>
2021-07-19 20:32:20 +0100

    Remove NETWORK_MODE=host configuration for linux users (#3773)

--- services/horizon/docker/start.sh
+++ services/horizon/docker/start.sh
@@ -25,19 +25,4 @@ case $NETWORK in
     ;;
 esac
 
-
-case $OSTYPE in
-linux-*)
-    if ! grep -E "^127.0.0.1 .*host\.docker\.internal.*$" /etc/hosts > /dev/null 2>&1; then
-        echo "Missing: \`127.0.0.1 host.docker.internal\` in /etc/hosts"
-        exit 1
-    fi
-
-    echo "NETWORK_MODE=host" > .env
-    ;;
-*)
-    echo "NETWORK_MODE=bridge" > .env
-    ;;
-esac
-
 docker-compose $DOCKER_FLAGS up --build -d
