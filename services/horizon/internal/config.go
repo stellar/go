d5c992eafbe86725fe4ff3c19a05589840885b35
Alfonso Acosta <alfonso@stellar.org>
2021-08-04 18:21:12 +0200

    Remove --path-find-timeout flag

--- services/horizon/internal/config.go
+++ services/horizon/internal/config.go
@@ -39,7 +39,6 @@ type Config struct {
 
 	SSEUpdateFrequency time.Duration
 	ConnectionTimeout  time.Duration
-	PathFindTimeout    time.Duration
 	RateQuota          *throttled.RateQuota
 	FriendbotURL       *url.URL
 	LogLevel           logrus.Level
--- services/horizon/internal/httpx/router.go
+++ services/horizon/internal/httpx/router.go
@@ -39,7 +39,6 @@ type RouterConfig struct {
 	SSEUpdateFrequency    time.Duration
 	StaleThreshold        uint
 	ConnectionTimeout     time.Duration
-	PathFindTimeout       time.Duration
 	NetworkPassphrase     string
 	MaxPathLength         uint
 	PathFinder            paths.Finder
@@ -185,12 +184,9 @@ func (r *Router) addRoutes(config *RouterConfig, rateLimiter *throttled.HTTPRate
 			MaxAssetsParamLength: maxAssetsForPathFinding,
 			PathFinder:           config.PathFinder,
 		}}
-		r.Route("/paths", func(r chi.Router) {
-			r.Use(timeoutMiddleware(config.PathFindTimeout))
-			r.With(stateMiddleware.Wrap).Method(http.MethodGet, "/", findPaths)
-			r.With(stateMiddleware.Wrap).Method(http.MethodGet, "/strict-receive", findPaths)
-			r.With(stateMiddleware.Wrap).Method(http.MethodGet, "/strict-send", findFixedPaths)
-		})
+		r.With(stateMiddleware.Wrap).Method(http.MethodGet, "/paths", findPaths)
+		r.With(stateMiddleware.Wrap).Method(http.MethodGet, "/paths/strict-receive", findPaths)
+		r.With(stateMiddleware.Wrap).Method(http.MethodGet, "/paths/strict-send", findFixedPaths)
 		r.With(stateMiddleware.Wrap).Method(
 			http.MethodGet,
 			"/order_book",
