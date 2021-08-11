5f859d92fcf320892e8c73569ce5c915fa488824
Alfonso Acosta <alfonso@stellar.org>
2021-08-11 13:00:14 +0200

    Stop using a separate table for assets (use a jsonb instead)

--- services/horizon/internal/db2/schema/migrations/49_liquidity_pools.sql
+++ services/horizon/internal/db2/schema/migrations/49_liquidity_pools.sql
@@ -6,6 +6,7 @@ CREATE TABLE liquidity_pools (
     fee integer NOT NULL,
     trustline_count bigint NOT NULL CHECK (trustline_count > 0),
     share_count bigint NOT NULL DEFAULT 0 CHECK(share_count >= 0),
+    assets jsonb NOT NULL,
     sponsor text,
     last_modified_ledger integer NOT NULL,
     PRIMARY KEY (id)
@@ -13,19 +14,7 @@ CREATE TABLE liquidity_pools (
 
 CREATE INDEX liquidity_pools_by_sponsor ON liquidity_pools USING BTREE(sponsor);
 
-CREATE TABLE liquidity_pool_assets (
-    liquidity_pool_id TEXT NOT NULL,
-    asset text NOT NULL,
-    reserve bigint DEFAULT 0 CHECK(reserve >= 0),
-    FOREIGN KEY (liquidity_pool_id) REFERENCES liquidity_pools(id),
-    PRIMARY KEY(liquidity_pool_id, asset) -- no asset duplicates in the same pool
-);
-
-CREATE INDEX liquidity_pool_assets_by_asset ON liquidity_pool_assets USING BTREE(asset);
-CREATE INDEX liquidity_pool_assets_by_pool ON liquidity_pool_assets USING BTREE(liquidity_pool_id);
-
 -- +migrate Down
 
-DROP TABLE liquidity_pool_assets cascade; -- remove it first, due to the foreign key
 DROP TABLE liquidity_pools cascade;
 
