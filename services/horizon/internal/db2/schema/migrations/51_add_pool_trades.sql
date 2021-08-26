327812595eceebffd490351910ce109092e46ca1
tamirms <tamirms@gmail.com>
2021-08-26 17:34:41 +0100

    Add migration for pool trades

---
+++ services/horizon/internal/db2/schema/migrations/51_add_pool_trades.sql
@@ -0,0 +1,27 @@
+-- +migrate Up
+
+DROP INDEX htrd_by_offer;
+DROP INDEX htrd_counter_lookup;
+
+ALTER TABLE history_trades DROP offer_id,
+                           ALTER base_account_id DROP NOT NULL,
+                           ALTER counter_account_id DROP NOT NULL,
+                           ADD base_liquidity_pool_id BIGINT,
+                           ADD counter_liquidity_pool_id BIGINT;
+
+CREATE INDEX htrd_by_base_liquidity_pool_id ON history_trades USING BTREE(base_liquidity_pool_id);
+CREATE INDEX htrd_by_counter_liquidity_pool_id ON history_trades USING BTREE(counter_liquidity_pool_id);
+
+-- +migrate Down
+
+DROP INDEX htrd_by_counter_liquidity_pool_id;
+DROP INDEX htrd_by_base_liquidity_pool_id;
+
+ALTER TABLE history_trades DROP counter_liquidity_pool_id,
+                           DROP base_liquidity_pool_id,
+                           ALTER counter_account_id DROP NOT NULL,
+                           ALTER base_account_id SET NOT NULL,
+                           ADD offer_id BIGINT;
+
+CREATE INDEX htrd_counter_lookup on history_trades USING BTREE(counter_asset_id);
+CREATE INDEX htrd_by_offer ON history_trades USING BTREE(offer_id);
