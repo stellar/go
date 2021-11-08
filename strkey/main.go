79c645d40db26264958f0b5373ff312d22942ae9
Marcelo Salloum <marcelo@stellar.org>
2021-11-08 10:54:08 -0300

    Moved MuxedAccount type to a separate file.

--- strkey/main.go
+++ strkey/main.go
@@ -4,9 +4,7 @@ import (
 	"bytes"
 	"encoding/base32"
 	"encoding/binary"
-	"fmt"
 
-	xdr "github.com/stellar/go-xdr/xdr3"
 	"github.com/stellar/go/strkey/internal/crc16"
 	"github.com/stellar/go/support/errors"
 )
@@ -266,69 +264,4 @@ func IsValidEd25519SecretSeed(i interface{}) bool {
 	_, err := Decode(VersionByteSeed, enc)
 
 	return err == nil
-}
-
-type MuxedAccount struct {
-	id      uint64
-	ed25519 [32]byte
-}
-
-// SetID populates the muxed account ID.
-func (m *MuxedAccount) SetID(id uint64) {
-	m.id = id
-}
-
-// SetAddress populates the muxed account ed25519 address.
-func (m *MuxedAccount) SetAddress(address string) error {
-	if !IsValidEd25519PublicKey(address) {
-		return errors.New("invalid ed25519 public key")
-	}
-
-	raw, err := Decode(VersionByteAccountID, address)
-	if err != nil {
-		return errors.Wrap(err, "decoding ed25519 address")
-	}
-	if len(raw) != 32 {
-		return fmt.Errorf("invalid binary length: %d", len(raw))
-	}
-
-	copy(m.ed25519[:], raw)
-
-	return nil
-}
-
-// ID returns the muxed account id according with SEP-23 definition for
-// multiplexed accounts.
-func (m *MuxedAccount) ID() uint64 {
-	return m.id
-}
-
-// Address returns the muxed account G-address according with SEP-23 definition
-// for multiplexed accounts.
-func (m *MuxedAccount) Address() (string, error) {
-	if m.ed25519 == [32]byte{} {
-		return "", errors.New("muxed account has no ed25519 key")
-	}
-
-	return Encode(VersionByteAccountID, m.ed25519[:])
-}
-
-// MuxedAddress returns the muxed account M-address according with SEP-23
-// definition for multiplexed accounts.
-func (m *MuxedAccount) MuxedAddress() (string, error) {
-	if m.ed25519 == [32]byte{} {
-		return "", errors.New("muxed account has no ed25519 key")
-	}
-
-	b := new(bytes.Buffer)
-	_, err := xdr.Marshal(b, m.id)
-	if err != nil {
-		return "", errors.Wrap(err, "marshaling muxed address id")
-	}
-
-	raw := make([]byte, 0, 40)
-	raw = append(raw, m.ed25519[:]...)
-	raw = append(raw, b.Bytes()...)
-
-	return Encode(VersionByteMuxedAccount, raw)
 }
---
+++ strkey/muxed_account_test.go
@@ -0,0 +1,75 @@
+package strkey
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMuxedAccount_id(t *testing.T) {
+	muxed := MuxedAccount{}
+	assert.Equal(t, uint64(0), muxed.ID())
+
+	muxed = MuxedAccount{id: uint64(9223372036854775808)}
+	assert.Equal(t, uint64(9223372036854775808), muxed.ID())
+}
+
+func TestMuxedAccount_setID(t *testing.T) {
+	muxed := MuxedAccount{}
+	muxed.SetID(123)
+	assert.Equal(t, uint64(123), muxed.ID())
+
+	muxed.SetID(456)
+	assert.Equal(t, uint64(456), muxed.ID())
+}
+
+func TestMuxedAccount_address(t *testing.T) {
+	muxed := MuxedAccount{}
+	publicKey, err := muxed.Address()
+	assert.EqualError(t, err, "muxed account has no ed25519 key")
+	assert.Empty(t, publicKey)
+
+	muxed = MuxedAccount{ed25519: [32]byte{63, 12, 52, 191, 147, 173, 13, 153, 113, 208, 76, 204, 144, 247, 5, 81, 28, 131, 138, 173, 151, 52, 164, 162, 251, 13, 122, 3, 252, 127, 232, 154}}
+	publicKey, err = muxed.Address()
+	assert.NoError(t, err)
+	assert.Equal(t, "GA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJVSGZ", publicKey)
+}
+
+func TestMuxedAccount_SetAddress(t *testing.T) {
+	muxed := MuxedAccount{}
+	err := muxed.SetAddress("")
+	assert.EqualError(t, err, "invalid ed25519 public key")
+
+	err = muxed.SetAddress("GA7QYNF7SOWQ3GLR2BGMZEHXAVIRZ")
+	assert.EqualError(t, err, "invalid ed25519 public key")
+
+	err = muxed.SetAddress("GA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJVSGZ")
+	assert.NoError(t, err)
+	publicKey, err := muxed.Address()
+	assert.NoError(t, err)
+	assert.Equal(t, "GA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJVSGZ", publicKey)
+	wantMuxed := MuxedAccount{ed25519: [32]byte{63, 12, 52, 191, 147, 173, 13, 153, 113, 208, 76, 204, 144, 247, 5, 81, 28, 131, 138, 173, 151, 52, 164, 162, 251, 13, 122, 3, 252, 127, 232, 154}}
+	assert.Equal(t, wantMuxed, muxed)
+
+	muxed.SetID(123)
+	wantMuxed = MuxedAccount{
+		ed25519: [32]byte{63, 12, 52, 191, 147, 173, 13, 153, 113, 208, 76, 204, 144, 247, 5, 81, 28, 131, 138, 173, 151, 52, 164, 162, 251, 13, 122, 3, 252, 127, 232, 154},
+		id:      123,
+	}
+	assert.Equal(t, wantMuxed, muxed)
+}
+
+func TestMuxedAccount_muxedAddress(t *testing.T) {
+	muxed := MuxedAccount{}
+	publicKey, err := muxed.MuxedAddress()
+	assert.EqualError(t, err, "muxed account has no ed25519 key")
+	assert.Empty(t, publicKey)
+
+	muxed = MuxedAccount{
+		id:      uint64(9223372036854775808),
+		ed25519: [32]byte{63, 12, 52, 191, 147, 173, 13, 153, 113, 208, 76, 204, 144, 247, 5, 81, 28, 131, 138, 173, 151, 52, 164, 162, 251, 13, 122, 3, 252, 127, 232, 154},
+	}
+	publicKey, err = muxed.MuxedAddress()
+	assert.NoError(t, err)
+	assert.Equal(t, "MA7QYNF7SOWQ3GLR2BGMZEHXAVIRZA4KVWLTJJFC7MGXUA74P7UJVAAAAAAAAAAAAAJLK", publicKey)
+}
