a3613da550af1fd431d92552d47c7ff1950625e0
Marcelo Salloum <marcelo@stellar.org>
2021-11-08 10:50:07 -0300

    Implement MuxedAccount setters.

--- strkey/main.go
+++ strkey/main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base32"
 	"encoding/binary"
+	"fmt"
 
 	xdr "github.com/stellar/go-xdr/xdr3"
 	"github.com/stellar/go/strkey/internal/crc16"
@@ -272,6 +273,30 @@ type MuxedAccount struct {
 	ed25519 [32]byte
 }
 
+// SetID populates the muxed account ID.
+func (m *MuxedAccount) SetID(id uint64) {
+	m.id = id
+}
+
+// SetAddress populates the muxed account ed25519 address.
+func (m *MuxedAccount) SetAddress(address string) error {
+	if !IsValidEd25519PublicKey(address) {
+		return errors.New("invalid ed25519 public key")
+	}
+
+	raw, err := Decode(VersionByteAccountID, address)
+	if err != nil {
+		return errors.Wrap(err, "decoding ed25519 address")
+	}
+	if len(raw) != 32 {
+		return fmt.Errorf("invalid binary length: %d", len(raw))
+	}
+
+	copy(m.ed25519[:], raw)
+
+	return nil
+}
+
 // ID returns the muxed account id according with SEP-23 definition for
 // multiplexed accounts.
 func (m *MuxedAccount) ID() uint64 {
@@ -288,6 +313,8 @@ func (m *MuxedAccount) Address() (string, error) {
 	return Encode(VersionByteAccountID, m.ed25519[:])
 }
 
+// MuxedAddress returns the muxed account M-address according with SEP-23
+// definition for multiplexed accounts.
 func (m *MuxedAccount) MuxedAddress() (string, error) {
 	if m.ed25519 == [32]byte{} {
 		return "", errors.New("muxed account has no ed25519 key")
