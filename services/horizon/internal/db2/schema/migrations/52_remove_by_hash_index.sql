93901f1995719746a4bcd11621952f5cfb510d9b
Bartek Nowotarski <bartek@nowotarski.info>
2021-11-17 17:41:42 +0100

    migration file

---
+++ services/horizon/internal/db2/schema/migrations/52_remove_by_hash_index.sql
@@ -0,0 +1,10 @@
+-- +migrate Up
+ALTER TABLE history_transactions ADD COLUMN transaction_hash_prefix bigint NULL;
+CREATE INDEX transaction_hash_prefix on history_transactions (transaction_hash_prefix);
+DROP INDEX by_hash;
+UPDATE history_transactions SET transaction_hash_prefix = ('x' || lpad(transaction_hash, 16, '0'))::bit(64)::bigint;
+
+-- +migrate Down
+CREATE INDEX by_hash ON history_transactions USING btree (transaction_hash);
+DROP INDEX transaction_hash_prefix;
+ALTER TABLE history_transactions DROP COLUMN transaction_hash_prefix;
