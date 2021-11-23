f4d2cb631f7220a2facdf1ef824df462567e5ac0
Bartek Nowotarski <bartek@nowotarski.info>
2021-11-23 13:40:04 +0100

    Revert index removal

--- services/horizon/internal/db2/schema/migrations/52_remove_by_hash_index.sql
+++ services/horizon/internal/db2/schema/migrations/52_remove_by_hash_index.sql
@@ -1,9 +1,7 @@
 -- +migrate Up
 ALTER TABLE history_transactions ADD COLUMN transaction_hash_prefix bigint;
 CREATE INDEX transaction_hash_prefix on history_transactions (transaction_hash_prefix);
-DROP INDEX by_hash;
 
 -- +migrate Down
-CREATE INDEX by_hash ON history_transactions USING btree (transaction_hash);
 DROP INDEX transaction_hash_prefix;
 ALTER TABLE history_transactions DROP COLUMN transaction_hash_prefix;
