431c3600c2026c08c6d95411a1c939a503f1c1ca
tamirms <tamirms@gmail.com>
2021-11-23 07:46:36 +0000

    Report memory allocations

--- exp/orderbook/graph_benchmark_test.go
+++ exp/orderbook/graph_benchmark_test.go
@@ -122,6 +122,7 @@ func BenchmarkVibrantPath(b *testing.B) {
 	}
 
 	b.ResetTimer()
+	b.ReportAllocs()
 	// https://horizon.stellar.org/paths/strict-send?source_asset_type=credit_alphanum4&source_asset_code=USDC&source_asset_issuer=GA5ZSEJYB37JRC5AVCIA5MOP4RHTM335X2KGX3IHOJAPP5RE34K4KZVN&source_amount=10&destination_assets=ARST%3AGCSAZVWXZKWS4XS223M5F54H2B6XPIIXZZGP7KEAIU6YSL5HDRGCI3DG
 
 	for i := 0; i < b.N; i++ {
@@ -157,6 +158,7 @@ func BenchmarkMultipleDestinationAssets(b *testing.B) {
 	}
 
 	b.ResetTimer()
+	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
 		_, _, err := graph.FindFixedPaths(
@@ -196,6 +198,7 @@ func BenchmarkTestData(b *testing.B) {
 	}
 
 	b.ResetTimer()
+	b.ReportAllocs()
 
 	for i := 0; i < b.N; i++ {
 		for _, req := range requests {
@@ -231,6 +234,8 @@ func BenchmarkLiquidityPoolDeposits(b *testing.B) {
 	amounts := createRandomAmounts(b.N)
 
 	b.ResetTimer()
+	b.ReportAllocs()
+
 	for _, amount := range amounts {
 		makeTrade(eurUsdLiquidityPool, usdAsset, tradeTypeDeposit, amount)
 	}
@@ -240,6 +245,8 @@ func BenchmarkLiquidityPoolExpectations(b *testing.B) {
 	amounts := createRandomAmounts(b.N)
 
 	b.ResetTimer()
+	b.ReportAllocs()
+
 	for _, amount := range amounts {
 		makeTrade(eurUsdLiquidityPool, usdAsset, tradeTypeExpectation, amount)
 	}
