02dbef9662e93e9f85123575da2262e6784b6b8f
Alfonso Acosta <alfonso@stellar.org>
2021-11-29 19:35:43 +0100

    exp/orderbook: Use uint128s for pool calculations to improve performance (#4104)

--- exp/orderbook/pools.go
+++ exp/orderbook/pools.go
@@ -2,7 +2,8 @@ package orderbook
 
 import (
 	"math"
-	"math/big"
+
+	"lukechampine.com/uint128"
 
 	"github.com/stellar/go/support/errors"
 	"github.com/stellar/go/xdr"
@@ -20,6 +21,7 @@ import (
 const (
 	tradeTypeDeposit     = iota // deposit into pool, what's the payout?
 	tradeTypeExpectation = iota // expect payout, what to deposit?
+	maxBips              = 10000
 )
 
 var (
@@ -27,6 +29,7 @@ var (
 	errBadPoolType   = errors.New("Unsupported liquidity pool: must be ConstantProduct")
 	errBadTradeType  = errors.New("Unknown pool exchange type requested")
 	errBadAmount     = errors.New("Exchange amount must be positive")
+	maxBips128       = uint128.From64(maxBips)
 )
 
 // makeTrade simulates execution of an exchange with a liquidity pool.
@@ -91,32 +94,31 @@ func makeTrade(
 //
 // It returns false if the calculation overflows.
 func calculatePoolPayout(reserveA, reserveB, received xdr.Int64, feeBips xdr.Int32) (xdr.Int64, bool) {
-	X, Y := big.NewInt(int64(reserveA)), big.NewInt(int64(reserveB))
-	F, x := big.NewInt(int64(feeBips)), big.NewInt(int64(received))
+	X, Y := uint128.From64(uint64(reserveA)), uint128.From64(uint64(reserveB))
+	F, x := uint128.From64(uint64(feeBips)), uint128.From64(uint64(received))
 
 	// would this deposit overflow the reserve?
-	if received > math.MaxInt64-reserveA {
+	// is feeBips within range?
+	if received > math.MaxInt64-reserveA || feeBips > maxBips {
 		return 0, false
 	}
 
 	// We do all of the math in bips, so it's all upscaled by this value.
-	maxBips := big.NewInt(10000)
-	f := new(big.Int).Sub(maxBips, F) // upscaled 1 - F
+	f := maxBips128.Sub(F) // upscaled 1 - F
 
 	// right half: X + (1 - F)x
-	denom := X.Mul(X, maxBips).Add(X, new(big.Int).Mul(x, f))
-	if denom.Cmp(big.NewInt(0)) == 0 { // avoid div-by-zero panic
+	denom := X.Mul(maxBips128).Add(x.Mul(f))
+	if denom.IsZero() { // avoid div-by-zero panic
 		return 0, false
 	}
 
 	// left half, a: (1 - F) Yx
-	numer := Y.Mul(Y, x).Mul(Y, f)
+	numer := Y.Mul(x).Mul(f)
 
 	// divide & check overflow
-	result := numer.Div(numer, denom)
+	result := numer.Div(denom)
 
-	i := xdr.Int64(result.Int64())
-	return i, result.IsInt64() && i > 0
+	return xdr.Int64(result.Lo), result.Hi == 0 && result.Lo <= math.MaxInt64
 }
 
 // calculatePoolExpectation determines how much of `reserveA` you would need to
@@ -128,34 +130,33 @@ func calculatePoolPayout(reserveA, reserveB, received xdr.Int64, feeBips xdr.Int
 func calculatePoolExpectation(
 	reserveA, reserveB, disbursed xdr.Int64, feeBips xdr.Int32,
 ) (xdr.Int64, bool) {
-	X, Y := big.NewInt(int64(reserveA)), big.NewInt(int64(reserveB))
-	F, y := big.NewInt(int64(feeBips)), big.NewInt(int64(disbursed))
+	X, Y := uint128.From64(uint64(reserveA)), uint128.From64(uint64(reserveB))
+	F, y := uint128.From64(uint64(feeBips)), uint128.From64(uint64(disbursed))
 
 	// sanity check: disbursing shouldn't underflow the reserve
-	if disbursed >= reserveB {
+	// and bips should be within the expected range
+	if disbursed >= reserveB || feeBips > maxBips {
 		return 0, false
 	}
 
 	// We do all of the math in bips, so it's all upscaled by this value.
-	maxBips := big.NewInt(10000)
-	f := new(big.Int).Sub(maxBips, F) // upscaled 1 - F
+	f := maxBips128.Sub(F) // upscaled 1 - F
 
-	denom := Y.Sub(Y, y).Mul(Y, f)     // right half: (Y - y)(1 - F)
-	if denom.Cmp(big.NewInt(0)) == 0 { // avoid div-by-zero panic
+	denom := Y.Sub(y).Mul(f) // right half: (Y - y)(1 - F)
+	if denom.IsZero() {      // avoid div-by-zero panic
 		return 0, false
 	}
 
-	numer := X.Mul(X, y).Mul(X, maxBips) // left half: Xy
+	numer := X.Mul(y).Mul(maxBips128) // left half: Xy
 
-	result, rem := new(big.Int), new(big.Int)
-	result.DivMod(numer, denom, rem)
+	result, rem := numer.QuoRem(denom)
 
 	// hacky way to ceil(): if there's a remainder, add 1
-	if rem.Cmp(big.NewInt(0)) > 0 {
-		result.Add(result, big.NewInt(1))
+	if rem.Cmp64(0) > 0 {
+		result = result.Add64(1)
 	}
 
-	return xdr.Int64(result.Int64()), result.IsInt64()
+	return xdr.Int64(result.Lo), result.Hi == 0 && result.Lo <= math.MaxInt64
 }
 
 // getOtherAsset returns the other asset in the liquidity pool. Note that
