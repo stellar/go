5f5b3dfb5e65015779a880c8ba7b6d7428321e92
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-12-08 11:59:36 -0800

    gxdr: generate using go:generate

--- gogenerate.sh
+++ gogenerate.sh
@@ -13,8 +13,5 @@ command -v go-bindata >/dev/null 2>&1 || (
 printf "Running go generate...\n"
 go generate ./...
 
-printf "Running make gxdr/xdr_generated.go...\n"
-make gxdr/xdr_generated.go
-
 printf "Checking for no diff...\n"
 git diff --exit-code || (echo "Files changed after running go generate. Run go generate ./... locally and update generated files." && exit 1)
