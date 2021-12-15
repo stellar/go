a4a16823c8ada8c506c49c4332dbde0a5093ca48
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-12-15 11:40:40 -0800

    Fix staticcheck

--- staticcheck.sh
+++ staticcheck.sh
@@ -3,9 +3,11 @@ set -e
 
 printf "Running staticcheck...\n"
 
+staticcheck='go run honnef.co/go/tools/cmd/staticcheck@2020.1.4'
+
 ls -d */ \
   | egrep -v '^vendor|^docs' \
-  | xargs -I {} staticcheck -tests=false -checks="all,-ST1003,-SA1019,-ST1005,-ST1000,-ST1016,-S1039,-ST1021,-ST1020,-ST1019,-SA4022" ./{}...
+  | xargs -I {} $staticcheck -tests=false -checks="all,-ST1003,-SA1019,-ST1005,-ST1000,-ST1016,-S1039,-ST1021,-ST1020,-ST1019,-SA4022" ./{}...
 
 # Check horizon for unused exported symbols (relying on the fact that it should be self-contained)
-go run honnef.co/go/tools/cmd/staticcheck@2020.1.4 -unused.whole-program -checks='U*' ./services/horizon/...
+$staticcheck -unused.whole-program -checks='U*' ./services/horizon/...
