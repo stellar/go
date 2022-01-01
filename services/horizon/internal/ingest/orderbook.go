df51071dce03e6f42c6ffef9545b891f26509640
Shawn Reuland <shawn@stellar.org>
2022-01-01 12:09:39 -0800

    1622: refactored GetAllOffers to a stream function

--- services/horizon/internal/ingest/orderbook.go
+++ services/horizon/internal/ingest/orderbook.go
@@ -132,26 +132,26 @@ func (o *OrderBookStream) update(ctx context.Context, status ingestionStatus) (b
 
 		defer o.graph.Discard()
 
-		offers, err := o.historyQ.GetAllOffers(ctx)
+		err := o.historyQ.StreamAllOffers(ctx, func(offer *history.Offer) error {
+			o.graph.AddOffers(offerToXDR(*offer))
+			return nil
+		})
+
 		if err != nil {
-			return true, errors.Wrap(err, "Error from GetAllOffers")
+			return true, errors.Wrap(err, "Error loading offers into orderbook")
 		}
 
-		liquidityPools, err := o.historyQ.GetAllLiquidityPools(ctx)
-		if err != nil {
-			return true, errors.Wrap(err, "Error from GetAllLiquidityPools")
-		}
-
-		for _, offer := range offers {
-			o.graph.AddOffers(offerToXDR(offer))
-		}
-
-		for _, liquidityPool := range liquidityPools {
-			liquidityPoolXDR, err := liquidityPoolToXDR(liquidityPool)
-			if err != nil {
-				return true, errors.Wrap(err, "Invalid liquidity pool row")
+		err = o.historyQ.StreamAllLiquidityPools(ctx, func(liquidityPool *history.LiquidityPool) error {
+			if liquidityPoolXDR, liquidityPoolErr := liquidityPoolToXDR(*liquidityPool); liquidityPoolErr != nil {
+				return errors.Wrapf(liquidityPoolErr, "Invalid liquidity pool row %v, unable to marshal to xdr", *liquidityPool)
+			} else {
+				o.graph.AddLiquidityPools(liquidityPoolXDR)
+				return nil
 			}
-			o.graph.AddLiquidityPools(liquidityPoolXDR)
+		})
+
+		if err != nil {
+			return true, errors.Wrap(err, "Error loading liquidity pools into orderbook")
 		}
 
 		if err := o.graph.Apply(status.LastIngestedLedger); err != nil {
@@ -209,9 +209,14 @@ func (o *OrderBookStream) update(ctx context.Context, status ingestionStatus) (b
 }
 
 func (o *OrderBookStream) verifyAllOffers(ctx context.Context, offers []xdr.OfferEntry) (bool, error) {
-	ingestionOffers, err := o.historyQ.GetAllOffers(ctx)
+	var ingestionOffers []*history.Offer
+	err := o.historyQ.StreamAllOffers(ctx, func(offer *history.Offer) error {
+		ingestionOffers = append(ingestionOffers, offer)
+		return nil
+	})
+
 	if err != nil {
-		return false, errors.Wrap(err, "Error from GetAllOffers")
+		return false, errors.Wrap(err, "Error loading all offers for orderbook verification")
 	}
 
 	mismatch := len(offers) != len(ingestionOffers)
@@ -226,7 +231,7 @@ func (o *OrderBookStream) verifyAllOffers(ctx context.Context, offers []xdr.Offe
 
 		for i, offerRow := range ingestionOffers {
 			offerEntry := offers[i]
-			offerRowXDR := offerToXDR(offerRow)
+			offerRowXDR := offerToXDR(*offerRow)
 			offerEntryBase64, err := o.encodingBuffer.MarshalBase64(&offerEntry)
 			if err != nil {
 				return false, errors.Wrap(err, "Error from marshalling offerEntry")
@@ -253,9 +258,15 @@ func (o *OrderBookStream) verifyAllOffers(ctx context.Context, offers []xdr.Offe
 }
 
 func (o *OrderBookStream) verifyAllLiquidityPools(ctx context.Context, liquidityPools []xdr.LiquidityPoolEntry) (bool, error) {
-	ingestionLiquidityPools, err := o.historyQ.GetAllLiquidityPools(ctx)
+	var ingestionLiquidityPools []*history.LiquidityPool
+
+	err := o.historyQ.StreamAllLiquidityPools(ctx, func(liquidityPool *history.LiquidityPool) error {
+		ingestionLiquidityPools = append(ingestionLiquidityPools, liquidityPool)
+		return nil
+	})
+
 	if err != nil {
-		return false, errors.Wrap(err, "Error from GetAllLiquidityPools")
+		return false, errors.Wrap(err, "Error loading all liquidity pools for orderbook verification")
 	}
 
 	mismatch := len(liquidityPools) != len(ingestionLiquidityPools)
@@ -271,7 +282,7 @@ func (o *OrderBookStream) verifyAllLiquidityPools(ctx context.Context, liquidity
 
 		for i, liquidityPoolRow := range ingestionLiquidityPools {
 			liquidityPoolEntry := liquidityPools[i]
-			liquidityPoolRowXDR, err := liquidityPoolToXDR(liquidityPoolRow)
+			liquidityPoolRowXDR, err := liquidityPoolToXDR(*liquidityPoolRow)
 			if err != nil {
 				return false, errors.Wrap(err, "Error from converting liquidity pool row to xdr")
 			}
@@ -322,7 +333,7 @@ func (o *OrderBookStream) Update(ctx context.Context) error {
 	}
 
 	// add 15 minute jitter so that not all horizon nodes are calling
-	// historyQ.GetAllOffers at the same time
+	// historyQ.StreamAllOffers at the same time
 	jitter := time.Duration(rand.Int63n(int64(15 * time.Minute)))
 	requiresVerification := o.lastLedger > 0 &&
 		time.Since(o.lastVerification) >= verificationFrequency+jitter
