483e05721152629cc6448914f5115bc8cdfd0803
Paul Bellamy <paul@stellar.org>
2022-01-13 17:39:26 +0000

    Set docker platform flag so core works on ARM Macs (#4177)

--- integration.sh
+++ integration.sh
@@ -12,7 +12,12 @@ export TRACY_NO_INVARIANT_CHECK=1 # This fails on my dev vm. - Paul
 # launch postgres if it's not already.
 if [[ "$(docker inspect integration_postgres -f '{{.State.Running}}')" != "true" ]]; then
   docker rm -f integration_postgres || true;
-  docker run -d --name integration_postgres --env POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 circleci/postgres:9.6.5-alpine
+  docker run -d \
+    --name integration_postgres \
+    --platform linux/amd64 \
+    --env POSTGRES_HOST_AUTH_METHOD=trust \
+    -p 5432:5432 \
+    circleci/postgres:9.6.5-alpine
 fi
 
 exec go test -timeout 25m github.com/stellar/go/services/horizon/internal/integration/... "$@"
