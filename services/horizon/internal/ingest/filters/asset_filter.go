65ef6f2b45bc0ecb3225e3c46af6c0e45687e3bd
Shawn Reuland <shawn@stellar.org>
2022-01-18 13:13:54 -0800

    #4170: fixed formatting on new code

--- services/horizon/internal/ingest/filters/asset_filter.go
+++ services/horizon/internal/ingest/filters/asset_filter.go
@@ -7,32 +7,29 @@ import (
 )
 
 type AssetFilterParms struct {
-	canonicalAssetList               []string // list of fully qualified asset canonical id <issuer:code>
-	assetIssuerList                  []string // list of just the account id as the asset issuer value, 
-	                                          // filter will match on any asset reference with same issuer
-	assetCodeList                    []string // list of just asset codes, filter will match on any asset reference with same code
-	resolveLiquidityPoolAsAsset      bool     // if a liquidity pool references a filtered asset, then include all operation 
-	                                          // types within 'traverseOperationsList' that are related to the pool
-	traverseEffects                  bool     // true means generate effects for any operations referencing a filtered asset
-	traverseOperationsList           []string // list of 'Offers, Trades, Payments, TrustLine, Claimable Balance', include any 
-	                                          // of these operation types when they reference a filtered asset 
+	canonicalAssetList []string // list of fully qualified asset canonical id <issuer:code>
+	assetIssuerList    []string // list of just the account id as the asset issuer value,
+	// filter will match on any asset reference with same issuer
+	assetCodeList               []string // list of just asset codes, filter will match on any asset reference with same code
+	resolveLiquidityPoolAsAsset bool     // if a liquidity pool references a filtered asset, then include all operation
+	// types within 'traverseOperationsList' that are related to the pool
+	traverseEffects        bool     // true means generate effects for any operations referencing a filtered asset
+	traverseOperationsList []string // list of 'Offers, Trades, Payments, TrustLine, Claimable Balance', include any
+	// of these operation types when they reference a filtered asset
 }
 
 type AssetFilter struct {
-	filterParams               *AssetFilterParms
+	filterParams *AssetFilterParms
 }
 
 func NewAssetFilter(filterParams *AssetFilterParms) *AssetFilter {
 	filter := &AssetFilter{
-		filterParams:         filterParams,
+		filterParams: filterParams,
 	}
 	return filter
 }
 
-
-
 func (p *AssetFilter) FilterTransaction(ctx context.Context, transaction ingest.LedgerTransaction) (bool, error) {
 	//TODO implement the asset filter based on filter params and intropsectin ops for matching references to asset in tx
-    return true, nil
+	return true, nil
 }
-
