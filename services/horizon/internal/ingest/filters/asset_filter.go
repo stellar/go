739e1075f9d78701f21b892caa34e55d4e6dccd9
Shawn Reuland <shawn@stellar.org>
2022-01-19 15:49:28 -0800

    #4170: fixed formatting checks

--- services/horizon/internal/ingest/filters/asset_filter.go
+++ services/horizon/internal/ingest/filters/asset_filter.go
@@ -12,10 +12,11 @@ import (
 )
 
 var logger *log.Entry
-func init () {
+
+func init() {
 	logger = log.WithFields(log.F{
-			"ingest filter": "asset",
-		})
+		"ingest filter": "asset",
+	})
 }
 
 type AssetFilterParms struct {
@@ -46,11 +47,11 @@ type AssetFilterParms struct {
 }
 
 type AssetFilter struct {
-	filterParams *AssetFilterParms
-	canonicalAssetsLookup map[string]bool 
-	assetIssuersLookup map[string]bool 
-	assetCodesLookup map[string]bool 
-	traverseOperationsLookup map[string]bool 
+	filterParams             *AssetFilterParms
+	canonicalAssetsLookup    map[string]bool
+	assetIssuersLookup       map[string]bool
+	assetCodesLookup         map[string]bool
+	traverseOperationsLookup map[string]bool
 }
 
 func NewAssetFilterFromParamsFile(filterParamsFilePath string) (*AssetFilter, error) {
@@ -69,10 +70,10 @@ func NewAssetFilterFromParamsFile(filterParamsFilePath string) (*AssetFilter, er
 
 func NewAssetFilterFromParams(filterParams *AssetFilterParms) *AssetFilter {
 	filter := &AssetFilter{
-		filterParams: filterParams,
-		canonicalAssetsLookup: listToMap(filterParams.CanonicalAssetList),
-		assetIssuersLookup: listToMap(filterParams.AssetIssuerList),
-		assetCodesLookup: listToMap(filterParams.AssetCodeList),
+		filterParams:             filterParams,
+		canonicalAssetsLookup:    listToMap(filterParams.CanonicalAssetList),
+		assetIssuersLookup:       listToMap(filterParams.AssetIssuerList),
+		assetCodesLookup:         listToMap(filterParams.AssetCodeList),
 		traverseOperationsLookup: listToMap(filterParams.TraverseOperationsList),
 	}
 	return filter
@@ -83,40 +84,40 @@ func (f *AssetFilter) CurrentFilterParameters() AssetFilterParms {
 }
 
 func (f *AssetFilter) FilterTransaction(ctx context.Context, transaction ingest.LedgerTransaction) (bool, error) {
-	if (!f.filterParams.Activated) {
+	if !f.filterParams.Activated {
 		return true, nil
 	}
 
 	tx, v1Exists := transaction.Envelope.GetV1()
-    if (!v1Exists) {
-        return true, nil
+	if !v1Exists {
+		return true, nil
 	}
 
 	var allowedOperations []*xdr.Operation
 
 	for _, operation := range tx.Tx.Operations {
 		var allowed = true
-		switch operation.Body.Type { 
-			case xdr.OperationTypePayment:
-				if (!f.assetMatchedFilter(&operation.Body.PaymentOp.Asset)) {
-					allowed = false
-				}
-			case xdr.OperationTypePathPaymentStrictReceive: 
-			    if (!f.assetMatchedFilter(&operation.Body.PathPaymentStrictReceiveOp.DestAsset) && !f.assetMatchedFilter(&operation.Body.PathPaymentStrictReceiveOp.SendAsset)) {
-					allowed = false
-				}
-			case xdr.OperationTypeLiquidityPoolDeposit:
-				if (!f.includeLiquidityPool(operation.Body.LiquidityPoolDepositOp.LiquidityPoolId)) {
-					allowed = false
-				}
-				
-			case xdr.OperationTypeLiquidityPoolWithdraw:
-				if (!f.includeLiquidityPool(operation.Body.LiquidityPoolWithdrawOp.LiquidityPoolId)) {
-					allowed = false
-				}
+		switch operation.Body.Type {
+		case xdr.OperationTypePayment:
+			if !f.assetMatchedFilter(&operation.Body.PaymentOp.Asset) {
+				allowed = false
+			}
+		case xdr.OperationTypePathPaymentStrictReceive:
+			if !f.assetMatchedFilter(&operation.Body.PathPaymentStrictReceiveOp.DestAsset) && !f.assetMatchedFilter(&operation.Body.PathPaymentStrictReceiveOp.SendAsset) {
+				allowed = false
+			}
+		case xdr.OperationTypeLiquidityPoolDeposit:
+			if !f.includeLiquidityPool(operation.Body.LiquidityPoolDepositOp.LiquidityPoolId) {
+				allowed = false
+			}
+
+		case xdr.OperationTypeLiquidityPoolWithdraw:
+			if !f.includeLiquidityPool(operation.Body.LiquidityPoolWithdrawOp.LiquidityPoolId) {
+				allowed = false
+			}
 		}
 
-        if (allowed && f.operationMatchedFilterDepth(operation.Body.Type)) {
+		if allowed && f.operationMatchedFilterDepth(operation.Body.Type) {
 			allowedOperations = append(allowedOperations, &operation)
 		}
 	}
@@ -126,50 +127,50 @@ func (f *AssetFilter) FilterTransaction(ctx context.Context, transaction ingest.
 }
 
 func (f *AssetFilter) operationMatchedFilterDepth(operationType xdr.OperationType) bool {
-    if (len (f.traverseOperationsLookup) < 1) {
+	if len(f.traverseOperationsLookup) < 1 {
 		return true
 	}
-    _, found := f.traverseOperationsLookup[operationType.String()]
+	_, found := f.traverseOperationsLookup[operationType.String()]
 	return found
 }
 
 func (f *AssetFilter) assetMatchedFilter(asset *xdr.Asset) bool {
 
-	var matched = false;
+	var matched = false
 
-    if _, found := f.canonicalAssetsLookup[asset.StringCanonical()]; found {
+	if _, found := f.canonicalAssetsLookup[asset.StringCanonical()]; found {
 		matched = true
 	} else if _, found := f.assetCodesLookup[asset.GetCode()]; found {
 		matched = true
 	} else if _, found := f.assetIssuersLookup[asset.GetIssuer()]; found {
 		matched = true
 	}
- 
-    return matched
+
+	return matched
 }
 
 func (f *AssetFilter) includeLiquidityPool(poolId xdr.PoolId) bool {
-	if (f.filterParams.ResolveLiquidityPoolAsAsset) {
+	if f.filterParams.ResolveLiquidityPoolAsAsset {
 		assets := f.resolveLiquidityPoolToAssetPair(poolId)
 		for _, asset := range assets {
-			if (!f.assetMatchedFilter(asset)) {
+			if !f.assetMatchedFilter(asset) {
 				return false
 			}
 		}
 		return true
 	}
-	return false;			
+	return false
 }
 
 func (f *AssetFilter) resolveLiquidityPoolToAssetPair(poolId xdr.PoolId) []*xdr.Asset {
 	//TODO - implement the resolutiuon to asset pair
-    return []*xdr.Asset{}
+	return []*xdr.Asset{}
 }
 
 func listToMap(list []string) map[string]bool {
-   set := make(map[string]bool)
-   for i := 0; i < len(list); i ++ {
-       set[list[i]] = true
-   }
-   return set
+	set := make(map[string]bool)
+	for i := 0; i < len(list); i++ {
+		set[list[i]] = true
+	}
+	return set
 }
