403e43713a65df942d09067a6091ed009a26e7b7
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-02-22 17:21:50 -0800

    remove unused.whole-program that is gone in new staticchecks

--- staticcheck.sh
+++ staticcheck.sh
@@ -9,7 +9,3 @@ printf "Running staticcheck $version...\n"
 ls -d */ \
   | egrep -v '^vendor|^docs' \
   | xargs -I {} $staticcheck -tests=false -checks="all,-ST1003,-SA1019,-ST1005,-ST1000,-ST1016,-S1039,-ST1021,-ST1020,-ST1019,-SA4022" ./{}...
-
-
-# Check horizon for unused exported symbols (relying on the fact that it should be self-contained)
-$staticcheck -unused.whole-program -checks='U*' ./services/horizon/...
