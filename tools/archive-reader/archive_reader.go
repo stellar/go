906e5bda4077bb45b73439d34e4cd4e37e0b6497
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-02-23 07:27:22 -0800

    all: staticcheck fixes (#4239)

--- tools/archive-reader/archive_reader.go
+++ tools/archive-reader/archive_reader.go
@@ -14,7 +14,7 @@ import (
 func main() {
 	ledgerPtr := flag.Uint64("ledger", 0, "`ledger to analyze` (tip: has to be of the form `ledger = 64*n - 1`, where n is > 0)")
 	flag.Parse()
-	var seqNum uint32 = uint32(*ledgerPtr)
+	seqNum := uint32(*ledgerPtr)
 
 	if seqNum == 0 {
 		flag.Usage()
