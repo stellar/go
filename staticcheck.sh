3861d326e74db1a356672a038e6e0bfdd9707faa
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-02-24 13:24:10 -0800

    Still run horizon check if can

--- staticcheck.sh
+++ staticcheck.sh
@@ -15,3 +15,8 @@ printf "Running staticcheck $version...\n"
 ls -d */ \
   | egrep -v '^vendor|^docs' \
   | xargs -I {} $staticcheck -tests=false -checks="all,-ST1003,-SA1019,-ST1005,-ST1000,-ST1016,-S1039,-ST1021,-ST1020,-ST1019,-SA4022" ./{}...
+
+if [ "$version" = "2020.1.4" ]; then
+  # Check horizon for unused exported symbols (relying on the fact that it should be self-contained)
+  $staticcheck -unused.whole-program -checks='U*' ./services/horizon/...
+fi
