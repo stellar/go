e1bc772b593146c2f7a0490d0fa41af136dc74c5
Bartek Nowotarski <bartek@nowotarski.info>
2022-03-01 00:44:11 +0100

    services/horizon: Add LedgerHashStore to Captive-Core config

--- services/horizon/internal/ingest/main.go
+++ services/horizon/internal/ingest/main.go
@@ -232,6 +232,7 @@ func NewSystem(config Config) (System, error) {
 					NetworkPassphrase:   config.NetworkPassphrase,
 					HistoryArchiveURLs:  []string{config.HistoryArchiveURL},
 					CheckpointFrequency: config.CheckpointFrequency,
+					LedgerHashStore:     ledgerbackend.NewHorizonDBLedgerHashStore(config.HistorySession),
 					Log:                 logger,
 					Context:             ctx,
 				},
