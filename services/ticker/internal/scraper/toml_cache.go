5a7857188952458d7e1c5d157c2d0b5f63d5973a
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2022-03-16 14:14:09 -0700

    services/ticker: cache only one toml at a time

--- services/ticker/internal/scraper/toml_cache.go
+++ services/ticker/internal/scraper/toml_cache.go
@@ -1,17 +1,27 @@
 package scraper
 
+// TOMLCache caches a max of one toml at a time. Old records are discarded when
+// new records are added.
 type TOMLCache struct {
-	tomlCache map[string]TOMLIssuer
+	set bool
+	tomlURL    string
+	tomlIssuer TOMLIssuer
 }
 
+// Get returns the toml for the given URL if it is cached. Ok is true only if
+// the toml returned was found in the cache.
 func (c TOMLCache) Get(tomlURL string) (tomlIssuer TOMLIssuer, ok bool) {
-	tomlIssuer, ok = c.tomlCache[tomlURL]
+	if c.set && c.tomlURL == tomlURL {
+		tomlIssuer = c.tomlIssuer
+		ok = true
+	}
 	return
 }
 
+// Set adds the given toml to the cache using the toml URL as its key for lookup
+// later.
 func (c *TOMLCache) Set(tomlURL string, tomlIssuer TOMLIssuer) {
-	if c.tomlCache == nil {
-		c.tomlCache = map[string]TOMLIssuer{}
-	}
-	c.tomlCache[tomlURL] = tomlIssuer
+	c.set = true
+	c.tomlURL = tomlURL
+	c.tomlIssuer = tomlIssuer
 }
