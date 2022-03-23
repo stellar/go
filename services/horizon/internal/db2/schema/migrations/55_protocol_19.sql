4574288d92676f95a3c6cdc4c70e432c5d3377e3
Paul Bellamy <paul@stellar.org>
2022-03-23 16:46:08 +0000

    use text instead of varchar, to avoid future migrations

--- services/horizon/internal/db2/schema/migrations/55_protocol_19.sql
+++ services/horizon/internal/db2/schema/migrations/55_protocol_19.sql
@@ -3,7 +3,7 @@ ALTER TABLE history_transactions ADD ledger_bounds                   int8range;
 ALTER TABLE history_transactions ADD min_account_sequence            bigint;
 ALTER TABLE history_transactions ADD min_account_sequence_age        bigint;
 ALTER TABLE history_transactions ADD min_account_sequence_ledger_gap bigint;
-ALTER TABLE history_transactions ADD extra_signers                   character varying(165)[];
+ALTER TABLE history_transactions ADD extra_signers                   text[];
 
 -- +migrate Down
 ALTER TABLE history_transactions DROP ledger_bounds;
