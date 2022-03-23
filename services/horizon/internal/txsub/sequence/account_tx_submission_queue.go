00d541d3f6ef75dd835a9e327268f0c0d3a7fcf7
Alfonso Acosta <alfonso@stellar.org>
2022-03-23 15:28:50 +0100

    Use a sorted slice instead of a heap

--- services/horizon/internal/txsub/sequence/account_tx_submission_queue.go
+++ services/horizon/internal/txsub/sequence/account_tx_submission_queue.go
@@ -1,7 +1,7 @@
 package sequence
 
 import (
-	"container/heap"
+	"sort"
 	"time"
 )
 
@@ -17,7 +17,7 @@ type AccountTxSubmissionQueue struct {
 	lastActiveAt            time.Time
 	timeout                 time.Duration
 	lastSeenAccountSequence uint64
-	queue                   pqueue
+	queue                   submissionQueue
 }
 
 // NewAccountTxSubmissionQueue creates a new *AccountTxSubmissionQueue
@@ -27,9 +27,6 @@ func NewAccountTxSubmissionQueue() *AccountTxSubmissionQueue {
 		timeout:      10 * time.Second,
 		queue:        nil,
 	}
-
-	heap.Init(&result.queue)
-
 	return result
 }
 
@@ -53,13 +50,13 @@ func (q *AccountTxSubmissionQueue) Size() int {
 //		possible
 func (q *AccountTxSubmissionQueue) Push(sequence uint64, minSeqNum *uint64) <-chan error {
 	ch := make(chan error, 1)
-	// From CAP 19: If minSeqNum is nil, the tx is only valid when sourceAccount's sequence number is seqNum - 1.
-	// Otherwise, valid when sourceAccount's sequence number n satisfies minSeqNum <= n < tx.seqNum.
+	// From CAP 21: If minSeqNum is nil, the txToSubmit is only valid when sourceAccount's sequence number is seqNum - 1.
+	// Otherwise, valid when sourceAccount's sequence number n satisfies minSeqNum <= n < txToSubmit.seqNum.
 	effectiveMinSeqNum := sequence - 1
 	if minSeqNum != nil {
 		effectiveMinSeqNum = *minSeqNum
 	}
-	heap.Push(&q.queue, item{
+	q.queue.insert(txToSubmit{
 		MinAccSeqNum: effectiveMinSeqNum,
 		MaxAccSeqNum: sequence - 1,
 		Chan:         ch,
@@ -81,14 +78,14 @@ func (q *AccountTxSubmissionQueue) NotifyLastAccountSequence(sequence uint64) {
 
 	// We need to traverse the full queue (ordered by MaxAccSeqNum)
 	// in case there is a transaction with a submittable MinSeqNum we can use later on.
-	for i := 0; i < q.Size(); {
+	for i := 0; i < len(q.queue); {
 		tx := q.queue[i]
 
 		removeWithErr := func(err error) {
 			tx.Chan <- err
 			close(tx.Chan)
 			wasChanged = true
-			heap.Remove(&q.queue, i)
+			q.queue.remove(i)
 		}
 
 		if q.lastSeenAccountSequence > tx.MaxAccSeqNum {
@@ -98,8 +95,8 @@ func (q *AccountTxSubmissionQueue) NotifyLastAccountSequence(sequence uint64) {
 			// within range, ready to submit!
 			removeWithErr(nil)
 		} else {
-			// we only need to increment the heap index when we don't remove
-			// an item
+			// we only need to increment the index when we don't remove
+			// a transaction
 			i++
 		}
 	}
@@ -111,52 +108,49 @@ func (q *AccountTxSubmissionQueue) NotifyLastAccountSequence(sequence uint64) {
 	}
 
 	// if the queue wasn't changed, see if it is too old, clear
-	// it and make room for other's
+	// it and make room for other submissions
 	if time.Since(q.lastActiveAt) > q.timeout {
-		for q.Size() > 0 {
-			entry := q.queue.Pop().(item)
-			entry.Chan <- ErrBadSequence
-			close(entry.Chan)
+		for i := 0; i < len(q.queue); i++ {
+			c := q.queue[i].Chan
+			c <- ErrBadSequence
+			close(c)
 		}
+		q.queue = nil
 	}
 }
 
-// item is a element of the priority queue
-type item struct {
+// txToSubmit represents a transaction being tracked in submissionQueue
+type txToSubmit struct {
 	MinAccSeqNum uint64 // minimum account sequence required to send the transaction
 	MaxAccSeqNum uint64 // maximum account sequence required to send the transaction
 	Chan         chan error
 }
 
-// pqueue is a priority queue used by AccountTxSubmissionQueue to manage buffered submissions.  It
-// implements heap.Interface.
-type pqueue []item
+// submissionQueue is a priority queue (implemented as a sorted slice),
+// used to track the submission order of transactions.
+// Lower indices have higher submission priority.
+type submissionQueue []txToSubmit
 
-func (pq pqueue) Len() int { return len(pq) }
-
-func (pq pqueue) Less(i, j int) bool {
-	// To maximize tx submission opportunity, order transactions by the account sequence
-	// which would result from successful submission (MaxAccSeqNum+1) but,
-	// if those are the same, by higher minimum sequence since there is less margin to send those
-	// (a smaller interval).
-	if pq[i].MaxAccSeqNum != pq[j].MaxAccSeqNum {
-		return pq[i].MaxAccSeqNum < pq[j].MaxAccSeqNum
+func (sq *submissionQueue) insert(tx txToSubmit) {
+	txHasLessPriorityThanIth := func(i int) bool {
+		// To maximize transaction submission opportunity, we prioritize transactions by the account sequence
+		// which would result from successful submission (i.e. MaxAccSeqNum+1) but,
+		// if those are the same, by higher minimum sequence since there is less margin to send those
+		// (a smaller interval).
+		if tx.MaxAccSeqNum != (*sq)[i].MaxAccSeqNum {
+			return tx.MaxAccSeqNum < (*sq)[i].MaxAccSeqNum
+		}
+		return tx.MinAccSeqNum > (*sq)[i].MinAccSeqNum
 	}
-	return pq[i].MinAccSeqNum > pq[j].MinAccSeqNum
+	i := sort.Search(len(*sq), txHasLessPriorityThanIth)
+	if len(*sq) == i {
+		*sq = append(*sq, tx)
+		return
+	}
+	*sq = append((*sq)[:i+1], (*sq)[i:]...)
+	(*sq)[i] = tx
 }
 
-func (pq pqueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-}
-
-func (pq *pqueue) Push(x interface{}) {
-	*pq = append(*pq, x.(item))
-}
-
-func (pq *pqueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	result := old[n-1]
-	*pq = old[0 : n-1]
-	return result
+func (sq *submissionQueue) remove(i int) {
+	*sq = append((*sq)[:i], (*sq)[i+1:]...)
 }
