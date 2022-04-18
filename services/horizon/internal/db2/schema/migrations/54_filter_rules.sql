b036594af6bdc9c3de524106237a28fd27d7b12f
Shawn Reuland <shawn@stellar.org>
2022-04-18 11:27:59 -0700

    #4221: added ddl for new txsub response table

--- services/horizon/internal/db2/schema/migrations/54_filter_rules.sql
+++ services/horizon/internal/db2/schema/migrations/54_filter_rules.sql
@@ -12,6 +12,12 @@ CREATE TABLE asset_filter_rules (
     last_modified bigint NOT NULL
 );
 
+CREATE TABLE txsub_response (
+    transaction_submitted bigint NOT NULL,
+    transaction_hash character varying(64) NOT NULL,
+    transaction_result text
+);
+
 -- insert the default disabled state for each supported filter implementation
 INSERT INTO account_filter_rules VALUES (false, '{}', 0);
 INSERT INTO asset_filter_rules VALUES (false, '{}', 0);
