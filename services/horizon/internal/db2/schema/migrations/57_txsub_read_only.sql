18719f81bf16175ddbbca3ac85b54e88ca366356
Shawn Reuland <shawn@stellar.org>
2022-06-06 12:19:14 -0700

    updated comments on tmp sql migration to use CREATE..AS..SELECT per agreement on pr feedback for using this mechanism

--- services/horizon/internal/db2/schema/migrations/57_txsub_read_only.sql
+++ services/horizon/internal/db2/schema/migrations/57_txsub_read_only.sql
@@ -4,9 +4,10 @@ DROP TABLE txsub_results;
 
 -- used to temporarily store filtered-out transactions
 -- needed by the transaction system
--- TODO - is this acceptable to do CREATE..SELECT..AS, 
+--  
 -- any future migrations that would change history_transactions
 -- require dropping history_transactions_filtered_tmp, and rerunning this CREATE..SELECT..AS
+-- to keep the tmp table in sync with schema.
 CREATE TABLE history_transactions_filtered_tmp AS 
   select * FROM history_transactions
   WHERE ledger_sequence IS NULL;
