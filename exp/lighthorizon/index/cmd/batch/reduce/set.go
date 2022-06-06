e0a0436b05114e2de09a2c42b82515d55be2e03b
George Kudrayvtsev <george@stellar.org>
2022-06-06 10:59:14 -0700

    Use a read-write mutex for 'free' concurrent set reads

--- exp/lighthorizon/index/cmd/batch/reduce/set.go
+++ exp/lighthorizon/index/cmd/batch/reduce/set.go
@@ -4,20 +4,20 @@ import "sync"
 
 // SafeStringSet is a simple thread-safe set.
 type SafeStringSet struct {
-    lock sync.Mutex
+    lock sync.RWMutex
     set  map[string]struct{}
 }
 
 func NewSafeStringSet() *SafeStringSet {
     return &SafeStringSet{
-        lock: sync.Mutex{},
+        lock: sync.RWMutex{},
         set:  map[string]struct{}{},
     }
 }
 
 func (set *SafeStringSet) Contains(key string) bool {
-    defer set.lock.Unlock()
-    set.lock.Lock()
+    defer set.lock.RUnlock()
+    set.lock.RLock()
     _, ok := set.set[key]
     return ok
 }
