9aff46a24462912c8d76db646f3c11e125a5d2e0
Alfonso Acosta <alfonso@stellar.org>
2022-07-01 15:43:58 +0200

    Run gofmt -w (I don't know why those files were changed)

--- exp/lighthorizon/index/cmd/batch/reduce/set.go
+++ exp/lighthorizon/index/cmd/batch/reduce/set.go
@@ -4,26 +4,26 @@ import "sync"
 
 // SafeStringSet is a simple thread-safe set.
 type SafeStringSet struct {
-    lock sync.RWMutex
-    set  map[string]struct{}
+	lock sync.RWMutex
+	set  map[string]struct{}
 }
 
 func NewSafeStringSet() *SafeStringSet {
-    return &SafeStringSet{
-        lock: sync.RWMutex{},
-        set:  map[string]struct{}{},
-    }
+	return &SafeStringSet{
+		lock: sync.RWMutex{},
+		set:  map[string]struct{}{},
+	}
 }
 
 func (set *SafeStringSet) Contains(key string) bool {
-    defer set.lock.RUnlock()
-    set.lock.RLock()
-    _, ok := set.set[key]
-    return ok
+	defer set.lock.RUnlock()
+	set.lock.RLock()
+	_, ok := set.set[key]
+	return ok
 }
 
 func (set *SafeStringSet) Add(key string) {
-    defer set.lock.Unlock()
-    set.lock.Lock()
-    set.set[key] = struct{}{}
+	defer set.lock.Unlock()
+	set.lock.Lock()
+	set.set[key] = struct{}{}
 }
