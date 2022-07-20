127e524ff4374f4b2c0f31c6373177b95f7759bc
Bartek Nowotarski <bartek@nowotarski.info>
2022-07-20 12:37:12 +0200

    services/horizon: Improve state verifier error message

--- services/horizon/internal/ingest/verify/main.go
+++ services/horizon/internal/ingest/verify/main.go
@@ -157,7 +157,7 @@ func (v *StateVerifier) Write(entry xdr.LedgerEntry) error {
 
 	if !bytes.Equal(actualEntryMarshaled, expectedEntryMarshaled) {
 		return ingest.NewStateError(errors.Errorf(
-			"Entry does not match the fetched entry. Expected: %s (pretransform = %s), actual: %s",
+			"Entry does not match the fetched entry. Expected (history archive): %s (pretransform = %s), actual (horizon): %s",
 			base64.StdEncoding.EncodeToString(expectedEntryMarshaled),
 			base64.StdEncoding.EncodeToString(preTransformExpectedEntryMarshaled),
 			base64.StdEncoding.EncodeToString(actualEntryMarshaled),
