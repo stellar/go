863480664b570f0797bda4b4906c48c7b84aa5d6
George Kudrayvtsev <george@stellar.org>
2022-07-25 09:57:21 -0700

    Re-order file contents to put initialization function at the top

--- exp/lighthorizon/archive/ingest_archive.go
+++ exp/lighthorizon/archive/ingest_archive.go
@@ -18,6 +18,52 @@ const (
 	maxLedgersToCache = (60 * 60 * 24) / 6 // 1 day of ledgers @ 6s each
 )
 
+type ArchiveConfig struct {
+	SourceUrl         string
+	NetworkPassphrase string
+	CacheDir          string
+}
+
+func NewIngestArchive(config ArchiveConfig) (Archive, error) {
+	// If the source URL is an S3 url and it has a region specified, we should
+	// try to extract it.
+	parsed, err := url.Parse(config.SourceUrl)
+	if err != nil {
+		return nil, errors.Wrapf(err, "%s is not a valid URL", config.SourceUrl)
+	}
+	region := ""
+	if parsed.Scheme == "s3" {
+		region = parsed.Query().Get("region")
+	}
+
+	// Now, set up a simple filesystem-like access to the backend and wrap it in
+	// a local on-disk LRU cache if we can.
+	source, err := historyarchive.ConnectBackend(
+		config.SourceUrl,
+		historyarchive.ConnectOptions{
+			Context:           context.Background(),
+			NetworkPassphrase: config.NetworkPassphrase,
+			S3Region:          region,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	cache, err := historyarchive.MakeFsCacheBackend(source, config.CacheDir, maxLedgersToCache)
+	if err != nil { // warn but continue w/o cache
+		log.WithField("path", config.CacheDir).
+			WithError(err).
+			Warnf("Failed to create cached ledger backend")
+		cache = source
+	} else {
+		log.WithField("path", config.CacheDir).Infof("On-disk cache configured")
+	}
+
+	ledgerBackend := ledgerbackend.NewHistoryArchiveBackend(cache)
+	return ingestArchive{ledgerBackend}, nil
+}
+
 // This is an implementation of LightHorizon Archive that uses the existing horizon ingestion backend.
 type ingestArchive struct {
 	*ledgerbackend.HistoryArchiveBackend
@@ -87,52 +133,6 @@ func (adaptation *ingestTransactionReaderAdaption) Read() (LedgerTransaction, er
 	tx.UnsafeMeta = ingestLedgerTransaction.UnsafeMeta
 
 	return tx, nil
-}
-
-type ArchiveConfig struct {
-	SourceUrl         string
-	NetworkPassphrase string
-	CacheDir          string
-}
-
-func NewIngestArchive(config ArchiveConfig) (Archive, error) {
-	// If the source URL is an S3 url and it has a region specified, we should
-	// try to extract it.
-	parsed, err := url.Parse(config.SourceUrl)
-	if err != nil {
-		return nil, errors.Wrapf(err, "%s is not a valid URL", config.SourceUrl)
-	}
-	region := ""
-	if parsed.Scheme == "s3" {
-		region = parsed.Query().Get("region")
-	}
-
-	// Now, set up a simple filesystem-like access to the backend and wrap it in
-	// a local on-disk LRU cache if we can.
-	source, err := historyarchive.ConnectBackend(
-		config.SourceUrl,
-		historyarchive.ConnectOptions{
-			Context:           context.Background(),
-			NetworkPassphrase: config.NetworkPassphrase,
-			S3Region:          region,
-		},
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	cache, err := historyarchive.MakeFsCacheBackend(source, config.CacheDir, maxLedgersToCache)
-	if err != nil { // warn but continue w/o cache
-		log.WithField("path", config.CacheDir).
-			WithError(err).
-			Warnf("Failed to create cached ledger backend")
-		cache = source
-	} else {
-		log.WithField("path", config.CacheDir).Infof("On-disk cache configured")
-	}
-
-	ledgerBackend := ledgerbackend.NewHistoryArchiveBackend(cache)
-	return ingestArchive{ledgerBackend}, nil
 }
 
 var _ Archive = (*ingestArchive)(nil) // ensure conformity to the interface
