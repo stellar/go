b7cf0e4fe285be415a197e34c2333fdf064f1433
Bartek Nowotarski <bartek@nowotarski.info>
2022-07-28 15:51:57 +0200

    fixes

--- tools/stellar-archivist/main_test.go
+++ tools/stellar-archivist/main_test.go
@@ -12,7 +12,7 @@ import (
 )
 
 func TestLastOption(t *testing.T) {
-	src_arch := historyarchive.MustConnect("mock://test", historyarchive.ConnectOptions{CheckpointFrequency: 64})
+	src_arch := historyarchive.MustConnect("mock://test", historyarchive.ArchiveOptions{CheckpointFrequency: 64})
 	assert.NotEqual(t, nil, src_arch)
 
 	var src_has historyarchive.HistoryArchiveState
@@ -28,8 +28,8 @@ func TestLastOption(t *testing.T) {
 }
 
 func TestRecentOption(t *testing.T) {
-	src_arch := historyarchive.MustConnect("mock://test1", historyarchive.ConnectOptions{CheckpointFrequency: 64})
-	dst_arch := historyarchive.MustConnect("mock://test2", historyarchive.ConnectOptions{CheckpointFrequency: 64})
+	src_arch := historyarchive.MustConnect("mock://test1", historyarchive.ArchiveOptions{CheckpointFrequency: 64})
+	dst_arch := historyarchive.MustConnect("mock://test2", historyarchive.ArchiveOptions{CheckpointFrequency: 64})
 	assert.NotEqual(t, nil, src_arch)
 	assert.NotEqual(t, nil, dst_arch)
 
