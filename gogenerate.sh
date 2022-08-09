6983017d56a53ed6a1c5705b6d6a4fd1c9767f24
George Kudrayvtsev <george@stellar.org>
2022-08-09 14:47:19 -0700

    Run `go fmt` after `go generate` in checks

--- gogenerate.sh
+++ gogenerate.sh
@@ -2,7 +2,7 @@
 set -e
 
 printf "Running go generate...\n"
-go generate ./...
+go generate ./... && go fmt ./...
 
 printf "Checking for no diff...\n"
 git diff --exit-code || (echo "Files changed after running go generate. Run go generate ./... locally and update generated files." && exit 1)
