34c98a3674eeba666709be42ce0938a95ce02f13
shawn <sreuland@users.noreply.github.com>
2022-08-10 12:57:32 -0700

    exp/lighthorizon: add horizon web docker/k8s deployment (#4519)

--- exp/lighthorizon/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-# Move to repo root
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd "$DIR/../.."
-
-docker build --tag lighthorizon --platform linux/amd64 -f exp/lighthorizon/Dockerfile .
--- exp/lighthorizon/index/cmd/single/main.go
+++ exp/lighthorizon/index/cmd/single/main.go
@@ -16,12 +16,12 @@ func main() {
 	sourceUrl := flag.String("source", "gcs://horizon-archive-poc", "history archive url to read txmeta files")
 	targetUrl := flag.String("target", "file://indexes", "where to write indexes")
 	networkPassphrase := flag.String("network-passphrase", network.TestNetworkPassphrase, "network passphrase")
-	start := flag.Int("start", 2, "ledger to start at (inclusive, default: earliest)")
-	end := flag.Int("end", 0, "ledger to end at (inclusive, default: latest)")
+	start := flag.Int("start", 2, "ledger to start at (inclusive, default: 2, the earliest)")
+	end := flag.Int("end", 0, "ledger to end at (inclusive, default: 0, the latest as of start time)")
 	modules := flag.String("modules", "accounts,transactions", "comma-separated list of modules to index (default: all)")
 	watch := flag.Bool("watch", false, "whether to watch the `source` for new "+
 		"txmeta files and index them (default: false). "+
-		"note: `-watch` implicitly implies `-end -1`")
+		"note: `-watch` implies a continuous `-end 0` to get to the latest ledger in txmeta files")
 	workerCount := flag.Int("workers", runtime.NumCPU()-1, "number of workers (default: # of CPUs - 1)")
 
 	flag.Parse()
