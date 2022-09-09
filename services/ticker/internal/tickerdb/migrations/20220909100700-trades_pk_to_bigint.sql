2c72cf1f6d40c8e4c31dddea0dedfc2db4613d02
Alex Cordeiro <accordeiro@gmail.com>
2022-09-09 10:33:30 -0700

    services/ticker: ensure seq type is upgraded

--- services/ticker/internal/tickerdb/migrations/20220909100700-trades_pk_to_bigint.sql
+++ services/ticker/internal/tickerdb/migrations/20220909100700-trades_pk_to_bigint.sql
@@ -1,9 +1,9 @@
 
 -- +migrate Up
 ALTER TABLE trades ALTER COLUMN id TYPE BIGINT;
-ALTER SEQUENCE trades_id_seq CYCLE;
+ALTER SEQUENCE trades_id_seq AS BIGINT CYCLE;
 
 
 -- +migrate Down
 ALTER TABLE trades ALTER COLUMN id TYPE INT;
-ALTER SEQUENCE trades_id_seq NO CYCLE;
+ALTER SEQUENCE trades_id_seq AS INT NO CYCLE;
