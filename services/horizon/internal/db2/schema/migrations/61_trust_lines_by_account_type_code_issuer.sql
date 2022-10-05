d82ca749bf6a829261934a7236343bcce285c9c7
Bartek Nowotarski <bartek@nowotarski.info>
2022-10-05 13:21:43 +0200

    f=ix

--- services/horizon/internal/db2/schema/migrations/61_trust_lines_by_account_type_code_issuer.sql
+++ services/horizon/internal/db2/schema/migrations/61_trust_lines_by_account_type_code_issuer.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 
-CREATE INDEX "trust_lines_by_account_type_code_issuer" IN trust_lines USING btree (account_id, asset_type, asset_code, asset_issuer);
+CREATE INDEX "trust_lines_by_account_type_code_issuer" ON trust_lines USING btree (account_id, asset_type, asset_code, asset_issuer);
 
 -- +migrate Down
 
