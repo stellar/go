737f9ce2f4e9b4fe2c13986ba82ad4d2c447d15d
tamirms <tamirms@gmail.com>
2023-04-07 20:22:25 +0100

    services/horizon: Refactor logmetrics package so it can be used by other services (#4837)

--- support/logmetrics/main.go
+++ support/logmetrics/main.go
@@ -1,60 +1,51 @@
 package logmetrics
 
 import (
+	"fmt"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
-	"github.com/stellar/go/support/log"
 )
 
 // Metrics is a logrus hook-compliant struct that records metrics about logging
 // when added to a logrus.Logger
 type Metrics map[logrus.Level]prometheus.Counter
 
-var DefaultMetrics = NewMetrics()
-
-func init() {
-	_, DefaultMetrics = New()
-}
-
-// New creates a new logger according to horizon specifications.
-func New() (l *log.Entry, m *Metrics) {
-	m = NewMetrics()
-	l = log.New()
-	l.SetLevel(logrus.WarnLevel)
-	l.AddHook(m)
-	return
-}
-
-// NewMetrics creates a new hook for recording metrics.
-func NewMetrics() *Metrics {
-	return &Metrics{
+// New creates a new hook for recording metrics.
+// New takes a namespace parameter which defines the namespace
+// for the prometheus metrics.
+func New(namespace string) Metrics {
+	return Metrics{
 		logrus.DebugLevel: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: "horizon", Subsystem: "log", Name: "debug_total",
+			Namespace: namespace, Subsystem: "log", Name: "debug_total",
 		}),
 		logrus.InfoLevel: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: "horizon", Subsystem: "log", Name: "info_total",
+			Namespace: namespace, Subsystem: "log", Name: "info_total",
 		}),
 		logrus.WarnLevel: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: "horizon", Subsystem: "log", Name: "warn_total",
+			Namespace: namespace, Subsystem: "log", Name: "warn_total",
 		}),
 		logrus.ErrorLevel: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: "horizon", Subsystem: "log", Name: "error_total",
+			Namespace: namespace, Subsystem: "log", Name: "error_total",
 		}),
 		logrus.PanicLevel: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: "horizon", Subsystem: "log", Name: "panic_total",
+			Namespace: namespace, Subsystem: "log", Name: "panic_total",
 		}),
 	}
 }
 
 // Fire is triggered by logrus, in response to a logging event
-func (m *Metrics) Fire(e *logrus.Entry) error {
-	(*m)[e.Level].Inc()
-	return nil
+func (m Metrics) Fire(e *logrus.Entry) error {
+	counter, ok := m[e.Level]
+	if ok {
+		counter.Inc()
+		return nil
+	}
+	return fmt.Errorf("level %v not supported", e.Level)
 }
 
 // Levels returns the logging levels that will trigger this hook to run.  In
 // this case, all of them.
-func (m *Metrics) Levels() []logrus.Level {
+func (m Metrics) Levels() []logrus.Level {
 	return []logrus.Level{
 		logrus.DebugLevel,
 		logrus.InfoLevel,
