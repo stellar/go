85e416118e5f4c51fdfe2c9122bc383ecb09367a
tamirms <tamirms@gmail.com>
2023-07-31 12:11:59 +0100

    services/horizon/internal/ingest/processors: Fix bump footprint expiration operation (#4989)

--- protocols/horizon/operations/main.go
+++ protocols/horizon/operations/main.go
@@ -377,7 +377,7 @@ type HostFunctionParameter struct {
 // The model for BumpFootprintExpiration assimilates BumpFootprintExpirationOp, but is simplified.
 type BumpFootprintExpiration struct {
 	Base
-	LedgersToExpire int64 `json:"ledgers_to_expire,string"`
+	LedgersToExpire uint32 `json:"ledgers_to_expire"`
 }
 
 // RestoreFootprint is the json resource representing a single RestoreFootprint.
