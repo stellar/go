6e8cafcd84704890705e82bd6bcded4be44b3124
Aditya Vyas <aditya.vyas@stellar.org>
2023-08-28 17:10:45 -0400

    Update txsub_test.go

--- services/horizon/internal/integration/txsub_test.go
+++ services/horizon/internal/integration/txsub_test.go
@@ -1,7 +1,6 @@
 package integration
 
 import (
-	"sync"
 	"testing"
 
 	"github.com/stellar/go/services/horizon/internal/test/integration"
@@ -15,38 +14,22 @@ func TestTxSub(t *testing.T) {
 	t.Run("transaction submission is successful when DISABLE_TX_SUB=false", func(t *testing.T) {
 		itest := integration.NewTest(t, integration.Config{})
 		master := itest.Master()
-		keys, accounts := itest.CreateAccounts(1, "1000")
 
-		var wg sync.WaitGroup
-		wg.Add(1)
-
-		seq, err := accounts[0].GetSequenceNumber()
-		assert.NoError(t, err)
-
-		account := txnbuild.SimpleAccount{
-			AccountID: keys[0].Address(),
-			Sequence:  seq,
+		op := txnbuild.Payment{
+			Destination: master.Address(),
+			Amount:      "10",
+			Asset:       txnbuild.NativeAsset{},
 		}
 
-		go func(account txnbuild.SimpleAccount) {
-			defer wg.Done()
+		txResp, err := itest.SubmitOperations(itest.MasterAccount(), master, &op)
+		assert.NoError(t, err)
 
-			op := txnbuild.Payment{
-				Destination: master.Address(),
-				Amount:      "10",
-				Asset:       txnbuild.NativeAsset{},
-			}
-
-			txResp := itest.MustSubmitOperations(&account, keys[0], &op)
-
-			tt.Equal(accounts[0].GetAccountID(), txResp.Account)
-			seq, err := account.GetSequenceNumber()
-			assert.NoError(t, err)
-			tt.Equal(seq, txResp.AccountSequence)
-			t.Logf("Done")
-		}(account)
-
-		wg.Wait()
+		var seq int64
+		tt.Equal(itest.MasterAccount().GetAccountID(), txResp.Account)
+		seq, err = itest.MasterAccount().GetSequenceNumber()
+		assert.NoError(t, err)
+		tt.Equal(seq, txResp.AccountSequence)
+		t.Logf("Done")
 	})
 
 	t.Run("transaction submission is not successful when DISABLE_TX_SUB=true", func(t *testing.T) {
@@ -55,8 +38,15 @@ func TestTxSub(t *testing.T) {
 				"DISABLE_TX_SUB": "true",
 			},
 		})
-		assert.PanicsWithError(t, "horizon error: \"Transaction Submission Disabled\" - check horizon.Error.Problem for more information", func() {
-			itest.CreateAccounts(1, "1000")
-		})
+		master := itest.Master()
+
+		op := txnbuild.Payment{
+			Destination: master.Address(),
+			Amount:      "10",
+			Asset:       txnbuild.NativeAsset{},
+		}
+
+		_, err := itest.SubmitOperations(itest.MasterAccount(), master, &op)
+		assert.Error(t, err)
 	})
 }
