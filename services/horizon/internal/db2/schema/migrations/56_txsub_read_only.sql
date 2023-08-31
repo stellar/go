1cf70ab8f64c053d4caf08827b3574ef20cf3351
Aditya Vyas <aditya.vyas@stellar.org>
2023-08-31 17:38:39 -0400

    Run gogenerate again

--- services/horizon/internal/db2/schema/migrations/56_txsub_read_only.sql
+++ services/horizon/internal/db2/schema/migrations/56_txsub_read_only.sql
@@ -5,8 +5,8 @@ DROP TABLE txsub_results;
 -- +migrate Down
 
 CREATE TABLE txsub_results (
-                               transaction_hash       varchar(64) NOT NULL UNIQUE,
-                               inner_transaction_hash varchar(64),
-                               tx_result              text, -- serialized history.Transaction
-                               submitted_at           timestamp NOT NULL DEFAULT NOW()
+    transaction_hash       varchar(64) NOT NULL UNIQUE,
+    inner_transaction_hash varchar(64),
+    tx_result              text, -- serialized history.Transaction
+    submitted_at           timestamp NOT NULL DEFAULT NOW()
 );
