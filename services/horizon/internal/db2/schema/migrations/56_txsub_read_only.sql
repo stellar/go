2246ae63d5075e83fd16762a28938d336d22286f
Aditya Vyas <aditya.vyas@stellar.org>
2023-08-31 17:37:19 -0400

    Add 56_txsub_read_only.sql again

---
+++ services/horizon/internal/db2/schema/migrations/56_txsub_read_only.sql
@@ -0,0 +1,12 @@
+-- +migrate Up
+
+DROP TABLE txsub_results;
+
+-- +migrate Down
+
+CREATE TABLE txsub_results (
+                               transaction_hash       varchar(64) NOT NULL UNIQUE,
+                               inner_transaction_hash varchar(64),
+                               tx_result              text, -- serialized history.Transaction
+                               submitted_at           timestamp NOT NULL DEFAULT NOW()
+);
