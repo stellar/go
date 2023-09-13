f85102d1fbe0aacbef38379408bec7e3a63ca38b
tamirms <tamirms@gmail.com>
2023-09-13 09:25:27 +0100

    services/horizon/internal: Add flag to limit request body size (#16)

--- services/horizon/internal/integration/txsub_test.go
+++ services/horizon/internal/integration/txsub_test.go
@@ -1,11 +1,16 @@
 package integration
 
 import (
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/services/horizon/internal/test/integration"
 	"github.com/stellar/go/txnbuild"
-	"github.com/stretchr/testify/assert"
+	"github.com/stellar/go/xdr"
 )
 
 func TestTxSub(t *testing.T) {
@@ -49,4 +54,57 @@ func TestTxSub(t *testing.T) {
 		_, err := itest.SubmitOperations(itest.MasterAccount(), master, &op)
 		assert.Error(t, err)
 	})
+}
+
+func TestTxSubLimitsBodySize(t *testing.T) {
+	if integration.GetCoreMaxSupportedProtocol() < 20 {
+		t.Skip("This test run does not support less than Protocol 20")
+	}
+
+	itest := integration.NewTest(t, integration.Config{
+		ProtocolVersion:  20,
+		EnableSorobanRPC: true,
+		HorizonEnvironment: map[string]string{
+			"MAX_HTTP_REQUEST_SIZE": "5000",
+		},
+	})
+
+	// establish which account will be contract owner, and load it's current seq
+	sourceAccount, err := itest.Client().AccountDetail(horizonclient.AccountRequest{
+		AccountID: itest.Master().Address(),
+	})
+	require.NoError(t, err)
+
+	contract := []byte(strings.Repeat("a", 10*1024))
+	installContractOp := &txnbuild.InvokeHostFunction{
+		HostFunction: xdr.HostFunction{
+			Type: xdr.HostFunctionTypeHostFunctionTypeUploadContractWasm,
+			Wasm: &contract,
+		},
+		SourceAccount: itest.Master().Address(),
+	}
+	preFlightOp, minFee := itest.PreflightHostFunctions(&sourceAccount, *installContractOp)
+	_, err = itest.SubmitOperationsWithFee(&sourceAccount, itest.Master(), minFee, &preFlightOp)
+	assert.EqualError(
+		t, err,
+		"horizon error: \"Transaction Malformed\" - check horizon.Error.Problem for more information",
+	)
+
+	sourceAccount, err = itest.Client().AccountDetail(horizonclient.AccountRequest{
+		AccountID: itest.Master().Address(),
+	})
+	require.NoError(t, err)
+
+	contract = []byte(strings.Repeat("a", 2*1024))
+	installContractOp = &txnbuild.InvokeHostFunction{
+		HostFunction: xdr.HostFunction{
+			Type: xdr.HostFunctionTypeHostFunctionTypeUploadContractWasm,
+			Wasm: &contract,
+		},
+		SourceAccount: itest.Master().Address(),
+	}
+	preFlightOp, minFee = itest.PreflightHostFunctions(&sourceAccount, *installContractOp)
+	tx, err := itest.SubmitOperationsWithFee(&sourceAccount, itest.Master(), minFee, &preFlightOp)
+	require.NoError(t, err)
+	require.True(t, tx.Successful)
 }
