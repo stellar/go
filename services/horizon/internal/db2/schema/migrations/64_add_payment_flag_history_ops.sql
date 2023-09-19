d64026c5c7af06f0065620bfccd7bc571033e70b
tamirms <tamirms@gmail.com>
2023-09-19 15:27:18 +0100

    Fix partial index

--- services/horizon/internal/db2/schema/migrations/64_add_payment_flag_history_ops.sql
+++ services/horizon/internal/db2/schema/migrations/64_add_payment_flag_history_ops.sql
@@ -2,7 +2,7 @@
 
 ALTER TABLE history_operations ADD is_payment boolean;
 CREATE INDEX "index_history_operations_on_is_payment" ON history_operations (is_payment)
-    WHERE is_payment IS NOT NULL;
+    WHERE is_payment = true;
 
 -- +migrate Down
 
