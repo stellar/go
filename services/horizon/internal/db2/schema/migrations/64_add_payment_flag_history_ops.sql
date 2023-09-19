3ae076e262f98009a84d37f91e99bf4c0c15ff3a
tamirms <tamirms@gmail.com>
2023-09-19 12:20:38 +0100

    Make index_history_operations_on_is_payment a partial index

--- services/horizon/internal/db2/schema/migrations/64_add_payment_flag_history_ops.sql
+++ services/horizon/internal/db2/schema/migrations/64_add_payment_flag_history_ops.sql
@@ -1,7 +1,8 @@
 -- +migrate Up
 
 ALTER TABLE history_operations ADD is_payment boolean;
-CREATE INDEX "index_history_operations_on_is_payment" ON history_operations USING btree (is_payment);
+CREATE INDEX "index_history_operations_on_is_payment" ON history_operations (is_payment)
+    WHERE is_payment IS NOT NULL;
 
 -- +migrate Down
 
