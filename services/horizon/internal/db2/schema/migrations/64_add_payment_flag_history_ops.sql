09784ecc5d0fffd2851bb99ebaa8e208b4ede6c4
tamirms <tamirms@gmail.com>
2023-09-22 10:03:40 +0100

    drop index_history_operations_on_is_payment

--- services/horizon/internal/db2/schema/migrations/64_add_payment_flag_history_ops.sql
+++ services/horizon/internal/db2/schema/migrations/64_add_payment_flag_history_ops.sql
@@ -1,10 +1,7 @@
 -- +migrate Up
 
 ALTER TABLE history_operations ADD is_payment boolean;
-CREATE INDEX "index_history_operations_on_is_payment" ON history_operations (is_payment)
-    WHERE is_payment = true;
 
 -- +migrate Down
 
-DROP INDEX "index_history_operations_on_is_payment";
 ALTER TABLE history_operations DROP COLUMN is_payment;
---
+++ services/horizon/internal/db2/schema/migrations/65_drop_payment_index.sql
@@ -0,0 +1,9 @@
+-- +migrate Up
+
+-- index_history_operations_on_is_payment was added in migration 64 but it turns out
+-- the index was not necessary, see https://github.com/stellar/go/issues/5059
+DROP INDEX IF EXISTS "index_history_operations_on_is_payment";
+
+-- +migrate Down
+
+CREATE INDEX "index_history_operations_on_is_payment" ON history_operations USING btree (is_payment);
