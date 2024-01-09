28ca57ae141c71132000dcd050a4e6ea508163a9
Urvi <urvi.savla@stellar.org>
2024-01-09 15:42:02 -0800

    Rewrite ledgerexporter using new xdr

---
+++ exp/services/ledgerexporter/exporter.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"github.com/stellar/go/ingest/ledgerbackend"
+	"github.com/stellar/go/support/storage"
+	"io"
+	"strconv"
+	"time"
+
+	//"fmt"
+	"github.com/stellar/go/xdr"
+)
+
+// ExportObject represents a file with metadata and binary data.
+type ExportObject struct {
+	// metadata
+	startLedger uint32
+	endLedger   uint32
+	fullPath    string
+	// Actual binary data
+	ledgerTxMetas xdr.TxMetaLedgerBatch
+}
+
+func NewExportObject(ledgerSeq uint32, fullPath string) *ExportObject {
+	exportObject := &ExportObject{
+		startLedger: ledgerSeq,
+		endLedger:   ledgerSeq,
+		fullPath:    fullPath,
+	}
+	exportObject.ledgerTxMetas.StartSequence = xdr.Uint32(ledgerSeq)
+	return exportObject
+}
+
+// AddLedgerTxMeta adds a TxMeta for a ledger to LedgerTxMetas
+func (e *ExportObject) AddLedgerTxMeta(txMeta xdr.LedgerCloseMeta) {
+	e.ledgerTxMetas.TxMetas = append(e.ledgerTxMetas.TxMetas, txMeta)
+	e.ledgerTxMetas.EndSequence = xdr.Uint32(txMeta.LedgerSequence())
+	e.endLedger = txMeta.LedgerSequence()
+}
+
+func (e *ExportObject) NumberOfLedgers() uint32 {
+	return uint32(len(e.ledgerTxMetas.TxMetas))
+}
+
+type ExporterConfig struct {
+	LedgersPerFile    uint32 `toml:"ledgers_per_file"`
+	FilesPerPartition uint32 `toml:"files_per_partition"`
+	DestinationUrl    string `toml:"destination_url"`
+}
+
+// Exporter manages the creation and handling of export objects.
+type Exporter struct {
+	config      ExporterConfig
+	destination *storage.Storage
+	backend     *ledgerbackend.LedgerBackend
+	//	mutex       sync.Mutex
+	fileMap map[string]*ExportObject
+}
+
+// NewExporter creates a new Exporter with the provided configuration.
+func NewExporter(config ExporterConfig, store *storage.Storage, backend *ledgerbackend.LedgerBackend) *Exporter {
+	return &Exporter{
+		config:      config,
+		destination: store,
+		backend:     backend,
+		fileMap:     make(map[string]*ExportObject),
+	}
+}
+
+// AddLedgerTxMeta adds ledger metadata to the current export object.
+func (e *Exporter) AddLedgerTxMeta(txMeta xdr.LedgerCloseMeta) error {
+	//e.mutex.Lock()
+	//defer e.mutex.Unlock()
+
+	objectName := e.getObjectName(txMeta.LedgerSequence())
+	exportObject, exists := e.fileMap[objectName]
+	if !exists {
+		fullPath := e.getPartitionName(txMeta.LedgerSequence()) + "/" + objectName
+		exportObject = NewExportObject(txMeta.LedgerSequence(), fullPath)
+		e.fileMap[objectName] = exportObject
+	}
+	exportObject.AddLedgerTxMeta(txMeta)
+	if exportObject.NumberOfLedgers() == e.config.LedgersPerFile {
+		// Current export object is full, upload it
+		e.Upload(exportObject)
+		fmt.Printf("seq:%d %s\n", txMeta.LedgerSequence(), exportObject.fullPath)
+		delete(e.fileMap, objectName)
+		exportObject = nil
+	}
+
+	return nil
+}
+
+// Upload uploads the serialized binary data of ledger TxMeta
+// to the specified destination
+func (e *Exporter) Upload(object *ExportObject) error {
+	blob, err := object.ledgerTxMetas.MarshalBinary()
+	if err != nil {
+		return err
+	}
+	return (*e.destination).PutFile(
+		object.fullPath,
+		io.NopCloser(bytes.NewReader(blob)),
+	)
+}
+
+// getPartitionName generates the directory prefix based on the ledger sequence.
+func (e *Exporter) getPartitionName(ledgerSeq uint32) string {
+	partitionSize := e.config.LedgersPerFile * e.config.FilesPerPartition
+	start := (ledgerSeq / partitionSize) * partitionSize
+	end := start + partitionSize - 1
+
+	return strconv.Itoa(int(start)) + "-" + strconv.Itoa(int(end))
+}
+
+// getFileName generates the file name based on the ledger sequence.
+func (e *Exporter) getObjectName(ledgerSeq uint32) string {
+	start := (ledgerSeq / e.config.LedgersPerFile) * e.config.LedgersPerFile
+	end := start + e.config.LedgersPerFile - 1
+
+	return strconv.Itoa(int(start)) + "-" + strconv.Itoa(int(end))
+}
+
+// Run iterates over the specified range of ledger numbers and uploads
+// the corresponding serialized binary data to the destination. The process
+// continues until the ending ledger number is reached.
+func (e *Exporter) Run(ctx context.Context, startLedger, endLedger uint32) {
+
+	for nextLedger := startLedger; endLedger < 1 || nextLedger <= endLedger; {
+		select {
+		case <-ctx.Done():
+			logger.Info("Exporter stopped due to context cancellation.")
+			return
+		default:
+			ledger, err := (*e.backend).GetLedger(ctx, nextLedger)
+			if err != nil {
+				//Handle error
+			}
+			time.Sleep(time.Duration(1) * time.Second)
+			e.AddLedgerTxMeta(ledger)
+			nextLedger++
+		}
+	}
+}
--- exp/services/ledgerexporter/main.go
+++ exp/services/ledgerexporter/main.go
@@ -1,176 +1,160 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"flag"
-	"io"
-	"os"
-	"strconv"
-	"strings"
-	"time"
-
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/pelletier/go-toml"
 	"github.com/stellar/go/ingest/ledgerbackend"
-	"github.com/stellar/go/network"
+	_ "github.com/stellar/go/network"
 	supportlog "github.com/stellar/go/support/log"
 	"github.com/stellar/go/support/storage"
-	"github.com/stellar/go/xdr"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
-var logger = supportlog.New()
+var (
+	logger             = supportlog.New()
+	config             Config
+	backend            ledgerbackend.LedgerBackend
+	destinationStorage storage.Storage
+	exporter           *Exporter
+)
+
+type StellarCoreConfig struct {
+	NetworkPassphrase     string   `toml:"network_passphrase"`
+	HistoryArchiveUrls    []string `toml:"history_archive_urls"`
+	StellarCoreBinaryPath string   `toml:"stellar_core_binary_path"`
+	CaptiveCoreTomlPath   string   `toml:"captive_core_toml_path"`
+	CaptiveCoreUseDb      bool     `toml:"captive_core_use_db"`
+}
+
+type Config struct {
+	Network           string            `toml:"network"`
+	ExporterConfig    ExporterConfig    `toml:"exporter_config"`
+	StellarCoreConfig StellarCoreConfig `toml:"stellar_core_config"`
+
+	//From command-line
+	StartLedger uint32 `toml:"start_ledger"`
+	EndLedger   uint32 `toml:"end_ledger"`
+}
 
 func main() {
-	targetUrl := flag.String("target", "gcs://horizon-archive-poc", "history archive url to write txmeta files")
-	stellarCoreBinaryPath := flag.String("stellar-core-binary-path", os.Getenv("STELLAR_CORE_BINARY_PATH"), "path to the stellar core binary")
-	networkPassphrase := flag.String("network-passphrase", network.TestNetworkPassphrase, "network passphrase")
-	historyArchiveUrls := flag.String("history-archive-urls", "https://history.stellar.org/prd/core-testnet/core_testnet_001", "comma-separated list of history archive urls to read from")
-	captiveCoreTomlPath := flag.String("captive-core-toml-path", os.Getenv("CAPTIVE_CORE_TOML_PATH"), "path to load captive core toml file from")
-	startingLedger := flag.Uint("start-ledger", 2, "ledger to start export from")
-	continueFromLatestLedger := flag.Bool("continue", false, "start export from the last exported ledger (as indicated in the target's /latest path)")
-	endingLedger := flag.Uint("end-ledger", 0, "ledger at which to stop the export (must be a closed ledger), 0 means no ending")
-	writeLatestPath := flag.Bool("write-latest-path", true, "update the value of the /latest path on the target")
-	captiveCoreUseDb := flag.Bool("captive-core-use-db", true, "configure captive core to store database on disk in working directory rather than in memory")
+	logger.SetLevel(supportlog.InfoLevel)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func() {
+		// Handle OS signals to gracefully terminate the service
+		ch := make(chan os.Signal, 1)
+		signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+		<-ch
+		logger.Info("Received termination signal, shutting down...")
+		cancel()
+	}()
+
+	loadConfig()
+
+	initLedgerBackend(config)
+
+	initDestinationStorage(config)
+
+	initExporter(config)
+
+	var wg sync.WaitGroup
+	wg.Add(1) // for the exporter
+
+	go func() {
+		exporter.Run(ctx, config.StartLedger, config.EndLedger)
+		wg.Done() // signal completion when Run finishes
+	}()
+
+	wg.Wait() // wait for the exporter to finish
+	logger.Info("Shutting down service.")
+
+}
+
+func initExporter(config Config) {
+	exporter = NewExporter(
+		config.ExporterConfig,
+		&destinationStorage,
+		&backend,
+	)
+}
+
+func initDestinationStorage(config Config) {
+	var err error
+	destinationStorage, err = storage.ConnectBackend(config.ExporterConfig.DestinationUrl, storage.ConnectOptions{})
+	logFatalIf(err, "Could not connect to destination storage")
+}
+
+func loadConfig() {
+	// Parse command-line options
+	startLedger := flag.Uint("start-ledger", 0, "Starting ledger")
+	endLedger := flag.Uint("end-ledger", 0, "Ending ledger")
+	configFilePath := flag.String("config-file", "config.toml", "Path to the TOML config file")
 	flag.Parse()
 
-	logger.SetLevel(supportlog.InfoLevel)
+	// Load config TOML file
+	cfg, err := toml.LoadFile(*configFilePath)
+	logFatalIf(err, "Error loading %s TOML file: %v", configFilePath)
 
+	// Unmarshal TOML data into the Config struct
+	err = cfg.Unmarshal(&config)
+	logFatalIf(err, "Error unmarshalling TOML config: %v", configFilePath)
+
+	config.StartLedger = uint32(*startLedger)
+	config.EndLedger = uint32(*endLedger)
+
+	// Validate and build the appropriate range
+	logger.Infof("processing requested range of -start-ledger=%v, -end-ledger=%v", config.StartLedger, config.EndLedger)
+
+	if config.StartLedger < 2 {
+		logger.Fatalf("-start-ledger must be >= 2")
+	}
+	if config.EndLedger != 0 && config.EndLedger < config.StartLedger {
+		logger.Fatalf("-end-ledger must be >= -start-ledger")
+	}
+}
+
+// Creates and initializes captive core ledger backend
+func initLedgerBackend(config Config) {
+	coreConfig := config.StellarCoreConfig
 	params := ledgerbackend.CaptiveCoreTomlParams{
-		NetworkPassphrase:  *networkPassphrase,
-		HistoryArchiveURLs: strings.Split(*historyArchiveUrls, ","),
-		UseDB:              *captiveCoreUseDb,
+		NetworkPassphrase:  coreConfig.NetworkPassphrase,
+		HistoryArchiveURLs: coreConfig.HistoryArchiveUrls,
+		UseDB:              coreConfig.CaptiveCoreUseDb,
 	}
-	if *captiveCoreTomlPath == "" {
-		logger.Fatal("Missing -captive-core-toml-path flag")
+	if coreConfig.CaptiveCoreTomlPath == "" {
+		logger.Fatal("Missing captive_core_toml_path in the config")
 	}
 
-	captiveCoreToml, err := ledgerbackend.NewCaptiveCoreTomlFromFile(*captiveCoreTomlPath, params)
-	logFatalIf(err, "Invalid captive core toml")
+	captiveCoreToml, err := ledgerbackend.NewCaptiveCoreTomlFromFile(coreConfig.CaptiveCoreTomlPath, params)
 
 	captiveConfig := ledgerbackend.CaptiveCoreConfig{
-		BinaryPath:          *stellarCoreBinaryPath,
+		BinaryPath:          coreConfig.StellarCoreBinaryPath,
 		NetworkPassphrase:   params.NetworkPassphrase,
 		HistoryArchiveURLs:  params.HistoryArchiveURLs,
 		CheckpointFrequency: 64,
 		Log:                 logger.WithField("subservice", "stellar-core"),
 		Toml:                captiveCoreToml,
-		UseDB:               *captiveCoreUseDb,
+		UseDB:               coreConfig.CaptiveCoreUseDb,
 	}
-	core, err := ledgerbackend.NewCaptive(captiveConfig)
+
+	// Create a new captive core backend
+	backend, err = ledgerbackend.NewCaptive(captiveConfig)
 	logFatalIf(err, "Could not create captive core instance")
 
-	target, err := storage.ConnectBackend(
-		*targetUrl,
-		storage.ConnectOptions{
-			Context:    context.Background(),
-			S3WriteACL: s3.ObjectCannedACLBucketOwnerFullControl,
-		},
-	)
-	logFatalIf(err, "Could not connect to target")
-	defer target.Close()
-
-	// Build the appropriate range for the given backend state.
-	startLedger := uint32(*startingLedger)
-	endLedger := uint32(*endingLedger)
-
-	logger.Infof("processing requested range of -start-ledger=%v, -end-ledger=%v", startLedger, endLedger)
-	if *continueFromLatestLedger {
-		if startLedger != 0 {
-			logger.Fatalf("-start-ledger and -continue cannot both be set")
-		}
-		startLedger = readLatestLedger(target)
-		logger.Infof("continue flag was enabled, next ledger found was %v", startLedger)
-	}
-
-	if startLedger < 2 {
-		logger.Fatalf("-start-ledger must be >= 2")
-	}
-	if endLedger != 0 && endLedger < startLedger {
-		logger.Fatalf("-end-ledger must be >= -start-ledger")
-	}
-
 	var ledgerRange ledgerbackend.Range
-	if endLedger == 0 {
-		ledgerRange = ledgerbackend.UnboundedRange(startLedger)
+	if config.EndLedger == 0 {
+		ledgerRange = ledgerbackend.UnboundedRange(config.StartLedger)
 	} else {
-		ledgerRange = ledgerbackend.BoundedRange(startLedger, endLedger)
+		ledgerRange = ledgerbackend.BoundedRange(config.StartLedger, config.EndLedger)
 	}
 
-	logger.Infof("preparing to export %s", ledgerRange)
-	err = core.PrepareRange(context.Background(), ledgerRange)
-	logFatalIf(err, "could not prepare range")
-
-	for nextLedger := startLedger; endLedger < 1 || nextLedger <= endLedger; {
-		ledger, err := core.GetLedger(context.Background(), nextLedger)
-		if err != nil {
-			logger.WithError(err).Warnf("could not fetch ledger %v, retrying", nextLedger)
-			time.Sleep(time.Second)
-			continue
-		}
-
-		if err = writeLedger(target, ledger); err != nil {
-			logger.WithError(err).Warnf(
-				"could not write ledger object %v, retrying",
-				uint64(ledger.LedgerSequence()))
-			continue
-		}
-
-		if *writeLatestPath {
-			if err = writeLatestLedger(target, nextLedger); err != nil {
-				logger.WithError(err).Warnf("could not write latest ledger %v", nextLedger)
-			}
-		}
-
-		nextLedger++
-	}
-
-}
-
-// readLatestLedger determines the latest ledger in the given backend (at the
-// /latest path), defaulting to Ledger #2 if one doesn't exist
-func readLatestLedger(backend storage.Storage) uint32 {
-	r, err := backend.GetFile("latest")
-	if os.IsNotExist(err) {
-		return 2
-	}
-
-	logFatalIf(err, "could not open latest ledger bucket")
-	defer r.Close()
-
-	var buf bytes.Buffer
-	_, err = io.Copy(&buf, r)
-	logFatalIf(err, "could not read latest ledger")
-
-	parsed, err := strconv.ParseUint(buf.String(), 10, 32)
-	logFatalIf(err, "could not parse latest ledger: %s", buf.String())
-	return uint32(parsed)
-}
-
-// writeLedger stores the given LedgerCloseMeta instance as a raw binary at the
-// /ledgers/<seqNum> path. If an error is returned, it may be transient so you
-// should attempt to retry.
-func writeLedger(backend storage.Storage, ledger xdr.LedgerCloseMeta) error {
-	toSerialize := xdr.SerializedLedgerCloseMeta{
-		V:  0,
-		V0: &ledger,
-	}
-	blob, err := toSerialize.MarshalBinary()
-	logFatalIf(err, "could not serialize ledger %v", ledger.LedgerSequence())
-	return backend.PutFile(
-		"ledgers/"+strconv.FormatUint(uint64(ledger.LedgerSequence()), 10),
-		io.NopCloser(bytes.NewReader(blob)),
-	)
-}
-
-func writeLatestLedger(backend storage.Storage, ledger uint32) error {
-	return backend.PutFile(
-		"latest",
-		io.NopCloser(
-			bytes.NewBufferString(
-				strconv.FormatUint(uint64(ledger), 10),
-			),
-		),
-	)
+	err = backend.PrepareRange(context.Background(), ledgerRange)
+	logFatalIf(err, "Could not prepare captive core ledger backend")
 }
 
 func logFatalIf(err error, message string, args ...interface{}) {
