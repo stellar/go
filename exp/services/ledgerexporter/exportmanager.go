06015f76d9a3e608812c395d309a7a8679d90428
Urvi <urvi.savla@stellar.org>
2024-01-11 21:58:19 -0800

    Improve handling of ledger range and added a unit test.

--- exp/services/ledgerexporter/exportmanager.go
+++ exp/services/ledgerexporter/exportmanager.go
@@ -11,21 +11,20 @@ import (
 // LedgerCloseMetaObject represents a file with metadata and binary data.
 type LedgerCloseMetaObject struct {
 	// file name
-	objectKey string
-
-	// file metadata
-	startLedger uint32
-	endLedger   uint32
-
+	objectKey     string
+	startSequence uint32
+	endSequence   uint32
 	// Actual binary data
 	data xdr.LedgerCloseMetaBatch
 }
 
 // AddLedgerCloseMeta adds a ledger
 func (f *LedgerCloseMetaObject) AddLedgerCloseMeta(ledgerCloseMeta xdr.LedgerCloseMeta) {
+	if f.startSequence == 0 {
+		f.data.StartSequence = xdr.Uint32(ledgerCloseMeta.LedgerSequence())
+	}
 	f.data.LedgerCloseMetas = append(f.data.LedgerCloseMetas, ledgerCloseMeta)
 	f.data.EndSequence = xdr.Uint32(ledgerCloseMeta.LedgerSequence())
-	f.endLedger = ledgerCloseMeta.LedgerSequence()
 }
 
 // LedgerCount returns the number of ledgers added so far
@@ -54,7 +53,12 @@ func (e *ExporterConfig) getObjectKey(ledgerSeq uint32) string {
 		fileStart := (ledgerSeq / e.LedgersPerFile) * e.LedgersPerFile
 		fileEnd := fileStart + e.LedgersPerFile - 1
 
-		objectKey += strconv.Itoa(int(fileStart)) + "-" + strconv.Itoa(int(fileEnd))
+		// Single ledger per file
+		if fileStart == fileEnd {
+			objectKey += strconv.Itoa(int(fileStart))
+		} else {
+			objectKey += strconv.Itoa(int(fileStart)) + "-" + strconv.Itoa(int(fileEnd))
+		}
 	}
 	return objectKey
 }
@@ -87,18 +91,32 @@ func (e *ExportManager) AddLedgerCloseMeta(ledgerCloseMeta xdr.LedgerCloseMeta) 
 	ledgerCloseMetaObject, exists := e.objectMap[objectKey]
 
 	if !exists {
-		// Create a new LedgerCloseMetaObject and add it to the map
-		ledgerCloseMetaObject = &LedgerCloseMetaObject{startLedger: ledgerSeq}
-		ledgerCloseMetaObject.data.StartSequence = xdr.Uint32(ledgerSeq)
-		ledgerCloseMetaObject.objectKey = objectKey
+		// Create a new LedgerCloseMetaObject and add it to the map.
+		ledgerCloseMetaObject = &LedgerCloseMetaObject{
+			objectKey:     objectKey,
+			startSequence: ledgerSeq,
+			endSequence:   ledgerSeq + e.config.LedgersPerFile - 1,
+		}
+
+		// Special case: Adjust the end ledger sequence for the first batch.
+		// Since the start ledger is 2 instead of 0, we want to ensure that the end ledger sequence
+		// does not exceed LedgersPerFile.
+		// For example, if LedgersPerFile is 64, the first batch should be 0-63, not 2-66.
+		if ledgerSeq < e.config.LedgersPerFile {
+			ledgerCloseMetaObject.endSequence = e.config.LedgersPerFile - 1
+		}
+
 		e.objectMap[objectKey] = ledgerCloseMetaObject
 	}
 
 	// Add ledger to the LedgerCloseMetaObject
 	ledgerCloseMetaObject.AddLedgerCloseMeta(ledgerCloseMeta)
 
-	if ledgerCloseMetaObject.LedgerCount() == e.config.LedgersPerFile {
+	//logger.Logf("ledger Seq: %d object: %s ledgercount: %d ledgersperfile: %d", ledgerSeq,
+
+	if ledgerSeq >= ledgerCloseMetaObject.endSequence {
 		// Current export object is full, send it for upload
+		// This is a blocking call!
 		e.LedgerCloseMetaObjectCh <- ledgerCloseMetaObject
 		// Remove it from the map
 		delete(e.objectMap, objectKey)
--- exp/services/ledgerexporter/exportmanager_test.go
+++ exp/services/ledgerexporter/exportmanager_test.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/stellar/go/ingest/ledgerbackend"
 	"github.com/stellar/go/xdr"
 	_ "github.com/stellar/go/xdr"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -21,24 +22,25 @@ type ExportManagerSuite struct {
 	suite.Suite
 	ctx         context.Context
 	mockBackend ledgerbackend.MockDatabaseBackend
-	testChan    chan *LedgerCloseMetaObject
 }
 
 func (s *ExportManagerSuite) SetupTest() {
 	s.ctx = context.Background()
 	s.mockBackend = ledgerbackend.MockDatabaseBackend{}
-	s.testChan = make(chan *LedgerCloseMetaObject)
 }
 
 func (s *ExportManagerSuite) TearDownTest() {
 }
 
 func (s *ExportManagerSuite) TestRun() {
-	config := ExporterConfig{LedgersPerFile: 1, FilesPerPartition: 10}
-	exporter := NewExportManager(config, &s.mockBackend, s.testChan)
 
-	start := 0
-	end := 20
+	testChan := make(chan *LedgerCloseMetaObject)
+	config := ExporterConfig{LedgersPerFile: 64, FilesPerPartition: 10}
+	exporter := NewExportManager(config, &s.mockBackend, testChan)
+	expectedObjectkeys := map[string]bool{}
+
+	start := 2
+	end := 255
 	for i := start; i <= end; i++ {
 		s.mockBackend.On("GetLedger", s.ctx, uint32(i)).Return(
 			xdr.LedgerCloseMeta{
@@ -52,18 +54,64 @@ func (s *ExportManagerSuite) TestRun() {
 					},
 				},
 			}, nil)
+		expectedObjectkeys[config.getObjectKey(uint32(i))] = true
 	}
 
+	actualObjectKeys := map[string]bool{}
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		for v := range s.testChan {
-			//TODO: Add asserts for objectkeys
-			fmt.Println(v.objectKey)
+		for v := range testChan {
+			actualObjectKeys[v.objectKey] = true
 		}
 	}()
 
 	exporter.Run(s.ctx, uint32(start), uint32(end))
 	wg.Wait()
+
+	assert.Equal(s.T(), expectedObjectkeys, actualObjectKeys)
+	s.mockBackend.AssertExpectations(s.T())
+}
+
+func (s *ExportManagerSuite) TestAddLedgerCloseMeta() {
+	testCh := make(chan *LedgerCloseMetaObject)
+
+	config := ExporterConfig{LedgersPerFile: 1, FilesPerPartition: 10}
+	exporter := NewExportManager(config, &s.mockBackend, testCh)
+	expectedObjectkeys := map[string]bool{}
+	actualObjectKeys := map[string]bool{}
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for v := range testCh {
+			actualObjectKeys[v.objectKey] = true
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		start := 0
+		end := 255
+		for i := start; i <= end; i++ {
+			exporter.AddLedgerCloseMeta(xdr.LedgerCloseMeta{
+				V0: &xdr.LedgerCloseMetaV0{
+					LedgerHeader: xdr.LedgerHeaderHistoryEntry{
+						Header: xdr.LedgerHeader{
+							LedgerSeq:      xdr.Uint32(i),
+							LedgerVersion:  xdr.Uint32(20),
+							BucketListHash: xdr.Hash{1, 2, 3},
+						},
+					},
+				},
+			})
+			expectedObjectkeys[config.getObjectKey(uint32(i))] = true
+		}
+	}()
+	time.Sleep(10 * time.Second)
+	close(testCh)
+	wg.Wait()
+	assert.Equal(s.T(), expectedObjectkeys, actualObjectKeys)
 }
