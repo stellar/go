2432bb66bc061e02ecb04ebcc72b64385538a7cc
tamirms <tamirms@gmail.com>
2024-03-13 21:47:25 +0000

    remove commented out code

--- support/config/config_option_test.go
+++ support/config/config_option_test.go
@@ -113,15 +113,6 @@ func TestConfigOption_optionalFlags_env_set(t *testing.T) {
 	}
 	configOpts.Init(cmd)
 
-	//prev := envVars
-	//envVars = map[string]bool{
-	//	"STRING": true,
-	//	"UINT":   true,
-	//}
-	//defer func() {
-	//	envVars = prev
-	//}()
-
 	defer os.Setenv("STRING", os.Getenv("STRING"))
 	defer os.Setenv("UINT", os.Getenv("UINT"))
 	os.Setenv("STRING", "str")
