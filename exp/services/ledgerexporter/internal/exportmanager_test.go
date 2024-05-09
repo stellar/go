18dee5c955a6ae4b39aa4769e185a684864063ad
chowbao <simon.chow@stellar.org>
2024-05-09 15:55:13 -0400

    ledgerexporter: Add lexicographical ordering token to filenames (#5305)
    * ledgerexporter: Add lexicographical ordering token to filenames
    
    * Add test
    
    * Update support/datastore/ledgerbatch_config_test.go
    
    Co-authored-by: tamirms <tamirms@gmail.com>
    
    * add zero and max uint32 edge case
    
    ---------
    
    Co-authored-by: tamirms <tamirms@gmail.com>

--- exp/services/ledgerexporter/internal/exportmanager_test.go
+++ exp/services/ledgerexporter/internal/exportmanager_test.go
@@ -2,7 +2,6 @@ package ledgerexporter
 
 import (
 	"context"
-	"fmt"
 	"sync"
 	"testing"
 	"time"
@@ -138,35 +137,6 @@ func (s *ExportManagerSuite) TestRunWithCanceledContext() {
 	require.EqualError(s.T(), err, "context canceled")
 }
 
-func (s *ExportManagerSuite) TestGetObjectKeyFromSequenceNumber() {
-	testCases := []struct {
-		filesPerPartition uint32
-		ledgerSeq         uint32
-		ledgersPerFile    uint32
-		fileSuffix        string
-		expectedKey       string
-	}{
-		{0, 5, 1, ".xdr.gz", "5.xdr.gz"},
-		{0, 5, 10, ".xdr.gz", "0-9.xdr.gz"},
-		{2, 10, 100, ".xdr.gz", "0-199/0-99.xdr.gz"},
-		{2, 150, 50, ".xdr.gz", "100-199/150-199.xdr.gz"},
-		{2, 300, 200, ".xdr.gz", "0-399/200-399.xdr.gz"},
-		{2, 1, 1, ".xdr.gz", "0-1/1.xdr.gz"},
-		{4, 10, 100, ".xdr.gz", "0-399/0-99.xdr.gz"},
-		{4, 250, 50, ".xdr.gz", "200-399/250-299.xdr.gz"},
-		{1, 300, 200, ".xdr.gz", "200-399.xdr.gz"},
-		{1, 1, 1, ".xdr.gz", "1.xdr.gz"},
-	}
-
-	for _, tc := range testCases {
-		s.T().Run(fmt.Sprintf("LedgerSeq-%d-LedgersPerFile-%d", tc.ledgerSeq, tc.ledgersPerFile), func(t *testing.T) {
-			config := datastore.LedgerBatchConfig{FilesPerPartition: tc.filesPerPartition, LedgersPerFile: tc.ledgersPerFile, FileSuffix: tc.fileSuffix}
-			key := config.GetObjectKeyFromSequenceNumber(tc.ledgerSeq)
-			require.Equal(t, tc.expectedKey, key)
-		})
-	}
-}
-
 func (s *ExportManagerSuite) TestAddLedgerCloseMeta() {
 	config := datastore.LedgerBatchConfig{LedgersPerFile: 1, FilesPerPartition: 10, FileSuffix: ".xdr.gz"}
 	registry := prometheus.NewRegistry()
@@ -174,7 +144,7 @@ func (s *ExportManagerSuite) TestAddLedgerCloseMeta() {
 	exporter, err := NewExportManager(config, &s.mockBackend, queue, registry)
 	require.NoError(s.T(), err)
 
-	expectedkeys := set.NewSet[string](10)
+	expectedKeys := set.NewSet[string](10)
 	actualKeys := set.NewSet[string](10)
 
 	wg := sync.WaitGroup{}
@@ -197,12 +167,12 @@ func (s *ExportManagerSuite) TestAddLedgerCloseMeta() {
 		require.NoError(s.T(), exporter.AddLedgerCloseMeta(context.Background(), datastore.CreateLedgerCloseMeta(i)))
 
 		key := config.GetObjectKeyFromSequenceNumber(i)
-		expectedkeys.Add(key)
+		expectedKeys.Add(key)
 	}
 
 	queue.Close()
 	wg.Wait()
-	require.Equal(s.T(), expectedkeys, actualKeys)
+	require.Equal(s.T(), expectedKeys, actualKeys)
 }
 
 func (s *ExportManagerSuite) TestAddLedgerCloseMetaContextCancel() {
--- support/datastore/ledgerbatch_config_test.go
+++ support/datastore/ledgerbatch_config_test.go
@@ -2,6 +2,9 @@ package datastore
 
 import (
 	"fmt"
+	"math"
+	"math/rand"
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -15,16 +18,16 @@ func TestGetObjectKeyFromSequenceNumber(t *testing.T) {
 		fileSuffix        string
 		expectedKey       string
 	}{
-		{0, 5, 1, ".xdr.gz", "5.xdr.gz"},
-		{0, 5, 10, ".xdr.gz", "0-9.xdr.gz"},
-		{2, 10, 100, ".xdr.gz", "0-199/0-99.xdr.gz"},
-		{2, 150, 50, ".xdr.gz", "100-199/150-199.xdr.gz"},
-		{2, 300, 200, ".xdr.gz", "0-399/200-399.xdr.gz"},
-		{2, 1, 1, ".xdr.gz", "0-1/1.xdr.gz"},
-		{4, 10, 100, ".xdr.gz", "0-399/0-99.xdr.gz"},
-		{4, 250, 50, ".xdr.gz", "200-399/250-299.xdr.gz"},
-		{1, 300, 200, ".xdr.gz", "200-399.xdr.gz"},
-		{1, 1, 1, ".xdr.gz", "1.xdr.gz"},
+		{0, 5, 1, ".xdr.gz", "FFFFFFFA--5.xdr.gz"},
+		{0, 5, 10, ".xdr.gz", "FFFFFFFF--0-9.xdr.gz"},
+		{2, 10, 100, ".xdr.gz", "FFFFFFFF--0-199/FFFFFFFF--0-99.xdr.gz"},
+		{2, 150, 50, ".xdr.gz", "FFFFFF9B--100-199/FFFFFF69--150-199.xdr.gz"},
+		{2, 300, 200, ".xdr.gz", "FFFFFFFF--0-399/FFFFFF37--200-399.xdr.gz"},
+		{2, 1, 1, ".xdr.gz", "FFFFFFFF--0-1/FFFFFFFE--1.xdr.gz"},
+		{4, 10, 100, ".xdr.gz", "FFFFFFFF--0-399/FFFFFFFF--0-99.xdr.gz"},
+		{4, 250, 50, ".xdr.gz", "FFFFFF37--200-399/FFFFFF05--250-299.xdr.gz"},
+		{1, 300, 200, ".xdr.gz", "FFFFFF37--200-399.xdr.gz"},
+		{1, 1, 1, ".xdr.gz", "FFFFFFFE--1.xdr.gz"},
 	}
 
 	for _, tc := range testCases {
@@ -33,5 +36,49 @@ func TestGetObjectKeyFromSequenceNumber(t *testing.T) {
 			key := config.GetObjectKeyFromSequenceNumber(tc.ledgerSeq)
 			require.Equal(t, tc.expectedKey, key)
 		})
+	}
+}
+
+func TestGetObjectKeyFromSequenceNumber_ObjectKeyDescOrder(t *testing.T) {
+	config := LedgerBatchConfig{
+		LedgersPerFile:    1,
+		FilesPerPartition: 10,
+		FileSuffix:        ".xdr.gz",
+	}
+	sequenceCount := 10000
+	sequenceMap := make(map[uint32]string)
+	keys := make([]uint32, len(sequenceMap))
+	count := 0
+
+	// Add 0 and MaxUint32 as edge cases
+	sequenceMap[0] = config.GetObjectKeyFromSequenceNumber(0)
+	keys = append(keys, 0)
+	sequenceMap[math.MaxUint32] = config.GetObjectKeyFromSequenceNumber(math.MaxUint32)
+	keys = append(keys, math.MaxUint32)
+
+	for {
+		if count >= sequenceCount {
+			break
+		}
+		randSequence := rand.Uint32()
+		if _, ok := sequenceMap[randSequence]; ok {
+			continue
+		}
+		sequenceMap[randSequence] = config.GetObjectKeyFromSequenceNumber(randSequence)
+		keys = append(keys, randSequence)
+		count++
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+
+	prev := sequenceMap[keys[0]]
+	for i := 1; i < sequenceCount; i++ {
+		curr := sequenceMap[keys[i]]
+		if prev <= curr {
+			t.Error("sequences not in lexicographic order")
+		}
+		prev = curr
 	}
 }
