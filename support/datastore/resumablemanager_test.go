c1f9e29c33ef6f059e3f4af80301cc173191a4f3
tamirms <tamirms@gmail.com>
2024-05-30 16:38:40 +0100

    code review fixes

--- support/datastore/resumablemanager_test.go
+++ support/datastore/resumablemanager_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 func TestResumability(t *testing.T) {
-
+	ctx := context.Background()
 	tests := []struct {
 		name              string
 		startLedger       uint32
@@ -23,6 +23,7 @@ func TestResumability(t *testing.T) {
 		latestLedger      uint32
 		errorSnippet      string
 		archiveError      error
+		registerMockCalls func(*MockDataStore)
 	}{
 		{
 			name:         "archive error when resolving network latest",
@@ -34,9 +35,10 @@ func TestResumability(t *testing.T) {
 				FilesPerPartition: uint32(1),
 				LedgersPerFile:    uint32(10),
 			},
-			networkName:  "test",
-			errorSnippet: "archive error",
-			archiveError: errors.New("archive error"),
+			networkName:       "test",
+			errorSnippet:      "archive error",
+			archiveError:      errors.New("archive error"),
+			registerMockCalls: func(store *MockDataStore) {},
 		},
 		{
 			name:         "End ledger same as start, data store has it",
@@ -49,6 +51,9 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(10),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFFF--0-9.xdr.zstd").Return(true, nil).Once()
+			},
 		},
 		{
 			name:         "End ledger same as start, data store does not have it",
@@ -61,6 +66,9 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(10),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFF5--10-19.xdr.zstd").Return(false, nil).Twice()
+			},
 		},
 		{
 			name:         "start and end ledger are in same file, data store does not have it",
@@ -73,6 +81,9 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(64),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFFF--0-6399/FFFFFFBF--64-127.xdr.zstd").Return(false, nil).Twice()
+			},
 		},
 		{
 			name:         "start and end ledger are in same file, data store has it",
@@ -85,18 +96,25 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(64),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFFF--0-6399/FFFFFF7F--128-191.xdr.zstd").Return(true, nil).Once()
+			},
 		},
 		{
-			name:         "ledger range overlaps with an range which is already exported",
+			name:         "ledger range overlaps with a range which is already exported",
 			startLedger:  2,
 			endLedger:    127,
 			absentLedger: 2,
 			findStartOk:  true,
 			ledgerBatchConfig: LedgerBatchConfig{
-				FilesPerPartition: uint32(1000),
+				FilesPerPartition: uint32(100),
 				LedgersPerFile:    uint32(64),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFFF--0-6399/FFFFFFBF--64-127.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFFFF--0-6399/FFFFFFFF--0-63.xdr.zstd").Return(false, nil).Once()
+			},
 		},
 		{
 			name:         "binary search encounters an error during datastore retrieval",
@@ -110,6 +128,9 @@ func TestResumability(t *testing.T) {
 			},
 			networkName:  "test",
 			errorSnippet: "datastore error happened",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFEB--20-29.xdr.zstd").Return(false, errors.New("datastore error happened")).Once()
+			},
 		},
 		{
 			name:         "Data store is beyond boundary aligned start ledger",
@@ -122,6 +143,11 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(10),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFCD--50-59.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFFE1--30-39.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFFD7--40-49.xdr.zstd").Return(false, nil).Once()
+			},
 		},
 		{
 			name:         "Data store is beyond non boundary aligned start ledger",
@@ -134,6 +160,10 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(10),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFFB9--70-79.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFFAF--80-89.xdr.zstd").Return(false, nil).Twice()
+			},
 		},
 		{
 			name:         "Data store is beyond start and end ledger",
@@ -146,6 +176,10 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(10),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFEFB--260-269.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFEF1--270-279.xdr.zstd").Return(true, nil).Once()
+			},
 		},
 		{
 			name:         "Data store is not beyond start ledger",
@@ -158,6 +192,12 @@ func TestResumability(t *testing.T) {
 				LedgersPerFile:    uint32(10),
 			},
 			networkName: "test",
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFFF87--120-129.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFF91--110-119.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFF9B--100-109.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFFA5--90-99.xdr.zstd").Return(false, nil).Once()
+			},
 		},
 		{
 			name:         "No start ledger provided",
@@ -169,8 +209,9 @@ func TestResumability(t *testing.T) {
 				FilesPerPartition: uint32(1),
 				LedgersPerFile:    uint32(10),
 			},
-			networkName:  "test",
-			errorSnippet: "Invalid start value",
+			networkName:       "test",
+			errorSnippet:      "Invalid start value",
+			registerMockCalls: func(store *MockDataStore) {},
 		},
 		{
 			name:         "No end ledger provided, data store not beyond start",
@@ -184,6 +225,16 @@ func TestResumability(t *testing.T) {
 			},
 			networkName:  "test2",
 			latestLedger: uint32(2000),
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFF9A1--1630-1639.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFA91--1390-1399.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFB13--1260-1269.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFB4F--1200-1209.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFB77--1160-1169.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFB6D--1170-1179.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFB81--1150-1159.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFFB8B--1140-1149.xdr.zstd").Return(false, nil).Once()
+			},
 		},
 		{
 			name:         "No end ledger provided, data store is beyond start",
@@ -197,6 +248,15 @@ func TestResumability(t *testing.T) {
 			},
 			networkName:  "test3",
 			latestLedger: uint32(3000),
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFF5B9--2630-2639.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF6A9--2390-2399.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF72B--2260-2269.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF735--2250-2259.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF73F--2240-2249.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF749--2230-2239.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF767--2200-2209.xdr.zstd").Return(true, nil).Once()
+			},
 		},
 		{
 			name:         "No end ledger provided, data store is beyond start and archive network latest, and partially into checkpoint frequency padding",
@@ -210,6 +270,15 @@ func TestResumability(t *testing.T) {
 			},
 			networkName:  "test4",
 			latestLedger: uint32(4000),
+			registerMockCalls: func(mockDataStore *MockDataStore) {
+				mockDataStore.On("Exists", ctx, "FFFFF1D1--3630-3639.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF0D7--3880-3889.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF05F--4000-4009.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF023--4060-4069.xdr.zstd").Return(true, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF005--4090-4099.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF00F--4080-4089.xdr.zstd").Return(false, nil).Once()
+				mockDataStore.On("Exists", ctx, "FFFFF019--4070-4079.xdr.zstd").Return(false, nil).Once()
+			},
 		},
 		{
 			name:         "No end ledger provided, start is beyond archive network latest and checkpoint frequency padding",
@@ -221,86 +290,19 @@ func TestResumability(t *testing.T) {
 				FilesPerPartition: uint32(1),
 				LedgersPerFile:    uint32(10),
 			},
-			networkName:  "test5",
-			latestLedger: uint32(5000),
-			errorSnippet: "Invalid start value of 5129, it is greater than network's latest ledger of 5128",
+			networkName:       "test5",
+			latestLedger:      uint32(5000),
+			errorSnippet:      "Invalid start value of 5129, it is greater than network's latest ledger of 5128",
+			registerMockCalls: func(store *MockDataStore) {},
 		},
 	}
-
-	ctx := context.Background()
-
-	mockDataStore := &MockDataStore{}
-
-	//"End ledger same as start, data store has it"
-	mockDataStore.On("Exists", ctx, "FFFFFFFF--0-9.xdr.zstd").Return(true, nil).Once()
-
-	//"End ledger same as start, data store does not have it"
-	mockDataStore.On("Exists", ctx, "FFFFFFF5--10-19.xdr.zstd").Return(false, nil).Twice()
-
-	// start and end ledger are in same file, data store does not have it
-	mockDataStore.On("Exists", ctx, "FFFFFFFF--0-6399/FFFFFFBF--64-127.xdr.zstd").Return(false, nil).Twice()
-
-	// start and end ledger are in same file, data store has it
-	mockDataStore.On("Exists", ctx, "FFFFFFFF--0-6399/FFFFFF7F--128-191.xdr.zstd").Return(true, nil).Once()
-
-	// ledger range overlaps with an range which is already exported
-	mockDataStore.On("Exists", ctx, "FFFFFFFF--0-63999/FFFFFFBF--64-127.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFFFF--0-63999/FFFFFFFF--0-63.xdr.zstd").Return(false, nil).Once()
-
-	//"binary search encounters an error during datastore retrieval",
-	mockDataStore.On("Exists", ctx, "FFFFFFEB--20-29.xdr.zstd").Return(false, errors.New("datastore error happened")).Once()
-
-	//"Data store is beyond boundary aligned start ledger"
-	mockDataStore.On("Exists", ctx, "FFFFFFCD--50-59.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFFE1--30-39.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFFD7--40-49.xdr.zstd").Return(false, nil).Once()
-
-	//"Data store is beyond non boundary aligned start ledger"
-	mockDataStore.On("Exists", ctx, "FFFFFFB9--70-79.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFFAF--80-89.xdr.zstd").Return(false, nil).Twice()
-
-	//"Data store is beyond start and end ledger"
-	mockDataStore.On("Exists", ctx, "FFFFFEFB--260-269.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFEF1--270-279.xdr.zstd").Return(true, nil).Once()
-
-	//"Data store is not beyond start ledger"
-	mockDataStore.On("Exists", ctx, "FFFFFF87--120-129.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFF91--110-119.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFF9B--100-109.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFFA5--90-99.xdr.zstd").Return(false, nil).Once()
-
-	//"No end ledger provided, data store not beyond start" uses latest from network="test2"
-	mockDataStore.On("Exists", ctx, "FFFFF9A1--1630-1639.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFA91--1390-1399.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFB13--1260-1269.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFB4F--1200-1209.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFB77--1160-1169.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFB6D--1170-1179.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFB81--1150-1159.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFFB8B--1140-1149.xdr.zstd").Return(false, nil).Once()
-
-	//"No end ledger provided, data store is beyond start" uses latest from network="test3"
-	mockDataStore.On("Exists", ctx, "FFFFF5B9--2630-2639.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF6A9--2390-2399.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF72B--2260-2269.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF735--2250-2259.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF73F--2240-2249.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF749--2230-2239.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF767--2200-2209.xdr.zstd").Return(true, nil).Once()
-
-	//"No end ledger provided, data store is beyond start and archive network latest, and partially into checkpoint frequency padding" uses latest from network="test4"
-	mockDataStore.On("Exists", ctx, "FFFFF1D1--3630-3639.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF0D7--3880-3889.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF05F--4000-4009.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF023--4060-4069.xdr.zstd").Return(true, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF005--4090-4099.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF00F--4080-4089.xdr.zstd").Return(false, nil).Once()
-	mockDataStore.On("Exists", ctx, "FFFFF019--4070-4079.xdr.zstd").Return(false, nil).Once()
-
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			mockArchive := &historyarchive.MockArchive{}
 			mockArchive.On("GetRootHAS").Return(historyarchive.HistoryArchiveState{CurrentLedger: tt.latestLedger}, tt.archiveError).Once()
+
+			mockDataStore := &MockDataStore{}
+			tt.registerMockCalls(mockDataStore)
 
 			resumableManager := NewResumableManager(mockDataStore, tt.networkName, tt.ledgerBatchConfig, mockArchive)
 			absentLedger, ok, err := resumableManager.FindStart(ctx, tt.startLedger, tt.endLedger)
@@ -315,8 +317,8 @@ func TestResumability(t *testing.T) {
 				// archives are only expected to be called when end = 0
 				mockArchive.AssertExpectations(t)
 			}
+			mockDataStore.AssertExpectations(t)
 		})
 	}
 
-	mockDataStore.AssertExpectations(t)
 }
