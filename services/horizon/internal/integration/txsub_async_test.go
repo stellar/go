ef5626c998211f8a56d06d2e13c167e7c3149f70
Aditya Vyas <adityavyas17@gmail.com>
2024-09-20 12:11:17 -0400

    Send an error response for Horizon.Problem (#5454)
    Add logic to distinguish whether the response object is `AsyncTransactionSubmissionResponse` or `Horizon.Problem` and send response accordingly.

--- services/horizon/internal/integration/txsub_async_test.go
+++ services/horizon/internal/integration/txsub_async_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/protocols/horizon"
@@ -129,6 +130,31 @@ func TestAsyncTxSub_SubmissionTryAgainLater(t *testing.T) {
 		TxStatus:       "TRY_AGAIN_LATER",
 		Hash:           "d5eb72a4c1832b89965850fff0bd9bba4b6ca102e7c89099dcaba5e7d7d2e049",
 	})
+}
+
+func TestAsyncTxSub_TransactionMalformed(t *testing.T) {
+	itest := integration.NewTest(t, integration.Config{
+		EnableSorobanRPC: true,
+		HorizonEnvironment: map[string]string{
+			"MAX_HTTP_REQUEST_SIZE": "1800",
+		},
+	})
+	master := itest.Master()
+
+	// establish which account will be contract owner, and load it's current seq
+	sourceAccount, err := itest.Client().AccountDetail(horizonclient.AccountRequest{
+		AccountID: itest.Master().Address(),
+	})
+	require.NoError(t, err)
+
+	installContractOp := assembleInstallContractCodeOp(t, master.Address(), "soroban_sac_test.wasm")
+	preFlightOp, minFee := itest.PreflightHostFunctions(&sourceAccount, *installContractOp)
+	txParams := integration.GetBaseTransactionParamsWithFee(&sourceAccount, minFee+txnbuild.MinBaseFee, &preFlightOp)
+	_, err = itest.AsyncSubmitTransaction(master, txParams)
+	assert.EqualError(
+		t, err,
+		"horizon error: \"Transaction Malformed\" - check horizon.Error.Problem for more information",
+	)
 }
 
 func TestAsyncTxSub_GetOpenAPISpecResponse(t *testing.T) {
