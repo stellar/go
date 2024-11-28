c7f37fbd0ee0fe1e71bde165995fc6d2c630cf8f
Karthik Iyer <karthik.iyer@stellar.org>
2024-11-28 10:55:09 -0800

    Undo all changes to parameters_test.go

--- services/horizon/internal/integration/parameters_test.go
+++ services/horizon/internal/integration/parameters_test.go
@@ -41,10 +41,32 @@ var networkParamArgs = map[string]string{
 	horizon.NetworkPassphraseFlagName:   "",
 }
 
+const (
+	SimpleCaptiveCoreToml = `
+		PEER_PORT=11725
+		ARTIFICIALLY_ACCELERATE_TIME_FOR_TESTING=true
+
+		UNSAFE_QUORUM=true
+		FAILURE_SAFETY=0
+
+		[[VALIDATORS]]
+		NAME="local_core"
+		HOME_DOMAIN="core.local"
+		PUBLIC_KEY="GD5KD2KEZJIGTC63IGW6UMUSMVUVG5IHG64HUTFWCHVZH2N2IBOQN7PS"
+		ADDRESS="localhost"
+		QUALITY="MEDIUM"`
+
+	StellarCoreURL = "http://localhost:11626"
+)
+
+var (
+	CaptiveCoreConfigErrMsg = "error generating captive core configuration: invalid config: "
+)
+
 // Ensures that BUCKET_DIR_PATH is not an allowed value for Captive Core.
 func TestBucketDirDisallowed(t *testing.T) {
 	config := `BUCKET_DIR_PATH="/tmp"
-		` + integration.SimpleCaptiveCoreToml
+		` + SimpleCaptiveCoreToml
 
 	confName, _, cleanup := createCaptiveCoreConfig(config)
 	defer cleanup()
@@ -81,7 +103,7 @@ func TestEnvironmentPreserved(t *testing.T) {
 
 	testConfig := integration.GetTestConfig()
 	testConfig.HorizonEnvironment = map[string]string{
-		"STELLAR_CORE_URL": integration.StellarCoreURL,
+		"STELLAR_CORE_URL": StellarCoreURL,
 	}
 	test := integration.NewTest(t, *testConfig)
 
@@ -90,7 +112,7 @@ func TestEnvironmentPreserved(t *testing.T) {
 	test.WaitForHorizonIngest()
 
 	envValue := os.Getenv("STELLAR_CORE_URL")
-	assert.Equal(t, integration.StellarCoreURL, envValue)
+	assert.Equal(t, StellarCoreURL, envValue)
 
 	test.Shutdown()
 
@@ -230,7 +252,7 @@ func TestNetworkEnvironmentVariable(t *testing.T) {
 
 // Ensures that the filesystem ends up in the correct state with Captive Core.
 func TestCaptiveCoreConfigFilesystemState(t *testing.T) {
-	confName, storagePath, cleanup := createCaptiveCoreConfig(integration.SimpleCaptiveCoreToml)
+	confName, storagePath, cleanup := createCaptiveCoreConfig(SimpleCaptiveCoreToml)
 	defer cleanup()
 
 	localParams := integration.MergeMaps(defaultCaptiveCoreParameters, map[string]string{
