5ba5b80bd73b2943e03717d943d7438a8f846ad3
George <george@stellar.org>
2025-01-22 10:29:59 -0800

    Remove unneeded proof schema

--- protocols/stellarcore/proof_response.go
+++
@@ -1,8 +0,0 @@
-package stellarcore
-
-// ProofResponse is the structure of Stellar Core's /getrestorationproof and
-// /getinvocationproof
-type ProofResponse struct {
-	Ledger uint32 `json:"ledger"`
-	Proof  string `json:"proof,omitempty"`
-}
