b92fa98f5863fd2991f78e48e3daee78bc5a8041
Shawn Reuland <shawn@stellar.org>
2025-03-28 11:16:09 -0700

    smaller rpc long polling routine for leaner example

--- exp/tools/ttp_poc/cli_tool/go/server/server.go
+++ exp/tools/ttp_poc/cli_tool/go/server/server.go
@@ -26,124 +26,85 @@ func NewEventServer(rpcEndpoint string, passphrase string) (*EventServer, error)
 	}, nil
 }
 
-// GetEvents implements the gRPC GetEvents method
 func (s *EventServer) GetTTPEvents(req *event_service.GetEventsRequest, stream event_service.EventService_GetTTPEventsServer) error {
 	ctx := stream.Context()
 
-	// Create initial GetLedgers request
+	// Create initial RPC GetLedgers request
 	getLedgersReq := protocol.GetLedgersRequest{
 		StartLedger: uint32(req.StartLedger),
+		Pagination:  &protocol.LedgerPaginationOptions{Limit: 1},
 	}
 
-	// If EndLedger < StartLedger, do infinite long polling
-	// this is naive, inefficient long polling approach
-	// with every request getting max of one ledger back
-	if req.EndLedger < req.StartLedger {
-		getLedgersReq.Pagination = &protocol.LedgerPaginationOptions{
-			Limit: 1,
-		}
-		for {
-			// Check if context is cancelled
-			select {
-			case <-ctx.Done():
-				err := ctx.Err()
-				log.Printf("Context cancelled in GetTTPEvents: %v", err)
-				return err
-			default:
-			}
+	// this example code acquires ledger metadata from an RPC server instance
+	// other sources of ledger metadata could be used as alternatives
+	// such as ledgerbackend's from the Ingest SDK for GCS buckets or Captive core.
 
-			// Get ledgers from RPC
-			resp, err := s.rpcClient.GetLedgers(ctx, getLedgersReq)
-			if err != nil {
-				log.Printf("Error getting ledgers from RPC: %v", err)
-				return err
-			}
-
-			if len(resp.Ledgers) == 0 {
-				// No ledgers, wait a bit before polling again
-				time.Sleep(5 * time.Second)
-				continue
-			}
-
-			lastSeq := 0
-			// Process each ledger
-			for _, ledgerInfo := range resp.Ledgers {
-				// Convert LedgerMetadata to xdr.LedgerCloseMeta
-				var ledgerMeta xdr.LedgerCloseMeta
-				if err := xdr.SafeUnmarshalBase64(ledgerInfo.LedgerMetadata, &ledgerMeta); err != nil {
-					log.Printf("Error unmarshaling ledger metadata: %v: %v", ledgerInfo.LedgerMetadata, err)
-					return err
-				}
-				lastSeq = int(ledgerMeta.LedgerSequence())
-
-				// Get events from ledger using the token_transfer processor
-				events, err := s.processor.EventsFromLedger(ledgerMeta)
-				if err != nil {
-					log.Printf("Error processing events from ledger %d: %v", ledgerInfo.Sequence, err)
-					return err
-				}
-
-				// Stream each event
-				for i := range events {
-					ttpEvent := events[i]
-					if err := stream.Send(ttpEvent); err != nil {
-						log.Printf("Error sending event to stream: %v", err)
-						return err
-					}
-				}
-			}
-
-			// naive approach to enable our example long polling
-			// cursor to stay at least one ledger back from latest ledger on rpc
-			if lastSeq >= (int(resp.LatestLedger) - 1) {
-				time.Sleep(7 * time.Second)
-			}
-
-			// Update cursor for next request
-			getLedgersReq.Pagination.Cursor = resp.Cursor
-			getLedgersReq.StartLedger = 0
-		}
-	} else {
-		// For finite range, we'll do naievly simple for example sake
-		// to make this a single request.
-		getLedgersReq.StartLedger = uint32(req.EndLedger)
-		getLedgersReq.Pagination = &protocol.LedgerPaginationOptions{
-			Limit: uint(req.EndLedger - req.StartLedger),
+	// this routine does a naive, inefficient long polling approach against the RPC
+	// with every RPC request getting a response with page size of just one ledger
+	for {
+		// Check if context is cancelled
+		select {
+		case <-ctx.Done():
+			err := ctx.Err()
+			log.Printf("Context cancelled in GetTTPEvents: %v", err)
+			return err
+		default:
 		}
 
 		// Get ledgers from RPC
 		resp, err := s.rpcClient.GetLedgers(ctx, getLedgersReq)
 		if err != nil {
-			log.Printf("Error getting ledgers from RPC: %v", err)
+			log.Printf("Error getting ledgers from RPC: %+v", err)
 			return err
 		}
 
+		if len(resp.Ledgers) == 0 {
+			// No ledgers, wait a bit before polling again
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		lastSeq := uint32(0)
 		// Process each ledger
 		for _, ledgerInfo := range resp.Ledgers {
-			// Convert LedgerMetadata to xdr.LedgerCloseMeta
+			// Convert LedgerMetadata in base64 encoding from RPC to xdr.LedgerCloseMeta
 			var ledgerMeta xdr.LedgerCloseMeta
-
 			if err := xdr.SafeUnmarshalBase64(ledgerInfo.LedgerMetadata, &ledgerMeta); err != nil {
 				log.Printf("Error unmarshaling ledger metadata: %v: %v", ledgerInfo.LedgerMetadata, err)
 				return err
 			}
+			lastSeq = ledgerMeta.LedgerSequence()
 
-			// Get events from ledger using the token_transfer processor
+			// Get events from ledger metadata using the token_transfer processor
 			events, err := s.processor.EventsFromLedger(ledgerMeta)
 			if err != nil {
 				log.Printf("Error processing events from ledger %d: %v", ledgerInfo.Sequence, err)
 				return err
 			}
 
-			// Stream each event
+			// Stream each token transfer event
 			for i := range events {
-				if err := stream.Send(events[i]); err != nil {
+				ttpEvent := events[i]
+				if err := stream.Send(ttpEvent); err != nil {
 					log.Printf("Error sending event to stream: %v", err)
 					return err
 				}
 			}
 		}
-	}
 
-	return nil
+		// if this was bounded range request, we're done now.
+		if req.EndLedger > 0 && lastSeq >= req.EndLedger {
+			return nil
+		}
+
+		// naive approach for unbounded ledger retrieval with long polling
+		// pause to stay at least one ledger back from latest ledger on rpc
+		if lastSeq >= resp.LatestLedger-1 {
+			time.Sleep(7 * time.Second)
+		}
+
+		// Update cursor for next request
+		getLedgersReq.Pagination.Cursor = resp.Cursor
+		getLedgersReq.StartLedger = 0
+	}
 }
