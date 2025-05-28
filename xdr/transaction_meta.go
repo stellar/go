40b743cf5a735c2f84cfff962a063e1b7832e535
Alfonso Acosta <alfonso@stellar.org>
2025-05-28 02:04:02 +0200

    Fix GetDiagnosticEvents() for meta v3

--- xdr/transaction_meta.go
+++ xdr/transaction_meta.go
@@ -33,11 +33,33 @@ func (t *TransactionMeta) GetDiagnosticEvents() ([]DiagnosticEvent, error) {
 	case 1, 2:
 		return nil, nil
 	case 3:
-		sorobanMeta := t.MustV3().SorobanMeta
-		if sorobanMeta == nil {
-			return nil, nil
+		var diagnosticEvents []DiagnosticEvent
+		var contractEvents []ContractEvent
+		if sorobanMeta := t.MustV3().SorobanMeta; sorobanMeta != nil {
+			diagnosticEvents = sorobanMeta.DiagnosticEvents
+			if len(diagnosticEvents) > 0 {
+				// all contract events and diag events for a single operation(by its index in the tx) were available
+				// in tx meta's DiagnosticEvents, no need to look anywhere else for events
+				return diagnosticEvents, nil
+			}
+
+			contractEvents = sorobanMeta.Events
+			if len(contractEvents) == 0 {
+				// no events were present in this tx meta
+				return nil, nil
+			}
 		}
-		return sorobanMeta.DiagnosticEvents, nil
+
+		// tx meta only provided contract events, no diagnostic events, we convert the contract
+		// event to a diagnostic event, to fit the response interface.
+		convertedDiagnosticEvents := make([]DiagnosticEvent, len(contractEvents))
+		for i, event := range contractEvents {
+			convertedDiagnosticEvents[i] = DiagnosticEvent{
+				InSuccessfulContractCall: true,
+				Event:                    event,
+			}
+		}
+		return convertedDiagnosticEvents, nil
 	case 4:
 		return t.MustV4().DiagnosticEvents, nil
 	default:
