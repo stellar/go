a867c0acfb1e59b7e69db869e6fc83a17b2db25a
Urvi <urvi.savla@stellar.org>
2025-06-06 22:24:11 -0700

    Add protocol 23 integration test

--- services/horizon/internal/ingest/main.go
+++ services/horizon/internal/ingest/main.go
@@ -31,7 +31,7 @@ import (
 const (
 	// MaxSupportedProtocolVersion defines the maximum supported version of
 	// the Stellar protocol.
-	MaxSupportedProtocolVersion uint32 = 22
+	MaxSupportedProtocolVersion uint32 = 23
 
 	// CurrentVersion reflects the latest version of the ingestion
 	// algorithm. This value is stored in KV store and is used to decide
