878d805864ba5c52c8a048190db8b697f3cefd4a
Urvi <urvi.savla@stellar.org>
2025-06-13 18:46:49 -0700

    Set BACKFILL_RESTORE_META to true

--- services/horizon/internal/test/integration/core_config.go
+++ services/horizon/internal/test/integration/core_config.go
@@ -54,6 +54,7 @@ mkdir="mkdir -p history/vs/{0}"
 
 const captiveCoreConfigTemplate = `
 ARTIFICIALLY_ACCELERATE_TIME_FOR_TESTING={{ .Accelerate }}
+BACKFILL_RESTORE_META=true
 
 NETWORK_PASSPHRASE="{{ .NetworkPassphrase }}"
 
