7af04162dacd2caa7eb5d9b616214bd25854e416
tamirms <tamirms@gmail.com>
2025-07-08 14:35:56 +0200

    remove extra semicolon

--- services/horizon/internal/db2/schema/migrations/70_replace_timestamp_trade_aggregations_brin_index.sql
+++ services/horizon/internal/db2/schema/migrations/70_replace_timestamp_trade_aggregations_brin_index.sql
@@ -1,6 +1,6 @@
 -- +migrate Up
 
-CREATE INDEX IF NOT EXISTS htrd_agg_timestamp ON history_trades_60000 USING btree (timestamp);;
+CREATE INDEX IF NOT EXISTS htrd_agg_timestamp ON history_trades_60000 USING btree (timestamp);
 
 DROP INDEX IF EXISTS htrd_agg_timestamp_brin;
 
