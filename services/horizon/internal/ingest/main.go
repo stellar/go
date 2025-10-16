3631e51d8942acabd116cdb40f1f5f159bc9a4eb
tamirms <tamirms@gmail.com>
2025-10-16 16:01:04 +0100

    Bump horizon ingest MaxSupportedProtocolVersion

--- services/horizon/internal/ingest/main.go
+++ services/horizon/internal/ingest/main.go
@@ -33,7 +33,7 @@ import (
 const (
 	// MaxSupportedProtocolVersion defines the maximum supported version of
 	// the Stellar protocol.
-	MaxSupportedProtocolVersion uint32 = 23
+	MaxSupportedProtocolVersion uint32 = 24
 
 	// CurrentVersion reflects the latest version of the ingestion
 	// algorithm. This value is stored in KV store and is used to decide
