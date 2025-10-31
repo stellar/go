33183ce70561de4800f5f427663497d0484e50ba
Karthik Iyer <karthik.iyer@stellar.org>
2025-10-31 13:05:44 -0700

    fix gofmt

--- ingest/ledgerbackend/rpc_backend_test.go
+++ ingest/ledgerbackend/rpc_backend_test.go
@@ -9,8 +9,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
-	"github.com/stellar/go/xdr"
 	"github.com/stellar/go/protocols/rpc"
+	"github.com/stellar/go/xdr"
 )
 
 type MockRPCClient struct {
